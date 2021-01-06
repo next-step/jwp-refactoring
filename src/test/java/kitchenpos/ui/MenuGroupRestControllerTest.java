58ff74551b02288a4b5a977dd4b4236b1b0d1d04
poppohoo <ccf05017@gmail.com>
2021-01-06 11:29:20 +0900

    test(MenuGroupRestControllerTest): get menu groups
    - 메뉴 그룹 목록을 가져오는 테스트 추가

--- src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
@@ -13,11 +13,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class MenuGroupRestControllerTest {
@@ -54,6 +56,21 @@ class MenuGroupRestControllerTest {
                 .content(objectMapper.writeValueAsString(menuGroupRequest)))
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", url + "/" + saved.getId()))
+        ;
+    }
+
+    @DisplayName("메뉴 그룹 목록을 요청할 수 있다.")
+    @Test
+    void getMenuGroupsTest() throws Exception {
+        // given
+        String url = "/api/menu-groups";
+
+        given(menuGroupService.list()).willReturn(Arrays.asList(new MenuGroup(), new MenuGroup()));
+
+        // when, then
+        mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
         ;
     }
 }
