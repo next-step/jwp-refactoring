2813d693272820cc3ea9893ec86a906df959b99b
poppohoo <ccf05017@gmail.com>
2021-01-06 11:55:58 +0900

    test(ProductRestController): about create and read
    - 정상적으로 상품 목록을 가져오고 생성할 수 있는지 확인하는 테스트 추가

---
+++ src/test/java/kitchenpos/ui/ProductRestControllerTest.java
@@ -0,0 +1,77 @@
+package kitchenpos.ui;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kitchenpos.application.ProductService;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductRestControllerTest {
+    private ProductRestController productRestController;
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
+
+    @Mock
+    private ProductService productService;
+
+    @BeforeEach
+    void setup() {
+        productRestController = new ProductRestController(productService);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(productRestController).build();
+
+        objectMapper = new ObjectMapper();
+    }
+
+    @DisplayName("상품을 등록할 수 있다.")
+    @Test
+    void createProductsTest() throws Exception {
+        // given
+        String url = "/api/products";
+        Product productRequest = new Product();
+        Product savedProduct = new Product();
+        savedProduct.setId(1L);
+        given(productService.create(any())).willReturn(savedProduct);
+
+        // when, then
+        mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(productRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", url + "/" + savedProduct.getId()))
+        ;
+    }
+
+    @DisplayName("상품 목록을 조회할 수 있다.")
+    @Test
+    void getProductsTest() throws Exception {
+        // given
+        String url = "/api/products";
+
+        given(productService.list()).willReturn(Arrays.asList(new Product(), new Product()));
+
+        // when, then
+        mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+        ;
+    }
+}
