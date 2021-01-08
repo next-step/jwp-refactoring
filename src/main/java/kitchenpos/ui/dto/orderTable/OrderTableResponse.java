127e34a001e4c36ea17127ee1cf98c820b855ddc
poppohoo <ccf05017@gmail.com>
2021-01-08 14:41:34 +0900

    feat(OrderTable): add dtos
    - 도메인을 보호하기 위한 DTO추가

---
+++ src/main/java/kitchenpos/ui/dto/orderTable/OrderTableResponse.java
@@ -0,0 +1,59 @@
+package kitchenpos.ui.dto.orderTable;
+
+import java.util.Objects;
+
+public class OrderTableResponse {
+    private Long id;
+    private Long tableGroupId;
+    private int numberOfGuests;
+    private boolean empty;
+
+    OrderTableResponse() {
+    }
+
+    public OrderTableResponse(final Long id, final Long tableGroupId, final int numberOfGuests, final boolean empty) {
+        this.id = id;
+        this.tableGroupId = tableGroupId;
+        this.numberOfGuests = numberOfGuests;
+        this.empty = empty;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getTableGroupId() {
+        return tableGroupId;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final OrderTableResponse that = (OrderTableResponse) o;
+        return numberOfGuests == that.numberOfGuests && empty == that.empty && Objects.equals(id, that.id) && Objects.equals(tableGroupId, that.tableGroupId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, tableGroupId, numberOfGuests, empty);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderTableResponse{" +
+                "id=" + id +
+                ", tableGroupId=" + tableGroupId +
+                ", numberOfGuests=" + numberOfGuests +
+                ", empty=" + empty +
+                '}';
+    }
+}
