3090c6b5d5ab58b338f3ea03186c1747f5529f38
poppohoo <ccf05017@gmail.com>
2021-01-08 10:29:12 +0900

    refactor(OrderLineItem): remove setter
    - 원자적인 도메인 동작을 위해 불필요한 setter 모두 제거

--- src/main/java/kitchenpos/domain/OrderLineItem.java
+++ src/main/java/kitchenpos/domain/OrderLineItem.java
@@ -9,7 +9,7 @@ public class OrderLineItem {
     public OrderLineItem() {
     }
 
-    OrderLineItem(final Long seq, final Long orderId, final Long menuId, final long quantity) {
+    public OrderLineItem(final Long seq, final Long orderId, final Long menuId, final long quantity) {
         this.seq = seq;
         this.orderId = orderId;
         this.menuId = menuId;
@@ -24,31 +24,15 @@ public class OrderLineItem {
         return seq;
     }
 
-    public void setSeq(final Long seq) {
-        this.seq = seq;
-    }
-
     public Long getOrderId() {
         return orderId;
-    }
-
-    public void setOrderId(final Long orderId) {
-        this.orderId = orderId;
     }
 
     public Long getMenuId() {
         return menuId;
     }
 
-    public void setMenuId(final Long menuId) {
-        this.menuId = menuId;
-    }
-
     public long getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(final long quantity) {
-        this.quantity = quantity;
     }
 }
