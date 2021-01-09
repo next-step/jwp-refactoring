06c558aa5fec9f933f098eed6a23080c016f4edc
cionman <topspin1278@naver.com>
2021-01-09 18:13:51 +0900

    refactor : kitchenpos-application 서브 모듈로 분리

---
+++ kitchenpos-application/src/test/java/kitchenpos/common/TestFixture.java
@@ -0,0 +1,60 @@
+package kitchenpos.common;
+
+import java.math.BigDecimal;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.Product;
+import kitchenpos.dto.MenuProductItem;
+import kitchenpos.dto.OrderItem;
+
+public class TestFixture {
+	public static final String 예제_메뉴그룹명 = "인도요리";
+	public static final String 예제_상품명 = "피자";
+	public static final BigDecimal 예제_상품_가격 = BigDecimal.valueOf(15000);
+	public static final long 조리상태인_테이블그룹_ID = 2L;
+	public static final long 예제_테이블_그룹_ID = 1L;
+	public static final BigDecimal 후라이드_가격 = BigDecimal.valueOf(16000);
+	public static final BigDecimal 양념치킨_가격 = BigDecimal.valueOf(16000);
+	public static final BigDecimal 예제_메뉴_가격 = BigDecimal.valueOf(20000);
+	public static final Long 예제_메뉴_그룹_ID = 1L;
+	public static final String 예제_메뉴명 = "치킨 선물 박스";
+	public static final long 예제테이블1_ID = 1L;
+	public static final long 예제테이블2_ID = 2L;
+	public static final long 비어있지않은테이블_ID = 7L;
+	public static final long 단체지정되어있지만_주문없는_테이블_ID = 5L;
+	public static final long 존재하지않는_테이블ID = 999L;
+	public static final long 빈테이블ID = 1L;
+	public static final long 주문대상_테이블ID = 3L;
+	public static final long 단체지정_테이블ID = 5L;
+	public static final long 주문상태_조리인_테이블ID = 7L;
+	public static final long 주문상태_식사인_테이블ID = 8L;
+	public static final long 조리상태_주문ID = 1L;
+	public static final long 식사상태_주문ID = 2L;
+	public static final long 완료상태_주문ID = 5L;
+
+	public static final OrderItem 주문_메뉴1 = OrderItem.of(1L, 1L);
+	public static final OrderItem 주문_메뉴2 = OrderItem.of(2L, 1L);
+	public static final OrderItem 주문_존재하지않은메뉴 = OrderItem.of(999L, 1L);
+	public static final MenuProductItem 메뉴_후라이드_갯수 = MenuProductItem.of(1L, 1L);
+	public static final MenuProductItem 메뉴_양념_갯수 = MenuProductItem.of(2L, 1L);
+
+
+	public static OrderTable 비어있지않은_테이블_객체() {
+		OrderTable table = OrderTable.create();
+		table.changeEmpty(false);
+		return table;
+	}
+
+	public static Order 일반_주문() {
+		return Order.create(비어있지않은_테이블_객체());
+	}
+
+	public static Order 완료된_주문() {
+		Order order = 일반_주문();
+		order.changeOrderStatus(OrderStatus.COMPLETION);
+		return order;
+	}
+}
---
+++ kitchenpos-application/src/test/resources/test-init.sql
@@ -0,0 +1,53 @@
+SET REFERENTIAL_INTEGRITY FALSE;
+TRUNCATE TABLE product;
+TRUNCATE TABLE order_line_item;
+TRUNCATE TABLE orders;
+TRUNCATE TABLE order_table;
+TRUNCATE TABLE menu_product;
+TRUNCATE TABLE table_group;
+TRUNCATE TABLE menu_group;
+TRUNCATE TABLE menu;
+
+INSERT INTO menu_group (id, name) VALUES (1, '중식');
+INSERT INTO menu_group (id, name) VALUES (2, '한식');
+INSERT INTO menu_group (id, name) VALUES (3, '야식');
+INSERT INTO menu_group (id, name) VALUES (4, '신메뉴');
+
+INSERT INTO product (id, name, price) VALUES (1, '후라이드', 16000);
+INSERT INTO product (id, name, price) VALUES (2, '양념치킨', 16000);
+INSERT INTO product (id, name, price) VALUES (3, '피자', 16000);
+INSERT INTO product (id, name, price) VALUES (4, '돈까스', 16000);
+INSERT INTO product (id, name, price) VALUES (5, '감자탕', 17000);
+INSERT INTO product (id, name, price) VALUES (6, '떡뽂이', 17000);
+
+INSERT INTO menu (id, name, price, menu_group_id) VALUES (1, '후라이드치킨', 16000, 2);
+INSERT INTO menu (id, name, price, menu_group_id) VALUES (2, '양념치킨', 16000, 2);
+INSERT INTO menu (id, name, price, menu_group_id) VALUES (3, '피자', 16000, 2);
+INSERT INTO menu (id, name, price, menu_group_id) VALUES (4, '돈까스', 16000, 2);
+INSERT INTO menu (id, name, price, menu_group_id) VALUES (5, '감자탕', 17000, 2);
+INSERT INTO menu (id, name, price, menu_group_id) VALUES (6, '떡뽂이', 17000, 2);
+
+INSERT INTO menu_product (seq, menu_id, product_id, quantity) VALUES (1, 1, 1, 1);
+INSERT INTO menu_product (seq, menu_id, product_id, quantity) VALUES (2, 2, 2, 1);
+INSERT INTO menu_product (seq, menu_id, product_id, quantity) VALUES (3, 3, 3, 1);
+INSERT INTO menu_product (seq, menu_id, product_id, quantity) VALUES (4, 4, 4, 1);
+INSERT INTO menu_product (seq, menu_id, product_id, quantity) VALUES (5, 5, 5, 1);
+INSERT INTO menu_product (seq, menu_id, product_id, quantity) VALUES (6, 6, 6, 1);
+INSERT INTO table_group(id, created_date) VALUES(1, CURRENT_TIMESTAMP());
+INSERT INTO table_group(id, created_date) VALUES(2, CURRENT_TIMESTAMP());
+INSERT INTO order_table (id, number_of_guests, empty) VALUES (1, 0, true);
+INSERT INTO order_table (id, number_of_guests, empty) VALUES (2, 0, true);
+INSERT INTO order_table (id, table_group_id, number_of_guests, empty) VALUES (3, 2, 3, false);
+INSERT INTO order_table (id, table_group_id, number_of_guests, empty) VALUES (4, 2, 3, false);
+INSERT INTO order_table (id, table_group_id, number_of_guests, empty) VALUES (5, 1, 1, false);
+INSERT INTO order_table (id, table_group_id, number_of_guests, empty) VALUES (6, 1, 1, false);
+INSERT INTO order_table (id, number_of_guests, empty) VALUES (7, 1, false);
+INSERT INTO order_table (id, number_of_guests, empty) VALUES (8, 1, false);
+INSERT INTO orders(id, order_table_id, order_status, ordered_time) VALUES (1, 7, 'COOKING', CURRENT_TIMESTAMP());
+INSERT INTO orders(id, order_table_id, order_status, ordered_time) VALUES (2, 8, 'MEAL', CURRENT_TIMESTAMP());
+INSERT INTO orders(id, order_table_id, order_status, ordered_time) VALUES (3, 3, 'COOKING', CURRENT_TIMESTAMP());
+INSERT INTO orders(id, order_table_id, order_status, ordered_time) VALUES (4, 4, 'COOKING', CURRENT_TIMESTAMP());
+INSERT INTO orders(id, order_table_id, order_status, ordered_time) VALUES (5, 1, 'COMPLETION', CURRENT_TIMESTAMP());
+INSERT INTO order_line_item(seq, order_id, menu_id, quantity) VALUES (1, 1, 1, 1);
+INSERT INTO order_line_item(seq, order_id, menu_id, quantity) VALUES (2, 2, 2, 2);
+SET REFERENTIAL_INTEGRITY TRUE;
