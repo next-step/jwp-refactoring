b3180acafc91450e2a14e675d740fbb93e9f4d93
hyeji <khji1210@naver.com>
2021-01-11 14:17:39 +0900

    feat : MenuGroupServiceTest 작성

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,33 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MenuGroupServiceTest {
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    @Test
+    public void test() {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setId(1L);
+        menuGroup.setName("두마리메뉴");
+        when(menuGroupDao.save(menuGroup)).thenReturn(menuGroup);
+
+        MenuGroup result = menuGroupService.create(menuGroup);
+
+        assertThat(result).isEqualTo(menuGroup);
+    }
+}
