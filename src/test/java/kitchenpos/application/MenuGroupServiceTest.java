a288f38932bbb2d61d46af759a4e084403a9474b
jaeseon_hwang <jshwang@deleo.co.kr>
2021-01-12 08:43:11 +0900

    test: MenuGroupServiceTest 추가

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,31 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuGroupServiceTest {
+
+	@Autowired
+	private MenuGroupService menuGroupService;
+
+	@DisplayName("메뉴그룹을 추가한다.")
+	@Test
+	void create() {
+		MenuGroup menuGroup = new MenuGroup();
+		menuGroup.setName("샐러드");
+
+		//when
+		MenuGroup savedMenuGroup = menuGroupService.create(menuGroup);
+
+		//then
+		List<MenuGroup> list = menuGroupService.list();
+		assertThat(list).contains(savedMenuGroup);
+	}
+}
