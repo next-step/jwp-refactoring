5ecac5792d004fe7d224df7ee947c2c6d9b2a353
tearight <tearightss@gmail.com>
2021-01-12 20:01:44 +0900

    test: add MockitoTest

---
+++ src/test/java/kitchenpos/MockitoTest.java
@@ -0,0 +1,14 @@
+package kitchenpos;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public abstract class MockitoTest {
+
+	protected MockitoTest() {
+	}
+}
