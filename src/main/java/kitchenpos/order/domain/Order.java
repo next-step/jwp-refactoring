194e2ce9e173eff7a1d8bfed7e5ecef2c5096309
yohan <good_1411@naver.com>
2021-01-14 00:52:53 +0900

    feat: 주문 애그리거트의 도메인 로직을 구현합니다.
    - [X] 주문을 생성한다.
            - [X] 주문 상태는 조리중으로 생성된다.
    - [X] 하나 이상의 주문 항목을 가져야 한다.
    - [X] 주문 테이블이 등록 불가 상태인 경우 생성할 수 없다.
    - [X] 주문의 상태를 변경할 수 있다.
    - [X] 주문의 상태가 이미 완료된 경우 변경할 수 없다.

---
+++ src/main/java/kitchenpos/order/domain/Order.java
@@ -0,0 +1,88 @@
+package kitchenpos.order.domain;
+
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.ordertable.domain.OrderTable;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    private static final int MIN_ORDER_ITEM_COUNT = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_table_id")
+    private OrderTable orderTable;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "order_status")
+    private OrderStatus orderStatus;
+
+    @CreationTimestamp
+    @Column(name = "ordered_time")
+    private LocalDateTime orderedTime;
+
+    @Embedded
+    private OrderLineItems orderLineItems;
+
+    protected Order() {
+    }
+
+    private Order(final OrderTable orderTable, final OrderStatus orderStatus, final List<OrderLineItem> orderLineItems) {
+        validate(orderTable, orderLineItems);
+        this.orderTable = Objects.requireNonNull(orderTable);
+        this.orderStatus = Objects.requireNonNull(orderStatus);
+        this.orderLineItems = new OrderLineItems(Objects.requireNonNull(orderLineItems), this);
+    }
+
+    public static Order of(final OrderTable orderTable, final List<OrderLineItem> orderLineItem) {
+        return new Order(orderTable, OrderStatus.COOKING, orderLineItem);
+    }
+
+    private void validate(final OrderTable orderTable, final List<OrderLineItem> orderLineItems) {
+        if (orderTable.isEmpty()) {
+            throw new IllegalArgumentException("주문 테이블이 등록 불가 상태인 경우 주문을 생성할 수 없다.");
+        }
+
+        if (orderLineItems.size() < MIN_ORDER_ITEM_COUNT) {
+            throw new IllegalArgumentException(String.format("%d개 이상 주문 항목이 있어야 합니다.", MIN_ORDER_ITEM_COUNT));
+        }
+    }
+
+    public void changeStatus(final OrderStatus orderStatus) {
+        if (OrderStatus.COMPLETION == this.orderStatus) {
+            throw new IllegalArgumentException("주문 완료 시 상태를 변경할 수 없습니다.");
+        }
+        this.orderStatus = Objects.requireNonNull(orderStatus);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public OrderTable getOrderTable() {
+        return orderTable;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public LocalDateTime getOrderedTime() {
+        return orderedTime;
+    }
+
+    public List<OrderLineItem> getOrderLineItems() {
+        return orderLineItems.getOrderLineItems();
+    }
+}
---
+++ src/main/java/kitchenpos/order/domain/OrderLineItem.java
@@ -0,0 +1,53 @@
+package kitchenpos.order.domain;
+
+import kitchenpos.menu.domain.Menu;
+
+import javax.persistence.*;
+
+@Entity
+public class OrderLineItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seq")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+
+    @Column(name = "quantity")
+    private long quantity;
+
+    protected OrderLineItem() {
+    }
+
+    public OrderLineItem(final Menu menu, final long quantity) {
+        this.menu = menu;
+        this.quantity = quantity;
+    }
+
+    public void assign(final Order order) {
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+}
