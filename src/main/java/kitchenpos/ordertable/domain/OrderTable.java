3bcfd3ab4e47cc86773d6eae84afde4f839985c1
yohan <good_1411@naver.com>
2021-01-13 22:23:14 +0900

    feat: 주문 테이블 애그리거트의 도메인 로직을 구현합니다.
    - [ ] 주문 테이블의 주문이 조리 중이거나 식사 중일때는 상태를 변경할 수 없다. (주문 도메인 개발 이후 진행)
    - [X] 주문 테이블을 생성한다.
    - [X] 손님 수는 0명 이상이어야 한다.
    - [X] 주문 테이블의 등록 가능 상태를 변경한다.
    - [X] 단체 지정이 되어 있다면 상태를 변경할 수 없다.
    - [X] 주문 테이블에 손님 수를 변경한다.
    - [X] 손님 수 변경 시 0명 이상이어야 한다.
    - [X] 등록 불가 상태인 주문 테이블인 경우 손님 수를 변경할 수 없다.

---
+++ src/main/java/kitchenpos/ordertable/domain/OrderTable.java
@@ -0,0 +1,71 @@
+package kitchenpos.ordertable.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class OrderTable {
+
+    private static final int MIN_NUMBER_OF_GUESTS = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "table_group_id")
+    private Long tableGroupId;
+
+    @Column(name = "number_of_guest")
+    private int numberOfGuests;
+
+    @Column(name = "empty")
+    private boolean empty;
+
+    protected OrderTable() {
+    }
+
+    public OrderTable(final int numberOfGuests, final boolean empty) {
+        this(null, numberOfGuests, empty);
+    }
+
+    public OrderTable(final Long tableGroupId, final int numberOfGuests, final boolean empty) {
+        this.tableGroupId = tableGroupId;
+        this.numberOfGuests = numberOfGuests;
+        this.empty = empty;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getTableGroupId() {
+        return tableGroupId;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public void changeEmpty(final boolean empty) {
+        if(Objects.nonNull(tableGroupId)){
+            throw new IllegalStateException("단체 지정이 되어 있다면 등록 상태를 변경할 수 없습니다.");
+        }
+        this.empty = empty;
+    }
+
+    public void changeNumberOfGuests(final int numberOfGuests) {
+        if (empty) {
+            throw new IllegalStateException("빈 테이블은 손님 수를 변경할 수 없습니다.");
+        }
+
+        if (numberOfGuests < MIN_NUMBER_OF_GUESTS) {
+            throw new IllegalArgumentException("손님 수는 0명 이상이어야 합니다.");
+        }
+        this.numberOfGuests = numberOfGuests;
+    }
+}
---
+++ src/test/java/kitchenpos/ordertable/domain/OrderTableTest.java
@@ -0,0 +1,97 @@
+package kitchenpos.ordertable.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("주문 테이블")
+class OrderTableTest {
+
+    @DisplayName("주문 테이블을 생성한다.")
+    @Test
+    void create() {
+        // given
+        int numberOfGuests = 5;
+        boolean empty = true;
+
+        // when
+        OrderTable orderTable = new OrderTable(numberOfGuests, empty);
+
+        // then
+        assertThat(orderTable).isNotNull();
+    }
+
+    @DisplayName("주문 테이블의 등록 가능 상태를 변경한다.")
+    @Test
+    void changeEmpty() {
+        // given
+        OrderTable orderTable = new OrderTable(5, true);
+
+        // when
+        orderTable.changeEmpty(false);
+
+        // then
+        assertThat(orderTable.isEmpty()).isFalse();
+    }
+
+    // todo 주문 도메인 개발 후 진행 예정
+    @DisplayName("주문 테이블의 주문이 조리 중이거나 식사 중일때는 상태를 변경할 수 없다.")
+    @Test
+    void cantChangeEmpty1() {
+        // given
+
+
+        // when
+
+
+        // then
+
+    }
+
+    @DisplayName("단체 지정이 되어 있다면 상태를 변경할 수 없다.")
+    @Test
+    void cantChangeEmpty2() {
+        // given
+        OrderTable orderTable = new OrderTable(1L, 5, true);
+
+        // when / then
+        assertThrows(IllegalStateException.class, () -> orderTable.changeEmpty(false));
+    }
+
+    @DisplayName("주문 테이블에 손님 수를 변한다.")
+    @Test
+    void changeNumberOfGuests() {
+        // given
+        OrderTable orderTable = new OrderTable( 5, false);
+
+        // when
+        orderTable.changeNumberOfGuests(3);
+
+        // then
+        assertThat(orderTable.getNumberOfGuests()).isEqualTo(3);
+    }
+
+    @DisplayName("손님 수 변경 시 0명 이상이어야 한다.")
+    @Test
+    void requiredNumberOfGuests() {
+        // given
+        OrderTable orderTable = new OrderTable( 5, false);
+
+        // when / then
+        assertThrows(IllegalArgumentException.class, () -> orderTable.changeNumberOfGuests(-1));
+
+    }
+
+    @DisplayName("등록 불가 상태인 주문 테이블인 경우 손님 수를 등록할 수 없다.")
+    @Test
+    void cantChangeNumberOfGuests() {
+        // given
+        OrderTable orderTable = new OrderTable( 5, true);
+
+        // when / then
+        assertThrows(IllegalStateException.class, () -> orderTable.changeNumberOfGuests(-1));
+    }
+
+}
