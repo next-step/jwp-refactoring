fc0170f5851a21428c8fa48d2f21b0291e8bf09e
jiho <jhhj424@naver.com>
2021-01-15 01:48:32 +0900

    test: 상품(Product) 테스트 작성

---
+++ src/test/java/kitchenpos/integrated/ProductServiceTest.java
@@ -0,0 +1,65 @@
+package kitchenpos.integrated;
+
+import kitchenpos.common.Price;
+import kitchenpos.product.application.ProductService;
+import kitchenpos.product.domain.Product;
+import kitchenpos.product.dto.ProductRequest;
+import kitchenpos.product.dto.ProductResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("상품 기능 테스트")
+@SpringBootTest
+class ProductServiceTest {
+    private Product product1;
+    private ProductRequest productRequest;
+
+    @Autowired
+    private ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+        product1 = new Product("후라이드", Price.of(16000));
+        productRequest = new ProductRequest(this.product1.getName(), this.product1.getPrice());
+    }
+
+    @Test
+    @DisplayName("상품을 등록할 수 있다.")
+    public void create() throws Exception {
+        // when
+        ProductResponse productResponse = productService.create(productRequest);
+
+        // then
+        assertThat(productResponse).isNotNull();
+        assertThat(productResponse.getName()).isEqualTo(this.product1.getName());
+        assertThat(productResponse.getPrice()).isEqualTo(this.product1.getPrice().get());
+    }
+
+    @Test
+    @DisplayName("상품의 가격이 올바르지 않으면 등록할 수 없다 : 상품의 가격은 0 원 이상이어야 한다.")
+    public void createFail() throws Exception {
+        // when then
+        assertThatThrownBy(() -> {
+            new Product("실패상품", Price.of(-1));
+            productService.create(productRequest);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("상품의 목록을 조회할 수 있다.")
+    public void list() throws Exception {
+        // when
+        List<ProductResponse> products = productService.findAll();
+
+        // then
+        assertThat(products).isNotEmpty();
+    }
+}
