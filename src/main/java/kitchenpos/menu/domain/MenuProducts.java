1efe73609c82d16b32e44940762fcc2ae36da6bf
jiho <jhhj424@naver.com>
2021-01-15 23:13:10 +0900

    refactor: 메뉴(Menu) 관련 기능 리팩토링 및 구현

---
+++ src/main/java/kitchenpos/menu/domain/MenuProducts.java
@@ -0,0 +1,38 @@
+package kitchenpos.menu.domain;
+
+import kitchenpos.common.Price;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class MenuProducts {
+    @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MenuProduct> menuProducts = new ArrayList<>();
+
+    protected MenuProducts() {
+    }
+
+    public MenuProducts(List<MenuProduct> menuProducts) {
+        this.menuProducts.addAll(menuProducts);
+    }
+
+    public boolean isEmpty() {
+        return menuProducts.isEmpty();
+    }
+
+    public List<MenuProduct> getMenuProducts() {
+        return menuProducts;
+    }
+
+    public Price calculateSumPrice() {
+        int sum = menuProducts.stream()
+                .map(MenuProduct::calculatePrice)
+                .mapToInt(Price::intValue)
+                .sum();
+        return Price.of(sum);
+    }
+}
---
+++ src/main/java/kitchenpos/menu/dto/MenuProductRequest.java
@@ -0,0 +1,25 @@
+package kitchenpos.menu.dto;
+
+public class MenuProductRequest {
+    private Long menuId;
+    private Long productId;
+    private long quantity;
+
+    public MenuProductRequest(Long menuId, Long productId, long quantity) {
+        this.menuId = menuId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getMenuId() {
+        return menuId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+}
