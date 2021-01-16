95e40bdb0130617dd79ad94d29051f450def25c8
jiho <jhhj424@naver.com>
2021-01-16 16:48:25 +0900

    refactor: 단체 지정(TableGroup) 관련 기능 리팩토링 및 구현

---
+++ src/main/java/kitchenpos/tablegroup/domain/OrderTables.java
@@ -0,0 +1,51 @@
+package kitchenpos.tablegroup.domain;
+
+import kitchenpos.ordertable.domain.OrderTable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class OrderTables {
+    @OneToMany(mappedBy = "tableGroup", cascade = CascadeType.PERSIST)
+    private List<OrderTable> orderTables = new ArrayList<>();
+
+    protected OrderTables() {
+    }
+
+    public OrderTables(List<OrderTable> orderTables) {
+        this.orderTables.addAll(orderTables);
+    }
+
+    public OrderTables(List<OrderTable> orderTables, TableGroup tableGroup) {
+        this.orderTables.addAll(orderTables);
+        changeTableGroup(tableGroup);
+    }
+
+    public boolean isEmptyTablesNotMore(int limit) {
+        long count = orderTables.stream()
+                .filter(OrderTable::isEmpty)
+                .count();
+        return count < limit;
+    }
+
+    public boolean hasOtherOrderTable() {
+        return orderTables.stream()
+                .anyMatch(OrderTable::hasOtherOrderTable);
+    }
+
+    private void changeTableGroup(TableGroup tableGroup) {
+        this.orderTables.forEach(orderTable -> orderTable.changeTableGroup(tableGroup));
+    }
+
+    public List<OrderTable> getOrderTables() {
+        return orderTables;
+    }
+
+    public boolean isEmpty() {
+        return orderTables.isEmpty();
+    }
+}
---
+++ src/main/java/kitchenpos/tablegroup/dto/TableGroupRequest.java
@@ -0,0 +1,17 @@
+package kitchenpos.tablegroup.dto;
+
+import kitchenpos.ordertable.dto.OrderTableRequest;
+
+import java.util.List;
+
+public class TableGroupRequest {
+    private List<OrderTableRequest> orderTables;
+
+    public TableGroupRequest(List<OrderTableRequest> orderTables) {
+        this.orderTables = orderTables;
+    }
+
+    public List<OrderTableRequest> getOrderTables() {
+        return orderTables;
+    }
+}
