d93151239c38ceb30ab8c38b00d823c80e7d4735
lazy-moon <magyeon@naver.com>
2021-01-17 14:47:31 +0900

    refactor: Product JPA로 변경

--- src/main/java/kitchenpos/application/ProductService.java
+++ src/main/java/kitchenpos/application/ProductService.java
@@ -1,6 +1,6 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.ProductDao;
+import kitchenpos.dao.ProductRepository;
 import kitchenpos.domain.Product;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,26 +9,27 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 
+@Transactional
 @Service
 public class ProductService {
-    private final ProductDao productDao;
+    private final ProductRepository productRepository;
 
-    public ProductService(final ProductDao productDao) {
-        this.productDao = productDao;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
-    @Transactional
-    public Product create(final Product product) {
+    public Product create(Product product) {
         final BigDecimal price = product.getPrice();
 
         if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException();
         }
 
-        return productDao.save(product);
+        return productRepository.save(product);
     }
 
+    @Transactional(readOnly = true)
     public List<Product> list() {
-        return productDao.findAll();
+        return productRepository.findAll();
     }
 }
