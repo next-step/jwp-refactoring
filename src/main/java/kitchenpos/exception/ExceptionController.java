a8859ddd3b94e57450e3808f74eb565fec53c929
lazy-moon <magyeon@naver.com>
2021-01-18 21:14:19 +0900

    feature: Exception 핸들러 추가

---
+++ src/main/java/kitchenpos/exception/ExceptionController.java
@@ -0,0 +1,16 @@
+package kitchenpos.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.persistence.PersistenceException;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler({DataIntegrityViolationException.class, BadRequestException.class, PersistenceException.class})
+    public ResponseEntity<?> handleIllegalArgsException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
