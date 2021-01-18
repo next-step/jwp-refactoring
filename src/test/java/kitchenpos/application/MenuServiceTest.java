ceebf040beab17a81d40ed73fd6f1cc8caaab2a6
shk3029 <shk30290@naver.com>
2021-01-18 10:25:51 +0900

    feat: MenuGroupService 통합 테스트 추가

---
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -0,0 +1,42 @@
+package kitchenpos.application;
+
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MenuServiceTest {
+
+    private final String NEW_MENU_GROUP_NAME = "세번째메뉴";
+
+    @Autowired
+    private MenuGroupService menuGroupService;
+
+    @DisplayName("메뉴그룹을 생성한다")
+    @Test
+    void create() {
+        MenuGroup menuGroup = menuGroupService.create(new MenuGroup(NEW_MENU_GROUP_NAME));
+        MenuGroup newMenuGroup = menuGroupService.findMenuGroupById(menuGroup.getId());
+
+        assertThat(newMenuGroup.getName()).isEqualTo(NEW_MENU_GROUP_NAME);
+    }
+
+    @DisplayName("메뉴 그룹을 조회한다")
+    @Test
+    void list() {
+        menuGroupService.create(new MenuGroup(NEW_MENU_GROUP_NAME));
+
+        List<String> menuGroupNames = menuGroupService.list().stream()
+                .map(MenuGroup::getName)
+                .collect(Collectors.toList());
+
+        assertThat(menuGroupNames).contains(NEW_MENU_GROUP_NAME);
+    }
+}
