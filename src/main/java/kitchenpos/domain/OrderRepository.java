80cd1f24d3948bfd7fb877b9aa163822bc8406a7
louisJu <rokmctkd6@gmail.com>
2021-01-19 15:57:55 +0900

    feat: 주문, 테이블 관련 repository 작성

---
+++ src/main/java/kitchenpos/domain/OrderRepository.java
@@ -0,0 +1,11 @@
+package kitchenpos.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    boolean existsByOrderTableIdAndOrderStatusIn(Long orderTableId, List<String> asList);
+
+    boolean existsByOrderTableIdInAndOrderStatusIn(List<Long> orderTableIds, List<String> asList);
+}
