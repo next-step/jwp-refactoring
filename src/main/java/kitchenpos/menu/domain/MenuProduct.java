f8b6304f9185fb0604277c1dc34c2067d3a73924
slamdunk7575 <slamdunk7575@gmail.com>
2021-01-20 18:32:37 +0900

    (refactor) MenuProduct 관련 서비스 리팩토링

---
+++ src/main/java/kitchenpos/menu/domain/MenuProduct.java
@@ -0,0 +1,64 @@
+package kitchenpos.menu.domain;
+
+import kitchenpos.common.Price;
+import kitchenpos.common.Quantity;
+import kitchenpos.product.domain.Product;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class MenuProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seq")
+    private Long seq;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Embedded
+    private Quantity quantity;
+
+    public MenuProduct() {
+    }
+
+    public MenuProduct(Product product, long quantity) {
+        this.product = product;
+        this.quantity = new Quantity(quantity);
+    }
+
+    public void updateMenu(Menu menu) {
+        this.menu = menu;
+    }
+
+    public Long getSeq() {
+        return seq;
+    }
+
+    public Long getMenuId() {
+        return menu.getId();
+    }
+
+    public Long getProductId() {
+        return product.getId();
+    }
+
+    public long getQuantity() {
+        return quantity.value();
+    }
+
+    public Price getPricePerQuantity() {
+        return product.calculatePricePerQuantity(quantity);
+    }
+
+    public BigDecimal getProductPrice() {
+        return this.product.getPrice();
+    }
+}
