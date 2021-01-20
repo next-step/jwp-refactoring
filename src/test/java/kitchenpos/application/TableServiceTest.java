4e7ac33ece32d98b72d71e98b8840c8dddeeeed4
byungkyu-ju <noel1227@gmail.com>
2021-01-20 17:10:32 +0900

    test(TableServiceTest): 테이블 비우기 예외상황 테스트케이스 구현

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -91,4 +91,41 @@ class TableServiceTest {
 		assertThat(finalSavedOrderTable.isEmpty()).isTrue();
 	}
 
+	@DisplayName("존재하지 않는 테이블은 비울 수 없다.")
+	@Test
+	void notExistOrderTableCannotChangeEmpty(){
+		// given
+		Long orderTableId = 1L;
+		OrderTable orderTable = mock(OrderTable.class);
+		when(orderTable.getId()).thenReturn(orderTableId);
+
+		when(orderTableDao.findById(orderTable.getId())).thenReturn(of(orderTable));
+		TableService tableService = new TableService(orderDao, orderTableDao);
+
+		// when - then
+		assertThatThrownBy(() -> {
+			tableService.changeEmpty(orderTableId, orderTable);
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("요리중이거나 식사중인 테이블이 있는 경우 테이블을 비울 수 없다.")
+	@Test
+	void mealOrCookingOrderTableCannotNotChangeEmpty(){
+		// given
+		Long orderTableId = 1L;
+		OrderTable orderTable = mock(OrderTable.class);
+		when(orderTable.getId()).thenReturn(orderTableId);
+		when(orderTable.getTableGroupId()).thenReturn(null);
+
+		when(orderTableDao.findById(orderTable.getId())).thenReturn(of(orderTable));
+		when(orderDao.existsByOrderTableIdAndOrderStatusIn(
+			orderTableId, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))).thenReturn(true);
+		TableService tableService = new TableService(orderDao, orderTableDao);
+
+		// when - then
+		assertThatThrownBy(() -> {
+			tableService.changeEmpty(orderTableId, orderTable);
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+
 }
