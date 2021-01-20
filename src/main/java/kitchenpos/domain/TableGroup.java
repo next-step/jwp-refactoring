5bc1c6d773c55653807698b1d64774db440b1b39
charlloss.dev <charlloss.dev@gmail.com>
2021-01-21 03:30:44 +0900

    feat(table group): 단체 지정 stubbing, mvc, acceptance 테스트 관련 개발

--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -4,31 +4,44 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class TableGroup {
-    private Long id;
-    private LocalDateTime createdDate;
-    private List<OrderTable> orderTables;
+	private Long id;
+	private LocalDateTime createdDate;
+	private List<OrderTable> orderTables;
 
-    public Long getId() {
-        return id;
-    }
+	public TableGroup() {
+	}
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
+	private TableGroup(Long id, LocalDateTime createdDate, List<OrderTable> orderTables) {
+		this.id = id;
+		this.createdDate = createdDate;
+		this.orderTables = orderTables;
+	}
 
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
+	public static TableGroup of(Long id, LocalDateTime createdDate, List<OrderTable> orderTables) {
+		return new TableGroup(id, createdDate, orderTables);
+	}
 
-    public void setCreatedDate(final LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public List<OrderTable> getOrderTables() {
-        return orderTables;
-    }
+	public void setId(final Long id) {
+		this.id = id;
+	}
 
-    public void setOrderTables(final List<OrderTable> orderTables) {
-        this.orderTables = orderTables;
-    }
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(final LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public List<OrderTable> getOrderTables() {
+		return orderTables;
+	}
+
+	public void setOrderTables(final List<OrderTable> orderTables) {
+		this.orderTables = orderTables;
+	}
 }
