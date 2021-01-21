70a58c1d89308bea6575b67ec5cb692441358f6e
orgojy <giveawesome@gmail.com>
2021-01-21 11:13:48 +0900

    refactor: 변경된 PK와 시간 추상화에 맞는 예시 DDL, DML 추가

--- src/main/resources/db/migration/V1__Initialize_project_tables.sql
+++ src/main/resources/db/migration/V1__Initialize_project_tables.sql
@@ -2,16 +2,17 @@ CREATE TABLE orders (
     id BIGINT(20) NOT NULL AUTO_INCREMENT,
     order_table_id BIGINT(20) NOT NULL,
     order_status VARCHAR(255) NOT NULL,
-    ordered_time DATETIME NOT NULL,
+    created_at DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE order_line_item (
-    seq BIGINT(20) NOT NULL AUTO_INCREMENT,
+    id BIGINT(20) NOT NULL AUTO_INCREMENT,
     order_id BIGINT(20) NOT NULL,
     menu_id BIGINT(20) NOT NULL,
     quantity BIGINT(20) NOT NULL,
-    PRIMARY KEY (seq)
+    created_at DATETIME NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE menu (
@@ -19,21 +20,24 @@ CREATE TABLE menu (
     name VARCHAR(255) NOT NULL,
     price DECIMAL(19, 2) NOT NULL,
     menu_group_id BIGINT(20) NOT NULL,
+    created_at DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE menu_group (
     id BIGINT(20) NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE menu_product (
-    seq BIGINT(20) NOT NULL AUTO_INCREMENT,
+    id BIGINT(20) NOT NULL AUTO_INCREMENT,
     menu_id BIGINT(20) NOT NULL,
     product_id BIGINT(20) NOT NULL,
     quantity BIGINT(20) NOT NULL,
-    PRIMARY KEY (seq)
+    created_at DATETIME NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE order_table (
@@ -41,12 +45,13 @@ CREATE TABLE order_table (
     table_group_id BIGINT(20),
     number_of_guests INT(11) NOT NULL,
     empty BIT(1) NOT NULL,
+    created_at DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE table_group (
     id BIGINT(20) NOT NULL AUTO_INCREMENT,
-    created_date DATETIME NOT NULL,
+    created_at DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -54,6 +59,7 @@ CREATE TABLE product (
     id BIGINT(20) NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     price DECIMAL(19, 2) NOT NULL,
+    created_at DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
 
