d23d545f1a7c8363b7e7763a862b6f94cbef112a
Pilhwan Kim <pilhwankim03@gmail.com>
2021-01-21 09:04:09 +0900

    fix(order): Order 엔티티에 OrderStatus의 초기값 지정

--- src/main/java/kitchenpos/order/domain/Order.java
+++ src/main/java/kitchenpos/order/domain/Order.java
@@ -17,7 +17,7 @@ public class Order {
     private OrderTable orderTable;
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus;
+    private OrderStatus orderStatus = OrderStatus.COOKING;
 
     private LocalDateTime orderedTime;
 
