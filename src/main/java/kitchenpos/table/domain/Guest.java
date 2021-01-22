5e5ec4da3ffbb1c1ba099208b6b8216feae2e3ee
charlloss.dev <charlloss.dev@gmail.com>
2021-01-23 03:47:57 +0900

    feat(order table): order table 엔티티 생성

---
+++ src/main/java/kitchenpos/table/domain/Guest.java
@@ -0,0 +1,34 @@
+package kitchenpos.table.domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Access(AccessType.FIELD)
+public class Guest {
+	public static final int GUEST_MINIMUM_COUNT = 0;
+	private int numberOfGuests;
+
+	protected Guest() {
+	}
+
+	private Guest(final int numberOfGuests) {
+		validateNumberOfGuests(numberOfGuests);
+		this.numberOfGuests = numberOfGuests;
+	}
+
+	public static Guest of(final int numberOfGuests) {
+		return new Guest(numberOfGuests);
+	}
+
+	private void validateNumberOfGuests(final int numberOfGuests) {
+		if (numberOfGuests < GUEST_MINIMUM_COUNT) {
+			throw new IllegalArgumentException("손님은 0명 이상이여야 합니다.");
+		}
+	}
+
+	public int getNumberOfGuests() {
+		return numberOfGuests;
+	}
+}
