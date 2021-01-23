d7693bac749066e3692b63b404fad5cdd3c8240c
byungkyu-ju <noel1227@gmail.com>
2021-01-23 11:44:38 +0900

    refactor(MenuGroup): mock기반 테스트 제거, menu 도메인 분리, dto기반 ui request, response로 변경

---
+++ src/main/java/kitchenpos/menu/dto/MenuGroupRequest.java
@@ -0,0 +1,22 @@
+package kitchenpos.menu.dto;
+
+/**
+ * @author : byungkyu
+ * @date : 2021/01/22
+ * @description :
+ **/
+public class MenuGroupRequest {
+
+	private String name;
+
+	public MenuGroupRequest() {
+	}
+
+	public MenuGroupRequest(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
