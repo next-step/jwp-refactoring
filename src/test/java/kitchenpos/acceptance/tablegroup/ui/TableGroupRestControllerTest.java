18c0b40e211051b32fed945ca9e626358fd03432
charlloss.dev <charlloss.dev@gmail.com>
2021-01-23 23:06:38 +0900

    test(table group): table group stubbing, mvc 테스트

--- src/test/java/kitchenpos/acceptance/tablegroup/ui/TableGroupRestControllerTest.java
+++ src/test/java/kitchenpos/acceptance/tablegroup/ui/TableGroupRestControllerTest.java
@@ -1,4 +1,4 @@
-package kitchenpos.ui;
+package kitchenpos.acceptance.tablegroup.ui;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
@@ -6,25 +6,31 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import kitchenpos.application.TableGroupService;
-import kitchenpos.domain.OrderTable;
-import kitchenpos.domain.TableGroup;
+import kitchenpos.ordertable.dto.OrderTableResponse;
+import kitchenpos.tablegroup.application.TableGroupService;
+import kitchenpos.tablegroup.dto.TableGroupRequest;
+import kitchenpos.tablegroup.dto.TableGroupResponse;
+import kitchenpos.tablegroup.ui.TableGroupRestController;
 
 @WebMvcTest(TableGroupRestController.class)
+@MockBean(JpaMetamodelMappingContext.class)
 class TableGroupRestControllerTest {
 	@Autowired
 	private MockMvc mvc;
@@ -33,28 +39,35 @@ class TableGroupRestControllerTest {
 	@MockBean
 	private TableGroupService tableGroupService;
 
+	private List<OrderTableResponse> orderTableResponses;
+
+	@BeforeEach
+	void setUp() {
+		orderTableResponses = Arrays.asList(
+			OrderTableResponse.of(1L, 5, false),
+			OrderTableResponse.of(2L, 7, false));
+	}
+
 	@DisplayName("테이블그룹을 등록한다.")
 	@Test
 	public void create() throws Exception {
 		// given
-		OrderTable table1 = OrderTable.of(1L, null, 0, true);
-		OrderTable table2 = OrderTable.of(2L, null, 5, false);
-		List<OrderTable> orderTables = Arrays.asList(table1, table2);
-		TableGroup expectedTableGroup = TableGroup.of(1L, null, orderTables);
-		given(tableGroupService.create(any())).willReturn(expectedTableGroup);
+		TableGroupRequest request = TableGroupRequest.of(Arrays.asList(1L, 2L));
+		TableGroupResponse response = TableGroupResponse.of(1L, LocalDateTime.now(), orderTableResponses);
+		given(tableGroupService.create(any())).willReturn(response);
 
 		// when
 		final ResultActions resultActions = mvc.perform(post("/api/table-groups")
 			.contentType(MediaType.APPLICATION_JSON)
-			.content(mapper.writeValueAsString(expectedTableGroup)))
+			.content(mapper.writeValueAsString(request)))
 			.andDo(print());
 
 		// then
 		resultActions
 			.andExpect(status().isCreated())
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(redirectedUrl("/api/table-groups" + "/" + expectedTableGroup.getId()))
-			.andExpect(jsonPath("$.id").value(expectedTableGroup.getId()))
+			.andExpect(redirectedUrl("/api/table-groups" + "/" + response.getId()))
+			.andExpect(jsonPath("$.id").value(response.getId()))
 			.andDo(log());
 	}
 }
