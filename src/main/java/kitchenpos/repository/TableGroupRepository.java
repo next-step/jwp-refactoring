3f3efbd1ccad45563a2287c86b5bb711ee9550b8
shk3029 <shk30290@naver.com>
2021-01-25 01:02:39 +0900

    feat: add TableGroupRepository

--- src/main/java/kitchenpos/repository/TableGroupRepository.java
+++ src/main/java/kitchenpos/repository/TableGroupRepository.java
@@ -1,4 +1,7 @@
 package kitchenpos.repository;
 
-public interface TableGroupRepository {
+import kitchenpos.domain.TableGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TableGroupRepository extends JpaRepository<TableGroup, Long> {
 }
