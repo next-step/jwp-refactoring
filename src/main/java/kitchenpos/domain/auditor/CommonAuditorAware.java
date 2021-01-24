ed688f86a95c62bfce1b94fa88b4f7521970968d
shk3029 <shk30290@naver.com>
2021-01-25 00:41:04 +0900

    feat: JpaAuditConfig 추가

---
+++ src/main/java/kitchenpos/domain/auditor/CommonAuditorAware.java
@@ -0,0 +1,14 @@
+package kitchenpos.domain.auditor;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class CommonAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        // @LastModifiedBy, @CreatedBy가 추가될 때 사용
+        return Optional.empty();
+    }
+}
