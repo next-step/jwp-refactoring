4c4772ba33f224e17d8a1756079af2a2519ea853
shk3029 <shk30290@naver.com>
2021-01-25 01:01:34 +0900

    feat: add TableGroup Domain, Repository

--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -1,11 +1,25 @@
 package kitchenpos.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class TableGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDate;
+
+    @OneToMany(mappedBy = "tableGroup")
     private List<OrderTable> orderTables;
 
     public TableGroup() {
---
+++ src/main/java/kitchenpos/repository/TableGroupRepository.java
@@ -0,0 +1,4 @@
+package kitchenpos.repository;
+
+public interface TableGroupRepository {
+}
