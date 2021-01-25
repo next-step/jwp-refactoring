a4c13deee99dbd3bdb11399ba3b74264ec913355
byungkyu-ju <noel1227@gmail.com>
2021-01-25 21:58:35 +0900

    refactor: 사용하지 않는 setter 삭제

--- src/main/java/kitchenpos/menu/domain/Menu.java
+++ src/main/java/kitchenpos/menu/domain/Menu.java
@@ -95,17 +95,8 @@ public class Menu {
     public Long getId() {
         return id;
     }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
     }
 
     public BigDecimal getPrice() {
