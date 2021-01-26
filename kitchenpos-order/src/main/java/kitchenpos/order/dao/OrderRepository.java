3addfca90e7f9076d0d70e0b7a5494c81b00e273
lazy-moon <magyeon@naver.com>
2021-01-27 02:06:31 +0900

    fix: 파라미터 Collection으로 변경

--- kitchenpos-order/src/main/java/kitchenpos/order/dao/OrderRepository.java
+++ kitchenpos-order/src/main/java/kitchenpos/order/dao/OrderRepository.java
@@ -6,11 +6,12 @@ import kitchenpos.table.domain.OrderTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
     boolean existsByOrderTableAndOrderStatusIn(OrderTable orderTable, List<OrderStatus> orderStatuses);
 
-    boolean existsByOrderTableInAndOrderStatusIn(List<OrderTable> orderTables, List<OrderStatus> orderStatuses);
+    boolean existsByOrderTableInAndOrderStatusIn(Collection<OrderTable> orderTables, List<OrderStatus> orderStatuses);
 }
