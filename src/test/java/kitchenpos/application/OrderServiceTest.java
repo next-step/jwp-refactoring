77955499d3f462db28a032184d9f1a2db626ba36
ycc4477 <ycc4477@gmail.com>
2021-01-29 00:26:13 +0900

    feat: order 생성 테스트

---
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -0,0 +1,131 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private MenuGroupService menuGroupService;
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private TableService tableService;
+    @Autowired
+    private MenuService menuService;
+
+    private Menu createdMenu;
+    private OrderTable createdOrderTable;
+
+    @BeforeEach
+    void setUp() {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName("양식");
+        MenuGroup createMenuGroup = menuGroupService.create(menuGroup);
+
+        Product product = new Product();
+        product.setName("알리오올리오");
+        product.setPrice(BigDecimal.valueOf(12_000));
+        Product createdProduct = productService.create(product);
+
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProductId(createdProduct.getId());
+        menuProduct.setQuantity(1L);
+
+        Menu menu = new Menu();
+        menu.setName("알리오올리오");
+        menu.setPrice(BigDecimal.valueOf(12_000));
+        menu.setMenuGroupId(createMenuGroup.getId());
+        menu.setMenuProducts(Collections.singletonList(menuProduct));
+
+        createdMenu = menuService.create(menu);
+
+        OrderTable orderTable = new OrderTable();
+        createdOrderTable = tableService.create(orderTable);
+
+    }
+
+    @Test
+    @DisplayName("order 생성")
+    void order_create_test() {
+        //given
+        Order orderRequest = ORDER_REQUEST_생성();
+        OrderLineItem orderLineItem = ORDER_LINE_ITEM_생성(createdMenu.getId());
+        orderRequest.setOrderLineItems(Collections.singletonList(orderLineItem));
+        orderRequest.setOrderTableId(createdOrderTable.getId());
+        //when
+        Order createdOrder = ORDER_생성_테스트(orderRequest);
+
+        //then
+        Assertions.assertAll(() -> {
+            assertThat(createdOrder.getId()).isNotNull();
+        });
+    }
+
+    @Test
+    @DisplayName("order에는 order line item이 1개 이상 존재해야한다.")
+    void order_create_order_item_null_test() {
+        //given
+        Order orderRequest = ORDER_REQUEST_생성();
+        OrderLineItem orderLineItem = ORDER_LINE_ITEM_생성(createdMenu.getId());
+        orderRequest.setOrderLineItems(Collections.emptyList());
+        orderRequest.setOrderTableId(createdOrderTable.getId());
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            Order createdOrder = ORDER_생성_테스트(orderRequest);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("order에는 order table이 1개 이상 존재해야한다.")
+    void order_create_order_table_null_test() {
+        //given
+        Order orderRequest = ORDER_REQUEST_생성();
+        OrderLineItem orderLineItem = ORDER_LINE_ITEM_생성(createdMenu.getId());
+        orderRequest.setOrderLineItems(Collections.singletonList(orderLineItem));
+
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            Order createdOrder = ORDER_생성_테스트(orderRequest);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private OrderLineItem ORDER_LINE_ITEM_생성(long menuId) {
+        OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setMenuId(menuId);
+        return orderLineItem;
+    }
+
+
+    private Order ORDER_생성_테스트(Order orderRequest) {
+        return orderService.create(orderRequest);
+    }
+
+    private Order ORDER_REQUEST_생성() {
+        Order order = new Order();
+        return order;
+    }
+}
