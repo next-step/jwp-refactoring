990b294a7d3f75b26d791139bc443c56240b9edd
Mingyu Lee <leemingyu05@gmail.com>
2021-01-28 23:54:36 +0900

    refactor: 메뉴 테스트 리팩토링

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -8,6 +8,7 @@ import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,6 +26,46 @@ import static org.mockito.BDDMockito.given;
 @DisplayName("애플리케이션 테스트 보호 - 메뉴 서비스")
 @ExtendWith(MockitoExtension.class)
 public class MenuServiceTest {
+
+    private Product 후라이드치킨;
+    private Product 양념치킨;
+    private MenuGroup 치킨세트;
+    private Menu 후라이드한마리양념치킨한마리;
+    private MenuProduct 후라이드치킨한마리;
+    private MenuProduct 양념치킨한마리;
+
+    @BeforeEach
+    public void setup() {
+        후라이드치킨 = new Product();
+        후라이드치킨.setId(1L);
+        후라이드치킨.setName("후라이드");
+        후라이드치킨.setPrice(BigDecimal.valueOf(10000));
+
+        양념치킨 = new Product();
+        양념치킨.setId(2L);
+        양념치킨.setName("양념");
+        양념치킨.setPrice(BigDecimal.valueOf(110000));
+
+        치킨세트 = new MenuGroup();
+        치킨세트.setId(1L);
+        치킨세트.setName("후라이드앙념치킨");
+
+        후라이드한마리양념치킨한마리 = new Menu();
+        후라이드한마리양념치킨한마리.setId(1L);
+        후라이드한마리양념치킨한마리.setName("후라이드+양념");
+        후라이드한마리양념치킨한마리.setMenuGroupId(치킨세트.getId());
+        후라이드한마리양념치킨한마리.setPrice(BigDecimal.valueOf(21000));
+
+        후라이드치킨한마리 = new MenuProduct();
+        후라이드치킨한마리.setProductId(후라이드치킨.getId());
+        후라이드치킨한마리.setQuantity(1);
+
+        양념치킨한마리 = new MenuProduct();
+        양념치킨한마리.setProductId(양념치킨.getId());
+        양념치킨한마리.setQuantity(1);
+
+        후라이드한마리양념치킨한마리.setMenuProducts(Arrays.asList(후라이드치킨한마리, 양념치킨한마리));
+    }
 
     @Mock
     private ProductDao productDao;
@@ -44,47 +85,17 @@ public class MenuServiceTest {
     @DisplayName("메뉴 생성")
     @Test
     void create() {
-        Product product1 = new Product();
-        product1.setId(1L);
-        product1.setName("후라이드");
-        product1.setPrice(BigDecimal.valueOf(10000));
+        given(menuGroupDao.existsById(치킨세트.getId())).willReturn(true);
+        given(productDao.findById(후라이드치킨.getId())).willReturn(Optional.of(후라이드치킨));
+        given(productDao.findById(양념치킨.getId())).willReturn(Optional.of(양념치킨));
+        given(menuDao.save(후라이드한마리양념치킨한마리)).willReturn(후라이드한마리양념치킨한마리);
+        given(menuProductDao.save(후라이드치킨한마리)).willReturn(후라이드치킨한마리);
+        given(menuProductDao.save(양념치킨한마리)).willReturn(양념치킨한마리);
 
-        Product product2 = new Product();
-        product2.setId(2L);
-        product2.setName("양념");
-        product2.setPrice(BigDecimal.valueOf(110000));
+        Menu savedMenu = menuService.create(후라이드한마리양념치킨한마리);
 
-        MenuGroup menuGroup = new MenuGroup();
-        menuGroup.setId(1L);
-        menuGroup.setName("후라이드앙념치킨");
-
-        Menu menu = new Menu();
-        menu.setId(1L);
-        menu.setName("후라이드+양념");
-        menu.setMenuGroupId(menuGroup.getId());
-        menu.setPrice(BigDecimal.valueOf(21000));
-
-        MenuProduct menuProduct1 = new MenuProduct();
-        menuProduct1.setProductId(product1.getId());
-        menuProduct1.setQuantity(1);
-
-        MenuProduct menuProduct2 = new MenuProduct();
-        menuProduct2.setProductId(product2.getId());
-        menuProduct2.setQuantity(1);
-
-        menu.setMenuProducts(Arrays.asList(menuProduct1, menuProduct2));
-
-        given(menuGroupDao.existsById(menuGroup.getId())).willReturn(true);
-        given(productDao.findById(product1.getId())).willReturn(Optional.of(product1));
-        given(productDao.findById(product2.getId())).willReturn(Optional.of(product2));
-        given(menuDao.save(menu)).willReturn(menu);
-        given(menuProductDao.save(menuProduct1)).willReturn(menuProduct1);
-        given(menuProductDao.save(menuProduct2)).willReturn(menuProduct2);
-
-        Menu savedMenu = menuService.create(menu);
-
-        assertThat(savedMenu).isEqualTo(menu);
-
+        assertThat(savedMenu).isEqualTo(후라이드한마리양념치킨한마리);
     }
+
 
 }
