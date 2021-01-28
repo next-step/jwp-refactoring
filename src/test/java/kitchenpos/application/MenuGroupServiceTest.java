8b965d8f55da55f026946a82c3447f5256b494ec
Mingyu Lee <leemingyu05@gmail.com>
2021-01-28 23:21:13 +0900

    test: 메뉴 그룹 생성

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,43 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@DisplayName("애플리케이션 테스트 보호 - 메뉴 그룹 서비스")
+@ExtendWith(MockitoExtension.class)
+public class MenuGroupServiceTest {
+
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    @DisplayName("메뉴 그룹 생성")
+    @Test
+    void create() {
+        MenuGroup request = new MenuGroup();
+        request.setName("메뉴그룹");
+
+        MenuGroup expected = new MenuGroup();
+        expected.setId(1L);
+        expected.setName(request.getName());
+
+        when(menuGroupDao.save(request)).thenReturn(expected);
+
+        MenuGroup savedMenuGroup = menuGroupService.create(request);
+
+        assertThat(savedMenuGroup).isEqualTo(expected);
+
+    }
+
+}
