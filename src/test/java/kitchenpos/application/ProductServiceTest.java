e0138ee5fd94bf924c74ad499fc59b03a885e574
Mingyu Lee <leemingyu05@gmail.com>
2021-01-28 23:42:57 +0900

    refactor: 상품 테스트 리팩토링

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -2,6 +2,7 @@ package kitchenpos.application;
 
 import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,11 +15,13 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 
 @DisplayName("애플리케이션 테스트 보호 - 상품 서비스")
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
+
+    private Product product;
 
     @Mock
     private ProductDao productDao;
@@ -26,37 +29,32 @@ public class ProductServiceTest {
     @InjectMocks
     private ProductService productService;
 
+    @BeforeEach
+    void setup() {
+        product = new Product();
+        product.setId(1L);
+        product.setName("강정치킨");
+        product.setPrice(BigDecimal.valueOf(17000));
+    }
+
     @DisplayName("상품 등록")
     @Test
     void create() {
-        Product request = new Product();
-        request.setName("강정치킨");
-        request.setPrice(BigDecimal.valueOf(17000));
-        Product expected = new Product();
-        expected.setId(1L);
-        expected.setName(request.getName());
-        expected.setPrice(request.getPrice());
+        given(productDao.save(product)).willReturn(product);
 
-        when(productDao.save(request)).thenReturn(expected);
+        Product savedProduct = productService.create(product);
 
-        Product savedProduct = productService.create(request);
-
-        assertThat(savedProduct).isEqualTo(expected);
+        assertThat(savedProduct).isEqualTo(product);
     }
 
     @DisplayName("상품 목록 조회")
     @Test
     void findAll() {
-        Product expected = new Product();
-        expected.setId(1L);
-        expected.setName("강정치킨");
-        expected.setPrice(BigDecimal.valueOf(17000));
-
-        when(productDao.findAll()).thenReturn(Collections.singletonList(expected));
+        given(productDao.findAll()).willReturn(Collections.singletonList(product));
 
         List<Product> products = productService.list();
 
-        assertThat(products).containsExactly(expected);
+        assertThat(products).containsExactly(product);
     }
 
 }
