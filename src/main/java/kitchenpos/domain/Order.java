8775d0c1ecec633ce398a26aa4bbdce290d042e5
ycc4477 <ycc4477@gmail.com>
2021-01-29 02:02:05 +0900

    refactor: JPA 적용 - Entity 생성 - DAO를 JpaRepository로 변환

--- src/main/java/kitchenpos/domain/Order.java
+++ src/main/java/kitchenpos/domain/Order.java
@@ -2,12 +2,22 @@ package kitchenpos.domain;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long orderTableId;
     private String orderStatus;
     private LocalDateTime orderedTime;
+    @OneToMany
     private List<OrderLineItem> orderLineItems;
 
     public Long getId() {
