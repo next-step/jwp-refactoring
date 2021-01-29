96d2c87eb709bf059e6954c49e1a21cc8f150fe4
Mingyu Lee <leemingyu05@gmail.com>
2021-01-29 18:59:47 +0900

    1단계 - 테스트를 통한 코드 보호 (#110)
    * docs: 요구사항 정리
    
    * test: 상품 등록
    
    * test: 상품 목록 조회
    
    * test: 메뉴 그룹 생성
    
    * test: 메뉴 그룹 목록 조회
    
    * test: 메뉴 생성
    
    * refactor: 상품 테스트 리팩토링
    
    * test: 상품 등록시 가격 정보가 없을 경우 예외
    
    * test: 상품 등록시 가격이 0보다 작을 경우 예외
    
    * refactor: 메뉴 그룹 서비스 테스트 리팩토링
    
    * refactor: 메뉴 테스트 리팩토링
    
    * test: 메뉴 생성 예외: 메뉴 가격이 없음
    
    * test: 메뉴 생성 예외: 메뉴 가격이 0보다 작음
    
    * test: 메뉴 생성 예외: 메뉴 그룹이 없음
    
    * test: 메뉴 생성 예외: 메뉴에 속한 메뉴상품 목록 중 데이터베이스에 없는 상품이 있음
    
    * docs: 잘못된 예외 사항 수정
    
    * test: 메뉴 생성 예외
    
    * 조회한 상품의 가격과 메뉴 상품의 수량을 곱한 금액의 합이 메뉴의 가격보다 작음
    
    * test: 메뉴 목록 조회
    
    * test: 주문테이블 생성
    
    * test: 주문테이블 목록 조회
    
    * docs: 잘못된 요구사항 분석 정리 수정
    
    * test: 주문테이블의 빈 테이블 여부를 변경한다.
    
    * test: 주문테이블의 빈 테이블 여부를 변경 예외
    
    * 주문테이블이 없음
    
    * test: 주문테이블의 빈 테이블 여부를 변경 예외
    
    * 테이블그룹이 있음
    
    * test: 주문테이블의 빈 테이블 여부를 변경 예외
    
    * 주문테이블의 주문 상태가 조리 또는 식사임
    
    * test: 주문테이블의 방문한 손님 수를 변경한다
    
    * test: 주문테이블의 방문한 손님 수를 변경 예외
    
    * 방문한 손님 수가 0보다 작음
    
    * test: 주문테이블의 방문한 손님 수를 변경 예외
    
    * 주문 테이블 정보가 없음
    
    * test: 주문테이블의 방문한 손님 수를 변경 예외
    
    * 빈 테이블임
    
    * test: 주문을 생성한다
    
    * test: 주문 생성 예외
    
    * 주문 항목 목록이 비어있음
    
    * test: 주문 생성 예외
    
    * 주문 항목의 메뉴 갯수와 저장된 메뉴 갯수가 다름
    
    * test: 주문 생성 예외
    
    * 주문테이블이 빈 테이블임
    
    * test: 주문 목록 조회
    
    * test: 단체 지정 생성
    
    * test: 단체 지정 생성 예외
    
    * 주문 테이블 목록이 비어있음
    
    * test: 단체 지정 생성 예외
    
    * 주문 테이블 목록 갯수가 2보다 적음
    
    * test: 단체 지정 생성 예외
    
    * 주문 테이블이 저장된 데이터가 아님
    
    * test: 단체 지정 생성 예외
    
    * 주문 테이블 중에 빈 테이블이 아닌 것이 있음
    
    * test: 단체 지정 생성 예외
    
    * 주문 테이블 중에 이미 단체지정된 것이 있음
    
    * test: 주문 테이블의 단체 지정 삭제
    
    * test: 주문 테이블의 단체 지정 삭제 예외
    
    * 주문 상태가 조리 또는 식사임

---
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -0,0 +1,165 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.MenuDao;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.dao.MenuProductDao;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.mockito.BDDMockito.given;
+
+@DisplayName("애플리케이션 테스트 보호 - 메뉴 서비스")
+@ExtendWith(MockitoExtension.class)
+public class MenuServiceTest {
+
+    private Product 후라이드치킨;
+    private Product 양념치킨;
+    private MenuGroup 치킨세트;
+    private Menu 후라이드한마리양념치킨한마리;
+    private MenuProduct 후라이드치킨한마리;
+    private MenuProduct 양념치킨한마리;
+
+    @BeforeEach
+    public void setup() {
+        후라이드치킨 = new Product();
+        후라이드치킨.setId(1L);
+        후라이드치킨.setName("후라이드");
+        후라이드치킨.setPrice(BigDecimal.valueOf(10000));
+
+        양념치킨 = new Product();
+        양념치킨.setId(2L);
+        양념치킨.setName("양념");
+        양념치킨.setPrice(BigDecimal.valueOf(11000));
+
+        치킨세트 = new MenuGroup();
+        치킨세트.setId(1L);
+        치킨세트.setName("후라이드앙념치킨");
+
+        후라이드한마리양념치킨한마리 = new Menu();
+        후라이드한마리양념치킨한마리.setId(1L);
+        후라이드한마리양념치킨한마리.setName("후라이드+양념");
+        후라이드한마리양념치킨한마리.setMenuGroupId(치킨세트.getId());
+        후라이드한마리양념치킨한마리.setPrice(BigDecimal.valueOf(21000));
+
+        후라이드치킨한마리 = new MenuProduct();
+        후라이드치킨한마리.setProductId(후라이드치킨.getId());
+        후라이드치킨한마리.setQuantity(1);
+
+        양념치킨한마리 = new MenuProduct();
+        양념치킨한마리.setProductId(양념치킨.getId());
+        양념치킨한마리.setQuantity(1);
+
+        후라이드한마리양념치킨한마리.setMenuProducts(Arrays.asList(후라이드치킨한마리, 양념치킨한마리));
+    }
+
+    @Mock
+    private ProductDao productDao;
+
+    @Mock
+    private MenuProductDao menuProductDao;
+
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    @Mock
+    private MenuDao menuDao;
+
+    @InjectMocks
+    private MenuService menuService;
+
+    @DisplayName("메뉴 생성")
+    @Test
+    void create() {
+        given(menuGroupDao.existsById(치킨세트.getId())).willReturn(true);
+        given(productDao.findById(후라이드치킨.getId())).willReturn(Optional.of(후라이드치킨));
+        given(productDao.findById(양념치킨.getId())).willReturn(Optional.of(양념치킨));
+        given(menuDao.save(후라이드한마리양념치킨한마리)).willReturn(후라이드한마리양념치킨한마리);
+        given(menuProductDao.save(후라이드치킨한마리)).willReturn(후라이드치킨한마리);
+        given(menuProductDao.save(양념치킨한마리)).willReturn(양념치킨한마리);
+
+        Menu savedMenu = menuService.create(후라이드한마리양념치킨한마리);
+
+        assertThat(savedMenu).isEqualTo(후라이드한마리양념치킨한마리);
+    }
+
+    @DisplayName("메뉴 생성 예외: 메뉴 가격이 없음")
+    @Test
+    void createThrowExceptionWhenNoMenuPrice() {
+        후라이드한마리양념치킨한마리.setPrice(null);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> menuService.create(후라이드한마리양념치킨한마리));
+    }
+
+    @DisplayName("메뉴 생성 예외: 메뉴 가격이 0보다 작음")
+    @Test
+    void createThrowExceptionWhenMenuPriceLessThanZero() {
+        후라이드한마리양념치킨한마리.setPrice(BigDecimal.valueOf(-1));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> menuService.create(후라이드한마리양념치킨한마리));
+    }
+
+    @DisplayName("메뉴 생성 예외: 메뉴 그룹이 없음")
+    @Test
+    void createThrowExceptionWhenNoMenuGroup() {
+        given(menuGroupDao.existsById(치킨세트.getId())).willReturn(false);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> menuService.create(후라이드한마리양념치킨한마리));
+    }
+
+    @DisplayName("메뉴 생성 예외: 메뉴에 속한 메뉴상품 목록 중 데이터베이스에 없는 상품이 있음")
+    @Test
+    void createThrowExceptionWhenNoMenuProducts() {
+        given(menuGroupDao.existsById(치킨세트.getId())).willReturn(true);
+        given(productDao.findById(후라이드치킨.getId())).willReturn(Optional.empty());
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> menuService.create(후라이드한마리양념치킨한마리));
+    }
+
+    @DisplayName("메뉴 생성 예외: 조회한 상품의 가격과 메뉴 상품의 수량을 곱한 금액의 합이 메뉴의 가격보다 작음")
+    @Test
+    void createThrowExceptionWhenMenuPriceNotEqualsSumOfProductPriceAndMenuProductQuantity() {
+        given(menuGroupDao.existsById(치킨세트.getId())).willReturn(true);
+        given(productDao.findById(후라이드치킨.getId())).willReturn(Optional.of(후라이드치킨));
+        given(productDao.findById(양념치킨.getId())).willReturn(Optional.of(양념치킨));
+        후라이드한마리양념치킨한마리.setPrice(BigDecimal.valueOf(99999));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> menuService.create(후라이드한마리양념치킨한마리));
+    }
+
+    @DisplayName("메뉴 목록 조회")
+    @Test
+    void list() {
+        given(menuDao.findAll()).willReturn(Collections.singletonList(후라이드한마리양념치킨한마리));
+        given(menuProductDao.findAllByMenuId(후라이드한마리양념치킨한마리.getId()))
+                .willReturn(후라이드한마리양념치킨한마리.getMenuProducts());
+
+        List<Menu> menus = menuService.list();
+
+        assertThat(menus).containsExactly(후라이드한마리양념치킨한마리);
+        assertThat(menus.get(0).getMenuProducts()).containsAll(후라이드한마리양념치킨한마리.getMenuProducts());
+    }
+
+}
---
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -0,0 +1,173 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.MenuDao;
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderLineItemDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.mockito.BDDMockito.given;
+
+@DisplayName("애플리케이션 테스트 보호 - 주문 서비스")
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceTest {
+
+    private Product 후라이드치킨;
+    private Product 양념치킨;
+    private MenuGroup 치킨세트;
+    private Menu 후라이드한마리양념치킨한마리;
+    private MenuProduct 후라이드치킨한마리;
+    private MenuProduct 양념치킨한마리;
+
+    private Order 주문;
+    private OrderLineItem 주문_항목;
+    private OrderTable 주문테이블;
+    private List<OrderLineItem> orderLineItems;
+    private List<Long> menuIds;
+
+    @Mock
+    private MenuDao menuDao;
+
+    @Mock
+    private OrderDao orderDao;
+
+    @Mock
+    private OrderLineItemDao orderLineItemDao;
+
+    @Mock
+    private OrderTableDao orderTableDao;
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @BeforeEach
+    public void setup() {
+        LocalDateTime now = LocalDateTime.now();
+
+        후라이드치킨 = new Product();
+        후라이드치킨.setId(1L);
+        후라이드치킨.setName("후라이드");
+        후라이드치킨.setPrice(BigDecimal.valueOf(10000));
+
+        양념치킨 = new Product();
+        양념치킨.setId(2L);
+        양념치킨.setName("양념");
+        양념치킨.setPrice(BigDecimal.valueOf(11000));
+
+        치킨세트 = new MenuGroup();
+        치킨세트.setId(1L);
+        치킨세트.setName("후라이드앙념치킨");
+
+        후라이드한마리양념치킨한마리 = new Menu();
+        후라이드한마리양념치킨한마리.setId(1L);
+        후라이드한마리양념치킨한마리.setName("후라이드+양념");
+        후라이드한마리양념치킨한마리.setMenuGroupId(치킨세트.getId());
+        후라이드한마리양념치킨한마리.setPrice(BigDecimal.valueOf(21000));
+
+        후라이드치킨한마리 = new MenuProduct();
+        후라이드치킨한마리.setProductId(후라이드치킨.getId());
+        후라이드치킨한마리.setQuantity(1);
+
+        양념치킨한마리 = new MenuProduct();
+        양념치킨한마리.setProductId(양념치킨.getId());
+        양념치킨한마리.setQuantity(1);
+
+        후라이드한마리양념치킨한마리.setMenuProducts(Arrays.asList(후라이드치킨한마리, 양념치킨한마리));
+
+        주문테이블 = new OrderTable();
+        주문테이블.setId(1L);
+        주문테이블.setNumberOfGuests(0);
+        주문테이블.setEmpty(false);
+
+        주문_항목 = new OrderLineItem();
+        주문_항목.setSeq(1L);
+        주문_항목.setMenuId(후라이드한마리양념치킨한마리.getId());
+        주문_항목.setQuantity(1);
+
+        주문 = new Order();
+        주문.setId(1L);
+        주문.setOrderTableId(주문테이블.getId());
+        주문.setOrderedTime(now);
+
+        orderLineItems = new ArrayList<>();
+        orderLineItems.add(주문_항목);
+        주문.setOrderLineItems(orderLineItems);
+
+        menuIds = 주문.getOrderLineItems().stream()
+                .map(OrderLineItem::getMenuId)
+                .collect(Collectors.toList());
+    }
+
+    @DisplayName("주문을 생성한다")
+    @Test
+    void create() {
+        given(menuDao.countByIdIn(menuIds)).willReturn(Long.valueOf(주문.getOrderLineItems().size()));
+        given(orderTableDao.findById(주문테이블.getId())).willReturn(Optional.of(주문테이블));
+        given(orderDao.save(주문)).willReturn(주문);
+        given(orderLineItemDao.save(주문_항목)).willReturn(주문_항목);
+
+        Order savedOrder = orderService.create(주문);
+
+        assertThat(savedOrder).isEqualTo(주문);
+
+    }
+
+    @DisplayName("주문 생성 예외: 주문 항목 목록이 비어있음")
+    @Test
+    void createThrowExceptionWhenOrderLineItemsEmpty() {
+        주문.setOrderLineItems(null);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> orderService.create(주문));
+    }
+
+    @DisplayName("주문 생성 예외: 주문 항목의 메뉴 갯수와 저장된 메뉴 갯수가 다름")
+    @Test
+    void createThrowExceptionWhenMenuCountDifferenceWithSavedMenuCount() {
+        OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setSeq(2L);
+        orderLineItem.setMenuId(2L);
+        주문.getOrderLineItems().add(orderLineItem);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> orderService.create(주문));
+    }
+
+    @DisplayName("주문 생성 예외: 주문테이블이 빈 테이블임")
+    @Test
+    void createThrowExceptionWhenOrderTableIsEmpty() {
+        주문테이블.setEmpty(true);
+        given(menuDao.countByIdIn(menuIds)).willReturn(Long.valueOf(주문.getOrderLineItems().size()));
+        given(orderTableDao.findById(주문테이블.getId())).willReturn(Optional.of(주문테이블));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> orderService.create(주문));
+    }
+
+    @DisplayName("주문 목록 조회")
+    @Test
+    void list() {
+        given(orderDao.findAll()).willReturn(Collections.singletonList(주문));
+        given(orderLineItemDao.findAllByOrderId(주문.getId())).willReturn(orderLineItems);
+
+        List<Order> orders = orderService.list();
+
+        assertThat(orders).containsExactly(주문);
+        assertThat(orders.get(0).getOrderLineItems()).containsAll(orderLineItems);
+    }
+
+}
