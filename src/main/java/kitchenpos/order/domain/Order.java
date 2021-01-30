c542d6134988edb96ca6160ea2ff53e75fe843cf
minjikim <carrykim1126@gmail.com>
2021-01-30 16:54:10 +0900

    step3 solve related application problem

--- src/main/java/kitchenpos/order/domain/Order.java
+++ src/main/java/kitchenpos/order/domain/Order.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Entity
+@Table(name = "orders")
 public class Order extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
