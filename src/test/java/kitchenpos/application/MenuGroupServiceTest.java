b5998d307e99f0437c9c5e96e8a571c957737c27
ohgillwhan <soora_genius@naver.com>
2021-06-28 20:56:37 +0900

    test : 정상적민 메뉴 그룹 저장 create, list 기능 테스트

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -1,12 +1,23 @@
 package kitchenpos.application;
 
 import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.internal.verification.VerificationModeFactory;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MenuGroupServiceTest {
@@ -23,12 +34,43 @@ class MenuGroupServiceTest {
     @Test
     @DisplayName("create - 정상적인 메뉴 그룹 저장")
     void 정상적인_메뉴_그룹_저장() {
+        // given
+        MenuGroup menuGroup = new MenuGroup();
+        given(menuGroupDao.save(menuGroup))
+                .willReturn(menuGroup);
 
+        // when
+        MenuGroup result = menuGroupService.create(menuGroup);
+
+        // then
+        assertThat(result)
+                .isEqualTo(result);
+
+        verify(menuGroupDao, VerificationModeFactory.only())
+                .save(menuGroup);
     }
 
     @Test
     @DisplayName("list - 정상적인 메뉴 그룹 조회")
     void 정상적인_메뉴_그룹_조회() {
+        // given
+        List<MenuGroup> menuGroups = Arrays.asList(
+                new MenuGroup(1L, "A"),
+                new MenuGroup(2L, "B"),
+                new MenuGroup(3L, "C")
+        );
 
+        given(menuGroupDao.findAll())
+                .willReturn(menuGroups);
+
+        // when
+        List<MenuGroup> list = menuGroupService.list();
+
+        // then
+        assertThat(list)
+                .containsExactlyElementsOf(menuGroups);
+
+        verify(menuGroupDao, VerificationModeFactory.only())
+                .findAll();
     }
 }
