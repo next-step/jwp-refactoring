f9e487735a4d9fbe364baca284758692e55b42af
ohgillwhan <soora_genius@naver.com>
2021-06-28 21:53:34 +0900

    test : 정상적인 상품 전체 조회

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -11,6 +11,8 @@ import org.mockito.internal.verification.VerificationModeFactory;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -60,6 +62,21 @@ class ProductServiceTest {
     @Test
     @DisplayName("list - 정상적인 상품 전체 조회")
     void 정상적인_상품_전체_조회() {
+        // given
+        Product product1 = new Product(1L, "A", BigDecimal.valueOf(0));
+        Product product2 = new Product(2L, "B", BigDecimal.valueOf(1));
 
+        // when
+        when(productDao.findAll())
+                .thenReturn(Arrays.asList(product1, product2));
+
+        List<Product> list = productService.list();
+
+        // then
+        assertThat(list)
+                .containsExactly(product1, product2);
+
+        verify(productDao, VerificationModeFactory.only())
+                .findAll();
     }
 }
