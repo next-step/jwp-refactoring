4ebcb62f3e365934edf48878417855546cb0783a
ohgillwhan <soora_genius@naver.com>
2021-06-28 20:26:41 +0900

    test : 주문 테이블 리팩터링을 위한 Business Object 테스트 케이스 작성

---
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -0,0 +1,81 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderTableDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class TableServiceTest {
+    @Mock
+    private OrderDao orderDao;
+
+    @Mock
+    private OrderTableDao orderTableDao;
+
+    private TableService tableService;
+
+    @BeforeEach
+    void setUp() {
+        this.tableService = new TableService(orderDao, orderTableDao);
+    }
+
+    @Test
+    @DisplayName("create - 정상적인 주문 테이블 등록")
+    void 정상적인_주문_테이블_등록() {
+
+    }
+
+    @Test
+    @DisplayName("list - 정상적인 주문 테이블 전체 조회")
+    void 정상적인_주문_테이블_전체_조회() {
+
+    }
+
+    @Test
+    @DisplayName("changeEmpty - 주문 테이블이 단체지정이 되어있을경우 IllegalArgumentException이 발생한다.")
+    void 주문_테이블이_단체지정이_되어있을경우_IllegalArgumentException이_발생한다() {
+
+    }
+
+    @Test
+    @DisplayName("changeEmpty - 주문 테이블에 등록된 주문들의 상태가 조리 또는 식사 일 경우 IllegalArgumentException이 발생한다.")
+    void 주문_테이블에_등록된_주문들의_상탱가_조리_또는_식사_일_경우_IllegalArgumentException이_발생한다() {
+
+    }
+
+    @Test
+    @DisplayName("changeEmpty - 정상적인 빈 테이블 변경")
+    void 정상적인_빈_테이블_변경() {
+
+    }
+
+    @Test
+    @DisplayName("changeNumberOfGuests - 방문한 손님 수가 0보다 적으면 IllegalArgumentException이 발생한다")
+    void 방문한_손님_수가_0보다_작으면_IllegalArgumentException이_발생한다() {
+
+    }
+
+    @Test
+    @DisplayName("changeNumberOfGuests - DB에서 변경을 원하는 주문 테이블을 가져오고, 주문 테이블이 없을경우 IllegalArgumentException이 발생한다.")
+    void DB에서_변경을_원하는_주문_테이블을_가져오고_주문_테이블이_없을경우_IllegalArgumentException이_발생한다() {
+
+    }
+
+    @Test
+    @DisplayName("changeNumberOfGuests - 주문 테이블이 빈 테이블이면 IllegalArgumentException이 발생한다.")
+    void 주문_테이블이_빈_테이블이면_IllegalArgumentException이_발생한다() {
+
+    }
+
+    @Test
+    @DisplayName("changeNumberOfGuest - 정상적인 방문한 손님 변경")
+    void 정상적인_방문한_손님_변경() {
+
+    }
+}
