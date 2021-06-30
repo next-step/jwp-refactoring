bafdad1b1699d20b5c4648efd6d4fe4636423c2f
ohgillwhan <soora_genius@naver.com>
2021-06-30 20:08:52 +0900

    feat : order의 1급컬렉션 생성 및 전체 끝나는지 확인하는 테스트 추가

---
+++ src/main/java/kitchenpos/domain/Orders.java
@@ -0,0 +1,31 @@
+package kitchenpos.domain;
+
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.Collections;
+import java.util.List;
+
+@Embeddable
+public class Orders {
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Order> orders;
+
+    protected Orders() {
+    }
+
+    public Orders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public boolean isAllFinished() {
+        boolean isAllFinished = orders.stream()
+                .allMatch(Order::isFinished);
+
+        return isAllFinished;
+    }
+
+    public List<Order> toCollection() {
+        return Collections.unmodifiableList(orders);
+    }
+}
---
+++ src/test/java/kitchenpos/domain/OrdersTest.java
@@ -0,0 +1,34 @@
+package kitchenpos.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OrdersTest {
+
+    @Test
+    @DisplayName("모든 주문이 안끝났으면 isAllFinished는 false이다")
+    void 모든_주문이_안끝났으면_isAllFinished는_false이다() {
+        Orders orders = new Orders(Arrays.asList(
+                new Order(null, null, null, OrderStatus.COOKING.name(), null, null),
+                new Order(null, null, null, OrderStatus.COMPLETION.name(), null, null))
+        );
+
+        assertThat(orders.isAllFinished()).isFalse();
+    }
+
+    @Test
+    @DisplayName("모든 주문이 끝났으면 isAllFinished는 true이다")
+    void 모든_주문이_끝났으면_isAllFinished는_true이다() {
+        Orders orders = new Orders(Arrays.asList(
+                new Order(null, null, null, OrderStatus.COMPLETION.name(), null, null),
+                new Order(null, null, null, OrderStatus.COMPLETION.name(), null, null))
+        );
+
+        assertThat(orders.isAllFinished()).isTrue();
+    }
+
+}
