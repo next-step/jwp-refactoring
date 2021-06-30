dc771596c12d3ebe4d26b30b5d021c5f4f0fd0ec
ohgillwhan <soora_genius@naver.com>
2021-06-30 16:01:05 +0900

    feat : TableGroup 생성을 위한 DTO 추가

---
+++ src/main/java/kitchenpos/domain/TableGroupCreate.java
@@ -0,0 +1,24 @@
+package kitchenpos.domain;
+
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+public class TableGroupCreate {
+    private List<Long> orderTableIds;
+
+    public TableGroupCreate(List<Long> orderTableIds) {
+        validate(orderTableIds);
+        this.orderTableIds = orderTableIds;
+    }
+
+    public List<Long> getOrderTableIds() {
+        return orderTableIds;
+    }
+
+    private void validate(List<Long> orderTableIds) {
+        if (CollectionUtils.isEmpty(orderTableIds) || orderTableIds.size() < 2) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
