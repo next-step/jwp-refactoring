5aa2ab77bfe2a885cf8e95d95c7b328ce4e0f6af
ohgillwhan <soora_genius@naver.com>
2021-06-30 17:07:04 +0900

    test : 모든 테이블의 모든 주문이 끝났으면 단체지정이 해제가 가능하다

--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -3,7 +3,6 @@ package kitchenpos.domain;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 public class TableGroup {
@@ -13,36 +12,35 @@ public class TableGroup {
 
     private LocalDateTime createdDate;
 
-    @OneToMany(mappedBy = "tableGroup")
-    private List<OrderTable> orderTables;
+    private OrderTables orderTables;
 
     public TableGroup() {
     }
 
     public TableGroup(Long id, LocalDateTime createdDate, List<OrderTable> orderTables) {
+        this(id, createdDate, new OrderTables(orderTables));
+    }
+
+    public TableGroup(Long id, LocalDateTime createdDate, OrderTables orderTables) {
         this.id = id;
         this.createdDate = createdDate;
         this.orderTables = orderTables;
     }
 
-    public static TableGroup create(TableGroupCreate tableGroupCreate, List<OrderTable> savedOrderTables) {
-        if (!tableGroupCreate.orderTableHasSize(savedOrderTables.size())) {
+    public static TableGroup create(TableGroupCreate tableGroupCreate, OrderTables orderTables) {
+        if (!tableGroupCreate.orderTableHasSize(orderTables.size())) {
             throw new IllegalArgumentException();
         }
 
-        for (final OrderTable savedOrderTable : savedOrderTables) {
-            if (!savedOrderTable.isEmpty() || Objects.nonNull(savedOrderTable.getTableGroupId())) {
-                throw new IllegalArgumentException();
-            }
-        }
-        TableGroup tableGroup = new TableGroup(null, LocalDateTime.now(), savedOrderTables);
-
-        for (final OrderTable savedOrderTable : savedOrderTables) {
-            savedOrderTable.setEmpty(false);
-            savedOrderTable.setTableGroup(tableGroup);
+        if (orderTables.isBookedAny()) {
+            throw new IllegalArgumentException();
         }
 
-        return tableGroup;
+        TableGroup tableGroup = new TableGroup(null, LocalDateTime.now(), orderTables);
+
+        orderTables.bookedBy(tableGroup);
+
+        return  tableGroup;
     }
 
     public Long getId() {
@@ -62,10 +60,14 @@ public class TableGroup {
     }
 
     public List<OrderTable> getOrderTables() {
-        return orderTables;
+        return orderTables.toCollection();
     }
 
-    public void setOrderTables(final List<OrderTable> orderTables) {
-        this.orderTables = orderTables;
+    public void ungroup() {
+        if (!orderTables.isUnGroupable()) {
+            throw new IllegalArgumentException();
+        }
+
+        orderTables.ungroup();
     }
 }
---
+++ src/test/java/kitchenpos/domain/TableGroupTest.java
@@ -0,0 +1,71 @@
+package kitchenpos.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableGroupTest {
+
+    @Test
+    @DisplayName("모든 테이블의 모든 주문이 안끝났으면 단체지정이 해제가 불가능하므로 IllegalStateException이 발생한다 ")
+    void 모든_테이블의_모든_주문이_안끝났으면_단체지정이_해제가_불가능하므로_IllegalStateException이_발생한다() {
+        List<Order> orders1 = Arrays.asList(
+                new Order(null, null, null, OrderStatus.COOKING.name(), null, null),
+                new Order(null, null, null, OrderStatus.COMPLETION.name(), null, null)
+        );
+        List<Order> orders2 = Arrays.asList(
+                new Order(null, null, null, OrderStatus.COMPLETION.name(), null, null),
+                new Order(null, null, null, OrderStatus.COMPLETION.name(), null, null)
+        );
+
+        OrderTables orderTables = new OrderTables(
+                Arrays.asList(
+                        new OrderTable(null, null, orders1, null, 1, false),
+                        new OrderTable(null, null, orders2, null, 1, false)
+                )
+        );
+
+        TableGroup tableGroup = new TableGroup(null, null, orderTables);
+
+        assertThatIllegalStateException().isThrownBy(() -> tableGroup.ungroup());
+    }
+
+
+    @Test
+    @DisplayName("모든 테이블의 모든 주문이 끝났으면 단체지정이 해제가 가능하다")
+    void 모든_테이블의_모든_주문이_끝났으면_단체지정이_해제가_가능하다() {
+        // given
+        List<Order> orders1 = Arrays.asList(
+                new Order(null, null, null, OrderStatus.COMPLETION.name(), null, null),
+                new Order(null, null, null, OrderStatus.COMPLETION.name(), null, null)
+        );
+        List<Order> orders2 = Arrays.asList(
+                new Order(null, null, null, OrderStatus.COMPLETION.name(), null, null),
+                new Order(null, null, null, OrderStatus.COMPLETION.name(), null, null)
+        );
+
+        OrderTables orderTables = new OrderTables(
+                Arrays.asList(
+                        new OrderTable(null, null, orders1, null, 1, false),
+                        new OrderTable(null, null, orders2, null, 1, false)
+                )
+        );
+
+        TableGroup tableGroup = new TableGroup(null, null, orderTables);
+
+        // when
+        tableGroup.ungroup();
+
+        // then
+        for (OrderTable orderTable : tableGroup.getOrderTables()) {
+            assertThat(orderTable.getTableGroupId()).isNull();
+        }
+
+    }
+}
