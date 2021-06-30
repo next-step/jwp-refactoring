ddec4e0b6fcbe35b159a2dff5601f80d7f110873
ohgillwhan <soora_genius@naver.com>
2021-06-30 16:17:57 +0900

    feat : OrderTable 일급컬렉션 추가 및 예약이 되어있는지 확인하는 함수 추가

---
+++ src/main/java/kitchenpos/domain/OrderTables.java
@@ -0,0 +1,22 @@
+package kitchenpos.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class OrderTables {
+    private List<OrderTable> orderTables;
+
+    public OrderTables(List<OrderTable> orderTables) {
+        this.orderTables = orderTables;
+    }
+
+    public boolean isBookedAny() {
+        for (OrderTable orderTable : orderTables) {
+            if (!orderTable.isEmpty() || Objects.nonNull(orderTable.getTableGroupId())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/kitchenpos/domain/OrderTablesTest.java
@@ -0,0 +1,50 @@
+package kitchenpos.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTablesTest {
+    @Test
+    @DisplayName("하나라도 Empty가 true이면 예약이 되어있다")
+    void 하나라도_Empty가_True_이면_예약이_되어있다() {
+        OrderTables orderTables = new OrderTables(
+                Arrays.asList(
+                        new OrderTable(1L, null, 1, false),
+                        new OrderTable(1L, null, 1, true)
+                )
+        );
+
+        assertThat(orderTables.isBookedAny()).isTrue();
+    }
+
+    @Test
+    @DisplayName("하나라도 TableGroup이 지정되있으면 예약이 되어있다")
+    void 하나라도_TableGroup이_지정되어있으면_예약이_되어있다() {
+        OrderTables orderTables = new OrderTables(
+                Arrays.asList(
+                        new OrderTable(1L, 1L, 1, true),
+                        new OrderTable(1L, null, 1, true)
+                )
+        );
+
+        assertThat(orderTables.isBookedAny()).isTrue();
+    }
+
+    @Test
+    @DisplayName("TableGroup이 지정이 안되어있고, 빈 테이블이면 예약이 안되어있다.")
+    void TableGroup이_지정이_안되어있고_빈_테이블이면_예약이_안되어있다() {
+        OrderTables orderTables = new OrderTables(
+                Arrays.asList(
+                        new OrderTable(1L, null, 1, true),
+                        new OrderTable(1L, null, 1, true)
+                )
+        );
+
+        assertThat(orderTables.isBookedAny()).isFalse();
+    }
+}
