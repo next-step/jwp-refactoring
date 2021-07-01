b26b282a4867697a7bbf7d494d0d6fa92560f2f5
ohgillwhan <soora_genius@naver.com>
2021-07-01 17:13:09 +0900

    refact : Fixture 사용하도록 수정

--- src/test/java/kitchenpos/domain/MenuProductTest.java
+++ src/test/java/kitchenpos/domain/MenuProductTest.java
@@ -1,5 +1,8 @@
 package kitchenpos.domain;
 
+import kitchenpos.fixture.CleanUp;
+import kitchenpos.fixture.ProductFixture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +10,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 class MenuProductTest {
+    @BeforeEach
+    void setUp() {
+        CleanUp.cleanUpTableFirst();
+    }
 
     @Test
     void getAmount() {
-        MenuProduct menuProduct = new MenuProduct(null, new Product(null, new Price(2)), 10);
+        MenuProduct menuProduct = new MenuProduct(null, ProductFixture.양념치킨_1000원, 10);
 
-        assertThat(menuProduct.getAmount()).isEqualTo(new Price(20));
+        assertThat(menuProduct.getAmount()).isEqualTo(new Price(10000));
     }
 
     @Test
