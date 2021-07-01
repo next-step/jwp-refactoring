e6158aae96cf33ae558f7fa624f91228bc0a7ab8
ohgillwhan <soora_genius@naver.com>
2021-07-01 17:34:49 +0900

    refact : Fixture 사용하도록 수정

--- src/test/java/kitchenpos/ui/TableRestControllerTest.java
+++ src/test/java/kitchenpos/ui/TableRestControllerTest.java
@@ -1,13 +1,13 @@
 package kitchenpos.ui;
 
 import kitchenpos.application.TableService;
-import kitchenpos.domain.NumberOfGuest;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.OrderTableCreate;
-import kitchenpos.domain.TableGroup;
 import kitchenpos.dto.request.ChangeEmptyRequest;
 import kitchenpos.dto.request.ChangeNumberOfGuestsRequest;
 import kitchenpos.dto.request.OrderTableCreateRequest;
+import kitchenpos.fixture.CleanUp;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,13 +19,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.Collections;
 
+import static kitchenpos.fixture.OrderTableFixture.사용중인_1명_1건_결제완료_1건_식사;
+import static kitchenpos.fixture.OrderTableFixture.사용중인_1명_테이블;
 import static kitchenpos.ui.JsonUtil.toJson;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -39,6 +38,11 @@ class TableRestControllerTest {
 
     @MockBean
     private TableService tableService;
+
+    @BeforeEach
+    void setUp() {
+        CleanUp.cleanUpOrderFirst();
+    }
 
     @Test
     @DisplayName("인원수 수정시 인원수가 0명미만이면 BadRequest가 발생한다")
@@ -56,44 +60,13 @@ class TableRestControllerTest {
     }
 
     @Test
-    @DisplayName("인원수 수정시 인원수가 0명 이상이면 정상이다")
-    void 인원수_수정시_인원수가_0명_이상이면_정상이다() throws Exception {
-        // given
-        ChangeNumberOfGuestsRequest changeNumberOfGuestsRequest = new ChangeNumberOfGuestsRequest(2);
-
-        OrderTable orderTable = new OrderTable(1L,
-                new TableGroup(1L, LocalDateTime.now(), Arrays.asList()),
-                Collections.emptyList(),
-                new NumberOfGuest(changeNumberOfGuestsRequest.getNumberOfGuests()),
-                false);
-
-        given(tableService.changeNumberOfGuests(anyLong(), any(NumberOfGuest.class)))
-                .willReturn(orderTable);
-
-        // when & then
-        mockMvc.perform(
-                put("/api/tables/1/number-of-guests")
-                        .content(toJson(changeNumberOfGuestsRequest))
-                        .contentType(MediaType.APPLICATION_JSON)
-        )
-                .andExpect(status().isOk())
-                .andExpect(validateOrderTable("$", orderTable));
-    }
-
-    @Test
     @DisplayName("정상적으로 테이블의 빈상태를 바꿀경우")
     void 정상적으로_테이블의_빈상태를_바꿀경우() throws Exception {
         // given
         ChangeEmptyRequest changeEmptyRequest = new ChangeEmptyRequest(true);
 
-        OrderTable orderTable = new OrderTable(1L,
-                new TableGroup(1L, LocalDateTime.now(), Arrays.asList()),
-                Collections.emptyList(),
-                new NumberOfGuest(1),
-                changeEmptyRequest.isEmpty());
-
         given(tableService.changeEmpty(1L, changeEmptyRequest.isEmpty()))
-                .willReturn(orderTable);
+                .willReturn(사용중인_1명_테이블);
 
         // when & then
         mockMvc.perform(
@@ -102,27 +75,15 @@ class TableRestControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
         )
                 .andExpect(status().isOk())
-                .andExpect(validateOrderTable("$", orderTable));
+                .andExpect(validateOrderTable("$", 사용중인_1명_테이블));
     }
 
     @Test
     @DisplayName("정상적으로 테이블의 리스트를 가져올경우")
     void 정상적으로_테이블의_리스트를_가져올경우() throws Exception {
         // given
-        OrderTable orderTable = new OrderTable(1L,
-                new TableGroup(1L, LocalDateTime.now(), Arrays.asList()),
-                Collections.emptyList(),
-                new NumberOfGuest(1),
-                false);
-
-        OrderTable orderTable2 = new OrderTable(2L,
-                new TableGroup(2L, LocalDateTime.now(), Arrays.asList()),
-                Collections.emptyList(),
-                new NumberOfGuest(2),
-                true);
-
         given(tableService.list())
-                .willReturn(Arrays.asList(orderTable, orderTable2));
+                .willReturn(Arrays.asList(사용중인_1명_1건_결제완료_1건_식사, 사용중인_1명_테이블));
 
         // when & then
         mockMvc.perform(
@@ -130,8 +91,8 @@ class TableRestControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
         )
                 .andExpect(status().isOk())
-                .andExpect(validateOrderTable("$.[0]", orderTable))
-                .andExpect(validateOrderTable("$.[1]", orderTable2));
+                .andExpect(validateOrderTable("$.[0]", 사용중인_1명_1건_결제완료_1건_식사))
+                .andExpect(validateOrderTable("$.[1]", 사용중인_1명_테이블));
     }
 
     @Test
@@ -140,14 +101,9 @@ class TableRestControllerTest {
         // given
         OrderTableCreateRequest orderTableCreateRequest = new OrderTableCreateRequest(1, false);
 
-        OrderTable orderTable = new OrderTable(1L,
-                new TableGroup(1L, LocalDateTime.now(), Arrays.asList()),
-                Collections.emptyList(),
-                new NumberOfGuest(1),
-                false);
 
         given(tableService.create(any(OrderTableCreate.class)))
-                .willReturn(orderTable);
+                .willReturn(사용중인_1명_1건_결제완료_1건_식사);
 
         // when & then
         mockMvc.perform(
@@ -156,7 +112,7 @@ class TableRestControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
         )
                 .andExpect(status().isCreated())
-                .andExpect(validateOrderTable("$", orderTable));
+                .andExpect(validateOrderTable("$", 사용중인_1명_1건_결제완료_1건_식사));
     }
 
     private ResultMatcher validateOrderTable(String prefix, OrderTable orderTable) {
