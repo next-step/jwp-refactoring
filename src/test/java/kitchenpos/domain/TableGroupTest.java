e12b297741cfac73177a233b252cefad8144855b
ohgillwhan <soora_genius@naver.com>
2021-07-01 17:31:48 +0900

    refact : Fixture 사용하도록 수정

--- src/test/java/kitchenpos/domain/TableGroupTest.java
+++ src/test/java/kitchenpos/domain/TableGroupTest.java
@@ -1,37 +1,31 @@
 package kitchenpos.domain;
 
+import kitchenpos.fixture.CleanUp;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static kitchenpos.fixture.OrderTableFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 class TableGroupTest {
+    @BeforeEach
+    void setUp() {
+        CleanUp.cleanUpOrderFirst();
+    }
 
     @Test
     @DisplayName("모든 테이블의 모든 주문이 안끝났으면 단체지정이 해제가 불가능하므로 IllegalStateException이 발생한다 ")
     void 모든_테이블의_모든_주문이_안끝났으면_단체지정이_해제가_불가능하므로_IllegalStateException이_발생한다() {
-        List<Order> orders1 = Arrays.asList(
-                new Order(null, null, OrderStatus.COOKING, null, null),
-                new Order(null, null, OrderStatus.COMPLETION, null, null)
-        );
-        List<Order> orders2 = Arrays.asList(
-                new Order(null, null, OrderStatus.COMPLETION, null, null),
-                new Order(null, null, OrderStatus.COMPLETION, null, null)
-        );
-
-        OrderTables orderTables = new OrderTables(
-                Arrays.asList(
-                        new OrderTable(null, orders1, 1, false),
-                        new OrderTable(null,  orders2, 1, false)
-                )
-        );
+        // given
+        OrderTables orderTables = new OrderTables(Arrays.asList(사용중인_1명_1건_결제완료_1건_식사, 사용중인_1명_2건_결제완료1));
 
         TableGroup tableGroup = new TableGroup(null, null, orderTables);
 
+        // when & then
         assertThatIllegalStateException().isThrownBy(() -> tableGroup.ungroup());
     }
 
@@ -40,21 +34,7 @@ class TableGroupTest {
     @DisplayName("모든 테이블의 모든 주문이 끝났으면 단체지정이 해제가 가능하다")
     void 모든_테이블의_모든_주문이_끝났으면_단체지정이_해제가_가능하다() {
         // given
-        List<Order> orders1 = Arrays.asList(
-                new Order(null, null, OrderStatus.COMPLETION, null, null),
-                new Order(null, null, OrderStatus.COMPLETION, null, null)
-        );
-        List<Order> orders2 = Arrays.asList(
-                new Order(null, null, OrderStatus.COMPLETION, null, null),
-                new Order(null, null, OrderStatus.COMPLETION, null, null)
-        );
-
-        OrderTables orderTables = new OrderTables(
-                Arrays.asList(
-                        new OrderTable(null, orders1, 1, false),
-                        new OrderTable(null, orders2, 1, false)
-                )
-        );
+        OrderTables orderTables = new OrderTables(Arrays.asList(사용중인_1명_2건_결제완료1, 사용중인_1명_2건_결제완료2));
 
         TableGroup tableGroup = new TableGroup(null, null, orderTables);
 
