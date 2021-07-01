036c2c140b15f76ad17cff72ecf0566a7a4ac598
ohgillwhan <soora_genius@naver.com>
2021-07-01 14:24:12 +0900

    refact : 사용하지 않는 함수 제거

--- src/main/java/kitchenpos/dao/MenuDao.java
+++ src/main/java/kitchenpos/dao/MenuDao.java
@@ -3,9 +3,5 @@ package kitchenpos.dao;
 import kitchenpos.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface MenuDao extends JpaRepository<Menu, Long> {
-    long countByIdIn(List<Long> menuIds);
 }
--- src/main/java/kitchenpos/dao/OrderLineItemDao.java
+++ src/main/java/kitchenpos/dao/OrderLineItemDao.java
@@ -3,9 +3,5 @@ package kitchenpos.dao;
 import kitchenpos.domain.OrderLineItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface OrderLineItemDao extends JpaRepository<OrderLineItem, Long> {
-    List<OrderLineItem> findAllByOrderId(Long orderId);
 }
