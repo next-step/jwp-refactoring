7658bb26fd7327c35d741934bddf54751b2bf455
ohgillwhan <soora_genius@naver.com>
2021-07-01 17:18:00 +0900

    refact : Fixture 사용하도록 수정

--- src/test/java/kitchenpos/domain/MenuTest.java
+++ src/test/java/kitchenpos/domain/MenuTest.java
@@ -2,27 +2,43 @@ package kitchenpos.domain;
 
 import kitchenpos.exception.MenuCheapException;
 import kitchenpos.exception.ProductNotExistException;
+import kitchenpos.fixture.CleanUp;
+import kitchenpos.fixture.ProductFixture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static kitchenpos.fixture.ProductFixture.양념치킨_1000원;
+import static kitchenpos.fixture.ProductFixture.콜라_100원;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class MenuTest {
+    private List<MenuProductCreate> menuProductCreates;
+    private List<Product> productList;
+
+    @BeforeEach
+    void setUp() {
+        CleanUp.cleanUpTableFirst();
+
+        this.menuProductCreates = Arrays.asList(
+                new MenuProductCreate(1L, 콜라_100원.getId(), 1L),
+                new MenuProductCreate(2L, 양념치킨_1000원.getId(), 2L)
+        );
+        productList = Arrays.asList(콜라_100원, ProductFixture.양념치킨_1000원);
+    }
 
     @Test
     @DisplayName("메듀에 등록할 메뉴와 메뉴 수가 틀리면 ProductNotExistException이 발생한다 ")
     void 메뉴에_등록할_메뉴와_메뉴_수가_틀리면_ProductNotExistException이_발생한다() {
         // given
-        List<MenuProductCreate> menuProductCreates = Arrays.asList(
-                new MenuProductCreate(1L, 1L, 1L),
-                new MenuProductCreate(2L, 2L, 2L)
-        );
-        MenuCreate menuCreate = new MenuCreate(null, null, null, menuProductCreates);
-        Products products = new Products(Arrays.asList(new Product("A", new Price(1))));
+        MenuCreate menuCreate = new MenuCreate("Hello", new Price(10), null, menuProductCreates);
+        List<Product> productList = Arrays.asList(콜라_100원);
+
+        Products products = new Products(productList);
 
         // when & then
         assertThatExceptionOfType(ProductNotExistException.class)
@@ -33,17 +49,9 @@ class MenuTest {
     @DisplayName("메뉴의 가격보다 금액이 더 싸면 MenuCheapException이 발생한다")
     void 메뉴의_가격보다_금액이_더_싸면_MenuCheapException이_발생한다() {
         // given
-        List<MenuProductCreate> menuProductCreates = Arrays.asList(
-                new MenuProductCreate(1L, 1L, 1L),
-                new MenuProductCreate(2L, 2L, 2L)
-        );
-        MenuCreate menuCreate = new MenuCreate(null, new Price(100), null, menuProductCreates);
-        Products products = new Products(
-                Arrays.asList(
-                        new Product(1L,"A", new Price(1)),
-                        new Product(2L,"A", new Price(2))
-                )
-        );
+        MenuCreate menuCreate = new MenuCreate(null, new Price(10000), null, menuProductCreates);
+
+        Products products = new Products(productList);
 
         // when & then
         assertThatExceptionOfType(MenuCheapException.class)
@@ -55,17 +63,9 @@ class MenuTest {
     void 정상적인_등록() {
         // given
         MenuGroup menuGroup = new MenuGroup(1L, "MENU_GROUP");
-        List<MenuProductCreate> menuProductCreates = Arrays.asList(
-                new MenuProductCreate(1L, 1L, 1L),
-                new MenuProductCreate(2L, 2L, 2L)
-        );
 
-        List<Product> productList = Arrays.asList(
-                new Product(1L, "A", new Price(1)),
-                new Product(2L, "A", new Price(2))
-        );
+        MenuCreate menuCreate = new MenuCreate(null, new Price(100), null, menuProductCreates);
 
-        MenuCreate menuCreate = new MenuCreate("NAME_OF_MENU", new Price(1), null, menuProductCreates);
         Products products = new Products(productList);
 
         // when
@@ -75,6 +75,7 @@ class MenuTest {
         assertThat(menu.getMenuGroup()).isEqualTo(menuGroup);
         assertThat(menu.getName()).isEqualTo(menuCreate.getName());
         assertThat(menu.getPrice()).isEqualTo(menuCreate.getPrice());
+
         assertThat(menu.getMenuProducts())
                 .map(item -> item.getProduct())
                 .containsExactlyElementsOf(productList);
