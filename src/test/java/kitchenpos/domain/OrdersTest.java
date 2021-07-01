1f4179285e10abae7f9080f52b9662fb67e2f93a
ohgillwhan <soora_genius@naver.com>
2021-07-01 17:20:01 +0900

    refact : Fixture 사용하도록 수정

--- src/test/java/kitchenpos/domain/OrdersTest.java
+++ src/test/java/kitchenpos/domain/OrdersTest.java
@@ -1,5 +1,8 @@
 package kitchenpos.domain;
 
+import kitchenpos.fixture.CleanUp;
+import kitchenpos.fixture.OrderFixture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,13 +12,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class OrdersTest {
 
+    @BeforeEach
+    void setUp() {
+        CleanUp.cleanUpTableFirst();
+    }
+
     @Test
     @DisplayName("모든 주문이 안끝났으면 isAllFinished는 false이다")
     void 모든_주문이_안끝났으면_isAllFinished는_false이다() {
-        Orders orders = new Orders(Arrays.asList(
-                new Order(null, null, OrderStatus.COOKING, null, null),
-                new Order(null, null, OrderStatus.COMPLETION, null, null))
-        );
+        Orders orders = new Orders(Arrays.asList(OrderFixture.식사1, OrderFixture.결제완료1));
 
         assertThat(orders.isAllFinished()).isFalse();
     }
@@ -23,10 +28,7 @@ class OrdersTest {
     @Test
     @DisplayName("모든 주문이 끝났으면 isAllFinished는 true이다")
     void 모든_주문이_끝났으면_isAllFinished는_true이다() {
-        Orders orders = new Orders(Arrays.asList(
-                new Order(null, null, OrderStatus.COMPLETION, null, null),
-                new Order(null, null, OrderStatus.COMPLETION, null, null))
-        );
+        Orders orders = new Orders(Arrays.asList(OrderFixture.결제완료1, OrderFixture.결제완료2));
 
         assertThat(orders.isAllFinished()).isTrue();
     }
