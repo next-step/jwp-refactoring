3e905aed9e541832daa4f011374bec6c366f627b
prodo-developer <wjdals300@gmail.com>
2021-07-03 02:46:53 +0900

    test : 도메인 또는 비지니스 테스트

---
+++ src/test/java/kitchenpos/common/PriceTest.java
@@ -0,0 +1,68 @@
+package kitchenpos.common;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayName("가격 테스트")
+class PriceTest {
+    private Price price;
+    private Price otherPrice;
+
+    @BeforeEach
+    void setUp() {
+        price = Price.of(1);
+        otherPrice = Price.of(10);
+    }
+
+    @Test
+    @DisplayName("가격이 올바르지 않으면 생성할 수 없다. : 0원 이상이어야 한다.")
+    public void createFail() {
+        // when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Price.of(-1))
+                .withMessageMatching("가격은 0 원 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("덧셈 처리 한다.")
+    public void add() {
+        // given
+        Price expected = Price.of(11);
+
+        // when
+        Price actual = price.add(otherPrice);
+
+        // then
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    @DisplayName("뺄셈 처리 한다.")
+    public void subtract() {
+        // given
+        Price expected = Price.of(9);
+
+        // when
+        Price actual = otherPrice.subtract(price);
+
+        // then
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    @DisplayName("곱셈 처리 한다.")
+    public void multiply() {
+        // given
+        Price expected = Price.of(10);
+
+        // when
+        Price actual = price.multiply(10);
+
+        // then
+        assertThat(expected).isEqualTo(actual);
+    }
+}
---
+++ src/test/java/kitchenpos/menu/domain/MenuProductsTest.java
@@ -0,0 +1,40 @@
+package kitchenpos.menu.domain;
+
+import kitchenpos.common.Price;
+import kitchenpos.product.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("메뉴상품들 테스트")
+class MenuProductsTest {
+    Product 후라이드;
+    Product 양념치킨;
+    MenuProduct menuProduct1;
+    MenuProduct menuProduct2;
+    MenuProducts menuProducts;
+
+    @BeforeEach
+    void setUp() {
+        후라이드 = new Product("후라이드", Price.of(16000));
+        양념치킨 = new Product("양념치킨", Price.of(16000));
+        menuProduct1 = new MenuProduct(후라이드, 2L);
+        menuProduct2 = new MenuProduct(양념치킨, 1L);
+        menuProducts = new MenuProducts(Arrays.asList(menuProduct1, menuProduct2));
+    }
+
+    @Test
+    @DisplayName("모든 메뉴 가격의 합을 구할 수 있다.")
+    public void calculateSumPrice() throws Exception {
+        // when
+        Price price = menuProducts.calculateSumPrice();
+
+        // then
+        assertThat(price).isNotNull();
+        assertThat(price).isEqualTo(menuProduct1.calculatePrice().add(menuProduct2.calculatePrice()));
+    }
+}
