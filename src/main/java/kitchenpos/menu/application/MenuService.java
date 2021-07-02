7a57ec86a83f44e91f1fc6155dec581a40550eab
prodo-developer <wjdals300@gmail.com>
2021-07-03 02:46:15 +0900

    refactor : 비지니스 로직 수정

---
+++ src/main/java/kitchenpos/menu/application/MenuService.java
@@ -0,0 +1,66 @@
+package kitchenpos.menu.application;
+
+import kitchenpos.menu.domain.Menu;
+import kitchenpos.menu.domain.MenuProducts;
+import kitchenpos.menu.dto.MenuProductRequest;
+import kitchenpos.menu.dto.MenuRequest;
+import kitchenpos.menu.dto.MenuResponse;
+import kitchenpos.menugroup.domain.MenuGroup;
+import kitchenpos.menu.domain.MenuProduct;
+import kitchenpos.product.domain.Product;
+import kitchenpos.menugroup.domain.MenuGroupRepository;
+import kitchenpos.menu.domain.MenuRepository;
+import kitchenpos.product.domain.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+@Service
+public class MenuService {
+    private final MenuRepository menuRepository;
+    private final MenuGroupRepository menuGroupRepository;
+    private final ProductRepository productRepository;
+
+    public MenuService(MenuRepository menuRepository, MenuGroupRepository menuGroupRepository, ProductRepository productRepository) {
+        this.menuRepository = menuRepository;
+        this.menuGroupRepository = menuGroupRepository;
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public MenuResponse create(final MenuRequest request) {
+        MenuGroup menuGroup = findMenuGroup(request.getMenuGroupId());
+        MenuProducts menuProducts = findMenuProducts(request.getMenuProducts());
+        Menu menu = request.toMenu(menuGroup, menuProducts);
+        return MenuResponse.of(menuRepository.save(menu));
+    }
+
+    public List<MenuResponse> findAll() {
+        List<Menu> menus = menuRepository.findAll();
+        return menus.stream()
+                .map(MenuResponse::of)
+                .collect(toList());
+    }
+
+    private MenuGroup findMenuGroup(Long menuGroupId) {
+        return menuGroupRepository.findById(menuGroupId)
+                .orElseThrow(() -> new IllegalArgumentException("등록 되지 않은 메뉴 그룹 입니다."));
+    }
+
+    private MenuProducts findMenuProducts(List<MenuProductRequest> request) {
+        return request
+                .stream()
+                .map(this::findMenuProduct)
+                .collect(collectingAndThen(toList(), MenuProducts::new));
+    }
+
+    private MenuProduct findMenuProduct(MenuProductRequest menuProductRequest) {
+        Product product = productRepository.findById(menuProductRequest.getProductId())
+                .orElseThrow(() -> new IllegalArgumentException("등록 되지 않은 상품 입니다."));
+        return new MenuProduct(product, menuProductRequest.getQuantity());
+    }
+}
---
+++ src/main/java/kitchenpos/order/application/OrderService.java
@@ -0,0 +1,71 @@
+package kitchenpos.order.application;
+
+import kitchenpos.menu.domain.Menu;
+import kitchenpos.menu.domain.MenuRepository;
+import kitchenpos.order.domain.*;
+import kitchenpos.order.dto.OrderLineItemResponse;
+import kitchenpos.order.dto.OrderRequest;
+import kitchenpos.order.dto.OrderResponse;
+import kitchenpos.ordertable.domain.OrderTable;
+import kitchenpos.ordertable.domain.OrderTableRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final OrderTableRepository orderTableRepository;
+    private final MenuRepository menuRepository;
+
+    public OrderService(OrderRepository orderRepository, OrderTableRepository orderTableRepository, MenuRepository menuRepository) {
+        this.orderRepository = orderRepository;
+        this.orderTableRepository = orderTableRepository;
+        this.menuRepository = menuRepository;
+    }
+
+    @Transactional
+    public OrderResponse create(final OrderRequest request) {
+        OrderTable orderTable = findOrderTableById(request.getOrderTableId());
+        OrderLineItems orderLineItems = findOrderLineItems(request.getOrderLineItems());
+        Order savedOrder = orderRepository.save(Order.of(orderTable, orderLineItems));
+        return OrderResponse.of(savedOrder);
+    }
+
+    public List<OrderResponse> findAll() {
+        List<Order> orders = orderRepository.findAll();
+        return OrderResponse.ofList(orders);
+    }
+
+    @Transactional
+    public OrderResponse changeOrderStatus(final Long orderId, final OrderStatus orderStatus) {
+        Order order = findOrderById(orderId);
+        order.changeOrderStatus(orderStatus);
+        return OrderResponse.of(order);
+    }
+
+    private OrderTable findOrderTableById(Long orderTableId) {
+        return orderTableRepository.findById(orderTableId)
+                .orElseThrow(() -> new IllegalArgumentException("주문 테이블이 없습니다."));
+    }
+
+    private Order findOrderById(Long orderId) {
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new IllegalArgumentException("주문이 없습니다."));
+    }
+
+    private OrderLineItems findOrderLineItems(List<OrderLineItemResponse> OrderLineItemResponses) {
+        List<OrderLineItem> orderLineItemList = OrderLineItemResponses.stream()
+                .map(this::toOrderLineItem)
+                .collect(Collectors.toList());
+        return new OrderLineItems(orderLineItemList);
+    }
+
+    private OrderLineItem toOrderLineItem(OrderLineItemResponse orderLineItemResponse) {
+        Menu menu = menuRepository.findById(orderLineItemResponse.getMenuId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메뉴입니다."));
+        return new OrderLineItem(menu, orderLineItemResponse.getQuantity());
+    }
+}
