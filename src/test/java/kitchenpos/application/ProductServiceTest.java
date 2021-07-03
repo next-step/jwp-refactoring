5ba1c6a534b9e2c13d87006aa140c54102eecb7d
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-07-03 12:23:29 +0900

    refactor(ProductServiceTest): 테스트 보완

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -3,6 +3,7 @@ package kitchenpos.application;
 import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -10,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -27,30 +29,37 @@ public class ProductServiceTest {
 
     @BeforeEach
     void setUp() {
-        후라이드치킨 = new Product(1L, "후라이드치킨", BigDecimal.valueOf(18000L));
+        후라이드치킨 = new Product( 1L, "후라이드치킨", BigDecimal.valueOf(18000L));
         양념치킨 = new Product(2L, "양념치킨", BigDecimal.valueOf(19000L));
 
         productService = new ProductService(productDao);
     }
 
+    @DisplayName("상품을 등록")
     @Test
-    void create() {
+    void 상품을_등록() {
         //given
-        when(productDao.save(후라이드치킨)).thenReturn(후라이드치킨);
+        Product product = new Product("후라이드치킨", BigDecimal.valueOf(18000L));
+        when(productDao.save(product)).thenReturn(후라이드치킨);
 
         //when
-        Product savedProduct = productService.create(후라이드치킨);
+        Product savedProduct = productService.create(product);
 
         //then
         assertThat(savedProduct.getId()).isEqualTo(후라이드치킨.getId());
     }
 
+    @DisplayName("상품_목록을_불러옴")
     @Test
-    void list() {
+    void 상품_목록을_부러옴() {
         //given
         when(productDao.findAll()).thenReturn(Arrays.asList(후라이드치킨, 양념치킨));
 
-        //when, then
-        assertThat(productService.list()).hasSize(2);
+        //when
+        List<Product> list = productService.list();
+
+        //then
+        assertThat(list).hasSize(2);
+        assertThat(list).containsExactly(후라이드치킨, 양념치킨);
     }
 }
