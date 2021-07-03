72dbbb84a4f235d63aff00bbb3d9ee5874fba1dc
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-07-03 13:26:06 +0900

    refactor(OrderTable): 입력값 받기 위한 생성자 추가

--- src/main/java/kitchenpos/domain/OrderTable.java
+++ src/main/java/kitchenpos/domain/OrderTable.java
@@ -9,6 +9,23 @@ public class OrderTable {
     public OrderTable() {
     }
 
+    public OrderTable(Long id) {
+        this.id = id;
+    }
+
+    public OrderTable(boolean empty) {
+        this.empty = empty;
+    }
+
+    public OrderTable(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public OrderTable(int numberOfGuests, boolean empty) {
+        this.numberOfGuests = numberOfGuests;
+        this.empty = empty;
+    }
+
     public OrderTable(Long id, Long tableGroupId, int numberOfGuests, boolean empty) {
         this.id = id;
         this.tableGroupId = tableGroupId;
