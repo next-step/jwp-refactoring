9e1cdbfcb57ca2a158bd645c4285d2f9bbaff2de
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-07-04 11:16:19 +0900

    refactor: Product Service Test Code Modify
    Product Service Test Code Modify

--- src/test/java/kitchenpos/product/service/ProductServiceTest.java
+++ src/test/java/kitchenpos/product/service/ProductServiceTest.java
@@ -17,16 +17,20 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ProductServiceTest {
 
     @Autowired
-    ProductService productService;
+    private ProductService productService;
 
     @Test
     @DisplayName("상품을 생성 한다")
     public void createProduct() {
+        //given
         String name = "치킨";
         BigDecimal price = new BigDecimal(20000);
         Product product = new Product(name, price);
 
+        //when
         Product createProduct = productService.create(product);
+
+        //then
         assertThat(createProduct.getName().equals(name)).isTrue();
         assertThat(createProduct.getPrice().compareTo(price)).isSameAs(0);
     }
@@ -35,17 +39,23 @@ public class ProductServiceTest {
     @Test
     @DisplayName("상품을 생성 실패 - 가격이 음수")
     public void createProductFailByPriceMinus() {
+        //given
         String name = "피자";
         BigDecimal price = new BigDecimal(-10000);
         Product product = new Product( name, price);
 
+        //when
+        //then
         assertThrows(IllegalArgumentException.class, () ->  productService.create(product));
     }
 
     @Test
     @DisplayName("상품 리스트를 가져온다")
     public void selectProductList() {
+        //when
         List<Product> products = productService.list();
+
+        //then
         for (Product product : products) {
             assertThat(product.getId()).isNotNull();
             assertThat(product.getName()).isNotNull();
