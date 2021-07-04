002e67ab5c91a2148fa568c80f8e9e2cd837f873
Smallmachines-Gin <63280671+Smallmachines-Gin@users.noreply.github.com>
2021-07-05 06:39:01 +0900

    [1단계 - 테스트를 통한 코드 보호] 리뷰 요청 드립니다! (#146)
    * docs: 키친포스 요구사항 정리
    
    * feat: 메뉴그룹서비스 테스트케이스 작성
    
    * test: 메뉴 서비스 테스트케이스 작성
    
    * test: 주문 서비스 테스트케이스 추가
    
    * test: 상품 서비스 관련 테스트 작성
    
    * test: 단체지정(table group) 서비스 테스트케이스 작성
    
    * test: 테이블 서비스 테스트케이스 작성
    
    Co-authored-by: itdar <thesockerrr@gmail.com>

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,8 +1,27 @@
 package kitchenpos.domain;
 
+import java.util.Objects;
+
 public class MenuGroup {
     private Long id;
     private String name;
+
+    public MenuGroup() {
+        this(null, null);
+    }
+
+    private MenuGroup(String name) {
+        this(null, name);
+    }
+
+    private MenuGroup(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static MenuGroup of(Long id, String name) {
+        return new MenuGroup(id, name);
+    }
 
     public Long getId() {
         return id;
@@ -18,5 +37,18 @@ public class MenuGroup {
 
     public void setName(final String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuGroup menuGroup = (MenuGroup) o;
+        return Objects.equals(id, menuGroup.id) && Objects.equals(name, menuGroup.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
