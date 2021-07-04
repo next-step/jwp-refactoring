86608a4f47a1e95f57b7e56e1b682a66cc0d27ca
accidentlywoo <accidentlywoo@gmail.com>
2021-07-04 19:12:08 +0900

    test : 주문테이블 서비스 테스트, 주문과 연관관계 심함. TODO 처리

---
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -0,0 +1,152 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.OrderTable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class TableServiceTest {
+	@Autowired
+	private OrderDao orderDao;
+
+	@Autowired
+	private OrderTableDao orderTableDao;
+
+	@Autowired
+	private TableService tableService;
+	
+	@Test
+	public void 주문테이블_생성_성공_테이블그룹없음_혼밥러(){
+		OrderTable orderTable = new OrderTable();
+		orderTable.setNumberOfGuests(1);
+		orderTable.setEmpty(false);
+
+		OrderTable savedOrderTable = tableService.create(orderTable);
+
+		assertThat(savedOrderTable.getTableGroupId()).isNull();
+		assertThat(savedOrderTable.getNumberOfGuests()).isEqualTo(1);
+		assertThat(savedOrderTable.isEmpty()).isFalse();
+	}
+
+	@Test
+	public void 주문테이블_리스트조회(){
+		OrderTable orderTable = new OrderTable();
+		orderTable.setNumberOfGuests(1);
+		orderTable.setEmpty(false);
+
+		tableService.create(orderTable);
+
+		List<OrderTable> orderTables = tableService.list();
+
+		assertThat(orderTables)
+				.extracting("numberOfGuests", "empty")
+				.contains(tuple(1, false));
+	}
+
+	@Test
+	public void 주문테이블_주문없음_처리_실패_주문테이블정보없음(){
+		assertThatThrownBy(() -> tableService.changeEmpty(9999L, new OrderTable()))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+//	@Test TODO with Order Test
+	public void 주문테이블_주문없음_처리_실패_주문테이블그룹정보있음(){
+		OrderTable orderTable = new OrderTable();
+		orderTable.setNumberOfGuests(1);
+		orderTable.setEmpty(false);
+
+		OrderTable savedOrderTable = tableService.create(orderTable);
+
+		savedOrderTable.setTableGroupId(1L);
+		assertThatThrownBy(() -> tableService.changeEmpty(savedOrderTable.getId(), savedOrderTable))
+		.isInstanceOf(IllegalArgumentException.class);
+	}
+
+//	@Test TODO with Order Test
+	public void 주문테이블_주문없음_처리_실패_주문상태가_COOKING_이면_변경안됨(){
+		OrderTable orderTable = new OrderTable();
+		orderTable.setNumberOfGuests(1);
+		orderTable.setEmpty(false);
+
+		OrderTable savedOrderTable = tableService.create(orderTable);
+
+		assertThatThrownBy(() -> tableService.changeEmpty(savedOrderTable.getId(), savedOrderTable))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+//	@Test TODO with Order Test
+	public void 주문테이블_주문없음_처리_실패_주문상태가_MEAL_이면_변경안됨(){
+		OrderTable orderTable = new OrderTable();
+		orderTable.setNumberOfGuests(1);
+		orderTable.setEmpty(false);
+
+		OrderTable savedOrderTable = tableService.create(orderTable);
+
+		assertThatThrownBy(() -> tableService.changeEmpty(savedOrderTable.getId(), savedOrderTable))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	public void 주문테이블_방문한_손님수_변경_성공(){
+		OrderTable orderTable = new OrderTable();
+		orderTable.setNumberOfGuests(1);
+		orderTable.setTableGroupId(1L);
+		orderTable.setEmpty(false);
+
+		OrderTable savedOrderTable = tableService.create(orderTable);
+
+		savedOrderTable.setNumberOfGuests(3); // Change Number Of Guest
+		tableService.changeNumberOfGuests(savedOrderTable.getId(), savedOrderTable);
+
+		assertThat(savedOrderTable.getTableGroupId()).isNull();
+		assertThat(savedOrderTable.getNumberOfGuests()).isEqualTo(3);
+		assertThat(savedOrderTable.isEmpty()).isFalse();
+	}
+
+	@Test
+	public void 주문테이블_방문한_손님수_변경_실패_손님수는_0_이상자연수(){
+		OrderTable orderTable = new OrderTable();
+		orderTable.setNumberOfGuests(0);
+		orderTable.setTableGroupId(1L);
+		orderTable.setEmpty(false);
+
+		OrderTable savedOrderTable = tableService.create(orderTable);
+
+		savedOrderTable.setNumberOfGuests(-1); // Change Number Of Guest
+		assertThatThrownBy(() -> tableService.changeNumberOfGuests(savedOrderTable.getId(), savedOrderTable))
+		.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	public void 주문테이블_방문한_손님수_변경_실패_빈테이블은_변경이아니고_생성하자(){
+		OrderTable orderTable = new OrderTable();
+		orderTable.setNumberOfGuests(0);
+		orderTable.setTableGroupId(1L);
+		orderTable.setEmpty(true);
+
+		OrderTable savedOrderTable = tableService.create(orderTable);
+
+		savedOrderTable.setNumberOfGuests(3); // Change Number Of Guest
+		assertThatThrownBy(() -> tableService.changeNumberOfGuests(savedOrderTable.getId(), savedOrderTable))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	public void 주문테이블_주문없음_처리_성공(){
+		OrderTable orderTable = new OrderTable();
+		orderTable.setNumberOfGuests(1);
+		orderTable.setTableGroupId(1L);
+		orderTable.setEmpty(false);
+
+		OrderTable savedOrderTable = tableService.create(orderTable);
+
+		tableService.changeEmpty(savedOrderTable.getId(), savedOrderTable);
+	}
+}
