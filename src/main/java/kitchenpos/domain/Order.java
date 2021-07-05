7e247c50836085ab0059fbf9becb76f59b7e6460
HyungjuJang <dacapolife87@gmail.com>
2021-07-05 16:46:58 +0900

    feat : Order 및 OrderLineItem Entity적용 및 repository추가

--- src/main/java/kitchenpos/domain/Order.java
+++ src/main/java/kitchenpos/domain/Order.java
@@ -1,14 +1,43 @@
 package kitchenpos.domain;
 
+import kitchenpos.table.domain.OrderTable;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Table(name = "orders")
+@Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long orderTableId;
     private String orderStatus;
     private LocalDateTime orderedTime;
+
+    @OneToMany(mappedBy = "orderId")
     private List<OrderLineItem> orderLineItems;
+
+    public Order() {
+    }
+
+    public Order(Long orderTableId, String orderStatus, LocalDateTime orderedTime, List<OrderLineItem> orderLineItems) {
+        this.orderTableId = orderTableId;
+        this.orderStatus = orderStatus;
+        this.orderedTime = orderedTime;
+        this.orderLineItems = orderLineItems;
+    }
+
+    public static Order newOrder(OrderTable orderTable, List<OrderLineItem> newOrderLineItems) {
+
+        if (orderTable.isEmpty()) {
+            throw new IllegalArgumentException("빈테이블은 주문을 할수 없습니다.");
+        }
+
+        return new Order(orderTable.getId(), OrderStatus.COOKING.name(), LocalDateTime.now(), newOrderLineItems);
+    }
 
     public Long getId() {
         return id;
--- src/main/java/kitchenpos/domain/OrderLineItem.java
+++ src/main/java/kitchenpos/domain/OrderLineItem.java
@@ -1,10 +1,34 @@
 package kitchenpos.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class OrderLineItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long seq;
     private Long orderId;
     private Long menuId;
     private long quantity;
+
+    public OrderLineItem() {
+    }
+
+    public OrderLineItem(Long menuId, long quantity) {
+        this.menuId = menuId;
+        this.quantity = quantity;
+    }
+
+    public OrderLineItem(Long seq, Long orderId, Long menuId, long quantity) {
+        this.seq = seq;
+        this.orderId = orderId;
+        this.menuId = menuId;
+        this.quantity = quantity;
+    }
 
     public Long getSeq() {
         return seq;
