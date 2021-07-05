9dff7985d4ca43f0f1de4da8ee3cdb402ec28fc5
HyungjuJang <dacapolife87@gmail.com>
2021-07-05 10:34:14 +0900

    feat : 생성자 추가

--- src/main/java/kitchenpos/table/dto/OrderTableRequest.java
+++ src/main/java/kitchenpos/table/dto/OrderTableRequest.java
@@ -6,6 +6,14 @@ public class OrderTableRequest {
     private int numberOfGuests;
     private boolean empty;
 
+    public OrderTableRequest() {
+    }
+
+    public OrderTableRequest(int numberOfGuests, boolean empty) {
+        this.numberOfGuests = numberOfGuests;
+        this.empty = empty;
+    }
+
     public Long getId() {
         return id;
     }
