ab863b9c983dd2f0751e823a4fce0fd40d497e08
HyungjuJang <dacapolife87@gmail.com>
2021-07-05 09:52:19 +0900

    feat : 응답 생성 메소드 추가

--- src/main/java/kitchenpos/dto/MenuGroupResponse.java
+++ src/main/java/kitchenpos/dto/MenuGroupResponse.java
@@ -1,8 +1,28 @@
 package kitchenpos.dto;
 
+import kitchenpos.domain.MenuGroup;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MenuGroupResponse {
     private Long id;
     private String name;
+
+    public MenuGroupResponse(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static MenuGroupResponse of(MenuGroup menuGroup) {
+        return new MenuGroupResponse(menuGroup.getId(), menuGroup.getName());
+    }
+
+    public static List<MenuGroupResponse> ofList(List<MenuGroup> menuGroups) {
+        return menuGroups.stream()
+                .map(MenuGroupResponse::of)
+                .collect(Collectors.toList());
+    }
 
     public Long getId() {
         return id;
