dcf92dc9ef06606ff276789a1c942e60dc49bdc0
HyungjuJang <dacapolife87@gmail.com>
2021-07-06 00:05:28 +0900

    feat : 단체테이블 여부 확인 validation로직 추가

--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -17,6 +17,24 @@ public class TableGroup {
     @OneToMany(mappedBy = "tableGroupId")
     private List<OrderTable> orderTables;
 
+    public TableGroup() {
+    }
+
+    public TableGroup(LocalDateTime createdDate, List<OrderTable> orderTables) {
+        this.createdDate = createdDate;
+        this.orderTables = orderTables;
+
+        if(isValidation(orderTables)) {
+            throw new IllegalArgumentException("빈테이블 또는 이미 단체테이블인 테이블이 존재하는 경우는 단체테이블을 없습니다.");
+        }
+    }
+
+    private boolean isValidation(List<OrderTable> orderTables) {
+        return orderTables.stream()
+                .anyMatch(orderTable -> orderTable.isUnableTableGroup());
+    }
+
+
     public Long getId() {
         return id;
     }
