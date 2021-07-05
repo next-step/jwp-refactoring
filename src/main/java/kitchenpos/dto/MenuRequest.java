168472f3b2411d1e8143d80add68a621ccea0695
HyungjuJang <dacapolife87@gmail.com>
2021-07-05 15:16:19 +0900

    feat : 생성자 추가

--- src/main/java/kitchenpos/dto/MenuRequest.java
+++ src/main/java/kitchenpos/dto/MenuRequest.java
@@ -12,6 +12,24 @@ public class MenuRequest {
     private Long menuGroupId;
     private List<MenuProduct> menuProducts;
 
+    public MenuRequest() {
+    }
+
+    public MenuRequest(String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+        this.menuProducts = menuProducts;
+    }
+
+    public MenuRequest(Long id, String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+        this.menuProducts = menuProducts;
+    }
+
     public Long getId() {
         return id;
     }
