5388f2eccdcf0c0f3a5547d1946058980fcd1865
ted.kang <ted.kang@kakaocorp.com>
2021-07-06 02:16:48 +0900

    feat: MenuGroupService2 구현 코드 작성

--- src/main/java/kitchenpos/menugroup/domain/MenuGroupEntity.java
+++ src/main/java/kitchenpos/menugroup/domain/MenuGroupEntity.java
@@ -20,6 +20,11 @@ public class MenuGroupEntity {
     this.name = name;
   }
 
+  public MenuGroupEntity(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
   public Long getId() {
     return id;
   }
