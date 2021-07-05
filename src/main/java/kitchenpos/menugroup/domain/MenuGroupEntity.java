4507f182ffef0ee7b111bdaff4c4d606798fc4d3
ted.kang <ted.kang@kakaocorp.com>
2021-07-06 02:02:06 +0900

    feat: MenuGroupEntity 테스트코드 작성 및 구현

---
+++ src/main/java/kitchenpos/menugroup/domain/MenuGroupEntity.java
@@ -0,0 +1,30 @@
+package kitchenpos.menugroup.domain;
+
+import javax.persistence.*;
+
+@Table(name = "menu_group")
+@Entity
+public class MenuGroupEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  protected MenuGroupEntity() {
+  }
+
+  public MenuGroupEntity(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
