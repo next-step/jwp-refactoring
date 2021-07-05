b52cf05246a0beb8ea62c8d217ae49868c537275
HyungjuJang <dacapolife87@gmail.com>
2021-07-05 13:30:59 +0900

    feat : Price 를 관리하는 클래스 추가

---
+++ src/main/java/kitchenpos/domain/Price.java
@@ -0,0 +1,40 @@
+package kitchenpos.domain;
+
+import javax.persistence.Embeddable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Embeddable
+public class Price {
+
+    private BigDecimal price;
+
+    public Price() {
+    }
+
+    public Price(BigDecimal price) {
+        this.price = price;
+
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("가격은 필수이고 0보다 작은값이 될수 없습니다.");
+        }
+    }
+
+    public BigDecimal multiply(BigDecimal quantity) {
+        return price.multiply(quantity);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Price price1 = (Price) o;
+        return Objects.equals(price, price1.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
+    }
+}
