b4cc55e1e2fe2c28ba40252758301f36acc2627e
bbbnam <39575145+bbbnam@users.noreply.github.com>
2021-07-06 11:36:13 +0900

    step1 - 🚀 테스트를 통한 코드 보호 (#163)
    * docs : 테이블 그룹 요구사항 정리
    
    * docs : 테이블 요구사항 정리
    
    * docs : 상품 요구사항 정리
    
    * docs : 주문 요구사항 정리
    
    * docs : 메뉴 요구사항 정리
    
    * docs : 메뉴그룹 요구사항 정리
    
    * test : 메뉴그룹 컨트롤러 테스트 작성
    
    * test : 메뉴그룹 서비스 테스트 작성
    
    * test : 불필요 어노테이션 제거
    
    * test : 메뉴 컨트롤러 테스트 작성
    
    * test : 테스트 코드 리팩토링
    
    * docs : 오타수정
    
    * test : 메뉴 서비스 테스트 작성
    
    * test : LocalDateTime 직렬화 문제로 ObjectMapper 설정 configuration 적용
    
    * test : 주문 컨트롤러 테스트 작성
    
    * test : 공통부분 beforeEach 적용
    
    * test : 주문 서비스 테스트
    
    * test : 상품 컨트롤러 테스트 작성
    
    * test : 상품 서비스 테스트 작성
    
    * test : 상품 컨트롤러 테스트 조회 내용 추가
    
    * test : 주문 테이블 컨트롤러 테스트
    
    * test : 주문 테이블 서비스 테스트
    
    * test : 테이블 그룹 컨트롤러 테스트
    
    * test : 테이블 그룹 서비스 테스트
    
    * style : 불필요 줄제거
    
    * refactor : 도메인 기본 생성자의 접근제어자 protected로 변경
    
    * refactor : 접근 제어자 변경으로 영향받는 로직 리팩토링

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,66 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductDao productDao;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @DisplayName("상품을 등록한다.")
+    @Test
+    void create() {
+        Product product = new Product(1L, "신상품", BigDecimal.valueOf(15000));
+        given(productDao.save(any())).willReturn(product);
+
+        Product created = productService.create(product);
+
+        assertThat(created).isEqualTo(product);
+    }
+
+    @DisplayName("상품을 등록에 실패한다. - 상품 등록시 가격값이 null 이거나 0보다 작으면 등록 실패한다.")
+    @Test
+    void fail_create() {
+        Product product1 = new Product(1L, "신상품", BigDecimal.valueOf(-1));
+        Product product2 = new Product(1L, "신상품", null);
+
+        assertThatThrownBy(() -> productService.create(product1))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> productService.create(product2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 리스트를 조회한다.")
+    @Test
+    void list() {
+        Product product1 = new Product(1L, "신상품1", BigDecimal.valueOf(15000));
+        Product product2 = new Product(2L, "신상품2", BigDecimal.valueOf(17000));
+        List<Product> products = Arrays.asList(product1, product2);
+        given(productDao.findAll()).willReturn(products);
+
+        List<Product> findProducts = productService.list();
+
+        Assertions.assertThat(findProducts).containsExactly(product1, product2);
+    }
+}
---
+++ src/test/java/kitchenpos/ui/MenuRestControllerTest.java
@@ -0,0 +1,79 @@
+package kitchenpos.ui;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kitchenpos.application.MenuService;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MenuRestController.class)
+class MenuRestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private MenuService menuService;
+
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true)) //한글 깨짐 처리
+                .build();
+    }
+
+    @DisplayName("메뉴를 등록한다.")
+    @Test
+    void create() throws Exception {
+        List<MenuProduct> menuProducts = Arrays.asList(new MenuProduct(7L, 7L, 7L, 1));
+        Menu menu = new Menu(7L, "쓰리라차치킨", BigDecimal.valueOf(20000), 2L, menuProducts);
+        String jsonString = objectMapper.writeValueAsString(menu);
+
+        given(menuService.create(any())).willReturn(menu);
+
+        mockMvc.perform(post("/api/menus")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonString))
+                .andExpect(status().isCreated());
+    }
+
+    @DisplayName("메뉴 리스트를 조회한다.")
+    @Test
+    void list() throws Exception {
+        List<MenuProduct> menuProducts = Arrays.asList(new MenuProduct(7L, 7L, 7L, 1));
+        Menu menu = new Menu(7L, "쓰리라차치킨", BigDecimal.valueOf(20000), 2L, menuProducts);
+
+        given(menuService.list()).willReturn(Arrays.asList(menu));
+
+        mockMvc.perform(get("/api/menus"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("쓰리라차치킨")));
+    }
+}
