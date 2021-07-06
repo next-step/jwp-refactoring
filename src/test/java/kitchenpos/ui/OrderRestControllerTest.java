028513d237870c586179d4ab67df0550fe157bbf
Minsu Kang <funnycoding91@gmail.com>
2021-07-06 23:17:37 +0900

    🚀 1단계 - 테스트를 통한 코드 보호 (#167)
    * docs: Define kitchenpos business requirements
    
    * docs: Add kitchenpos domain graph
    
    * test: Add product business object test code
    
    * test: Add menu group business object test code
    
    * test: Add menu business object test code
    
    * test: Add order business object test code
    
    * test: Add order table business object test code
    
    * test: Add table group business object test code
    
    * test: Add product WebMvcTest code
    
    * test: Add menu group WebMvcTest code
    
    * test: Add menu WebMvcTest code
    
    * test: Add order WebMvcTest code
    
    * test: Add order table WebMvcTest code
    
    * test: Add table group WebMvcTest code

---
+++ src/test/java/kitchenpos/ui/OrderRestControllerTest.java
@@ -0,0 +1,110 @@
+package kitchenpos.ui;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kitchenpos.application.OrderService;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderStatus;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = OrderRestController.class)
+class OrderRestControllerTest {
+    private static final String URI = "/api/orders";
+
+    @Autowired
+    private OrderRestController orderRestController;
+
+    @MockBean
+    private OrderService orderService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private MockMvc mockMvc;
+    private Order 주문;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(orderRestController)
+                .addFilter(new CharacterEncodingFilter(StandardCharsets.UTF_8.name(), true))
+                .alwaysDo(print())
+                .build();
+
+        주문 = new Order();
+        주문.setId(1L);
+        주문.setOrderTableId(1L);
+        주문.setOrderStatus(OrderStatus.COOKING.name());
+        주문.setOrderedTime(LocalDateTime.now());
+        주문.setOrderLineItems(Lists.list(new OrderLineItem()));
+    }
+
+    @DisplayName("주문을 추가한다.")
+    @Test
+    void create() throws Exception {
+        //given
+        given(orderService.create(any())).willReturn(주문);
+
+        //when
+        ResultActions actions = mockMvc.perform(post(URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(주문)));
+
+        //then
+        actions.andExpect(status().isCreated())
+                .andExpect(header().string("location", URI + "/1"))
+                .andExpect(content().string(containsString("1")))
+                .andExpect(content().string(containsString("COOKING")));
+    }
+
+    @DisplayName("주문을 모두 조회한다.")
+    @Test
+    void list() throws Exception {
+        //given
+        given(orderService.list()).willReturn(Lists.list(주문));
+
+        //when
+        ResultActions actions = mockMvc.perform(get(URI));
+
+        //then
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$[0].id").isNotEmpty())
+                .andExpect(jsonPath("$[0].orderStatus").value(OrderStatus.COOKING.name()));
+    }
+
+    @DisplayName("특정 주문의 상태를 변경한다.")
+    @Test
+    void changeOrderStatus() throws Exception {
+        //given
+        given(orderService.changeOrderStatus(any(), any())).willReturn(주문);
+
+        //when
+        ResultActions actions = mockMvc.perform(put(URI + "/{orderId}/order-status", 주문.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(주문)));
+
+        //then
+        actions.andExpect(status().isOk());
+    }
+}
