359edc7f82ab63904ab111db43b9648adbaccc23
HyungjuJang <dacapolife87@gmail.com>
2021-07-07 21:35:29 +0900

    refactor : entity변경에따른 수정

--- src/main/java/kitchenpos/menu/dto/MenuProductResponse.java
+++ src/main/java/kitchenpos/menu/dto/MenuProductResponse.java
@@ -49,6 +49,6 @@ public class MenuProductResponse {
     }
 
     public static MenuProductResponse of(MenuProduct menuProduct) {
-        return new MenuProductResponse(menuProduct.getSeq(), menuProduct.getMenuId(), menuProduct.getProduct().getId(), menuProduct.getQuantity());
+        return new MenuProductResponse(menuProduct.getSeq(), menuProduct.getMenu().getId(), menuProduct.getProduct().getId(), menuProduct.getQuantity());
     }
 }
--- src/main/java/kitchenpos/menu/dto/MenuResponse.java
+++ src/main/java/kitchenpos/menu/dto/MenuResponse.java
@@ -66,6 +66,10 @@ public class MenuResponse {
                 .stream()
                 .map(MenuProductResponse::of)
                 .collect(Collectors.toList());
-        return new MenuResponse(menu.getId(), menu.getName(), menu.getPrice().getPrice(), menu.getMenuGroupId(), menuProductResponses);
+        return new MenuResponse(menu.getId(),
+                menu.getName(),
+                menu.getPrice().getPrice(),
+                menu.getMenuGroup().getId(),
+                menuProductResponses);
     }
 }
