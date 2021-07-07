e939333dcecaf389e4bcab0d34d962238231e1db
HyungjuJang <dacapolife87@gmail.com>
2021-07-07 11:04:24 +0900

    feat : 컬럼명 어노테이션 추가

--- src/main/java/kitchenpos/menu/domain/MenuGroup.java
+++ src/main/java/kitchenpos/menu/domain/MenuGroup.java
@@ -1,17 +1,16 @@
 package kitchenpos.menu.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class MenuGroup {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
     public MenuGroup() {
