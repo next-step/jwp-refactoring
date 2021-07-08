3ba048abe93c997bfc4086ad6f4286efc2e56d02
oper912 <oper912@naver.com>
2021-07-08 15:50:49 +0900

    test: Controller test 오류 수정

--- src/test/java/kitchenpos/product/ui/ProductRestControllerTest.java
+++ src/test/java/kitchenpos/product/ui/ProductRestControllerTest.java
@@ -1,8 +1,9 @@
-package kitchenpos.ui;
+package kitchenpos.product.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kitchenpos.application.ProductService;
-import kitchenpos.domain.Product;
+import kitchenpos.config.MockMvcTestConfig;
+import kitchenpos.product.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,27 +13,24 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.math.BigDecimal;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = {ProductRestController.class})
+@MockMvcTestConfig
 class ProductRestControllerTest {
     private static final String PRODUCT_API_URI = "/api/products";
 
     @Autowired
-    private ProductRestController productRestController;
+    private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -40,17 +38,11 @@ class ProductRestControllerTest {
     @MockBean
     private ProductService productService;
 
-    private MockMvc mockMvc;
     private Product 강정치킨;
     private Product 후라이드;
 
     @BeforeEach
     void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(productRestController)
-                .addFilter(new CharacterEncodingFilter(StandardCharsets.UTF_8.name(), true))
-                .alwaysDo(print())
-                .build();
-
         강정치킨 = new Product(1L, "강정치킨", BigDecimal.valueOf(17000));
         후라이드 = new Product(2L, "후라이드", BigDecimal.valueOf(16000));
     }
@@ -59,7 +51,7 @@ class ProductRestControllerTest {
     @Test
     void createTest() throws Exception {
         // given
-        given(productService.create(any())).willReturn(강정치킨);
+        when(productService.create(any())).thenReturn(강정치킨);
 
         // when
         ResultActions actions = mockMvc.perform(post(PRODUCT_API_URI)
@@ -67,7 +59,8 @@ class ProductRestControllerTest {
                 .content(objectMapper.writeValueAsString(강정치킨)));
 
         // then
-        actions.andExpect(status().isCreated())
+        actions.andDo(print())
+                .andExpect(status().isCreated())
                 .andExpect(header().string("location", PRODUCT_API_URI + "/1"))
                 .andExpect(content().string(containsString("강정치킨")));
     }
@@ -76,13 +69,14 @@ class ProductRestControllerTest {
     @Test
     void listTest() throws Exception {
         // given
-        given(productService.list()).willReturn(Arrays.asList(강정치킨, 후라이드));
+        when(productService.list()).thenReturn(Arrays.asList(강정치킨, 후라이드));
 
         // when
         ResultActions actions = mockMvc.perform(get(PRODUCT_API_URI));
 
         // then
-        actions.andExpect(status().isOk())
+        actions.andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(content().string(containsString("강정치킨")))
                 .andExpect(content().string(containsString("후라이드")));
     }
