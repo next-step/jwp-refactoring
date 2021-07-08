7439dd97340491392c4738b591dc85015f3e212e
oper912 <oper912@naver.com>
2021-07-08 19:19:42 +0900

    feat: `Price` domain 분리 및 최소 값 테스트

---
+++ src/test/java/kitchenpos/product/domain/PriceTest.java
@@ -0,0 +1,22 @@
+package kitchenpos.product.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PriceTest {
+
+    @DisplayName("0보다 작은 값을 입력하면 예외가 발생한다.")
+    @Test
+    void validateValue() {
+        // given
+        BigDecimal price = BigDecimal.valueOf(-1);
+
+        // when & then
+        assertThatThrownBy(() -> Price.from(price))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
