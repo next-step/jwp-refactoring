a6c38584a36c360aab9e07283f76f25ddb3aa067
HoonHee Jeong <jhh992000@gmail.com>
2021-07-08 12:00:28 +0900

    refactor: 단체지정 해제 유효성검증 도메인으로 이동

--- src/main/java/kitchenpos/table/domain/OrderTable.java
+++ src/main/java/kitchenpos/table/domain/OrderTable.java
@@ -15,6 +15,7 @@ import kitchenpos.order.domain.Order;
 import kitchenpos.order.domain.Orders;
 import kitchenpos.table.exception.CannotChangeNumberOfGuestException;
 import kitchenpos.table.exception.CannotChangeTableEmptyException;
+import kitchenpos.table.exception.CannotUngroupOrderTableException;
 
 @Entity
 public class OrderTable {
@@ -23,6 +24,7 @@ public class OrderTable {
     public static final String THERE_IS_AN_ONGOING_ORDER = "진행중(조리 or 식사)인 경우 빈 테이블로 변경이 불가능하다.";
     public static final String THE_NUMBER_OF_GUESTS_CANNOT_BE_NEGATIVE = "변경하려는 손님 수는 음수일 수 없다.";
     public static final String THERE_IS_AN_EMPTY_ORDER_TABLE = "빈 테이블의 주문 테이블은 손님 수를 변경할 수 없습니다.";
+    public static final String THERE_IS_A_HISTORY_OF_ORDERS_AT_AN_ONGOING = "진행중(조리 or 식사)인 단계의 주문 이력이 존재할 경우 해제가 불가능하다.";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -123,6 +125,9 @@ public class OrderTable {
     }
 
     public void ungroup() {
+        if (isNotCompleted()) {
+            throw new CannotUngroupOrderTableException(THERE_IS_A_HISTORY_OF_ORDERS_AT_AN_ONGOING);
+        }
         this.tableGroup = null;
     }
 
--- src/main/java/kitchenpos/table/domain/OrderTables.java
+++ src/main/java/kitchenpos/table/domain/OrderTables.java
@@ -5,14 +5,12 @@ import java.util.Collections;
 import java.util.List;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
-import kitchenpos.table.exception.CannotUngroupOrderTableException;
 import kitchenpos.table.exception.OutOfOrderTableException;
 import org.springframework.util.CollectionUtils;
 
 @Embeddable
 public class OrderTables {
 
-    public static final String THERE_IS_A_HISTORY_OF_ORDERS_AT_AN_ONGOING = "진행중(조리 or 식사)인 단계의 주문 이력이 존재할 경우 해제가 불가능하다.";
     public static final int MIN_ORDER_TABLE_COUNT = 2;
 
     @OneToMany(mappedBy = "tableGroup", orphanRemoval = true)
@@ -42,15 +40,6 @@ public class OrderTables {
     }
 
     public void upgroup() {
-        orderTables.forEach(orderTable -> {
-            validationOrderTable(orderTable);
-            orderTable.ungroup();
-        });
-    }
-
-    private void validationOrderTable(OrderTable orderTable) {
-        if (orderTable.isNotCompleted()) {
-            throw new CannotUngroupOrderTableException(THERE_IS_A_HISTORY_OF_ORDERS_AT_AN_ONGOING);
-        }
+        orderTables.forEach(OrderTable::ungroup);
     }
 }
