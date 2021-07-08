08737ba60aea97bfaa7a0b7624fb294da51384d1
BongJin Jo <plintcsz@gmail.com>
2021-07-08 20:51:41 +0900

    [1단계 - 테스트를 통한 코드 보호] 리뷰 요청드립니다. (#156)
    * docs: 요구사항 작성
    
    * test: 비지니스 레이어 테스트 코드 작성
    
    * docs: 요구사항에 주문 상태 추가
    
    * test: 테스트 메소드 하나에 포함되어 있던 여러 개 케이스를 별도 테스트 메소드로 분리
    
    * test: when->thenReturn 패턴을 given->willReturn 패튼으로 변경

---
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -0,0 +1,154 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kitchenpos.dao.MenuDao;
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderLineItemDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderTable;
+
+@ExtendWith(MockitoExtension.class)
+class OrderServiceTest {
+    @Mock
+    private MenuDao menuDao;
+
+    @Mock
+    private OrderDao orderDao;
+
+    @Mock
+    private OrderLineItemDao orderLineItemDao;
+
+    @Mock
+    private OrderTableDao orderTableDao;
+
+    @InjectMocks
+    private OrderService orderService;
+
+    private final Long orderId = 1L;
+    private Order order;
+    private Order savedOrder;
+    private OrderLineItem orderLineItem;
+    private OrderLineItem orderLineItem2;
+
+    @BeforeEach
+    void setUp() {
+        order = new Order();
+        savedOrder = new Order();
+        orderLineItem = new OrderLineItem();
+        orderLineItem2 = new OrderLineItem();
+    }
+
+    @Test
+    void create() {
+        // given
+        order.setOrderTableId(1L);
+        order.setOrderLineItems(Arrays.asList(orderLineItem, orderLineItem2));
+        given(menuDao.countByIdIn(anyList())).willReturn(2L);
+        given(orderTableDao.findById(order.getOrderTableId())).willReturn(Optional.of(new OrderTable()));
+
+        savedOrder.setId(1L);
+        given(orderDao.save(order)).willReturn(savedOrder);
+
+        OrderLineItem savedOrderLineItem = new OrderLineItem();
+        OrderLineItem savedOrderLineItem2 = new OrderLineItem();
+        given(orderLineItemDao.save(orderLineItem)).willReturn(savedOrderLineItem);
+        given(orderLineItemDao.save(orderLineItem2)).willReturn(savedOrderLineItem2);
+
+        // when
+        final Order actual = orderService.create(order);
+
+        // then
+        verify(orderDao).save(order);
+        verify(orderLineItemDao).save(orderLineItem);
+        verify(orderLineItemDao).save(orderLineItem2);
+        assertThat(actual.getOrderLineItems()).isEqualTo(Arrays.asList(savedOrderLineItem, savedOrderLineItem2));
+    }
+
+    @DisplayName("아이템이 없는 주문을 생성할 때 예외가 발생하는지 테스트")
+    @Test
+    void given_OrderHasEmptyItem_when_Create_then_ThrownException() {
+        // given
+        Order order = new Order();
+
+        // when
+        final Throwable throwable = catchThrowable(() -> orderService.create(order));
+
+        // then
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("아이템이 하나만 있는 주문을 생성할 때 예외가 발생하는지 테스트")
+    @Test
+    void given_OrderHasOnlyOneItem_when_Create_then_ThrownException() {
+        // given
+        Order order = new Order();
+        order.setOrderLineItems(Collections.singletonList(new OrderLineItem()));
+        given(menuDao.countByIdIn(anyList())).willReturn(2L);
+
+        // when
+        final Throwable differentSizeException = catchThrowable(() -> orderService.create(order));
+
+        // then
+        assertThat(differentSizeException).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void list() {
+        // given
+        order.setId(1L);
+        given(orderDao.findAll()).willReturn(Collections.singletonList(order));
+        final List<OrderLineItem> orderLineItems = Collections.singletonList(orderLineItem);
+        given(orderLineItemDao.findAllByOrderId(order.getId())).willReturn(orderLineItems);
+
+        // when
+        final List<Order> actual = orderService.list();
+
+        // then
+        verify(orderDao).findAll();
+        assertThat(actual.get(0).getOrderLineItems()).isEqualTo(orderLineItems);
+    }
+
+    @Test
+    void changeOrderStatus() {
+        // given
+        order.setOrderStatus("COOKING");
+        Order savedOrder = new Order();
+        given(orderDao.findById(orderId)).willReturn(Optional.of(savedOrder));
+
+        // when
+        orderService.changeOrderStatus(orderId, order);
+
+        // then
+        verify(orderDao).save(savedOrder);
+        assertThat(savedOrder.getOrderStatus()).isEqualTo(order.getOrderStatus());
+    }
+
+    @Test
+    void given_CompletedOrder_when_ChangeOrderStatus_then_ThrownException() {
+        // given
+        order.setOrderStatus("COMPLETION");
+
+        // when
+        final Throwable throwable = catchThrowable(() -> orderService.changeOrderStatus(orderId, order));
+
+        // then
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+    }
+}
