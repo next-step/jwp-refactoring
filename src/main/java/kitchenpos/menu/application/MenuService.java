660433956d7cd419e646adade435a21c20374b47
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-07-08 23:56:27 +0900

    refactor(MenuService): 메서드 리팩토링

--- src/main/java/kitchenpos/menu/application/MenuService.java
+++ src/main/java/kitchenpos/menu/application/MenuService.java
@@ -3,7 +3,9 @@ package kitchenpos.menu.application;
 import kitchenpos.menu.domain.Menu;
 import kitchenpos.menu.domain.MenuProduct;
 import kitchenpos.menu.domain.MenuProducts;
+import kitchenpos.menu.dto.MenuProductRequest;
 import kitchenpos.menu.dto.MenuResponse;
+import kitchenpos.menu.ui.exception.NoMenuGroupException;
 import kitchenpos.menugroup.domain.MenuGroupRepository;
 import kitchenpos.menu.domain.MenuRepository;
 import kitchenpos.menu.dto.MenuRequest;
@@ -13,7 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,32 +33,27 @@ public class MenuService {
     }
 
     public MenuResponse create(MenuRequest menuRequest) {
-        Menu menu = new Menu(
-                menuRequest.getName(),
-                menuRequest.getPrice(),
-                menuRequest.getMenuGroupId()
-        );
-
-        menu.validatePrice();
+        Menu menu = Menu.of(menuRequest);
 
         if (!menuGroupRepository.existsById(menu.getMenuGroupId())) {
-            throw new IllegalArgumentException();
+            throw new NoMenuGroupException();
         }
 
         final Menu savedMenu = menuRepository.save(menu);
 
-        final List<MenuProduct> menuProductList =
-                menuRequest.getMenuProducts().stream().map(menuProductRequest -> new MenuProduct(
-                        menu,
-                        productRepository.findById(menuProductRequest.getProductId()).orElseThrow(NoSuchElementException::new),
-                        menuProductRequest.getQuantity()
-                )).collect(Collectors.toList());
+        MenuProducts menuProducts = new MenuProducts();
 
-        MenuProducts menuProducts = new MenuProducts(menuProductList);
+        for (MenuProductRequest menuProductRequest : menuRequest.getMenuProducts()) {
+            productRepository.findById(menuProductRequest.getProductId()).ifPresent(
+                    product -> menuProducts.add(
+                            new MenuProduct(savedMenu, product, menuProductRequest.getQuantity())
+                    )
+            );
+        }
 
         BigDecimal sum = menuProducts.sumOfMenuProducts();
 
-        menu.compareMenuPriceToProductsSum(sum);
+        savedMenu.compareMenuPriceToProductsSum(sum);
 
         savedMenu.addMenuProducts(menuProducts);
 
