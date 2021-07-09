283e6261c4c9bbd6a63d51a1348d7dca8ff62dec
bbbnam <bbbnam@naver.com>
2021-07-09 16:26:22 +0900

    test : 테스트 깨진것 수정

--- src/test/java/kitchenpos/order/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/order/application/OrderServiceTest.java
@@ -1,9 +1,11 @@
 package kitchenpos.order.application;
 
+import kitchenpos.menu.domain.Menu;
 import kitchenpos.menu.domain.MenuRepository;
 import kitchenpos.order.domain.*;
+import kitchenpos.order.dto.OrderRequest;
+import kitchenpos.order.dto.OrderResponse;
 import kitchenpos.table.domain.OrderTable;
-import kitchenpos.table.domain.OrderTableDao;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
@@ -30,56 +33,57 @@ class OrderServiceTest {
     MenuRepository menuRepository;
 
     @Mock
-    OrderDao orderDao;
+    OrderRepository orderRepository;
 
     @Mock
-    OrderLineItemDao orderLineItemDao;
-
-    @Mock
-    OrderTableDao orderTableDao;
+    OrderTableRepository orderTableRepository;
 
     @InjectMocks
     private OrderService orderService;
 
-    private Long orderTableId = 1L;
-    private Long newOrderTableId = 2L;
-    private OrderLineItem orderLineItem1;
-    private OrderLineItem orderLineItem2;
-    private List<OrderLineItem> orderLineItems;
-    private Order order;
+    private Long 주문테이블Id = 1L;
+    private Long new주문테이블Id = 2L;
+    private OrderLineItem 주문수량메뉴1;
+    private OrderLineItem 주문수량메뉴2;
+    private List<OrderLineItem> 주문내역들;
+    private Order 주문;
+    private OrderRequest 주문요청;
     private Order chageStatusOrder;
-    private Order newOrder;
+    private Order 다른주문;
+    private Menu 치킨메뉴;
+    private Menu 튀김메뉴;
 
     @BeforeEach
     void setUp() {
-        orderLineItem1 = new OrderLineItem(1L, 1L, 1L, 1L);
-        orderLineItem2 = new OrderLineItem(2L, 1L, 1L, 1L);
-        orderLineItems = Arrays.asList(orderLineItem1, orderLineItem2);
-        order = new Order(1L, orderTableId, OrderStatus.COOKING.name(), LocalDateTime.now(), orderLineItems);
-        chageStatusOrder = new Order(1L, orderTableId, OrderStatus.MEAL.name(), LocalDateTime.now(), orderLineItems);
-        newOrder = new Order(2L, newOrderTableId, OrderStatus.MEAL.name(), LocalDateTime.now(), orderLineItems);
+        치킨메뉴 = new Menu(1L, "치킨메뉴", BigDecimal.valueOf(20000));
+        튀김메뉴 = new Menu(2L, "튀김메뉴", BigDecimal.valueOf(10000));
+        주문수량메뉴1 = new OrderLineItem(1L, 1L, 치킨메뉴.getId(), 1L);
+        주문수량메뉴2 = new OrderLineItem(2L, 1L, 튀김메뉴.getId(), 1L);
+        주문내역들 = Arrays.asList(주문수량메뉴1, 주문수량메뉴2);
+        주문 = new Order(1L, 주문테이블Id, OrderStatus.COOKING.name(), LocalDateTime.now(), 주문내역들);
+        주문요청 = new OrderRequest(주문테이블Id, 주문내역들);
+        chageStatusOrder = new Order(1L, 주문테이블Id, OrderStatus.MEAL.name(), LocalDateTime.now(), 주문내역들);
+        다른주문 = new Order(2L, new주문테이블Id, OrderStatus.MEAL.name(), LocalDateTime.now(), 주문내역들);
     }
 
     @DisplayName("주문을 등록한다.")
     @Test
     void create() {
-        given(menuRepository.findAllById(anyList()).size()).willReturn(orderLineItems.size());
-        given(orderTableDao.findById(anyLong())).willReturn(Optional.of(new OrderTable(1L, null, 0, false)));
-        given(orderDao.save(any())).willReturn(order);
-        given(orderLineItemDao.save(orderLineItem1)).willReturn(orderLineItem1);
-        given(orderLineItemDao.save(orderLineItem2)).willReturn(orderLineItem2);
+        given(menuRepository.findAllById(anyList())).willReturn(Arrays.asList(치킨메뉴, 튀김메뉴));
+        given(orderTableRepository.findById(anyLong())).willReturn(Optional.of(new OrderTable(1L, null, 0, false)));
+        given(orderRepository.save(any())).willReturn(주문);
 
-        Order savedOrder = orderService.create(order);
+        OrderResponse savedOrder = orderService.create(주문요청);
 
         assertAll(
-                () -> assertThat(savedOrder).isEqualTo(order),
-                () -> assertThat(savedOrder.getOrderLineItems()).contains(orderLineItem1, orderLineItem2));
+                () -> assertThat(savedOrder.getId()).isEqualTo(주문.getId()),
+                () -> assertThat(savedOrder.getOrderLineItems()).contains(주문수량메뉴1, 주문수량메뉴2));
     }
 
     @DisplayName("주문을 등록을 실패한다 - 주문에 포함된 주문된 메뉴들이(주문 아이템[OrderLineItem]) 없으면 실패한다.")
     @Test
     void fail_create1() {
-        Order order = new Order(1L, 1L, OrderStatus.COOKING.name(), LocalDateTime.now(), Collections.emptyList());
+        OrderRequest order = new OrderRequest(1L, Collections.emptyList());
 
         assertThatThrownBy(() -> orderService.create(order))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -88,8 +92,8 @@ class OrderServiceTest {
     @DisplayName("주문을 등록을 실패한다 - 등록되지 않은 메뉴 주문시 등록 실패한다.")
     @Test
     void fail_create2() {
-        Order order = new Order(1L, 1L, OrderStatus.COOKING.name(), LocalDateTime.now(), orderLineItems);
-        given(menuRepository.findAllById(any()).size()).willReturn(orderLineItems.size() - 1);
+        OrderRequest order = new OrderRequest(1L, 주문내역들);
+        given(menuRepository.findAllById(any())).willReturn(Collections.emptyList());
 
         assertThatThrownBy(() -> orderService.create(order))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -98,7 +102,8 @@ class OrderServiceTest {
     @DisplayName("주문을 등록을 실패한다 - 주문 테이블이 없는 주문은 등록 실패한다.")
     @Test
     void fail_create3() {
-        given(menuRepository.findAllById(any()).size()).willReturn(orderLineItems.size());
+        OrderRequest order = new OrderRequest(1L, 주문내역들);
+        given(menuRepository.findAllById(any())).willReturn(Arrays.asList(치킨메뉴, 튀김메뉴));
 
         assertThatThrownBy(() -> orderService.create(order))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -107,8 +112,9 @@ class OrderServiceTest {
     @DisplayName("주문을 등록을 실패한다 - 주문 테이블이 empty (true) 상태면 주문 등록 실패한다.")
     @Test
     void fail_create4() {
-        given(menuRepository.findAllById(any()).size()).willReturn(orderLineItems.size() - 1);
-        given(orderTableDao.findById(any())).willReturn(Optional.of(new OrderTable(1L, 1L, 2, true)));
+        OrderRequest order = new OrderRequest(1L, 주문내역들);
+        given(menuRepository.findAllById(any())).willReturn(Arrays.asList(치킨메뉴, 튀김메뉴));
+        given(orderTableRepository.findById(any())).willReturn(Optional.of(new OrderTable(1L, 1L, 2, true)));
 
         assertThatThrownBy(() -> orderService.create(order))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -117,40 +123,39 @@ class OrderServiceTest {
     @DisplayName("주문을 조회한다.")
     @Test
     void list() {
-        given(orderDao.findAll()).willReturn(Arrays.asList(order, newOrder));
+        given(orderRepository.findAll()).willReturn(Arrays.asList(주문, 다른주문));
 
         List<Order> orders = orderService.list();
 
-        assertThat(orders).containsExactly(order, newOrder);
+        assertThat(orders).containsExactly(주문, 다른주문);
     }
 
     @DisplayName("주문 상태를 변경한다.")
     @Test
     void changeOrderStatus() {
-        given(orderDao.findById(anyLong())).willReturn(Optional.ofNullable(chageStatusOrder));
-        given(orderLineItemDao.findAllByOrderId(anyLong())).willReturn(orderLineItems);
+        given(orderRepository.findById(anyLong())).willReturn(Optional.ofNullable(chageStatusOrder));
 
-        Order changed = orderService.changeOrderStatus(this.order.getId(), chageStatusOrder);
+        Order changed = orderService.changeOrderStatus(주문.getId(), chageStatusOrder);
 
         assertAll(
                 () -> assertThat(changed).isEqualTo(chageStatusOrder),
-                () -> assertThat(changed.getOrderLineItems()).isEqualTo(orderLineItems));
+                () -> assertThat(changed.getOrderLineItems()).isEqualTo(주문내역들));
     }
 
     @DisplayName("주문 상태를 변경을 실패한다 - 기존에 등록된 주문이 없으면 주문 상태 변경에 실패한다.")
     @Test
     void fail_changeOrderStatus1() {
-        given(orderDao.findById(anyLong())).willReturn(Optional.empty());
+        given(orderRepository.findById(anyLong())).willReturn(Optional.empty());
 
-        assertThatThrownBy(() -> orderService.changeOrderStatus(this.order.getId(), chageStatusOrder))
+        assertThatThrownBy(() -> orderService.changeOrderStatus(주문.getId(), chageStatusOrder))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("주문 상태를 변경을 실패한다 - 주문 상태가 기존에 계산 완료(COMPLETION) 상태일 경우 주문 상태 변경에 실패한다.")
     @Test
     void fail_changeOrderStatus2() {
-        Order order = new Order(1L, 1L, OrderStatus.COMPLETION.name(), LocalDateTime.now(), orderLineItems);
-        given(orderDao.findById(anyLong())).willReturn(Optional.ofNullable(order));
+        Order order = new Order(1L, 1L, OrderStatus.COMPLETION.name(), LocalDateTime.now(), 주문내역들);
+        given(orderRepository.findById(anyLong())).willReturn(Optional.ofNullable(order));
 
         assertThatThrownBy(() -> orderService.changeOrderStatus(order.getId(), chageStatusOrder))
                 .isInstanceOf(IllegalArgumentException.class);
