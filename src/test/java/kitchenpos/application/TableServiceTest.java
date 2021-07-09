fecd1533307bdfc949784ec2ca4ab0244719a3d6
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-07-09 15:29:51 +0900

    test : TableService 단위테스트작성 (테이블 빈 상태로 변경)

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -1,6 +1,8 @@
 package kitchenpos.application;
 
+import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,8 +14,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,9 +25,12 @@ class TableServiceTest {
 
     private OrderTable table;
     private OrderTable table2;
+    private OrderTable table3;
 
     @Mock
     private OrderTableDao orderTableDao;
+    @Mock
+    private OrderDao orderDao;
 
     @InjectMocks
     private TableService tableService;
@@ -39,6 +46,11 @@ class TableServiceTest {
         table2.setId(2L);
         table2.setNumberOfGuests(0);
         table2.setEmpty(true);
+
+        table3 = new OrderTable();
+        table3.setId(3L);
+        table3.setNumberOfGuests(0);
+        table3.setEmpty(false);
     }
 
     @DisplayName("테이블 정보를 등록한다")
@@ -65,5 +77,51 @@ class TableServiceTest {
 
         // then
         assertThat(actual).isEqualTo(Arrays.asList(table, table2));
+    }
+
+    @DisplayName("테이블을 빈 상태로 변경한다")
+    @Test
+    void changeEmpty() {
+        // given
+        given(orderTableDao.findById(table3.getId())).willReturn(Optional.of(table3));
+        given(orderDao.existsByOrderTableIdAndOrderStatusIn(table3.getId(), Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))).willReturn(false);
+        given(orderTableDao.save(table3)).willReturn(table3);
+
+        // when
+        table3.setEmpty(true);
+        OrderTable actual = tableService.changeEmpty(table3.getId(), table3);
+
+        // then
+        assertThat(actual.isEmpty()).isTrue();
+    }
+
+    @DisplayName("테이블 빈 상태로 변경 - 테이블에 연결된 조리, 식사 상태의 주문이 존재하는 경우 변경 불가")
+    @Test
+    void changeEmpty_existsCookingOrMealOrder() {
+        // given
+        given(orderTableDao.findById(table3.getId())).willReturn(Optional.of(table3));
+        given(orderDao.existsByOrderTableIdAndOrderStatusIn(table3.getId(), Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))).willReturn(true);
+
+        // when
+        table3.setEmpty(true);
+
+        // then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> tableService.changeEmpty(table3.getId(), table3));
+    }
+
+    @DisplayName("테이블 빈 상태로 변경 - 테이블에 연결된 테이블 그룹이 존재하는 경우 변경 불가")
+    @Test
+    void changeEmpty_existsTableGroup() {
+        // given
+        given(orderTableDao.findById(table3.getId())).willReturn(Optional.of(table3));
+
+        // when
+        table3.setTableGroupId(1L);
+        table3.setEmpty(true);
+
+        // then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> tableService.changeEmpty(table3.getId(), table3));
     }
 }
