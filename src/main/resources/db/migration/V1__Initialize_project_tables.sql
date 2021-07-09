a8282fdd8801217277afa563bfd0b4195a609e20
lenkim <joenggyu0@gmail.com>
2021-07-09 21:21:40 +0900

    refactor: DROP CONSTRAINT in MenuProduct

--- src/main/resources/db/migration/V1__Initialize_project_tables.sql
+++ src/main/resources/db/migration/V1__Initialize_project_tables.sql
@@ -77,6 +77,10 @@ ALTER TABLE menu_product
     ADD CONSTRAINT fk_menu_product_menu
         FOREIGN KEY (menu_id) REFERENCES menu (id);
 
+ALTER TABLE menu_product
+    ADD CONSTRAINT fk_menu_product_product
+        FOREIGN KEY (product_id) REFERENCES product (id);
+
 ALTER TABLE order_table
     ADD CONSTRAINT fk_order_table_table_group
         FOREIGN KEY (table_group_id) REFERENCES table_group (id);
---
+++ src/main/resources/db/migration/V3__Remove_dependency.sql
@@ -0,0 +1,2 @@
+ALTER TABLE menu_product
+DROP CONSTRAINT fk_menu_product_product;
