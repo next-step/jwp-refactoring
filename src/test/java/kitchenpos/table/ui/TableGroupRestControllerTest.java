94916c212863c5f545389ea3b816b5eece1efabf
oper912 <oper912@naver.com>
2021-07-10 00:38:35 +0900

    refactor(tableGroup): API 요청 및 응답 entity -> dto 변경

--- src/test/java/kitchenpos/table/ui/TableGroupRestControllerTest.java
+++ src/test/java/kitchenpos/table/ui/TableGroupRestControllerTest.java
@@ -1,10 +1,11 @@
 package kitchenpos.table.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kitchenpos.config.MockMvcTestConfig;
 import kitchenpos.table.application.TableGroupService;
-import kitchenpos.table.domain.OrderTable;
-import kitchenpos.table.domain.TableGroup;
-import org.junit.jupiter.api.BeforeEach;
+import kitchenpos.table.dto.OrderTableResponse;
+import kitchenpos.table.dto.TableGroupRequest;
+import kitchenpos.table.dto.TableGroupResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
@@ -27,11 +25,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = TableGroupRestController.class)
+@MockMvcTestConfig
 class TableGroupRestControllerTest {
     private static final String TABLE_GROUP_API_URI = "/api/table-groups";
 
     @Autowired
-    private TableGroupRestController tableGroupRestController;
+    private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -39,49 +38,36 @@ class TableGroupRestControllerTest {
     @MockBean
     private TableGroupService tableGroupService;
 
-    private MockMvc mockMvc;
-    private TableGroup tableGroup;
-    private OrderTable orderTable;
-
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(tableGroupRestController)
-                .addFilter(new CharacterEncodingFilter(StandardCharsets.UTF_8.name(), true))
-                .alwaysDo(print())
-                .build();
-
-        orderTable = new OrderTable(1L, 1L, 1, true);
-        tableGroup = new TableGroup(1L, LocalDateTime.now(), Arrays.asList(orderTable));
-    }
-
     @DisplayName("단체 지정을 등록할 수 있다.")
     @Test
     void createTest() throws Exception {
         // given
+        OrderTableResponse orderTableResponse1 = new OrderTableResponse(1L, 1L, 1, false);
+        OrderTableResponse orderTableResponse2 = new OrderTableResponse(2L, 1L, 1, false);
+        TableGroupResponse tableGroup = new TableGroupResponse(1L, LocalDateTime.now(), Arrays.asList(orderTableResponse1, orderTableResponse2));
         given(tableGroupService.create(any())).willReturn(tableGroup);
+
+        TableGroupRequest tableGroupRequest = new TableGroupRequest(Arrays.asList(1L, 2L));
 
         // when
         ResultActions actions = mockMvc.perform(post(TABLE_GROUP_API_URI)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(tableGroup)));
+                .content(objectMapper.writeValueAsString(tableGroupRequest)));
 
         // then
-        actions.andExpect(status().isCreated())
-                .andExpect(header().string("location", TABLE_GROUP_API_URI + "/1"))
-                .andExpect(jsonPath("$.id").isNotEmpty())
-                .andExpect(jsonPath("$.orderTables").isNotEmpty())
-                .andExpect(jsonPath("orderTables[0].id").value(orderTable.getId()))
-                .andExpect(jsonPath("orderTables[0].empty").value(orderTable.isEmpty()))
-                .andExpect(jsonPath("orderTables[0].numberOfGuests").value(orderTable.getNumberOfGuests()));
+        actions.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location", TABLE_GROUP_API_URI + "/1"));
     }
 
     @DisplayName("단체 지정을 해제할 수 있다.")
     @Test
     void ungroupTest() throws Exception {
         // when
-        ResultActions actions = mockMvc.perform(delete(TABLE_GROUP_API_URI + "/{tableGroupId}", tableGroup.getId()));
+        ResultActions actions = mockMvc.perform(delete(TABLE_GROUP_API_URI + "/{tableGroupId}", 1L));
 
         // then
-        actions.andExpect(status().isNoContent());
+        actions.andDo(print())
+                .andExpect(status().isNoContent());
     }
 }
