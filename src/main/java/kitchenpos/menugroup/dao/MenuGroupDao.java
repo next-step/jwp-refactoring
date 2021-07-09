fdeffdbfcff49dd1f1c05ca417d523a83380cfd1
kyle <cupeanimus@naver.com>
2021-07-10 01:37:01 +0900

    refactor : refactor for menugroup packaging
    메뉴그룹 관련 패키징 변경

--- src/main/java/kitchenpos/menugroup/dao/MenuGroupDao.java
+++ src/main/java/kitchenpos/menugroup/dao/MenuGroupDao.java
@@ -1,6 +1,6 @@
-package kitchenpos.dao;
+package kitchenpos.menugroup.dao;
 
-import kitchenpos.domain.MenuGroup;
+import kitchenpos.menugroup.domain.MenuGroup;
 
 import java.util.List;
 import java.util.Optional;
