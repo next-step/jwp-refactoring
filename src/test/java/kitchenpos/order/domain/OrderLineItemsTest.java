4d10281011dc69ed4b181c7688d58cb287cbf67e
bbbnam <bbbnam@naver.com>
2021-07-10 01:36:58 +0900

    test : Order create 메소드 관련 도메인 객체 테스트 작성

---
+++ src/test/java/kitchenpos/order/domain/OrderLineItemsTest.java
@@ -0,0 +1,39 @@
+package kitchenpos.order.domain;
+
+import kitchenpos.menu.domain.Menu;
+import kitchenpos.order.exception.NotInitMenuException;
+import kitchenpos.order.exception.NotOrderLineItemsException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class OrderLineItemsTest {
+
+    private Menu 치킨메뉴 = new Menu(1L, "치킨메뉴",BigDecimal.valueOf(20000));
+    private Menu 튀김메뉴 = new Menu(2L, "튀김메뉴", BigDecimal.valueOf(10000));
+    private OrderLineItem 주문수량메뉴1 = new OrderLineItem(1L, 1L, 치킨메뉴.getId(), 1L);
+    private OrderLineItem 주문수량메뉴2 = new OrderLineItem(2L, 1L, 튀김메뉴.getId(), 1L);
+    private List<OrderLineItem> 주문내역들 = Arrays.asList(주문수량메뉴1, 주문수량메뉴2);
+
+    @DisplayName("주문 생성에 실패한다. - 주문 내역이 없으면 주문 등록에 실패한다.")
+    @Test
+    void fail_create1() {
+        assertThatThrownBy(() -> new OrderLineItems(Collections.emptyList()))
+                .isInstanceOf(NotOrderLineItemsException.class);
+    }
+
+    @DisplayName("주문 생성에 실패한다. - 등록되지 않은 메뉴 주문시 등록 실패한다.")
+    @Test
+    void fail_create2() {
+        int 주문내역중_등록된메뉴_갯수 = 주문내역들.size() - 1;
+        OrderLineItems orderLineItems = new OrderLineItems(주문내역들);
+        assertThatThrownBy(() -> orderLineItems.checkInitOrderLineItems(주문내역중_등록된메뉴_갯수))
+                .isInstanceOf(NotInitMenuException.class);
+    }
+}
---
+++ src/test/java/kitchenpos/order/domain/OrderTest.java
@@ -0,0 +1,38 @@
+package kitchenpos.order.domain;
+
+import kitchenpos.order.exception.NotOrderLineItemsException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class OrderTest {
+
+    private String 주문상태 = OrderStatus.COOKING.name();
+    private OrderLineItem 주문수량메뉴1 = new OrderLineItem(1L, 1L, 1L, 1L);
+    private OrderLineItem 주문수량메뉴2 = new OrderLineItem(2L, 1L, 1L, 1L);
+    private List<OrderLineItem> 주문한내역들 = Arrays.asList(주문수량메뉴1, 주문수량메뉴2);
+
+    @DisplayName("주문을 생성한다.")
+    @Test
+    void createOrder() {
+        Order order = new Order(1L, 1L, 주문상태, LocalDateTime.now(), 주문한내역들);
+
+        assertThat(order.getOrderStatus()).isEqualTo(주문상태);
+        assertThat(order.getOrderLineItems().get(0)).isEqualTo(주문수량메뉴1);
+        assertThat(order.getOrderLineItems().get(1)).isEqualTo(주문수량메뉴2);
+    }
+
+    @DisplayName("주문 생성에 실패한다. - 주문 내역이 없으면 주문 등록에 실패한다.")
+    @Test
+    void fail_create() {
+        assertThatThrownBy(() -> new Order(1L, 1L, 주문상태, LocalDateTime.now(), Collections.emptyList()))
+                .isInstanceOf(NotOrderLineItemsException.class);
+    }
+}
