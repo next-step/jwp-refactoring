edae12792d7bd698c1bd31d66000cf7d0f466ac8
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-07-11 22:33:15 +0900

    refactor(TableGroupService): Event 추가

--- src/main/java/kitchenpos/tablegroup/application/TableGroupService.java
+++ src/main/java/kitchenpos/tablegroup/application/TableGroupService.java
@@ -1,23 +1,18 @@
 package kitchenpos.tablegroup.application;
 
 import kitchenpos.order.domain.OrderRepository;
-import kitchenpos.order.domain.OrderStatus;
-import kitchenpos.ordertable.domain.OrderTable;
 import kitchenpos.ordertable.domain.OrderTableRepository;
-import kitchenpos.ordertable.domain.OrderTables;
-import kitchenpos.ordertable.dto.OrderTableIdRequest;
 import kitchenpos.tablegroup.domain.TableGroup;
 import kitchenpos.tablegroup.domain.TableGroupRepository;
 import kitchenpos.tablegroup.dto.TableGroupRequest;
 import kitchenpos.tablegroup.dto.TableGroupResponse;
+import kitchenpos.tablegroup.event.TableGroupCreatedEvent;
+import kitchenpos.tablegroup.event.TableGroupUnlinkEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -25,60 +20,25 @@ public class TableGroupService {
     private final OrderRepository orderRepository;
     private final OrderTableRepository orderTableRepository;
     private final TableGroupRepository tableGroupRepository;
+    private final ApplicationEventPublisher publisher;
 
-    public TableGroupService(OrderRepository orderRepository, OrderTableRepository orderTableRepository, TableGroupRepository tableGroupRepository) {
+    public TableGroupService(OrderRepository orderRepository, OrderTableRepository orderTableRepository, TableGroupRepository tableGroupRepository, ApplicationEventPublisher publisher) {
         this.orderRepository = orderRepository;
         this.orderTableRepository = orderTableRepository;
         this.tableGroupRepository = tableGroupRepository;
+        this.publisher = publisher;
     }
 
     public TableGroupResponse create(TableGroupRequest tableGroupRequest) {
-        List<OrderTableIdRequest> orderTableIdRequests = generateOrderTableIdRequests(tableGroupRequest);
+        publisher.publishEvent(new TableGroupCreatedEvent(tableGroupRequest.getOrderTables()));
 
-        List<Long> orderTableIds = orderTableIdRequests.stream()
-                .map(OrderTableIdRequest::getId)
-                .collect(Collectors.toList());
-
-        List<OrderTable> savedOrderTables = orderTableRepository.findAllByIdIn(orderTableIds);
-
-        for (OrderTable savedOrderTable : savedOrderTables) {
-            savedOrderTable.changeEmpty(false);
-        }
-
-        TableGroup savedTableGroup = generateTableGroup(savedOrderTables);
+        TableGroup tableGroup = new TableGroup(LocalDateTime.now());
+        TableGroup savedTableGroup = tableGroupRepository.save(tableGroup);
 
         return TableGroupResponse.of(savedTableGroup);
     }
 
-    private TableGroup generateTableGroup(List<OrderTable> savedOrderTables) {
-        TableGroup tableGroup = new TableGroup(LocalDateTime.now());
-        TableGroup savedTableGroup = tableGroupRepository.save(tableGroup);
-
-        return savedTableGroup;
-    }
-
-    private List<OrderTableIdRequest> generateOrderTableIdRequests(TableGroupRequest tableGroupRequest) {
-        List<OrderTableIdRequest> orderTableIdRequests = tableGroupRequest.getOrderTables();
-
-        if (CollectionUtils.isEmpty(orderTableIdRequests) || orderTableIdRequests.size() < 2) {
-            throw new IllegalArgumentException();
-        }
-        return orderTableIdRequests;
-    }
-
     public void ungroup(final Long tableGroupId) {
-        final List<OrderTable> orderTableList = orderTableRepository.findAllByTableGroupId(tableGroupId);
-
-        OrderTables orderTables = new OrderTables(orderTableList);
-        List<Long> orderTableIds = orderTables.orderTableIds();
-
-        if (orderRepository.existsByOrderTableInAndOrderStatusIn(
-                orderTableIds, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))) {
-            throw new IllegalArgumentException();
-        }
-
-        for (final OrderTable orderTable : orderTableList) {
-            orderTable.unlinkTableGroup();
-        }
+        publisher.publishEvent(new TableGroupUnlinkEvent(tableGroupId));
     }
 }
