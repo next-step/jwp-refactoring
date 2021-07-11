41f70048b35209587360a677d93c3139967e1ad8
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-07-11 22:32:33 +0900

    refactor(OrderTables): 필요없는 코드 삭제

--- src/main/java/kitchenpos/ordertable/domain/OrderTables.java
+++ src/main/java/kitchenpos/ordertable/domain/OrderTables.java
@@ -12,24 +12,11 @@ public class OrderTables {
 
     public OrderTables(List<OrderTable> orderTables) {
         this.orderTables = orderTables;
-        validateOrderTableNotEmpty();
-    }
-
-    public void validateOrderTableNotEmpty() {
-        for (final OrderTable orderTable : orderTables) {
-            if (!orderTable.isEmpty() || Objects.nonNull(orderTable.getTableGroup())) {
-                throw new IllegalArgumentException();
-            }
-        }
     }
 
     public List<Long> orderTableIds() {
         return orderTables.stream()
                 .map(OrderTable::getId)
                 .collect(Collectors.toList());
-    }
-
-    public List<OrderTable> getOrderTables() {
-        return orderTables;
     }
 }
