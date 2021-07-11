ecc0919b266a23d6027a9373f9914d30a5bce627
HyungjuJang <dacapolife87@gmail.com>
2021-07-12 00:39:17 +0900

    refactor : 상위 익셉션을 처리하도록 변경

--- kitchenpos-web/src/main/java/kitchenpos/common/CommonControllerAdvice.java
+++ kitchenpos-web/src/main/java/kitchenpos/common/CommonControllerAdvice.java
@@ -1,32 +1,16 @@
 package kitchenpos.common;
 
-import kitchenpos.common.exception.InvalidPriceException;
-import kitchenpos.common.exception.InvalidQuantityException;
-import kitchenpos.menu.exception.NotExistMenuException;
-import kitchenpos.menu.exception.NotExistMenuGroupException;
-import kitchenpos.order.exception.NotExistOrderException;
-import kitchenpos.order.exception.UnableChangeEmptyStatusException;
-import kitchenpos.order.exception.UnableChangeOrderStatusException;
-import kitchenpos.order.exception.UnableCreateOrderException;
-import kitchenpos.product.exception.NotExistProductException;
-import kitchenpos.table.exception.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice(annotations = RestController.class)
 public class CommonControllerAdvice {
 
-    @ExceptionHandler({InvalidPriceException.class, InvalidQuantityException.class,
-            NotExistMenuException.class, NotExistMenuGroupException.class,
-            NotExistOrderException.class, UnableChangeEmptyStatusException.class,
-            UnableChangeOrderStatusException.class, UnableCreateOrderException.class,
-            NotExistProductException.class, InvalidNumberOfGeustsException.class,
-            NotExistOrderTableException.class, NotExistTableException.class,
-            UnableChangeEmptyOrderTableException.class, UnableChangeNumberOfGuestsException.class,
-            UnableCreateTableGroupException.class, UnableOrderCausedByEmptyTableException.class,
-            UnableUngroupStatusException.class
+    @ExceptionHandler({NoSuchElementException.class, IllegalArgumentException.class
     })
     public ResponseEntity handleIllegalArgsException(Exception e) {
         return ResponseEntity.badRequest().build();
