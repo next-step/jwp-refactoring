b59d2e71944d2cfe340b68a6f299ca8dc6ad770e
Lee-Chungsun <cndtjs0218@naver.com>
2021-07-11 22:26:39 +0900

    test : menugroup domain test

---
+++ src/test/java/kitchenpos/manu/domain/MenuGroupTest.java
@@ -0,0 +1,14 @@
+package kitchenpos.manu.domain;
+
+import kitchenposNew.menu.domain.MenuGroup;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MenuGroupTest {
+    @Test
+    void 메뉴_그룹_생성() {
+        MenuGroup menuGroup = new MenuGroup("분식메뉴");
+        assertThat(menuGroup).isEqualTo(new MenuGroup("분식메뉴"));
+    }
+}
