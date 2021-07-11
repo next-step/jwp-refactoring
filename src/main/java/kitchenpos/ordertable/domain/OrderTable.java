9cf39ca0d62dc2be6f5d22e8a4152ed6df5f5a91
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-07-11 22:31:52 +0900

    refactor(OrderTable): Exception 추가에 따른 변경

--- src/main/java/kitchenpos/ordertable/domain/OrderTable.java
+++ src/main/java/kitchenpos/ordertable/domain/OrderTable.java
@@ -1,5 +1,6 @@
 package kitchenpos.ordertable.domain;
 
+import kitchenpos.ordertable.exception.AlreadyHaveTableGroupException;
 import kitchenpos.tablegroup.domain.TableGroup;
 
 import javax.persistence.*;
@@ -45,7 +46,7 @@ public class OrderTable {
 
     public void validateTableGroupNonNull() {
         if (Objects.nonNull(this.getTableGroup())) {
-            throw new IllegalArgumentException();
+            throw new AlreadyHaveTableGroupException();
         }
     }
 
