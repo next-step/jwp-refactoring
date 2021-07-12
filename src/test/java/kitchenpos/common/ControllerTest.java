9cb1494029b0ff9828e134bd3aa24d0cdd5d32db
Jiseon Lee <leejoyc9310@gmail.com>
2021-07-12 20:39:02 +0900

    feat : JPA에서의 ControllerTest를 위해 @MockBean 추가

--- src/test/java/kitchenpos/common/ControllerTest.java
+++ src/test/java/kitchenpos/common/ControllerTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -18,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
+@MockBean(JpaMetamodelMappingContext.class)
 public abstract class ControllerTest<T> {
 
     private MockMvc mockMvc;
