8fb0b3cf010ebe02d5da4b2cf75b5b5cfb4d09c9
lenkim <joenggyu0@gmail.com>
2021-07-12 20:09:34 +0900

    feat: create Common Module
    - 기존의 존재하던 Exception Type 을 해당 모듈로 이동함.

--- common/src/main/java/kitchenpos/exception/InvalidPriceException.java
+++ common/src/main/java/kitchenpos/exception/InvalidPriceException.java
@@ -1,9 +1,5 @@
 package kitchenpos.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidPriceException extends IllegalArgumentException {
 
     public InvalidPriceException(String s) {
--- common/src/main/java/kitchenpos/exception/order/InvalidOrderStatusException.java
+++ common/src/main/java/kitchenpos/exception/order/InvalidOrderStatusException.java
@@ -1,9 +1,6 @@
 package kitchenpos.exception.order;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidOrderStatusException extends IllegalArgumentException {
 
     public InvalidOrderStatusException(String s) {
