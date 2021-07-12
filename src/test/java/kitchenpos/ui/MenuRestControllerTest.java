d88c82e1ef451bf9deb4500031116749aecd8fdd
younghoon <bblnotbb@yahho.com>
2021-07-13 01:47:04 +0900

    feat : implements menu rest controller test

--- src/test/java/kitchenpos/ui/MenuRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuRestControllerTest.java
@@ -1,10 +1,12 @@
 package kitchenpos.ui;
 
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
@@ -12,31 +14,31 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
-import kitchenpos.application.MenuGroupService;
-import kitchenpos.domain.MenuGroup;
+import kitchenpos.application.MenuService;
+import kitchenpos.domain.Menu;
 
-@WebMvcTest(MenuGroupRestController.class)
-public class MenuGroupRestControllerTest extends WebMvcTestConfiguration{
+@WebMvcTest(MenuRestController.class)
+public class MenuRestControllerTest extends WebMvcTestConfiguration {
 	@MockBean
-	private MenuGroupService menuGroupService;
+	private MenuService menuService;
 
 	@Test
 	void createTest() throws Exception {
 		// Given
-		when(menuGroupService.create(any())).thenReturn(new MenuGroup());
+		when(menuService.create(any())).thenReturn(new Menu());
 		// When, Then
-		mockMvc.perform(post("/api/menu-groups")
-			.content(objectMapper.writeValueAsString(new MenuGroup())).contentType(MediaType.APPLICATION_JSON))
+		mockMvc.perform(post("/api/menus")
+			.content(objectMapper.writeValueAsString(new Menu())).contentType(MediaType.APPLICATION_JSON))
 			.andDo(print())
 			.andExpect(status().isCreated());
 	}
 
 	@Test
-	void list() throws Exception {
+	void listTest() throws Exception {
 		// Given
-		when(menuGroupService.list()).thenReturn(Arrays.asList(new MenuGroup("1번 메뉴 그룹"), new MenuGroup("2번 메뉴 그룹")));
+		when(menuService.list()).thenReturn(Arrays.asList(new Menu("1번 메뉴", new BigDecimal(10000), 1L), new Menu("2번 메뉴", new BigDecimal(20000), 2L)));
 		// When, Then
-		mockMvc.perform(get("/api/menu-groups"))
+		mockMvc.perform(get("/api/menus"))
 			.andDo(print())
 			.andExpect(status().isOk());
 	}
