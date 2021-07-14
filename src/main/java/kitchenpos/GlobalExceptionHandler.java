df3e2d1b4ce96c4e09027810274ac3f4be7096a3
Jiseon Lee <leejoyc9310@gmail.com>
2021-07-14 20:43:24 +0900

    feat : CannotUpdateException 처리하도록 핸들러에 등록

--- src/main/java/kitchenpos/GlobalExceptionHandler.java
+++ src/main/java/kitchenpos/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package kitchenpos;
 
 import kitchenpos.exception.CannotFindException;
+import kitchenpos.exception.CannotUpdateException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,5 +18,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CannotFindException.class)
     public ResponseEntity<Void> handleCannotFindException(CannotFindException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @ExceptionHandler(CannotUpdateException.class)
+    public ResponseEntity<Void> handleCannotUpdateException(CannotUpdateException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 }
