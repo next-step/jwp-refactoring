59886d85b1a42a71204ec638f214091daafc25e0
younghoon <bblnotbb@yahho.com>
2021-07-16 05:19:24 +0900

    feat : specify menu group test

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -27,23 +28,27 @@ public class MenuGroupServiceTest {
 		menuGroupService = new MenuGroupService(menuGroupDao);
 	}
 
+	@DisplayName("메뉴 그룹을 등록한다.")
 	@Test
 	void createMenuGroup() {
 		// given
-		when(menuGroupDao.save(any())).thenReturn(new MenuGroup());
+		when(menuGroupDao.save(any())).thenReturn(new MenuGroup("메뉴 그룹 1번"));
 		// when
 		MenuGroup menuGroup = menuGroupService.create(new MenuGroup());
 		// then
-		assertThat(menuGroup).isEqualTo(new MenuGroup());
+		assertThat(menuGroup.getName()).isEqualTo("메뉴 그룹 1번");
 	}
 
+	@DisplayName("메뉴 그룹을 조회한다.")
 	@Test
 	void findMenuGroup() {
 		// given
-		when(menuGroupDao.findAll()).thenReturn(Arrays.asList(new MenuGroup(), new MenuGroup()));
+		when(menuGroupDao.findAll()).thenReturn(Arrays.asList(new MenuGroup("메뉴 그룹 1번"), new MenuGroup("메뉴 그룹 2번")));
 		// when
 		List<MenuGroup> menuGroups = menuGroupService.list();
 		// then
 		assertThat(menuGroups.size()).isEqualTo(2);
+		assertThat(menuGroups.get(0).getName()).isEqualTo("메뉴 그룹 1번");
+		assertThat(menuGroups.get(1).getName()).isEqualTo("메뉴 그룹 2번");
 	}
 }
