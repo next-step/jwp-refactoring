f2c7a95d242ab49c14ff9e7aab02a7c35908deed
kyle <cupeanimus@naver.com>
2021-07-17 07:45:03 +0900

    refactor : split api and domain project
    기존 프로젝트를 domain과 api 두 영역으로 나눈다.(내부 외부)

---
+++ module-api/src/main/java/kitchenpos/ModuleApiApplication.java
@@ -0,0 +1,13 @@
+package kitchenpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleApiApplication.class, args);
+    }
+
+}
---
+++ module-api/src/test/java/kitchenpos/ModuleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kitchenpos.moduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
