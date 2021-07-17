4061f261dc4571d5444f341e92f3f9c3ba580958
박소정 <psj6414@naver.com>
2021-07-17 16:34:35 +0900

    refactor: order 모듈 추가

---
+++ order/src/main/java/kitchenpos/order/domain/Order.java
@@ -0,0 +1,61 @@
+package kitchenpos.order.domain;
+
+import kitchenpos.table.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@AttributeOverride(name = "createdDate", column = @Column(name = "ORDERED_TIME"))
+public class Order extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderTableId;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    public Order() {}
+
+    public Order(Long id, Long orderTableId, OrderStatus orderStatus) {
+        this.id = id;
+        this.orderTableId = orderTableId;
+        this.orderStatus = orderStatus;
+        this.orderStatus = orderStatus;
+    }
+
+    public Order(Long orderTableId, OrderStatus orderStatus) {
+        this.orderTableId = orderTableId;
+        this.orderStatus = orderStatus;
+        this.orderStatus = orderStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getOrderTableId() {
+        return orderTableId;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void updateOrderStatus(final OrderStatus orderStatus) {
+        validCompletionStatus();
+        this.orderStatus = orderStatus;
+    }
+
+    public void validCompletionStatus() {
+        if (this.orderStatus == OrderStatus.COMPLETION) {
+            throw new IllegalArgumentException("완료된 주문은 상태를 변경할 수 없습니다.");
+        }
+    }
+
+    public boolean isOrderStatusCompletion() {
+        return orderStatus == OrderStatus.COMPLETION;
+    }
+}
---
+++ order/src/test/java/kitchenpos/order/domain/OrderTest.java
@@ -0,0 +1,91 @@
+package kitchenpos.order.domain;
+
+import kitchenpos.menu.domain.Menu;
+import kitchenpos.menu.domain.MenuGroup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import kitchenpos.table.domain.OrderTable;
+import kitchenpos.table.domain.TableGroup;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("주문 테이블 관련 기능 테스트")
+public class OrderTest {
+    private TableGroup tableGroup;
+    private Menu menu;
+    private OrderTable orderTable;
+    private OrderLineItem orderLineItem;
+    @BeforeEach
+    void setUp() {
+        tableGroup = new TableGroup(1L);
+        menu = new Menu(1L, "뿌링클치즈볼", new BigDecimal(18000), new MenuGroup());
+
+        orderTable = 주문테이블_생성(1L, tableGroup, 3, false);
+        orderLineItem = 주문_목록_생성(1L, menu.getId(), 3L);
+    }
+
+    @DisplayName("주문을 등록한다.")
+    @Test
+    void create() {
+        Order createdOrder = 주문_생성_요청(orderTable, OrderStatus.COOKING);
+
+        주문_등록됨(createdOrder);
+    }
+
+    @DisplayName("주문 상태를 변경한다.")
+    @Test
+    void changedStatus() {
+        Order createdOrder = 주문_생성_요청(orderTable, OrderStatus.COOKING);
+
+        주문_상태_변경_요청(createdOrder, OrderStatus.MEAL);
+
+        주문_상태_변경됨(createdOrder);
+    }
+
+    @DisplayName("주문이 완료된 경우 상태변경이 불가능하다.")
+    @Test
+    void changeException() {
+        Order createdOrder = 주문_생성_요청(orderTable, OrderStatus.COMPLETION);
+
+        완료된_주문_변경시_예외_발생(createdOrder, OrderStatus.COOKING);
+    }
+
+    private void 주문_등록됨(Order createdOrder) {
+        assertThat(createdOrder).isNotNull();
+        assertThat(createdOrder.getOrderStatus()).isEqualTo(OrderStatus.COOKING);
+    }
+
+    public static OrderTable 주문테이블_생성(Long id, TableGroup tableGroup, int numberOfGuest, boolean empty) {
+        return new OrderTable(id, tableGroup, numberOfGuest, empty);
+    }
+
+
+    private void 주문_상태_변경됨(Order createdOrder) {
+        assertThat(createdOrder.getOrderStatus()).isEqualTo(OrderStatus.MEAL);
+    }
+
+
+    public static OrderLineItem 주문_목록_생성(Long seq, Long menuId, Long quantity) {
+        return new OrderLineItem(seq, menuId, quantity);
+    }
+
+    public static Order 주문_생성_요청(OrderTable orderTable, OrderStatus orderStatus) {
+        return new Order(orderTable.getId(), orderStatus);
+    }
+
+    private void 완료된_주문_변경시_예외_발생(Order createdOrder, OrderStatus cooking) {
+        assertThatThrownBy(() -> {
+            createdOrder.updateOrderStatus(cooking);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("완료된 주문은 상태를 변경할 수 없습니다.");
+    }
+
+    private void 주문_상태_변경_요청(Order createdOrder, OrderStatus meal) {
+        createdOrder.updateOrderStatus(meal);
+    }
+
+}
