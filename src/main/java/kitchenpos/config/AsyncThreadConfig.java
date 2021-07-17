c51a0f83bf344f1265589be8dbb4fc14d1734a52
ttuop <plintcsz@gmail.com>
2021-07-17 23:32:51 +0900

    feat: 비동기 설정 추가

---
+++ src/main/java/kitchenpos/config/AsyncThreadConfig.java
@@ -0,0 +1,24 @@
+package kitchenpos.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncThreadConfig {
+
+    @Bean
+    public Executor asyncThreadTaskExecutor() {
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(200);
+        executor.setThreadNamePrefix("kitchenpos-");
+
+        return executor;
+    }
+}
