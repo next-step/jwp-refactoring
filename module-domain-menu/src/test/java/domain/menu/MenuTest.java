516a3140c0387c0e3f7d36622946f5e64793a334
mskangg <funnycoding91@gmail.com>
2021-07-17 15:08:55 +0900

    test: add domain-menu test code

--- module-domain-menu/src/test/java/domain/menu/MenuTest.java
+++ module-domain-menu/src/test/java/domain/menu/MenuTest.java
@@ -2,14 +2,36 @@ package domain.menu;
 
 import common.valueobject.exception.InvalidNameException;
 import common.valueobject.exception.NegativePriceException;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MenuTest {
+
+    @Test
+    void create() {
+        //when
+        MenuGroup menuGroup = MenuGroup.of("1인세트");
+        BigDecimal menuPrice = BigDecimal.valueOf(8000L);
+        Product product1 = Product.of("제육볶음", BigDecimal.valueOf(8000L));
+        Product product2 = Product.of("콜라", BigDecimal.valueOf(1000L));
+        MenuProduct menuProduct1 = MenuProduct.of(product1, 1);
+        MenuProduct menuProduct2 = MenuProduct.of(product2, 1);
+
+        //then
+        Menu menu = Menu.create("제육+음료세트", menuPrice, menuGroup, Lists.list(menuProduct1, menuProduct2));
+
+        //then
+        assertThat(menu.getMenuGroup()).isEqualTo(menuGroup);
+        assertThat(menu.getMenuProducts()).contains(menuProduct1, menuProduct2);
+        assertThat(menu.getName().getValue()).isEqualTo("제육+음료세트");
+        assertThat(menu.getPrice().getValue()).isEqualTo(BigDecimal.valueOf(8000L));
+    }
 
     @DisplayName("메뉴의 총 가격은 0원 이상이어야한다.")
     @Test
