958ad2b33abe577738db85fb487fb5ac37971303
박소정 <psj6414@naver.com>
2021-07-17 16:34:51 +0900

    refactor: product 모듈 추가

---
+++ product/src/main/java/kitchenpos/product/domain/Product.java
@@ -0,0 +1,45 @@
+package kitchenpos.product.domain;
+
+import kitchenpos.product.common.Name;
+import kitchenpos.product.common.Price;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Name name;
+
+    @Embedded
+    private Price price;
+
+    public Product() {}
+
+    public Product(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = new Name(name);
+        this.price = new Price(price);
+    }
+
+    public Product(String name, BigDecimal price) {
+        this.name = new Name(name);
+        this.price = new Price(price);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name.getName();
+    }
+
+    public BigDecimal getPrice() {
+        return price.getPrice();
+    }
+}
