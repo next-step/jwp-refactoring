cb590c5bb096ccdf7591109911292c36e301e83d
ttuop <plintcsz@gmail.com>
2021-07-18 14:38:24 +0900

    refactor: 각 모듈별 소스와 리소스 파일 이동

---
+++ module-common/src/test/java/kitchenpos/common/domain/PriceTest.java
@@ -0,0 +1,46 @@
+package kitchenpos.common.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PriceTest {
+
+    @DisplayName("가격 도메인 유효성 체크 테스트")
+    @ParameterizedTest
+    @MethodSource("providePrice")
+    void validate(final BigDecimal price) {
+        // when
+        final Throwable throwable = catchThrowable(() -> new Price(price));
+
+        // then
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> providePrice() {
+        return Stream.of(
+            Arguments.of((Object)null),
+            Arguments.of(new BigDecimal(-1))
+        );
+    }
+
+    @DisplayName("숫자 크기를 비교하는 테스트 ")
+    @Test
+    void greaterThan() {
+        // given
+        final Price price = new Price(new BigDecimal(100));
+
+        // when
+        final boolean actual = price.greaterThan(new BigDecimal(10));
+
+        // then
+        assertThat(actual).isEqualTo(true);
+    }
+}
---
+++ module-order/src/test/java/kitchenpos/ApplicationTest.java
@@ -0,0 +1,15 @@
+package kitchenpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootConfiguration
+@EnableAutoConfiguration
+public class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
