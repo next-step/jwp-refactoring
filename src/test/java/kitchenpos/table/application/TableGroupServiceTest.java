1ef32237e4b650d7e256889fd408686e5ef27785
mwkwon <mwkwon0110@gmail.com>
2021-07-18 14:01:12 +0900

    bugfix: 테스트 코드 실패 케이스 수정

--- src/test/java/kitchenpos/table/application/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/table/application/TableGroupServiceTest.java
@@ -28,6 +28,9 @@ import static org.mockito.Mockito.when;
 class TableGroupServiceTest {
 
     @Mock
+    private TableService tableService;
+
+    @Mock
     private TableGroupRepository tableGroupRepository;
 
     @Mock
@@ -48,9 +51,8 @@ class TableGroupServiceTest {
     @Test
     void 테이블_그룹_생성() {
         LocalDateTime now = LocalDateTime.now();
-
         OrderTables orderTables = new OrderTables(Arrays.asList(일번_테이블, 이번_테이블));
-
+        when(tableService.findAllByIds(Arrays.asList(1L, 2L))).thenReturn(orderTables);
         TableGroup tableGroup = new TableGroup(1L, now, orderTables);
         when(tableGroupRepository.save(any(TableGroup.class))).thenReturn(tableGroup);
         TableGroupResponse expected = tableGroupService.create(new TableGroupRequest(Arrays.asList(1L, 2L)));
