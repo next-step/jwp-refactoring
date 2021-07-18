fec48708c0318553b958ffc8b3dbc4bf81c225a1
younghoon <bblnotbb@yahho.com>
2021-07-18 17:55:00 +0900

    fix : fix errors in order acceptance test

--- src/test/java/kitchenpos/order/acceptance/OrderAcceptanceTest.java
+++ src/test/java/kitchenpos/order/acceptance/OrderAcceptanceTest.java
@@ -19,12 +19,17 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kitchenpos.AcceptanceTest;
 import kitchenpos.menu.dto.MenuRequest;
+import kitchenpos.menu.dto.MenuResponse;
 import kitchenpos.menuGroup.dto.MenuGroupRequest;
 import kitchenpos.menu.dto.MenuProductRequest;
+import kitchenpos.menuGroup.dto.MenuGroupResponse;
 import kitchenpos.order.dto.OrderRequest;
+import kitchenpos.order.dto.OrderResponse;
+import kitchenpos.product.dto.ProductResponse;
 import kitchenpos.table.dto.OrderLineItemRequest;
 import kitchenpos.table.dto.OrderTableRequest;
 import kitchenpos.product.dto.ProductRequest;
+import kitchenpos.table.dto.OrderTableResponse;
 
 public class OrderAcceptanceTest extends AcceptanceTest {
 	@DisplayName("주문 등록, 상태변경 조회 시나리오")
@@ -33,28 +38,28 @@ public class OrderAcceptanceTest extends AcceptanceTest {
 		// Backgroud
 		// Given
 		ExtractableResponse<Response> menuGroupResponse = createMenuGroup(new MenuGroupRequest("인기 메뉴"));
-		MenuGroupRequest menuGroup = menuGroupResponse.as(MenuGroupRequest.class);
+		MenuGroupResponse menuGroup = menuGroupResponse.as(MenuGroupResponse.class);
 		// And
 		ExtractableResponse<Response> productResponse = createProduct(new ProductRequest("매운 라면", 8000L));
-		ProductRequest product = productResponse.as(ProductRequest.class);
+		ProductResponse product = productResponse.as(ProductResponse.class);
 		// And
 		ExtractableResponse<Response> menuCreatedResponse = createMenu(new MenuRequest("라면 메뉴", new BigDecimal(8000), menuGroup.getId(), Arrays.asList(new MenuProductRequest(product.getId(), 2L))));
-		MenuRequest createdMenu = menuCreatedResponse.as(MenuRequest.class);
+		MenuResponse createdMenu = menuCreatedResponse.as(MenuResponse.class);
 		// And
 		ExtractableResponse<Response> tableCreatedResponse = createOrderTable(new OrderTableRequest(2, false));
-		OrderTableRequest createdOrderTable = tableCreatedResponse.as(OrderTableRequest.class);
+		OrderTableResponse createdOrderTable = tableCreatedResponse.as(OrderTableResponse.class);
 
 		// Scenario
 		// When
 		ExtractableResponse<Response> orderCreatedResponse = createOrder(new OrderRequest(createdOrderTable.getId(), "COOKING", LocalDateTime.now(), Arrays.asList(new OrderLineItemRequest(createdMenu.getId(), 2))));
-		OrderRequest createdOrder = orderCreatedResponse.as(OrderRequest.class);
+		OrderResponse createdOrder = orderCreatedResponse.as(OrderResponse.class);
 		// Then
 		assertThat(orderCreatedResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 		assertThat(createdOrder.getOrderStatus()).isEqualTo("COOKING");
 
 		// When
 		ExtractableResponse<Response> changeOrderResponse = changeOrderStatus(createdOrder.getId(), new OrderRequest("MEAL"));
-		OrderRequest statusChangedOrder = changeOrderResponse.as(OrderRequest.class);
+		OrderResponse statusChangedOrder = changeOrderResponse.as(OrderResponse.class);
 		// Then
 		assertThat(changeOrderResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
 		assertThat(statusChangedOrder.getOrderStatus()).isEqualTo("MEAL");
@@ -76,16 +81,16 @@ public class OrderAcceptanceTest extends AcceptanceTest {
 		// Backgroud
 		// Given
 		ExtractableResponse<Response> menuGroupResponse = createMenuGroup(new MenuGroupRequest("인기 메뉴"));
-		MenuGroupRequest menuGroup = menuGroupResponse.as(MenuGroupRequest.class);
+		MenuGroupResponse menuGroup = menuGroupResponse.as(MenuGroupResponse.class);
 		// And
 		ExtractableResponse<Response> productResponse = createProduct(new ProductRequest("매운 라면", 8000L));
-		ProductRequest product = productResponse.as(ProductRequest.class);
+		ProductResponse product = productResponse.as(ProductResponse.class);
 		// And
 		ExtractableResponse<Response> menuCreatedResponse = createMenu(new MenuRequest("라면 메뉴", new BigDecimal(8000), menuGroup.getId(), Arrays.asList(new MenuProductRequest(product.getId(), 2L))));
-		MenuRequest createdMenu = menuCreatedResponse.as(MenuRequest.class);
+		MenuResponse createdMenu = menuCreatedResponse.as(MenuResponse.class);
 		// And
 		ExtractableResponse<Response> tableCreatedResponse = createOrderTable(new OrderTableRequest(2, false));
-		OrderTableRequest createdOrderTable = tableCreatedResponse.as(OrderTableRequest.class);
+		OrderTableResponse createdOrderTable = tableCreatedResponse.as(OrderTableResponse.class);
 
 		// Scenario
 		// When
@@ -110,7 +115,7 @@ public class OrderAcceptanceTest extends AcceptanceTest {
 
 		// Given : 주문 등록
 		ExtractableResponse<Response> orderCreatedResponse = createOrder(new OrderRequest(createdOrderTable.getId(), "COOKING", LocalDateTime.now(), Arrays.asList(new OrderLineItemRequest(createdMenu.getId(), 2))));
-		OrderRequest createdOrder = orderCreatedResponse.as(OrderRequest.class);
+		OrderResponse createdOrder = orderCreatedResponse.as(OrderResponse.class);
 		// When
 		ExtractableResponse<Response> changeStatusWithNotExistsStatus = changeOrderStatus(createdOrder.getId(), new OrderRequest("PREPARING"));
 		// Then
@@ -118,7 +123,7 @@ public class OrderAcceptanceTest extends AcceptanceTest {
 
 		// Given
 		ExtractableResponse<Response> changeOrderResponse = changeOrderStatus(createdOrder.getId(), new OrderRequest("COMPLETION"));
-		OrderRequest statusChangedOrder = changeOrderResponse.as(OrderRequest.class);
+		OrderResponse statusChangedOrder = changeOrderResponse.as(OrderResponse.class);
 		// When
 		ExtractableResponse<Response> changeStatusWithCompletedOrder = changeOrderStatus(createdOrder.getId(), new OrderRequest("MEAL"));
 		// Then
