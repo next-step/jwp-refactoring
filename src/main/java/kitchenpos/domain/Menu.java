3027a2bdadfffdd34669f1603c10a1969fd0d839
IW.MOON <mooniw02@gmail.com>
2021-12-07 00:17:22 +0900

    feat: equals & hash code 재정의

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -1,7 +1,9 @@
 package kitchenpos.domain;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Menu {
     private Long id;
@@ -9,6 +11,22 @@ public class Menu {
     private BigDecimal price;
     private Long menuGroupId;
     private List<MenuProduct> menuProducts;
+
+    public Menu() {
+    }
+
+    public Menu(String name, BigDecimal price, Long menuGroupId) {
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+    }
+
+    public void addMenuProduct(MenuProduct menuProduct) {
+        if(menuProducts == null){
+            menuProducts = new ArrayList<>();
+        }
+        menuProducts.add(menuProduct);
+    }
 
     public Long getId() {
         return id;
@@ -48,5 +66,18 @@ public class Menu {
 
     public void setMenuProducts(final List<MenuProduct> menuProducts) {
         this.menuProducts = menuProducts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Menu menu = (Menu) o;
+        return Objects.equals(id, menu.id) && Objects.equals(name, menu.name) && Objects.equals(price, menu.price) && Objects.equals(menuGroupId, menu.menuGroupId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, menuGroupId, menuProducts);
     }
 }
