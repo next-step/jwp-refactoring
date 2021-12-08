3194821c6d2fcd1f3a1cc9d8a67e98fad28d3c52
cus4703 <cus4703@gmail.com>
2021-12-09 00:40:55 +0900

    refactor(MenuServiceTest): mock 설정 코드량 간소화

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -9,8 +9,8 @@ import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
@@ -18,14 +18,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import static kitchenpos.application.MenuGroupServiceTest.menuGroup;
 import static kitchenpos.application.ProductServiceTest.product;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.*;
 
 class MenuServiceTest {
@@ -35,121 +33,6 @@ class MenuServiceTest {
     private MenuProductDao menuProductDao;
     private ProductDao productDao;
     private MenuService menuService;
-
-    @BeforeEach
-    void setUp() {
-        menuDao = mock(MenuDao.class);
-        menuGroupDao = mock(MenuGroupDao.class);
-        menuProductDao = mock(MenuProductDao.class);
-        productDao = mock(ProductDao.class);
-        menuService = new MenuService(menuDao, menuGroupDao, menuProductDao, productDao);
-    }
-
-    static Stream<Arguments> baseDB() {
-        final Menu menu = menu(1L, "후라이드치킨", BigDecimal.valueOf(15000), 1L);
-        final MenuGroup menuGroup = menuGroup(1L, "후라이드 한마리");
-        final Product product = product(1L, "후라이드", BigDecimal.valueOf(15000));
-        final MenuProduct menuProduct = menuProduct(1L, menu, product, 1L);
-        menu.setMenuProducts(new ArrayList<>(Arrays.asList(menuProduct)));
-
-        return Stream.of(arguments(menu, menuGroup, product, menuProduct));
-    }
-
-    @ParameterizedTest
-    @MethodSource("baseDB")
-    void 메뉴_생성(Menu menu, MenuGroup menuGroup, Product product, MenuProduct menuProduct) {
-        // given
-        when(menuGroupDao.existsById(anyLong())).thenReturn(true);
-        when(productDao.findById(anyLong())).thenReturn(Optional.of(product));
-        when(menuDao.save(any(Menu.class))).thenReturn(menu);
-        when(menuProductDao.save(any(MenuProduct.class))).thenReturn(menuProduct);
-
-        // when
-        Menu actual = menuService.create(menu);
-
-        // then
-        assertAll(
-                () -> assertThat(actual).isEqualTo(menu),
-                () -> assertThat(actual.getMenuGroupId()).isEqualTo(menuGroup.getId()),
-                () -> assertThat(actual.getMenuProducts()).contains(menuProduct)
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("baseDB")
-    void 메뉴_생성_메뉴그룹_존재하지_않음(Menu menu, MenuGroup menuGroup, Product product, MenuProduct menuProduct) {
-        // given
-        when(menuGroupDao.existsById(anyLong())).thenReturn(false);
-        when(productDao.findById(anyLong())).thenReturn(Optional.of(product));
-        when(menuDao.save(any(Menu.class))).thenReturn(menu);
-        when(menuProductDao.save(any(MenuProduct.class))).thenReturn(menuProduct);
-
-        // when
-        assertThatThrownBy(() -> {
-            Menu actual = menuService.create(menu);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @MethodSource("baseDB")
-    void 메뉴_생성_상품_존재하지_않음(Menu menu, MenuGroup menuGroup, Product product, MenuProduct menuProduct) {
-        // given
-        when(menuGroupDao.existsById(anyLong())).thenReturn(true);
-        when(productDao.findById(anyLong())).thenReturn(Optional.empty());
-        when(menuDao.save(any(Menu.class))).thenReturn(menu);
-        when(menuProductDao.save(any(MenuProduct.class))).thenReturn(menuProduct);
-
-        // when
-        assertThatThrownBy(() -> {
-            Menu actual = menuService.create(menu);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @MethodSource("baseDB")
-    void 메뉴_생성_금액_오류(Menu menu, MenuGroup menuGroup, Product product, MenuProduct menuProduct) {
-        // given
-        product.setPrice(BigDecimal.valueOf(0));
-        when(menuGroupDao.existsById(anyLong())).thenReturn(true);
-        when(productDao.findById(anyLong())).thenReturn(Optional.of(product));
-        when(menuDao.save(any(Menu.class))).thenReturn(menu);
-        when(menuProductDao.save(any(MenuProduct.class))).thenReturn(menuProduct);
-
-        // when
-        assertThatThrownBy(() -> {
-            Menu actual = menuService.create(menu);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @MethodSource("baseDB")
-    void 메뉴_내역_조회() {
-        // given
-        final Menu menu = menu(1L, "후라이드치킨", BigDecimal.valueOf(15000), 1L);
-        final MenuGroup menuGroup = menuGroup(1L, "후라이드 한마리");
-        final Product product = product(1L, "후라이드", BigDecimal.valueOf(1000));
-        final MenuProduct menuProduct = menuProduct(1L, menu, product, 1L);
-        menu.setMenuProducts(new ArrayList<>(Arrays.asList(menuProduct)));
-        when(menuDao.findAll()).thenReturn(new ArrayList<>(Arrays.asList(menu)));
-
-        List<Menu> actual = menuService.list();
-
-        assertAll(
-                () -> assertThat(actual).contains(menu),
-                () -> assertThat(actual).hasSize(1)
-        );
-
-    }
-
-    public static Menu menu(Long id, String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
-        Menu menu = new Menu();
-        menu.setId(id);
-        menu.setName(name);
-        menu.setPrice(price);
-        menu.setMenuGroupId(menuGroupId);
-        menu.setMenuProducts(menuProducts);
-        return menu;
-    }
 
     public static Menu menu(Long id, String name, BigDecimal price, Long menuGroupId) {
         Menu menu = new Menu();
@@ -167,5 +50,104 @@ class MenuServiceTest {
         menuProduct.setProductId(product.getId());
         menuProduct.setQuantity(quantity);
         return menuProduct;
+    }
+
+    @BeforeEach
+    void setUp() {
+        menuDao = mock(MenuDao.class);
+        menuGroupDao = mock(MenuGroupDao.class);
+        menuProductDao = mock(MenuProductDao.class);
+        productDao = mock(ProductDao.class);
+        menuService = new MenuService(menuDao, menuGroupDao, menuProductDao, productDao);
+    }
+
+    Menu setMockData(boolean groupStatus) {
+        final Menu menu = menu(1L, "후라이드치킨", BigDecimal.valueOf(15000), 1L);
+        final MenuGroup menuGroup = menuGroup(1L, "후라이드 한마리");
+        final Product product = product(1L, "후라이드", BigDecimal.valueOf(15000));
+        final MenuProduct menuProduct = menuProduct(1L, menu, product, 1L);
+        menu.setMenuProducts(new ArrayList<>(Arrays.asList(menuProduct)));
+
+        when(menuGroupDao.existsById(anyLong())).thenReturn(groupStatus);
+        when(productDao.findById(anyLong())).thenReturn(Optional.of(product));
+        when(menuDao.save(any(Menu.class))).thenReturn(menu);
+        when(menuProductDao.save(any(MenuProduct.class))).thenReturn(menuProduct);
+
+        return menu;
+    }
+
+    @Test
+    void 메뉴_생성() {
+        // given
+        boolean isExistsMenuGroup = true;
+        Menu menu = setMockData(isExistsMenuGroup);
+
+        // when
+        Menu actual = menuService.create(menu);
+
+        // then
+        assertAll(
+                () -> assertThat(actual).isEqualTo(menu),
+                () -> assertThat(actual.getMenuGroupId()).isEqualTo(menu.getMenuGroupId()),
+                () -> assertThat(actual.getMenuProducts()).containsAll(menu.getMenuProducts())
+        );
+    }
+
+    @Test
+    void 메뉴_생성_메뉴그룹_존재하지_않음() {
+        // given
+        boolean isExistsMenuGroup = false;
+        Menu menu = setMockData(isExistsMenuGroup);
+
+        // when
+        assertThatThrownBy(() -> {
+            Menu actual = menuService.create(menu);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴_생성_상품_존재하지_않음() {
+        // given
+        boolean isExistsMenuGroup = true;
+        Menu menu = setMockData(isExistsMenuGroup);
+        when(productDao.findById(anyLong())).thenReturn(Optional.empty());
+
+        // when
+        assertThatThrownBy(() -> {
+            Menu actual = menuService.create(menu);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴_생성_상품_금액_오류() {
+        // given
+        boolean isExistsMenuGroup = true;
+        Menu menu = setMockData(isExistsMenuGroup);
+        final Product product = product(1L, "후라이드", BigDecimal.valueOf(1000));
+        when(productDao.findById(anyLong())).thenReturn(Optional.of(product));
+
+        // when
+        assertThatThrownBy(() -> {
+            Menu actual = menuService.create(menu);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴_내역_조회() {
+        // given
+        final Menu menu = menu(1L, "후라이드치킨", BigDecimal.valueOf(15000), 1L);
+        final MenuGroup menuGroup = menuGroup(1L, "후라이드 한마리");
+        final Product product = product(1L, "후라이드", BigDecimal.valueOf(1000));
+        final MenuProduct menuProduct = menuProduct(1L, menu, product, 1L);
+        menu.setMenuProducts(new ArrayList<>(Arrays.asList(menuProduct)));
+        when(menuDao.findAll()).thenReturn(new ArrayList<>(Arrays.asList(menu)));
+
+        List<Menu> actual = menuService.list();
+
+        assertAll(
+                () -> assertThat(actual).contains(menu),
+                () -> assertThat(actual).hasSize(1)
+        );
+
     }
 }
