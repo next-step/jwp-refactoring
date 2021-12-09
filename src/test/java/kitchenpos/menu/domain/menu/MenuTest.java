2c818689a393a09c117840cfa840ac7feb0a4a57
cus4703 <cus4703@gmail.com>
2021-12-10 01:09:08 +0900

    feat(MenuTest): 메뉴 테스트 작성

--- src/test/java/kitchenpos/menu/domain/menu/MenuTest.java
+++ src/test/java/kitchenpos/menu/domain/menu/MenuTest.java
@@ -1,5 +1,6 @@
-package kitchenpos.menu.domain.product;
+package kitchenpos.menu.domain.menu;
 
+import kitchenpos.menu.domain.menugroup.MenuGroup;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -7,70 +8,75 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static kitchenpos.menu.domain.fixture.ProductDomainFixture.product;
+import static kitchenpos.menu.domain.fixture.MenuGroupDomainFixture.menuGroup;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("상품 관리")
-class ProductTest {
+@DisplayName("메뉴 관리")
+class MenuTest {
 
     @Nested
-    @DisplayName("상품 생성")
-    class CreateProduct {
+    @DisplayName("메뉴 생성")
+    class CreateMenu {
         @Test
         @DisplayName("성공")
         public void create() {
             // given
-            String name = "양념 치킨";
+            String name = "후라이드 치킨";
             BigDecimal price = BigDecimal.valueOf(15000);
+            MenuGroup menuGroup = menuGroup("치킨 세트");
 
             // when
-            Product actual = product(name, price);
+            Menu actual = new Menu(name, price, menuGroup);
 
             // then
             assertAll(
                     () -> assertThat(actual.getName()).isEqualTo(name),
-                    () -> assertThat(actual.getProductPrice().getPrice()).isEqualTo(price)
+                    () -> assertThat(actual.getMenuPrice().getPrice()).isEqualTo(price),
+                    () -> assertThat(actual.getMenuGroup()).isEqualTo(menuGroup)
             );
         }
 
         @Test
-        @DisplayName("실패 - 상품명이 없음")
+        @DisplayName("실패 - 메뉴명 없음")
         public void failNameEmpty() {
             // given
             String name = "";
             BigDecimal price = BigDecimal.valueOf(15000);
+            MenuGroup menuGroup = menuGroup("치킨 세트");
 
             // when
             Assertions.assertThatThrownBy(() -> {
-                Product actual = product(name, price);
+                Menu actual = new Menu(name, price, menuGroup);
             }).isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
-        @DisplayName("실패 - 금액이 없음")
-        public void failPriceEmpty() {
+        @DisplayName("실패 - 메뉴 그룹 없음")
+        public void failMenuGroupEmpty() {
             // given
-            String name = "양념 치킨";
+            String name = "후라이드 치킨";
+            BigDecimal price = BigDecimal.valueOf(15000);
+            MenuGroup menuGroup = null;
 
             // when
             Assertions.assertThatThrownBy(() -> {
-                Product actual = product(name, null);
+                Menu actual = new Menu(name, price, menuGroup);
             }).isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
-        @DisplayName("실패 - 상품의 가격이 0보다 작음")
+        @DisplayName("실패 - 잘못된 메뉴 가격 입력")
         public void failPriceIllegal() {
             // given
-            String name = "양념 치킨";
+            String name = "후라이드 치킨";
             BigDecimal price = BigDecimal.valueOf(-1);
+            MenuGroup menuGroup = menuGroup("치킨 세트");
 
             // when
             Assertions.assertThatThrownBy(() -> {
-                Product actual = product(name, price);
+                Menu actual = new Menu(name, price, menuGroup);
             }).isInstanceOf(IllegalArgumentException.class);
         }
     }
-
 }
