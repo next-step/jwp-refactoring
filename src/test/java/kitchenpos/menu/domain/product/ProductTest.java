1d136f4bc9e063005a7afbba5bdd46c51ba1d7f4
cus4703 <cus4703@gmail.com>
2021-12-10 00:45:53 +0900

    feat(test): Product 테스트 작성

---
+++ src/test/java/kitchenpos/menu/domain/product/ProductTest.java
@@ -0,0 +1,74 @@
+package kitchenpos.menu.domain.product;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("상품 관리")
+class ProductTest {
+
+    @Nested
+    class CreateProduct {
+        @Test
+        @DisplayName("성공")
+        public void create() {
+            // given
+            String name = "양념 치킨";
+            BigDecimal price = BigDecimal.valueOf(15000);
+
+            // when
+            Product actual = new Product(name, price);
+
+            // then
+            assertAll(
+                    () -> assertThat(actual.getName()).isEqualTo(name),
+                    () -> assertThat(actual.getProductPrice().getPrice()).isEqualTo(price)
+            );
+        }
+
+        @Test
+        @DisplayName("실패 - 상품명이 없음")
+        public void failNameEmpty() {
+            // given
+            String name = "";
+            BigDecimal price = BigDecimal.valueOf(15000);
+
+            // when
+            Assertions.assertThatThrownBy(() -> {
+                Product actual = new Product(name, price);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("실패 - 금액이 없음")
+        public void failPriceEmpty() {
+            // given
+            String name = "양념 치킨";
+
+            // when
+            Assertions.assertThatThrownBy(() -> {
+                Product actual = new Product(name, null);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("실패 - 상품의 가격이 0보다 작음")
+        public void failPriceIllegal() {
+            // given
+            String name = "양념 치킨";
+            BigDecimal price = BigDecimal.valueOf(-1);
+
+            // when
+            Assertions.assertThatThrownBy(() -> {
+                Product actual = new Product(name, price);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+}
