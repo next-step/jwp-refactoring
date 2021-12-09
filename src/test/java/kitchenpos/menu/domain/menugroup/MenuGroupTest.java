a97233e431933db20d5aff2452553759308a2a04
cus4703 <cus4703@gmail.com>
2021-12-10 00:31:23 +0900

    feat(test): MenuGroup 테스트 작성

---
+++ src/test/java/kitchenpos/menu/domain/menugroup/MenuGroupTest.java
@@ -0,0 +1,43 @@
+package kitchenpos.menu.domain.menugroup;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("메뉴 그룹 관리")
+class MenuGroupTest {
+
+    @Nested
+    @DisplayName("메뉴 그룹 생성")
+    class CreateMenuGroup {
+
+        @Test
+        @DisplayName("성공")
+        public void create() {
+            // given
+            String expected = "치킨 세트";
+
+            // when
+            MenuGroup actual = new MenuGroup(expected);
+
+            // then
+            assertThat(actual.getName()).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("실패 - 그룹 메뉴명 없음.")
+        public void fail() {
+            // given
+            String expected = "";
+
+            // when
+            assertThatThrownBy(() -> {
+                MenuGroup actual = new MenuGroup(expected);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
+
+    }
+}
