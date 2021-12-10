89f5da20692ad807f08607d551d4f4f210bb1e03
cus4703 <cus4703@gmail.com>
2021-12-11 01:14:07 +0900

    feat(Menu): 메뉴 인수 테스트 작성

--- src/test/java/kitchenpos/menu/domain/menu/MenuAcceptanceTest.java
+++ src/test/java/kitchenpos/menu/domain/menu/MenuAcceptanceTest.java
@@ -1,84 +1,105 @@
 package kitchenpos.menu.domain.menu;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import kitchenpos.menu.domain.menugroup.MenuGroup;
-import kitchenpos.menu.dto.MenuRequest;
+import kitchenpos.menu.dto.*;
+import kitchenpos.utils.AcceptanceTest;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static kitchenpos.menu.domain.fixture.MenuAcceptanceFixture.메뉴_생성;
-import static kitchenpos.menu.domain.fixture.MenuDomainFixture.후라이드_치킨;
-import static kitchenpos.menu.domain.fixture.MenuGroupDomainFixture.menuGroup;
+import static kitchenpos.menu.domain.fixture.MenuDomainFixture.메뉴_생성_요청;
+import static kitchenpos.menu.domain.fixture.MenuGroupDomainFixture.*;
+import static kitchenpos.menu.domain.fixture.ProductDomainFixture.*;
+import static kitchenpos.utils.AcceptanceFixture.응답_BAD_REQUEST;
+import static kitchenpos.utils.AcceptanceFixture.응답_CREATE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("메뉴 관리")
-class MenuAcceptanceTest {
+class MenuAcceptanceTest extends AcceptanceTest {
 
-//    @Nested
-//    @DisplayName("인수 테스트 - 메뉴 생성")
-//    class CreateMenu {
-//        @Test
-//        @DisplayName("성공")
-//        public void create() {
-//            // given
-//            MenuRequest menuRequest = new MenuRequest();
-//            메뉴_생성();
-//
-//            // when
-//            Menu actual = new Menu(name, price, menuGroup);
-//
-//            // then
-//            assertAll(
-//                    () -> assertThat(actual.getName()).isEqualTo(name),
-//                    () -> assertThat(actual.getMenuPrice().getPrice()).isEqualTo(price),
-//                    () -> assertThat(actual.getMenuGroup()).isEqualTo(menuGroup)
-//            );
-//        }
-//
-//        @Test
-//        @DisplayName("실패 - 메뉴명 없음")
-//        public void failNameEmpty() {
-//            // given
-//            String name = "";
-//            BigDecimal price = BigDecimal.valueOf(15000);
-//            MenuGroup menuGroup = menuGroup("치킨 세트");
-//
-//            // when
-//            Assertions.assertThatThrownBy(() -> {
-//                Menu actual = new Menu(name, price, menuGroup);
-//            }).isInstanceOf(IllegalArgumentException.class);
-//        }
-//
-//        @Test
-//        @DisplayName("실패 - 메뉴 그룹 없음")
-//        public void failMenuGroupEmpty() {
-//            // given
-//            String name = "후라이드 치킨";
-//            BigDecimal price = BigDecimal.valueOf(15000);
-//            MenuGroup menuGroup = null;
-//
-//            // when
-//            Assertions.assertThatThrownBy(() -> {
-//                Menu actual = new Menu(name, price, menuGroup);
-//            }).isInstanceOf(IllegalArgumentException.class);
-//        }
-//
-//        @Test
-//        @DisplayName("실패 - 잘못된 메뉴 가격 입력")
-//        public void failPriceIllegal() {
-//            // given
-//            String name = "후라이드 치킨";
-//            BigDecimal price = BigDecimal.valueOf(-1);
-//            MenuGroup menuGroup = menuGroup("치킨 세트");
-//
-//            // when
-//            Assertions.assertThatThrownBy(() -> {
-//                Menu actual = new Menu(name, price, menuGroup);
-//            }).isInstanceOf(IllegalArgumentException.class);
-//        }
-//    }
+    private void 메뉴_생성됨(ExtractableResponse<Response> actual, MenuRequest menuRequest) {
+        MenuResponse response = actual.as(MenuResponse.class);
+        assertAll(
+                () -> assertThat(response.getName()).isEqualTo(menuRequest.getName()),
+                () -> assertThat(response.getMenuPrice().getPrice()).isEqualTo(menuRequest.getPrice()),
+                () -> assertThat(response.getMenuGroup().getId()).isEqualTo(menuRequest.getMenuGroupId()),
+                () -> assertThat(response.getMenuProducts()).hasSize(2)
+        );
+    }
+
+    @Nested
+    @DisplayName("인수 테스트 - 메뉴 생성")
+    class CreateMenu {
+        @Test
+        @DisplayName("성공")
+        public void create() {
+            // given
+            MenuProductRequest 사이다_한개 = MenuProductRequest.of(사이다_생성됨.getId(), 1);
+            MenuProductRequest 양념소스_두개 = MenuProductRequest.of(양념소스_생성됨.getId(), 2);
+            MenuRequest 후라이드_치킨 = MenuRequest.of("후라이드 치킨", BigDecimal.valueOf(15000),
+                    일인_세트_생성됨.getId(), Lists.newArrayList(사이다_한개, 양념소스_두개));
+
+            // when
+            ExtractableResponse<Response> actual = 메뉴_생성_요청(후라이드_치킨);
+
+            // then
+            응답_CREATE(actual);
+            메뉴_생성됨(actual, 후라이드_치킨);
+        }
+
+        @Test
+        @DisplayName("실패 - 메뉴명 없음")
+        public void failNameEmpty() {
+            // given
+            MenuProductRequest 사이다_한개 = MenuProductRequest.of(사이다_생성됨.getId(), 1);
+            MenuProductRequest 양념소스_두개 = MenuProductRequest.of(양념소스_생성됨.getId(), 2);
+            MenuRequest 후라이드_치킨 = MenuRequest.of("", BigDecimal.valueOf(15000),
+                    일인_세트_생성됨.getId(), Lists.newArrayList(사이다_한개, 양념소스_두개));
+
+            // when
+            ExtractableResponse<Response> actual = 메뉴_생성_요청(후라이드_치킨);
+
+            // then
+            응답_BAD_REQUEST(actual);
+        }
+
+        @Test
+        @DisplayName("실패 - 메뉴 그룹 없음")
+        public void failMenuGroupEmpty() {
+            // given
+            MenuProductRequest 사이다_한개 = MenuProductRequest.of(사이다_생성됨.getId(), 1);
+            MenuProductRequest 양념소스_두개 = MenuProductRequest.of(양념소스_생성됨.getId(), 2);
+            MenuRequest 후라이드_치킨 = MenuRequest.of("후라이드 치킨", BigDecimal.valueOf(15000),
+                    null, Lists.newArrayList(사이다_한개, 양념소스_두개));
+
+            // when
+            ExtractableResponse<Response> actual = 메뉴_생성_요청(후라이드_치킨);
+
+            // then
+            응답_BAD_REQUEST(actual);
+        }
+
+        @Test
+        @DisplayName("실패 - 잘못된 메뉴 가격 입력")
+        public void failPriceIllegal() {
+            // given
+            MenuProductRequest 사이다_한개 = MenuProductRequest.of(사이다_생성됨.getId(), 1);
+            MenuProductRequest 양념소스_두개 = MenuProductRequest.of(양념소스_생성됨.getId(), 2);
+            MenuRequest 후라이드_치킨 = MenuRequest.of("후라이드 치킨", BigDecimal.valueOf(-1),
+                    일인_세트_생성됨.getId(), Lists.newArrayList(사이다_한개, 양념소스_두개));
+
+            // when
+            ExtractableResponse<Response> actual = 메뉴_생성_요청(후라이드_치킨);
+
+            // then
+            응답_BAD_REQUEST(actual);
+        }
+    }
 }
