e01c06b2f19e7399897b2b785b36a3b2e633f08e
cus4703 <cus4703@gmail.com>
2021-12-10 21:57:23 +0900

    refactor(fixture): 픽스처 수정

--- src/test/java/kitchenpos/menu/domain/fixture/MenuGroupDomainFixture.java
+++ src/test/java/kitchenpos/menu/domain/fixture/MenuGroupDomainFixture.java
@@ -4,6 +4,7 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kitchenpos.menu.domain.menugroup.MenuGroup;
 import kitchenpos.menu.dto.MenuGroupRequest;
+import kitchenpos.menu.dto.MenuGroupResponse;
 
 import static kitchenpos.utils.AcceptanceFixture.get;
 import static kitchenpos.utils.AcceptanceFixture.post;
@@ -11,7 +12,13 @@ import static kitchenpos.utils.AcceptanceFixture.post;
 public class MenuGroupDomainFixture {
 
     public static MenuGroup 일인_세트 = menuGroup("일인 세트");
+    public static MenuGroupRequest 일인_세트_요청 = MenuGroupRequest.from("일인 세트");
+    public static MenuGroupResponse 일인_세트_등록됨 = 메뉴_그룹_생성_요청(일인_세트_요청).as(MenuGroupResponse.class);
+
     public static MenuGroup 패밀리_세트 = menuGroup("패밀리 세트");
+    public static MenuGroupRequest 패밀리_세트_요청 = MenuGroupRequest.from("패밀리_세트 세트");
+    public static MenuGroupResponse 패밀리_세트_등록됨 = 메뉴_그룹_생성_요청(패밀리_세트_요청).as(MenuGroupResponse.class);
+
 
     public static MenuGroup menuGroup(String name) {
         return new MenuGroup(name);
--- src/test/java/kitchenpos/menu/domain/fixture/ProductDomainFixture.java
+++ src/test/java/kitchenpos/menu/domain/fixture/ProductDomainFixture.java
@@ -1,13 +1,36 @@
 package kitchenpos.menu.domain.fixture;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import kitchenpos.menu.domain.product.Product;
+import kitchenpos.menu.dto.ProductRequest;
+import kitchenpos.menu.dto.ProductResponse;
 
 import java.math.BigDecimal;
 
+import static kitchenpos.utils.AcceptanceFixture.get;
+import static kitchenpos.utils.AcceptanceFixture.post;
+
 public class ProductDomainFixture {
     public static Product 후라이드 = product("후라이드", BigDecimal.valueOf(15000));
+    public static ProductRequest 후라이드_요청 = ProductRequest.of("후라이드", BigDecimal.valueOf(15000));
+    public static ProductResponse 후라이드_등록됨 = 상품_생성_요청(후라이드_요청).as(ProductResponse.class);
+    public static Product 사이다 = product("사이다", BigDecimal.valueOf(1000));
+    public static ProductRequest 사이다_요청 = ProductRequest.of("사이다", BigDecimal.valueOf(1000));
+    public static ProductResponse 사이다_등록됨 = 상품_생성_요청(사이다_요청).as(ProductResponse.class);
 
+    public static Product 양념소스 = product("양념 소스", BigDecimal.valueOf(500));
+    public static ProductRequest 양념소스_요청 = ProductRequest.of("양념소스", BigDecimal.valueOf(1000));
+    public static ProductResponse 양념소스_등록됨 = 상품_생성_요청(양념소스_요청).as(ProductResponse.class);
     public static Product product(String name, BigDecimal price) {
         return new Product(name, price);
+    }
+
+    public static ExtractableResponse<Response> 상품_생성_요청(ProductRequest productRequest) {
+        return post("/api/products", productRequest);
+    }
+
+    public static ExtractableResponse<Response> 상품_조회_요청() {
+        return get("/api/products");
     }
 }
