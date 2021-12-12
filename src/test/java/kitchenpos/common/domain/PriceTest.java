16d6592666e0fc3cdc28799a2cad20f025c8bf88
devyonghee <devyonghee@gmail.com>
2021-12-13 03:04:30 +0900

    test(price) : 가격 테스트 케이스 추가

--- src/test/java/kitchenpos/common/domain/PriceTest.java
+++ src/test/java/kitchenpos/common/domain/PriceTest.java
@@ -1,5 +1,6 @@
 package kitchenpos.common.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
@@ -7,6 +8,7 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("가격")
@@ -35,5 +37,45 @@ class PriceTest {
         assertThatIllegalArgumentException()
             .isThrownBy(() -> Price.from(BigDecimal.valueOf(value)))
             .withMessageEndingWith("반드시 0이상 이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("합하기")
+    void sum() {
+        //given
+        Price price = Price.from(BigDecimal.ONE);
+
+        //when
+        Price sum = price.sum(Price.from(BigDecimal.ONE));
+
+        //then
+        assertThat(sum).isEqualTo(Price.from(BigDecimal.valueOf(2)));
+    }
+
+    @ParameterizedTest(name = "[{index}] 10은 {1} 보다 작거나 같음 여부가 {1}")
+    @CsvSource({"1,false", "10,true", "11,true"})
+    @DisplayName("같거나 이하 여부")
+    void equalOrLessThan(long value, boolean expected) {
+        //given
+        Price price = Price.from(BigDecimal.TEN);
+
+        //when
+        boolean equalOrLessThan = price.equalOrLessThan(Price.from(BigDecimal.valueOf(value)));
+
+        //then
+        assertThat(equalOrLessThan).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("곱하기")
+    void multiply() {
+        //given
+        Price price = Price.from(BigDecimal.ONE);
+
+        //when
+        Price multiply = price.multiply(10);
+
+        //then
+        assertThat(multiply).isEqualTo(Price.from(BigDecimal.TEN));
     }
 }
