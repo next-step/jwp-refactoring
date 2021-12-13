81eeb54fcc10ab80d289ae02c31bc348be68acb1
Arasia <rlacndtjs261@gmail.com>
2021-12-14 00:26:28 +0900

    test: Product 인수테스트 및 Service Test 작성

---
+++ src/test/java/kitchenpos/acceptance/ProductAcceptTest.java
@@ -0,0 +1,39 @@
+package kitchenpos.acceptance;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.AcceptanceTest;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static kitchenpos.acceptance.step.ProductAcceptStep.상품_등록_요청;
+import static kitchenpos.acceptance.step.ProductAcceptStep.상품_등록_확인;
+import static kitchenpos.acceptance.step.ProductAcceptStep.상품_목록_조회_요청;
+import static kitchenpos.acceptance.step.ProductAcceptStep.상품_목록_조회_확인;
+
+@DisplayName("상품 인수테스트")
+class ProductAcceptTest extends AcceptanceTest {
+    @DisplayName("상품을 관리한다")
+    @Test
+    void 상품을_관리한다() {
+        // given
+        Product 상품_등록_요청_데이터 = new Product();
+        상품_등록_요청_데이터.setName("강정치킨");
+        상품_등록_요청_데이터.setPrice(BigDecimal.valueOf(17_000));
+
+        // when
+        ExtractableResponse<Response> 상품_등록_응답 = 상품_등록_요청(상품_등록_요청_데이터);
+
+        // then
+        Product 등록된_상품 = 상품_등록_확인(상품_등록_응답, 상품_등록_요청_데이터);
+
+        // when
+        ExtractableResponse<Response> 상품_목록_조회_응답 = 상품_목록_조회_요청();
+
+        // then
+        상품_목록_조회_확인(상품_목록_조회_응답, 등록된_상품);
+    }
+}
---
+++ src/test/java/kitchenpos/acceptance/step/ProductAcceptStep.java
@@ -0,0 +1,68 @@
+package kitchenpos.acceptance.step;
+
+import io.restassured.mapper.TypeRef;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.domain.Product;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import static kitchenpos.utils.RestAssuredUtil.get;
+import static kitchenpos.utils.RestAssuredUtil.post;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class ProductAcceptStep {
+    private static final String BASE_URL = "/api/products";
+
+    public static ExtractableResponse<Response> 상품_등록_요청(Product 등록_요청_데이터) {
+        return post(BASE_URL, 등록_요청_데이터);
+    }
+
+    public static Product 상품_등록_확인(ExtractableResponse<Response> 상품_등록_응답, Product 등록_요청_데이터) {
+        Product 등록된_상품 = 상품_등록_응답.as(Product.class);
+
+        assertAll(
+                () -> assertThat(상품_등록_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(등록된_상품).satisfies(등록된_상품_확인(등록_요청_데이터))
+        );
+
+        return 등록된_상품;
+    }
+
+    private static Consumer<Product> 등록된_상품_확인(Product 등록_요청_데이터) {
+        return product -> {
+            assertThat(product.getId()).isNotNull();
+            assertThat(product.getName()).isEqualTo(등록_요청_데이터.getName());
+            assertThat(product.getPrice()).isEqualByComparingTo(등록_요청_데이터.getPrice());
+        };
+    }
+
+    public static ExtractableResponse<Response> 상품_목록_조회_요청() {
+        return get(BASE_URL);
+    }
+
+    public static void 상품_목록_조회_확인(ExtractableResponse<Response> 상품_목록_조회_응답, Product 등록된_상품) {
+        List<Product> 조회된_상품_목록 = 상품_목록_조회_응답.as(new TypeRef<List<Product>>() {
+        });
+
+        assertAll(
+                () -> assertThat(상품_목록_조회_응답.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(조회된_상품_목록).satisfies(조회된_상품_목록_확인(등록된_상품))
+        );
+    }
+
+    private static Consumer<List<? extends Product>> 조회된_상품_목록_확인(Product 등록된_상품) {
+        return products -> {
+            assertThat(products.size()).isOne();
+            assertThat(products).first()
+                    .satisfies(product -> {
+                        assertThat(product.getId()).isEqualTo(등록된_상품.getId());
+                        assertThat(product.getName()).isEqualTo(등록된_상품.getName());
+                        assertThat(product.getPrice()).isEqualByComparingTo(등록된_상품.getPrice());
+                    });
+        };
+    }
+}
