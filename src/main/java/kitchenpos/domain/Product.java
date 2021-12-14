9c0d48fae5b2f3140b6ccc2f0d94ac7fc6c8633a
Wangki Lee <lwk3007@gmail.com>
2021-12-14 19:16:11 +0900

    test: menu service test 작성

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -12,6 +12,11 @@ public class Product {
     }
 
     public Product(String name, BigDecimal price) {
+        this(null, name, price);
+    }
+
+    public Product(Long id, String name, BigDecimal price) {
+        this.id = id;
         this.name = name;
         this.price = price;
     }
