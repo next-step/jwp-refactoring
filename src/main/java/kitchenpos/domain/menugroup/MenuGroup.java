9b80cd3ef23f3c71231e58df107456de5ae0a1c5
tyanotyrano <tyrano09@gmail.com>
2021-12-14 20:25:11 +0900

    test(MenuGroupTest.java): MenuGroup 테스트 코드 작성
    MenuGroup 테스트 코드를 작성한다.
    - MenuGroupTest : MenuGroup 테스트 코드 작성
    - MenuGroup, MenuGroupResponse : Name 도메인 적용

--- src/main/java/kitchenpos/domain/menugroup/MenuGroup.java
+++ src/main/java/kitchenpos/domain/menugroup/MenuGroup.java
@@ -1,11 +1,14 @@
 package kitchenpos.domain.menugroup;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import kitchenpos.domain.Name;
 
 @Entity
 @Table(name = "menu_group")
@@ -16,8 +19,8 @@ public class MenuGroup {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+    @Embedded
+    private Name name;
 
     protected MenuGroup() {}
 
@@ -27,7 +30,7 @@ public class MenuGroup {
     }
 
     private MenuGroup(String name) {
-        this.name = name;
+        this.name = Name.from(name);
     }
 
     public static MenuGroup from(String name) {
@@ -42,7 +45,7 @@ public class MenuGroup {
         return id;
     }
 
-    public String getName() {
+    public Name getName() {
         return name;
     }
 }
---
+++ src/test/java/kitchenpos/domain/menugroup/MenuGroupTest.java
@@ -0,0 +1,28 @@
+package kitchenpos.domain.menugroup;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MenuGroupTest {
+
+    @DisplayName("MenuGroup 은 Name 으로 생성된다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"가", "가나", "가나12", "가나12ab"})
+    void creat1(String name) {
+        // when & then
+        assertThatNoException().isThrownBy(() -> MenuGroup.from(name));
+    }
+
+    @DisplayName("MenuGroup 생성 시, Name 이 null 또는 빈 문자열이면 예외가 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void creat2(String name) {
+        // when & then
+        assertThatIllegalArgumentException().isThrownBy(() -> MenuGroup.from(name))
+                                            .withMessageContaining("이름이 비어있습니다.");
+    }
+}
