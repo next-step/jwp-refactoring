ed9b62d5ee1c82db1ed6171ab82c8fd2eab6b0e0
ch3224bin <ahrocksil@gmail.com>
2021-12-15 17:16:45 +0900

    test(kitchenpos): 상품 관리 기능 인수 테스트 및 서비스 테스트 작성

---
+++ src/test/java/kitchenpos/utils/Http.java
@@ -0,0 +1,25 @@
+package kitchenpos.utils;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+public class Http {
+    public static ExtractableResponse<Response> get(String url) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get(url)
+                .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> post(String url, Object params) {
+        return RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post(url)
+                .then().log().all().extract();
+    }
+}
