9e34e2c1c5d5d31764f38c459ab7732c01c018b5
keepbang <rlqud1125@naver.com>
2021-12-16 16:21:50 +0900

    test: 테이블 그룹 테스트 작성

---
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -0,0 +1,10 @@
+package kitchenpos.application;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("주문 관리 테스트")
+public class OrderServiceTest {
+}
---
+++ src/test/java/kitchenpos/domain/OrderTest.java
@@ -0,0 +1,5 @@
+package kitchenpos.domain;
+
+public class OrderTest {
+
+}
