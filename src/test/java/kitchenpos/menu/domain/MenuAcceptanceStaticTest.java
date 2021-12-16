05587c76c55fb9f976763c40061259259764bf32
moon <hoopster@airi.kr>
2021-12-16 21:53:55 +0900

    refac (MenuAcceptanceTest) : 메뉴 관련 인수테스트 리팩터링

--- src/test/java/kitchenpos/menu/domain/MenuAcceptanceStaticTest.java
+++ src/test/java/kitchenpos/menu/domain/MenuAcceptanceStaticTest.java
@@ -2,7 +2,6 @@ package kitchenpos.menu.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,6 +12,9 @@ import org.springframework.http.MediaType;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import kitchenpos.menu.dto.MenuProductRequest;
+import kitchenpos.menu.dto.MenuRequest;
+import kitchenpos.menu.dto.MenuResponse;
 import kitchenpos.product.dto.ProductResponse;
 
 public class MenuAcceptanceStaticTest {
@@ -29,35 +31,29 @@ public class MenuAcceptanceStaticTest {
 
 	public static void 메뉴_목록_조회됨(ExtractableResponse<Response> response, List<Long> idList) {
 		List<Long> responseIdList = response.body()
-			.jsonPath().getList(".", Menu.class)
+			.jsonPath().getList(".", MenuResponse.class)
 			.stream()
-			.map(Menu::getId)
+			.map(MenuResponse::getId)
 			.collect(Collectors.toList());
 
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
 		assertThat(responseIdList).containsAnyElementsOf(idList);
 	}
 
-	public static Menu 메뉴가_생성_되어있음(Menu menu) {
-		return 메뉴_생성_요청(menu).as(Menu.class);
+	public static MenuResponse 메뉴가_생성_되어있음(MenuRequest menu) {
+		return 메뉴_생성_요청(menu).as(MenuResponse.class);
 	}
 
-	public static void 메뉴_생성_실패됨(ExtractableResponse<Response> response) {
-		assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+	public static void 메뉴_생성_실패됨(ExtractableResponse<Response> response, int status) {
+		assertThat(response.statusCode()).isEqualTo(status);
 	}
 
-	public static Menu 메뉴_생성_요청값_생성(String name, Integer price, Long menuGroupId, List<MenuProduct> menuProductList) {
-		Menu menu = new Menu();
-		menu.setName(name);
-		if (price != null) {
-			menu.setPrice(new BigDecimal(price));
-		}
-		menu.setMenuGroupId(menuGroupId);
-		menu.setMenuProducts(menuProductList);
-		return menu;
+	public static MenuRequest 메뉴_생성_요청값_생성(String name, Integer price, Long menuGroupId,
+		List<MenuProductRequest> menuProducts) {
+		return MenuRequest.of(name, price, menuGroupId, menuProducts);
 	}
 
-	public static ExtractableResponse<Response> 메뉴_생성_요청(Menu params) {
+	public static ExtractableResponse<Response> 메뉴_생성_요청(MenuRequest params) {
 		return RestAssured.given().log().all()
 			.contentType(MediaType.APPLICATION_JSON_VALUE)
 			.body(params)
@@ -71,11 +67,8 @@ public class MenuAcceptanceStaticTest {
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 	}
 
-	public static List<MenuProduct> 메뉴_상품_생성되어_있음(ProductResponse product) {
-		MenuProduct menuProduct = new MenuProduct();
-		menuProduct.setProductId(product.getId());
-		menuProduct.setQuantity(2L);
-
-		return Collections.singletonList(menuProduct);
+	public static List<MenuProductRequest> 메뉴_상품_요청_생성_되어_있음(ProductResponse product) {
+		MenuProductRequest menuProductRequest = MenuProductRequest.of(product.getId(), 2L);
+		return Collections.singletonList(menuProductRequest);
 	}
 }
--- src/test/java/kitchenpos/menu/domain/MenuAcceptanceTest.java
+++ src/test/java/kitchenpos/menu/domain/MenuAcceptanceTest.java
@@ -10,32 +10,37 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kitchenpos.AcceptanceTest;
 import kitchenpos.menu.dto.MenuGroupResponse;
+import kitchenpos.menu.dto.MenuProductRequest;
+import kitchenpos.menu.dto.MenuRequest;
+import kitchenpos.menu.dto.MenuResponse;
 import kitchenpos.product.dto.ProductResponse;
 
 @DisplayName("인수테스트 : 메뉴 관련")
 class MenuAcceptanceTest extends AcceptanceTest {
 
 	private MenuGroupResponse 두마리_메뉴_그룹;
-	private List<MenuProduct> 불닭_두마리_메뉴_상품_리스트;
+	private List<MenuProductRequest> 불닭_두마리_메뉴_상품_리스트;
 	private ProductResponse 존재하지_않는_상품;
+	private ProductResponse 불닭;
 
 	@BeforeEach
 	void setup() {
 		두마리_메뉴_그룹 = 메뉴_그룹_생성되어_있음(메뉴_그룹_생성_요청값_생성("두마리메뉴"));
-		ProductResponse 불닭 = 상품이_생성_되어있음(상품_요청값_생성("불닭", 16000));
-		불닭_두마리_메뉴_상품_리스트 = 메뉴_상품_생성되어_있음(불닭);
+		불닭 = 상품이_생성_되어있음(상품_요청값_생성("불닭", 16000));
+		불닭_두마리_메뉴_상품_리스트 = 메뉴_상품_요청_생성_되어_있음(불닭);
 		존재하지_않는_상품 = ProductResponse.of(100L, "null", 0);
 	}
 
 	@Test
 	void 메뉴를_생성한다() {
 		// give
-		Menu 불닭_두마리_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", 19000, 두마리_메뉴_그룹.getId(), 불닭_두마리_메뉴_상품_리스트);
+		MenuRequest 불닭_두마리_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", 19000, 두마리_메뉴_그룹.getId(), 불닭_두마리_메뉴_상품_리스트);
 
 		// when
 		ExtractableResponse<Response> response = 메뉴_생성_요청(불닭_두마리_메뉴_생성_요청값);
@@ -47,68 +52,68 @@ class MenuAcceptanceTest extends AcceptanceTest {
 	@Test
 	void 메뉴_가격을_넣지_않은_경우_생성_실패한다() {
 		// given
-		Menu 가격이_없는_불닭_두마리_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", null, 두마리_메뉴_그룹.getId(), 불닭_두마리_메뉴_상품_리스트);
+		MenuRequest 가격이_없는_불닭_두마리_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", null, 두마리_메뉴_그룹.getId(), 불닭_두마리_메뉴_상품_리스트);
 
 		// when
 		ExtractableResponse<Response> response = 메뉴_생성_요청(가격이_없는_불닭_두마리_메뉴_생성_요청값);
 
 		// then
-		메뉴_생성_실패됨(response);
+		메뉴_생성_실패됨(response, HttpStatus.BAD_REQUEST.value());
 	}
 
 	@Test
 	void 메뉴_가격이_0보다_작은_경우_생성_실패한다() {
 		// given
-		Menu 가격이_0보다_작은_불닭_두마리_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", -1500, 두마리_메뉴_그룹.getId(), 불닭_두마리_메뉴_상품_리스트);
+		MenuRequest 가격이_0보다_작은_불닭_두마리_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", -1500, 두마리_메뉴_그룹.getId(), 불닭_두마리_메뉴_상품_리스트);
 
 		// when
 		ExtractableResponse<Response> response = 메뉴_생성_요청(가격이_0보다_작은_불닭_두마리_메뉴_생성_요청값);
 
 		// then
-		메뉴_생성_실패됨(response);
+		메뉴_생성_실패됨(response, HttpStatus.BAD_REQUEST.value());
 	}
 
 	@Test
 	void 메뉴_그룹이_존재하지_않을_경우_생성_실패한다() {
 		// given
-		Menu 메뉴_그룹이_존재하지_않는_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", 19000, 100L, 불닭_두마리_메뉴_상품_리스트);
+		MenuRequest 메뉴_그룹이_존재하지_않는_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", 19000, 100L, 불닭_두마리_메뉴_상품_리스트);
 
 		// when
 		ExtractableResponse<Response> response = 메뉴_생성_요청(메뉴_그룹이_존재하지_않는_메뉴_생성_요청값);
 
 		// then
-		메뉴_생성_실패됨(response);
+		메뉴_생성_실패됨(response, HttpStatus.BAD_REQUEST.value());
 	}
 
 	@Test
 	void 메뉴에_등록할_상품이_존재하지_않을_경우_생성_실패한다() {
 		// given
-		List<MenuProduct> 메뉴_상품_리스트 = 메뉴_상품_생성되어_있음(존재하지_않는_상품);
-		Menu 존재하지_않는_상품_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", 19000, 10L, 메뉴_상품_리스트);
+		List<MenuProductRequest> 메뉴_상품_요청_리스트 = 메뉴_상품_요청_생성_되어_있음(존재하지_않는_상품);
+		MenuRequest 존재하지_않는_상품_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", 19000, 10L, 메뉴_상품_요청_리스트);
 
 		// when
 		ExtractableResponse<Response> response = 메뉴_생성_요청(존재하지_않는_상품_메뉴_생성_요청값);
 
 		// then
-		메뉴_생성_실패됨(response);
+		메뉴_생성_실패됨(response, HttpStatus.BAD_REQUEST.value());
 	}
 
 	@Test
 	void 메뉴의_가격이_메뉴_상품들의_가격을_더한_값보다_크면_생성_실패한다() {
 		// given
-		Menu 불닭_두마리_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", 40000, 두마리_메뉴_그룹.getId(), 불닭_두마리_메뉴_상품_리스트);
+		MenuRequest 불닭_두마리_메뉴_생성_요청값 = 메뉴_생성_요청값_생성("불닭+불닭", 40000, 두마리_메뉴_그룹.getId(), 불닭_두마리_메뉴_상품_리스트);
 
 		// when
 		ExtractableResponse<Response> response = 메뉴_생성_요청(불닭_두마리_메뉴_생성_요청값);
 
 		// then
-		메뉴_생성_실패됨(response);
+		메뉴_생성_실패됨(response, HttpStatus.BAD_REQUEST.value());
 	}
 
 	@Test
 	void 메뉴_목록을_조회한다() {
 		// given
-		Menu 불닭_메뉴 = 메뉴가_생성_되어있음(메뉴_생성_요청값_생성("불닭+불닭", 19000, 두마리_메뉴_그룹.getId(), 불닭_두마리_메뉴_상품_리스트));
+		MenuResponse 불닭_메뉴 = 메뉴가_생성_되어있음(메뉴_생성_요청값_생성("불닭+불닭", 19000, 두마리_메뉴_그룹.getId(), 불닭_두마리_메뉴_상품_리스트));
 
 		// when
 		ExtractableResponse<Response> response = 메뉴_목록_조회_요청();
