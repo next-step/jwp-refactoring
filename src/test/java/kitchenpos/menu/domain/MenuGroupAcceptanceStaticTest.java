8e2d84d4baaa9bc8ccd342ab68898865f9e0fef1
moon <hoopster@airi.kr>
2021-12-16 15:23:45 +0900

    refac (MenuGroupAcceptanceTest) : 메뉴 그룹 관련 인수테스트 리팩터링

--- src/test/java/kitchenpos/menu/domain/MenuGroupAcceptanceStaticTest.java
+++ src/test/java/kitchenpos/menu/domain/MenuGroupAcceptanceStaticTest.java
@@ -11,13 +11,15 @@ import org.springframework.http.MediaType;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import kitchenpos.menu.dto.MenuGroupRequest;
+import kitchenpos.menu.dto.MenuGroupResponse;
 
 public class MenuGroupAcceptanceStaticTest {
 
 	public static final String MENU_GROUP_PATH = "/api/menu-groups";
 
-	public static MenuGroup 메뉴_그룹_생성되어_있음(MenuGroup params) {
-		return 메뉴_그룹_생성_요청(params).as(MenuGroup.class);
+	public static MenuGroupResponse 메뉴_그룹_생성되어_있음(MenuGroupRequest params) {
+		return 메뉴_그룹_생성_요청(params).as(MenuGroupResponse.class);
 	}
 
 	public static ExtractableResponse<Response> 메뉴_그룹_목록_조회_요청() {
@@ -30,22 +32,20 @@ public class MenuGroupAcceptanceStaticTest {
 
 	public static void 메뉴_그룹_목록_조회됨(ExtractableResponse<Response> response, List<Long> idList) {
 		List<Long> responseIdList = response.body()
-			.jsonPath().getList(".", MenuGroup.class)
+			.jsonPath().getList(".", MenuGroupResponse.class)
 			.stream()
-			.map(MenuGroup::getId)
+			.map(MenuGroupResponse::getId)
 			.collect(Collectors.toList());
 
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
 		assertThat(responseIdList).containsAnyElementsOf(idList);
 	}
 
-	public static MenuGroup 메뉴_그룹_생성_요청값_생성(String name) {
-		MenuGroup menuGroup = new MenuGroup();
-		menuGroup.setName(name);
-		return menuGroup;
+	public static MenuGroupRequest 메뉴_그룹_생성_요청값_생성(String name) {
+		return MenuGroupRequest.from(name);
 	}
 
-	public static ExtractableResponse<Response> 메뉴_그룹_생성_요청(MenuGroup params) {
+	public static ExtractableResponse<Response> 메뉴_그룹_생성_요청(MenuGroupRequest params) {
 		return RestAssured.given().log().all()
 			.contentType(MediaType.APPLICATION_JSON_VALUE)
 			.body(params)
--- src/test/java/kitchenpos/menu/domain/MenuGroupAcceptanceTest.java
+++ src/test/java/kitchenpos/menu/domain/MenuGroupAcceptanceTest.java
@@ -11,12 +11,14 @@ import org.junit.jupiter.api.Test;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kitchenpos.AcceptanceTest;
+import kitchenpos.menu.dto.MenuGroupRequest;
+import kitchenpos.menu.dto.MenuGroupResponse;
 
 @DisplayName("인수테스트 : 메뉴 그룹 관련")
 class MenuGroupAcceptanceTest extends AcceptanceTest {
 
-	private MenuGroup 두마리메뉴_그룹_생성_요청값;
-	private MenuGroup 한마리메뉴_그룹_생성_요청값;
+	private MenuGroupRequest 두마리메뉴_그룹_생성_요청값;
+	private MenuGroupRequest 한마리메뉴_그룹_생성_요청값;
 
 	@BeforeEach
 	void setup() {
@@ -37,8 +39,8 @@ class MenuGroupAcceptanceTest extends AcceptanceTest {
 	@Test
 	void 메뉴_그룹_목록을_조회한다() {
 		// given
-		MenuGroup 두마리_메뉴_그룹_생성_되어있음 = 메뉴_그룹_생성되어_있음(두마리메뉴_그룹_생성_요청값);
-		MenuGroup 한마리_메뉴_그룹_생성_되어있음 = 메뉴_그룹_생성되어_있음(한마리메뉴_그룹_생성_요청값);
+		MenuGroupResponse 두마리_메뉴_그룹_생성_되어있음 = 메뉴_그룹_생성되어_있음(두마리메뉴_그룹_생성_요청값);
+		MenuGroupResponse 한마리_메뉴_그룹_생성_되어있음 = 메뉴_그룹_생성되어_있음(한마리메뉴_그룹_생성_요청값);
 
 		// when
 		ExtractableResponse<Response> response = 메뉴_그룹_목록_조회_요청();
