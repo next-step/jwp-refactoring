b3e58240cf1b922135f7a57983e304a12422fdfb
ch3224bin <ahrocksil@gmail.com>
2021-12-16 17:48:59 +0900

    refactor(kitchenpos): 인수테스트 시 테이블 truncate 구문 수정

--- src/test/java/kitchenpos/utils/DatabaseCleanup.java
+++ src/test/java/kitchenpos/utils/DatabaseCleanup.java
@@ -13,8 +13,7 @@ import java.util.List;
 public class DatabaseCleanup {
     private final JdbcTemplate jdbcTemplate;
     // 테스트를 위한 하드코딩으로 이해 부탁드립니다.
-    private final List<String> tableNamesUsedId = Arrays.asList("menu", "menu_group", "orders", "order_table", "product", "table_group");
-    private final List<String> tableNamesUsedSeq = Arrays.asList("order_line_item", "menu_product");
+    private final List<String> tableNames = Arrays.asList("menu", "menu_group", "orders", "order_table", "product", "table_group", "order_line_item", "menu_product");
 
     public DatabaseCleanup(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -24,13 +23,8 @@ public class DatabaseCleanup {
     public void execute() {
         jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY FALSE");
 
-        for (String tableName : tableNamesUsedId) {
-            jdbcTemplate.execute("TRUNCATE TABLE " + tableName);
-            jdbcTemplate.execute("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1");
-        }
-        for (String tableName : tableNamesUsedSeq) {
-            jdbcTemplate.execute("TRUNCATE TABLE " + tableName);
-            jdbcTemplate.execute("ALTER TABLE " + tableName + " ALTER COLUMN SEQ RESTART WITH 1");
+        for (String tableName : tableNames) {
+            jdbcTemplate.execute("TRUNCATE TABLE " + tableName + " RESTART IDENTITY;");
         }
         jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY TRUE");
     }
