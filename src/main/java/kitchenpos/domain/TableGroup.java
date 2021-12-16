80d35c6e386754883fce93712454c0c291d9fda1
ch3224bin <ahrocksil@gmail.com>
2021-12-16 22:20:01 +0900

    test(kitchenpos): 단체 지정 서비스 서비스 테스트 작성

--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -2,6 +2,7 @@ package kitchenpos.domain;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 public class TableGroup {
     private Long id;
@@ -9,6 +10,12 @@ public class TableGroup {
     private List<OrderTable> orderTables;
 
     public TableGroup() {
+    }
+
+    public TableGroup(Long id, LocalDateTime createdDate, List<OrderTable> orderTables) {
+        this.id = id;
+        this.createdDate = createdDate;
+        this.orderTables = orderTables;
     }
 
     public TableGroup(List<OrderTable> orderTables) {
@@ -37,5 +44,18 @@ public class TableGroup {
 
     public void setOrderTables(final List<OrderTable> orderTables) {
         this.orderTables = orderTables;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TableGroup that = (TableGroup) o;
+        return Objects.equals(id, that.id) && Objects.equals(createdDate, that.createdDate) && Objects.equals(orderTables, that.orderTables);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createdDate, orderTables);
     }
 }
