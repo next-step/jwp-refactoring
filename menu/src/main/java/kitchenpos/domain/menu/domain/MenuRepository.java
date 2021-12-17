7d0eeeb75dd0b39e8e4f24d8004f730597c8dc80
peter-alethio <peter@alethio.io>
2021-12-17 23:32:26 +0900

    chore: 멀티 모듈 설정

---
+++ menu/src/main/java/kitchenpos/domain/menu/domain/MenuRepository.java
@@ -0,0 +1,14 @@
+package kitchenpos.domain.menu.domain;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MenuRepository {
+    Menu save(Menu entity);
+
+    Optional<Menu> findById(Long id);
+
+    List<Menu> findAll();
+
+    long countByIdIn(List<Long> ids);
+}
---
+++ order/src/main/java/kitchenpos/domain/table/application/TableService.java
@@ -0,0 +1,56 @@
+package kitchenpos.domain.table.application;
+
+import kitchenpos.domain.table.domain.OrderTable;
+import kitchenpos.domain.table.domain.OrderTableRepository;
+import kitchenpos.domain.table.dto.TableRequest;
+import kitchenpos.exception.BusinessException;
+import kitchenpos.exception.ErrorCode;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class TableService {
+
+    private final OrderTableRepository orderTableRepository;
+    private final OrderTableValidator orderTableValidator;
+
+    public TableService(
+            OrderTableRepository orderTableRepository,
+            OrderTableValidator orderTableValidator
+    ) {
+        this.orderTableRepository = orderTableRepository;
+        this.orderTableValidator = orderTableValidator;
+    }
+
+    @Transactional
+    public OrderTable create(final TableRequest request) {
+        return orderTableRepository.save(request.toOrderTable());
+    }
+
+    public List<OrderTable> list() {
+        return orderTableRepository.findAll();
+    }
+
+    @Transactional
+    public OrderTable changeEmpty(final Long orderTableId, final TableRequest request) {
+        orderTableValidator.validateCompleteTable(orderTableId);
+
+        final OrderTable orderTable = getOrderTable(orderTableId);
+        orderTable.changeEmpty(request.isEmpty());
+        return orderTable;
+    }
+
+    @Transactional
+    public OrderTable changeNumberOfGuests(final Long orderTableId, final TableRequest request) {
+        final OrderTable orderTable = getOrderTable(orderTableId);
+        orderTable.changeNumberOfGuests(request.getNumberOfGuests());
+        return orderTable;
+    }
+
+    private OrderTable getOrderTable(Long orderTableId) {
+        return orderTableRepository.findById(orderTableId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ORDER_TABLE_NOT_FOUND));
+    }
+}
