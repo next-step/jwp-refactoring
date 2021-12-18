8d1b39ac80d28b10816b8efabc287661504ec121
vicjed <vicjed@icloud.com>
2021-12-19 06:17:54 +0900

    test(Fixture) : 픽스처 수정

--- src/test/java/common/OrderTableFixture.java
+++ src/test/java/common/OrderTableFixture.java
@@ -4,11 +4,19 @@ import kitchenpos.domain.OrderTable;
 
 public class OrderTableFixture {
 
-    public static OrderTable 첫번째_테이블() {
+    public static OrderTable 첫번째_주문테이블() {
         return new OrderTable(1L, null, 1, false);
     }
 
-    public static OrderTable 두번째_테이블() {
+    public static OrderTable 두번째_주문테이블() {
         return new OrderTable(2L, null, 2, false);
+    }
+
+    public static OrderTable 단체지정_첫번째_주문테이블() {
+        return new OrderTable(1L, null, 1, true);
+    }
+
+    public static OrderTable 단체지정_두번째_주문테이블() {
+        return new OrderTable(2L, null, 2, true);
     }
 }
--- src/test/java/common/TableGroupFixture.java
+++ src/test/java/common/TableGroupFixture.java
@@ -1,7 +1,7 @@
 package common;
 
-import static common.OrderTableFixture.두번째_테이블;
-import static common.OrderTableFixture.첫번째_테이블;
+import static common.OrderTableFixture.단체지정_두번째_주문테이블;
+import static common.OrderTableFixture.단체지정_첫번째_주문테이블;
 import static java.util.Arrays.asList;
 
 import java.time.LocalDateTime;
@@ -10,7 +10,7 @@ import kitchenpos.domain.TableGroup;
 public class TableGroupFixture {
 
     public static TableGroup 단체테이블_첫번째_두번째() {
-        return new TableGroup(1L, LocalDateTime.now(), asList(첫번째_테이블(), 두번째_테이블()));
+        return new TableGroup(1L, LocalDateTime.now(), asList(단체지정_첫번째_주문테이블(), 단체지정_두번째_주문테이블()));
     }
 
 }
