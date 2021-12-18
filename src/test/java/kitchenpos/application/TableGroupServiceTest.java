936ae19279f26303dc1725f270c1a9cf9f1404e9
ChangSubKwak <paradise1732@gmail.com>
2021-12-19 01:21:18 +0900

    test: 테이블그룹(TableGroup)의 서비스 테스트 케이스 작성

---
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -0,0 +1,125 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.*;
+import java.util.*;
+import java.util.Arrays;
+import java.util.stream.*;
+
+import org.assertj.core.util.*;
+import org.junit.jupiter.api.*;
+
+import kitchenpos.dao.*;
+import kitchenpos.domain.*;
+
+@DisplayName("테이블그룹 관련 테스트")
+class TableGroupServiceTest {
+    private OrderDao orderDao;
+    private OrderTableDao orderTableDao;
+    private TableGroupDao tableGroupDao;
+
+    private TableGroupService tableGroupService;
+    private OrderTable orderTable;
+    private List<OrderTable> orderTables;
+    private TableGroup tableGroup;
+
+    @BeforeEach
+    void setUp() {
+        orderDao = mock(OrderDao.class);
+        orderTableDao = mock(OrderTableDao.class);
+        tableGroupDao = mock(TableGroupDao.class);
+        tableGroupService = new TableGroupService(orderDao, orderTableDao, tableGroupDao);
+
+        tableGroup = TableGroup.of(1L, LocalDateTime.now(), null);
+
+        orderTables = Lists.newArrayList(
+            OrderTable.of(1L, null, 4, true),
+            OrderTable.of(2L, null, 6, true)
+        );
+        tableGroup = TableGroup.of(1L, LocalDateTime.now(), orderTables);
+    }
+
+    @DisplayName("create메서드에 생성을 원하는 TableGroup 객체를 인자로 하여 호출하면, 생성된 객체를 반환한다.")
+    @Test
+    void createTest() {
+        when(tableGroupDao.save(tableGroup)).thenReturn(tableGroup);
+        when(orderTableDao.findAllByIdIn(Lists.newArrayList(1L, 2L))).thenReturn(orderTables);
+        assertThat(tableGroupService.create(tableGroup)).isEqualTo(tableGroup);
+    }
+
+    @DisplayName("create메서드에 생성을 원하는 TableGroup 객체에 등록된 OrderTable의 개수가 1개 이하이면, 예외를 던진다.")
+    @Test
+    void exceptionTest1() {
+        orderTables = Lists.newArrayList(OrderTable.of(1L, null, 4, true));
+        tableGroup.setOrderTables(orderTables);
+        assertThatThrownBy(
+            () -> tableGroupService.create(tableGroup)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("create메서드에 생성을 원하는 TableGroup 객체에 등록된 OrderTable의 개수와 저장된 OrderTable의 개수가 일치하지 않으면, 예외를 던진다.")
+    @Test
+    void exceptionTest2() {
+        List<OrderTable> threeOrderTables = Lists.newArrayList(
+            OrderTable.of(1L, null, 4, true),
+            OrderTable.of(2L, null, 4, true),
+            OrderTable.of(3L, null, 4, true)
+        );
+        tableGroup.setOrderTables(threeOrderTables);
+
+        assertThatThrownBy(
+            () -> tableGroupService.create(tableGroup)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("create메서드 호출시, 저장된 OrderTable의 객체가 빈 객체이거나 널이면, 예외를 던진다.")
+    @Test
+    void exceptionTest3() {
+        when(orderTableDao.findAllByIdIn(Lists.newArrayList(1L, 2L))).thenReturn(
+            Lists.newArrayList(
+                OrderTable.of(1L, null, 4, true),
+                new OrderTable()
+            )
+        );
+
+        assertThatThrownBy(
+            () -> tableGroupService.create(tableGroup)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("upgroup메서드에 TableGroup 객체의 식별자를 인자로 하여 호출하면, 저장된 TableGroup을 삭제(null 세팅)한다.")
+    @Test
+    void ungroupTest() {
+        List<OrderTable> threeOrderTables = Lists.newArrayList(
+            OrderTable.of(1L, tableGroup.getId(), 4, true),
+            OrderTable.of(2L, tableGroup.getId(), 4, true),
+            OrderTable.of(3L, tableGroup.getId(), 4, true)
+        );
+        tableGroup.setOrderTables(threeOrderTables);
+        tableGroupService.ungroup(tableGroup.getId());
+    }
+
+    @DisplayName("upgroup메서드를 호출할 때, OrderTable의 상태가 조리 또는 식사중이면, 예외를 던진다.")
+    @Test
+    void exceptionTest4() {
+        orderTables = Lists.newArrayList(
+            OrderTable.of(1L, tableGroup.getId(), 4, true),
+            OrderTable.of(2L, tableGroup.getId(), 6, true)
+        );
+
+        List<Long> orderTableIds = orderTables.stream()
+            .map(OrderTable::getId)
+            .collect(Collectors.toList());
+
+        when(orderTableDao.findAllByTableGroupId(tableGroup.getId())).thenReturn(orderTables);
+        when(orderDao.existsByOrderTableIdInAndOrderStatusIn(
+            orderTableIds, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))
+        ).thenReturn(true);
+
+        assertThatThrownBy(
+            () -> tableGroupService.ungroup(tableGroup.getId())
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+}
