64012b8882eea5f0c2cc92d314c8c7d9fa008bd3
Chaeyun Lee <chaeyunn@iochord.com>
2021-12-18 19:22:55 +0900

    test: 메뉴 그룹 서비스 테스트

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,71 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+
+@ExtendWith(MockitoExtension.class)
+public class MenuGroupServiceTest {
+
+	private MenuGroupService menuGroupService;
+
+	@Mock
+	private MenuGroupDao menuGroupDao;
+
+	@BeforeEach
+	public void setup(){
+		this.menuGroupService = new MenuGroupService(menuGroupDao);
+	}
+
+	@DisplayName("메뉴 그룹을 생성한다")
+	@Test
+	void createTest(){
+		// given
+		MenuGroup menuGroup = new MenuGroup();
+		menuGroup.setName("추천메뉴");
+
+		MenuGroup persist = new MenuGroup();
+		persist.setId(1L);
+		persist.setName("추천메뉴");
+
+		given(menuGroupDao.save(any())).willReturn(persist);
+
+		// when
+		MenuGroup result = menuGroupService.create(menuGroup);
+
+		// then
+		assertThat(result.getId()).isEqualTo(persist.getId());
+		assertThat(result.getName()).isEqualTo(persist.getName());
+	}
+
+	@DisplayName("메뉴 그룹 목록을 조회한다")
+	@Test
+	void listTest(){
+		// given
+		List<MenuGroup> persist = new ArrayList<>();
+		MenuGroup menuGroup1 = new MenuGroup();
+		menuGroup1.setId(1L);
+		menuGroup1.setName("추천메뉴");
+		persist.add(menuGroup1);
+		
+		given(menuGroupDao.findAll()).willReturn(persist);
+		
+		// when
+		List<MenuGroup> result = menuGroupService.list();
+		
+		// then
+		assertThat(result).containsAll(persist);
+	}
+}
