1633c179b95e648b1e063f93cce5f8273a8befb8
Jeongkyo Kim <chavitak@gmail.com>
2021-12-18 15:59:31 +0900

    test: ProductService에 대한 테스트를 추가한다

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,93 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+    private Product 양념치킨;
+    private Product 순살치킨;
+    private List<Product> list;
+
+    @Mock
+    private ProductDao mockDao;
+
+    @BeforeEach
+    void setUp() {
+        양념치킨 = new Product();
+        양념치킨.setName("양념치킨");
+        양념치킨.setPrice(new BigDecimal(-16_000));
+        순살치킨 = new Product();
+        순살치킨.setName("순살치킨");
+        순살치킨.setPrice(new BigDecimal(-15_000));
+        list = new ArrayList<>();
+        list.add(양념치킨);
+        list.add(순살치킨);
+    }
+
+    @DisplayName("상품을 생성한다")
+    @Test
+    public void createTest() {
+        // given
+        given(mockDao.save(양념치킨)).willReturn(양념치킨);
+
+        // when
+        ProductService productService = new ProductService(mockDao);
+        Product returnedProduct = productService.create(양념치킨);
+
+        // then
+        assertThat(returnedProduct.getName()).isEqualTo("양념치킨");
+    }
+
+    @DisplayName("가격이 없는 상품을 생성한다")
+    @Test
+    public void createNullProceProductTest() {
+        // given
+        Product product = new Product();
+        product.setName("양념치킨");
+
+        // when
+        ProductService productService = new ProductService(mockDao);
+
+        assertThatThrownBy(() -> productService.create(product)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("가격이 음수인 상품을 생성한다")
+    @Test
+    public void createMinusProceProductTest() {
+        // when
+        ProductService productService = new ProductService(mockDao);
+
+        assertThatThrownBy(() -> productService.create(양념치킨)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품을 조회한다")
+    @Test
+    public void listTest() {
+        // given
+        when(mockDao.findAll()).thenReturn(list);
+
+        // when
+        ProductService productService = new ProductService(mockDao);
+        List<Product> products = productService.list();
+
+        // then
+        assertThat(products).containsExactlyElementsOf(Arrays.asList(양념치킨, 순살치킨));
+    }
+}
