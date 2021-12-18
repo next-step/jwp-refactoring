8aaea58105360d7fcecf7d3bc719f7a4a5673913
Chaeyun Lee <chaeyunn@iochord.com>
2021-12-18 22:23:05 +0900

    test: TableGroupService 테스트

---
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -0,0 +1,150 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.dao.TableGroupDao;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
+
+@ExtendWith(MockitoExtension.class)
+public class TableGroupServiceTest {
+
+	@InjectMocks
+	private TableGroupService tableGroupService;
+
+	@Mock
+	private OrderTableDao orderTableDao;
+
+	@Mock
+	private TableGroupDao tableGroupDao;
+
+	@Mock
+	private OrderDao orderDao;
+
+	@DisplayName("단체 지정을 생성한다")
+	@Test
+	void createTest() {
+		// given
+		TableGroup tableGroup = new TableGroup();
+		List<OrderTable> orderTableList = new ArrayList<>();
+		OrderTable orderTable1 = new OrderTable();
+		orderTable1.setId(1L);
+		orderTable1.setEmpty(true);
+		OrderTable orderTable2 = new OrderTable();
+		orderTable2.setId(2L);
+		orderTable2.setEmpty(true);
+		orderTableList.add(orderTable1);
+		orderTableList.add(orderTable2);
+		tableGroup.setOrderTables(orderTableList);
+
+		given(orderTableDao.findAllByIdIn(any())).willReturn(orderTableList);
+		given(tableGroupDao.save(any())).willReturn(tableGroup);
+
+		// when
+		tableGroup = tableGroupService.create(tableGroup);
+
+		// then
+		assertThat(tableGroup.getOrderTables().size()).isEqualTo(2);
+	}
+
+	@DisplayName("생성 시, 주문 테이블 수가 2개 이상이어야 한다")
+	@Test
+	void createTest2() {
+		// given
+		TableGroup tableGroup = new TableGroup();
+		List<OrderTable> orderTableList = new ArrayList<>();
+		OrderTable orderTable1 = new OrderTable();
+		orderTable1.setId(1L);
+		orderTable1.setEmpty(true);
+		orderTableList.add(orderTable1);
+		tableGroup.setOrderTables(orderTableList);
+
+		// when, then
+		assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("생성 시, 주문 테이블들이 모두 존재해야 한다")
+	@Test
+	void createTest3() {
+		// given
+		TableGroup tableGroup = new TableGroup();
+		List<OrderTable> orderTableList = new ArrayList<>();
+		OrderTable orderTable1 = new OrderTable();
+		orderTable1.setId(1L);
+		orderTable1.setEmpty(true);
+		OrderTable orderTable2 = new OrderTable();
+		orderTable2.setId(2L);
+		orderTable2.setEmpty(true);
+		orderTableList.add(orderTable1);
+		orderTableList.add(orderTable2);
+		tableGroup.setOrderTables(orderTableList);
+
+		given(orderTableDao.findAllByIdIn(any())).willReturn(new ArrayList<>());
+
+		// when, then
+		assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("생성 시, 테이블 인원이 0인 주문테이블은 하나라도 있으면 안된다")
+	@Test
+	void createTest4() {
+		// given
+		TableGroup tableGroup = new TableGroup();
+		List<OrderTable> orderTableList = new ArrayList<>();
+		OrderTable orderTable1 = new OrderTable();
+		orderTable1.setId(1L);
+		orderTable1.setEmpty(false);
+		OrderTable orderTable2 = new OrderTable();
+		orderTable2.setId(2L);
+		orderTable2.setEmpty(true);
+		orderTableList.add(orderTable1);
+		orderTableList.add(orderTable2);
+		tableGroup.setOrderTables(orderTableList);
+
+		given(orderTableDao.findAllByIdIn(any())).willReturn(orderTableList);
+
+		// when, then
+		assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("단체 지정을 해제한다")
+	@Test
+	void ungroupTest() {
+		// given
+		given(orderDao.existsByOrderTableIdInAndOrderStatusIn(any(), any()))
+			.willReturn(false);
+
+		// when
+		tableGroupService.ungroup(1L);
+	}
+
+	@DisplayName("단체 지정을 해제 시, 조리 중이거나 식사 중인 테이블은 안된다")
+	@Test
+	void ungroupTest2() {
+		// given
+		given(orderDao.existsByOrderTableIdInAndOrderStatusIn(any(), any()))
+			.willReturn(true);
+
+		// when
+		assertThatThrownBy(() -> tableGroupService.ungroup(1L))
+			.isInstanceOf(IllegalArgumentException.class);
+
+	}
+
+}
