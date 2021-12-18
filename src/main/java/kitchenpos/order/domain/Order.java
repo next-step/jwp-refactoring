39a4026f44e3f9a95dfea9cca6cc67d2359c4642
Arasia <rlacndtjs261@gmail.com>
2021-12-19 00:13:27 +0900

    fix: Order의 OrderTable 연관관계 수정

--- src/main/java/kitchenpos/order/domain/Order.java
+++ src/main/java/kitchenpos/order/domain/Order.java
@@ -10,9 +10,13 @@ import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
@@ -26,7 +30,8 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_table_id", foreignKey = @ForeignKey(name = "fk_order_table_order"))
     private OrderTable orderTable;
 
     @Enumerated(EnumType.STRING)
