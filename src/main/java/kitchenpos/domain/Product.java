7ea75aa0a6ba3ea1f5bc6ff4d648d576e61783e6
Chaeyun Lee <chaeyunn@iochord.com>
2021-12-18 23:47:10 +0900

    fix: 도메인 객체 원상태로 복구

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -3,42 +3,32 @@ package kitchenpos.domain;
 import java.math.BigDecimal;
 
 public class Product {
-    private Long id;
-    private String name;
-    private BigDecimal price;
+	private Long id;
+	private String name;
+	private BigDecimal price;
 
-    public Product() {
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Product(Long id, String name, BigDecimal price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
+	public void setId(final Long id) {
+		this.id = id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
+	public void setName(final String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public BigDecimal getPrice() {
+		return price;
+	}
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
-    }
-
+	public void setPrice(final BigDecimal price) {
+		this.price = price;
+	}
 
 }
--- src/test/java/kitchenpos/domain/MenuGroupTest.java
+++
@@ -1,6 +0,0 @@
-package kitchenpos.domain;
-
-public class MenuGroupTest {
-
-	public static final MenuGroup 치킨류 = new MenuGroup(1L, "치킨류");
-}
