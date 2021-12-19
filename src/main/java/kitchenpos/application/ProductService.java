b6f270bc649a73f54caea3aadfc86f0d308ec474
jerry92k <jens92.k@gmail.com>
2021-12-19 18:19:51 +0900

    chore(ProductService): 오류메세지 추가

--- src/main/java/kitchenpos/application/ProductService.java
+++ src/main/java/kitchenpos/application/ProductService.java
@@ -2,6 +2,7 @@ package kitchenpos.application;
 
 import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Product;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,24 +12,24 @@ import java.util.Objects;
 
 @Service
 public class ProductService {
-    private final ProductDao productDao;
+	private final static String ERROR_MESSAGE_PRICE_VALUE="상품의 가격은 0원 이상이어야 합니다.";
+	private final ProductDao productDao;
 
-    public ProductService(final ProductDao productDao) {
-        this.productDao = productDao;
-    }
+	public ProductService(final ProductDao productDao) {
+		this.productDao = productDao;
+	}
 
-    @Transactional
-    public Product create(final Product product) {
-        final BigDecimal price = product.getPrice();
+	@Transactional
+	public Product create(final Product product) {
+		final BigDecimal price = product.getPrice();
 
-        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException();
-        }
+		if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+			throw new IllegalArgumentException(ERROR_MESSAGE_PRICE_VALUE);
+		}
+		return productDao.save(product);
+	}
 
-        return productDao.save(product);
-    }
-
-    public List<Product> list() {
-        return productDao.findAll();
-    }
+	public List<Product> list() {
+		return productDao.findAll();
+	}
 }
