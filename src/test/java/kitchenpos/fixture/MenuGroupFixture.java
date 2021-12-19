3b5c969cf2abfaccf9a7d476b81b1a81fb332fae
Junhong-Kim <jhk1005jhk@gmail.com>
2021-12-19 13:40:31 +0900

    test: fixture 생성 리팩토링

--- src/test/java/kitchenpos/fixture/MenuGroupFixture.java
+++ src/test/java/kitchenpos/fixture/MenuGroupFixture.java
@@ -4,8 +4,8 @@ import kitchenpos.domain.MenuGroup;
 
 public class MenuGroupFixture {
 
-    public static final MenuGroup 추천_메뉴_그룹 = MenuGroupFixture.create(1L, "추천_메뉴_그룹");
-    public static final MenuGroup 시즌_메뉴_그룹 = MenuGroupFixture.create(2L, "시즌_메뉴_그룹");
+    public static final MenuGroup 추천_메뉴_그룹 = create(1L, "추천_메뉴_그룹");
+    public static final MenuGroup 시즌_메뉴_그룹 = create(2L, "시즌_메뉴_그룹");
 
     private MenuGroupFixture() {
         throw new UnsupportedOperationException();
--- src/test/java/kitchenpos/fixture/ProductFixture.java
+++ src/test/java/kitchenpos/fixture/ProductFixture.java
@@ -6,8 +6,8 @@ import java.math.BigDecimal;
 
 public class ProductFixture {
 
-    public static final Product 강정치킨 = ProductFixture.create(1L, "강정치킨", BigDecimal.valueOf(17_000));
-    public static final Product 페퍼로니피자 = ProductFixture.create(2L, "페퍼로니피자", BigDecimal.valueOf(20_000));
+    public static final Product 강정치킨 = create(1L, "강정치킨", BigDecimal.valueOf(17_000));
+    public static final Product 페퍼로니피자 = create(2L, "페퍼로니피자", BigDecimal.valueOf(20_000));
 
     private ProductFixture() {
         throw new UnsupportedOperationException();
