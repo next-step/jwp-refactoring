d1d937d6b11d657f81f42b77def561604d6c7dc5
Junhong-Kim <jhk1005jhk@gmail.com>
2021-12-19 12:27:18 +0900

    test: 메뉴 그룹 테스트 코드 추가
    - 메뉴 그룹 서비스 테스트
    - 메뉴 그룹 인수 테스트

---
+++ src/test/java/kitchenpos/acceptance/MenuGroupAcceptanceTest.java
@@ -0,0 +1,64 @@
+package kitchenpos.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.AcceptanceTest;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("메뉴 그룹 관련 기능")
+class MenuGroupAcceptanceTest extends AcceptanceTest {
+
+    private static final String API_URL = "/api/menu-groups";
+
+    @DisplayName("메뉴 그룹을 관한다.")
+    @Test
+    void manageMenuGroup() {
+        // given
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName("추천_메뉴_그룹");
+
+        // when
+        ExtractableResponse<Response> 메뉴_그룹_생성_응답 = 메뉴_그룹_생성_요청(menuGroup);
+        // then
+        메뉴_그룹_생성됨(메뉴_그룹_생성_응답);
+
+        // when
+        ExtractableResponse<Response> 메뉴_그룹_목록_조회_응답 = 메뉴_그룹_목록_조회_요청();
+        // then
+        메뉴_그룹_목록_조회됨(메뉴_그룹_목록_조회_응답);
+    }
+
+    private ExtractableResponse<Response> 메뉴_그룹_생성_요청(MenuGroup params) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post(API_URL)
+                .then().log().all()
+                .extract();
+    }
+
+    private void 메뉴_그룹_생성됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private ExtractableResponse<Response> 메뉴_그룹_목록_조회_요청() {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get(API_URL)
+                .then().log().all()
+                .extract();
+    }
+
+    private void 메뉴_그룹_목록_조회됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+}
