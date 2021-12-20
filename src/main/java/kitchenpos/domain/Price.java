4438b4611f09c3cb409d5a97eaf91a9d3fc53553
Junhwan Kim <junhwan.tech@gmail.com>
2021-12-21 01:30:45 +0900

    refactor: 가격을 Price로 원시값 포장

---
+++ src/main/java/kitchenpos/domain/Price.java
@@ -0,0 +1,53 @@
+package kitchenpos.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Price {
+
+    private static final int ZERO = 0;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    protected Price() {
+    }
+
+    private Price(BigDecimal price) {
+        validate(price);
+
+        this.price = price;
+    }
+
+    private void validate(BigDecimal price) {
+        validateNotNull(price);
+        validateNotNegative(price);
+    }
+
+    private void validateNotNull(BigDecimal price) {
+        if (Objects.isNull(price)) {
+            throw new IllegalArgumentException("가격은 null 일 수 없습니다.");
+        }
+    }
+
+    private void validateNotNegative(BigDecimal price) {
+        if (price.compareTo(BigDecimal.ZERO) < ZERO) {
+            throw new IllegalArgumentException("가격은 0보다 작을 수 없습니다.");
+        }
+    }
+
+    public static Price of(BigDecimal price) {
+        return new Price(price);
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Price multiply(BigDecimal toMultiply) {
+        return new Price(price.multiply(toMultiply));
+    }
+}
