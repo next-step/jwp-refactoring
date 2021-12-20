8b2fda7190335828f76b4642e5cab8b9fa99ff1b
moon <hoopster@airi.kr>
2021-12-20 16:29:52 +0900

    refac (OrderServiceTest, TableServiceTest) : 주문 관련 서비스 테스트 리팩터링

--- src/test/java/kitchenpos/order/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/order/application/OrderServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,36 +14,50 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import kitchenpos.menu.domain.Menu;
 import kitchenpos.menu.domain.MenuRepository;
 import kitchenpos.order.domain.Order;
-import kitchenpos.order.domain.OrderDao;
-import kitchenpos.order.domain.OrderLineItem;
-import kitchenpos.order.domain.OrderLineItemDao;
+import kitchenpos.order.domain.OrderRepository;
 import kitchenpos.order.domain.OrderStatus;
-import kitchenpos.table.domain.OrderTable;
-import kitchenpos.table.domain.OrderTableDao;
+import kitchenpos.order.domain.OrderTable;
+import kitchenpos.order.domain.OrderTableRepository;
+import kitchenpos.order.dto.OrderLineItemRequest;
+import kitchenpos.order.dto.OrderRequest;
+import kitchenpos.order.dto.OrderStatusRequest;
+import kitchenpos.order.exception.OrderException;
+import kitchenpos.tableGroup.exception.TableException;
 
 @DisplayName("주문 : 서비스 테스트")
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
 
 	@Mock
+	OrderRepository orderRepository;
+
+	@Mock
+	OrderTableRepository orderTableRepository;
+
+	@Mock
 	MenuRepository menuRepository;
-
-	@Mock
-	OrderDao orderDao;
-
-	@Mock
-	OrderTableDao orderTableDao;
-
-	@Mock
-	OrderLineItemDao orderLineItemDao;
 
 	@Mock
 	Order order;
 
 	@Mock
+	Menu menu;
+
+	@Mock
 	OrderTable orderTable;
+
+	@Mock
+	OrderLineItemRequest orderLineItemRequest;
+
+	@Mock
+	List<OrderLineItemRequest> orderLineItemRequests;
+
+	private OrderRequest orderRequest;
+
+	private OrderStatusRequest orderStatusRequest;
 
 	@InjectMocks
 	private OrderService orderService;
@@ -50,85 +65,56 @@ class OrderServiceTest {
 	@DisplayName("주문 생성시 주문 항목이 비어있는 경우 예외처리 테스트")
 	@Test
 	void createOrderEmptyOrderLineItems() {
-		// when
-		when(order.getOrderLineItems()).thenReturn(Collections.emptyList());
+		// given
+		given(orderTableRepository.findById(anyLong())).willReturn(Optional.of(orderTable));
+		orderRequest = OrderRequest.of(orderTable.getId(), Collections.emptyList());
 
-		// then
+		// when // then
 		assertThatThrownBy(() -> {
-			orderService.create(order);
-		}).isInstanceOf(IllegalArgumentException.class);
-	}
-
-	@DisplayName("주문 생성시 주문 항목이 메뉴에 존재하지 않은 경우 예외처리 테스트")
-	@Test
-	void createOrderCompareMenuSize() {
-		// when
-		when(order.getOrderLineItems()).thenReturn(Collections.singletonList(new OrderLineItem()));
-		when(menuRepository.countByIdIn(anyList())).thenReturn(3L);
-
-		// then
-		assertThatThrownBy(() -> {
-			orderService.create(order);
-		}).isInstanceOf(IllegalArgumentException.class);
+			orderService.create(orderRequest);
+		}).isInstanceOf(OrderException.class);
 	}
 
 	@DisplayName("주문 생성시 주문 테이블이 존재하지 않은 경우 예외처리 테스트")
 	@Test
 	void createOrderUnknownOrderTable() {
 		// given
-		given(order.getOrderLineItems()).willReturn(Collections.singletonList(new OrderLineItem()));
-		given(menuRepository.countByIdIn(anyList())).willReturn(1L);
+		orderRequest = OrderRequest.of(orderTable.getId(), Collections.singletonList(orderLineItemRequest));
 
 		// when
-		when(orderTableDao.findById(order.getOrderTableId())).thenThrow(IllegalArgumentException.class);
+		when(orderTableRepository.findById(anyLong())).thenThrow(TableException.class);
 
 		// then
 		assertThatThrownBy(() -> {
-			orderService.create(order);
-		}).isInstanceOf(IllegalArgumentException.class);
-	}
-
-	@DisplayName("주문 생성시 주문 테이블이 비어있는 경우 예외처리 테스트")
-	@Test
-	void createOrderEmptyOrderTable() {
-		// given
-		given(order.getOrderLineItems()).willReturn(Collections.singletonList(new OrderLineItem()));
-		given(menuRepository.countByIdIn(anyList())).willReturn(1L);
-		given(orderTableDao.findById(order.getOrderTableId())).willReturn(Optional.of(orderTable));
-
-		// when
-		when(orderTable.isEmpty()).thenReturn(true);
-
-		// then
-		assertThatThrownBy(() -> {
-			orderService.create(order);
-		}).isInstanceOf(IllegalArgumentException.class);
+			orderService.create(orderRequest);
+		}).isInstanceOf(TableException.class);
 	}
 
 	@DisplayName("주문 생성 테스트")
 	@Test
 	void createOrder() {
 		// given
-		given(order.getOrderLineItems()).willReturn(Collections.singletonList(new OrderLineItem()));
-		given(menuRepository.countByIdIn(anyList())).willReturn(1L);
-		given(orderTableDao.findById(order.getOrderTableId())).willReturn(Optional.of(orderTable));
-		given(orderTable.isEmpty()).willReturn(false);
+		given(orderTableRepository.findById(anyLong())).willReturn(Optional.of(orderTable));
+		given(menuRepository.findById(anyLong())).willReturn(Optional.of(menu));
+		given(orderLineItemRequest.getMenuId()).willReturn(1L);
+		given(orderLineItemRequest.getQuantity()).willReturn(2L);
+		orderRequest = OrderRequest.of(orderTable.getId(), Collections.singletonList(orderLineItemRequest));
 
 		// when
-		when(orderDao.save(order)).thenReturn(order);
+		when(orderRepository.save(any(Order.class))).thenReturn(order);
 
 		// then
-		assertThat(orderService.create(order)).isEqualTo(order);
+		assertThat(orderService.create(orderRequest)).isEqualTo(order);
 	}
 
 	@DisplayName("주문 목록 조회 테스트")
 	@Test
 	void getList() {
 		// given
-		given(orderLineItemDao.findAllByOrderId(anyLong())).willReturn(Collections.singletonList(new OrderLineItem()));
+		orderRequest = OrderRequest.of(orderTable.getId(), Collections.singletonList(orderLineItemRequest));
 
 		// when
-		when(orderDao.findAll()).thenReturn(Collections.singletonList(order));
+		when(orderRepository.findAll()).thenReturn(Collections.singletonList(order));
 
 		// then
 		assertThat(orderService.list()).containsExactly(order);
@@ -138,40 +124,44 @@ class OrderServiceTest {
 	@Test
 	void changeOrderStatusUnknownOrder() {
 		// when
-		when(orderDao.findById(anyLong())).thenThrow(IllegalArgumentException.class);
+		when(orderRepository.findById(anyLong())).thenThrow(OrderException.class);
+		orderStatusRequest = OrderStatusRequest.from(OrderStatus.MEAL);
 
 		// then
 		assertThatThrownBy(() -> {
-			orderService.changeOrderStatus(anyLong(), order);
-		}).isInstanceOf(IllegalArgumentException.class);
+			orderService.changeOrderStatus(anyLong(), orderStatusRequest);
+		}).isInstanceOf(OrderException.class);
 	}
 
 	@DisplayName("주문 상태 변경시 이미 완료 상태인 경우 예외처리 테스트")
 	@Test
 	void changeOrderCompletionStatus() {
 		// given
-		given(orderDao.findById(anyLong())).willReturn(Optional.of(order));
+		given(orderRepository.findById(anyLong())).willReturn(Optional.of(order));
+		orderStatusRequest = OrderStatusRequest.from(OrderStatus.MEAL);
 
 		// when
-		when(order.getOrderStatus()).thenReturn(OrderStatus.COMPLETION.name());
+		doThrow(OrderException.class)
+			.when(order)
+			.updateOrderStatus(orderStatusRequest.getOrderStatus());
 
 		// then
 		assertThatThrownBy(() -> {
-			orderService.changeOrderStatus(anyLong(), order);
-		}).isInstanceOf(IllegalArgumentException.class);
+			orderService.changeOrderStatus(anyLong(), orderStatusRequest);
+		}).isInstanceOf(OrderException.class);
 	}
 
 	@DisplayName("주문 상태 변경 테스트")
 	@Test
 	void changeOrderStatus() {
 		// given
-		given(orderDao.findById(anyLong())).willReturn(Optional.of(order));
-		given(order.getOrderStatus()).willReturn(OrderStatus.MEAL.name());
+		given(orderRepository.findById(anyLong())).willReturn(Optional.of(order));
+		orderStatusRequest = OrderStatusRequest.from(OrderStatus.MEAL);
 
 		// when
-		when(orderDao.save(order)).thenReturn(order);
+		when(orderRepository.save(order)).thenReturn(order);
 
 		// then
-		assertThat(orderService.changeOrderStatus(anyLong(), order)).isEqualTo(order);
+		assertThat(orderService.changeOrderStatus(anyLong(), orderStatusRequest)).isEqualTo(order);
 	}
 }
