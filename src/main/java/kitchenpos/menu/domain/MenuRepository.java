0626c338cc3bc7d4029e951230660267e6a84d32
moon <hoopster@airi.kr>
2021-12-20 16:34:04 +0900

    refac : 사용하지 않는 리파지토리 메소드 삭제

--- src/main/java/kitchenpos/menu/domain/MenuRepository.java
+++ src/main/java/kitchenpos/menu/domain/MenuRepository.java
@@ -1,12 +1,6 @@
 package kitchenpos.menu.domain;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-	Optional<Menu> findById(Long id);
-
-	long countByIdIn(List<Long> ids);
 }
--- src/main/java/kitchenpos/product/domain/ProductRepository.java
+++ src/main/java/kitchenpos/product/domain/ProductRepository.java
@@ -1,9 +1,6 @@
 package kitchenpos.product.domain;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	Optional<Product> findById(Long id);
 }
