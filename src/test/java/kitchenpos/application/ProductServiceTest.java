178aac2d7304409c0ad9748b007f5032127d18f8
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-12-20 14:25:18 +0900

    test(ProductServiceTest): 상품 서비스 테스트 작성

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,94 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@DisplayName("상품 서비스 테스트")
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
+
+    @Mock
+    private ProductDao productDao;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @DisplayName("상품을 등록할 수 있다.")
+    @Test
+    void create() {
+        // given
+        final Product request = createProductRequest("후라이드", BigDecimal.valueOf(16_000));
+        final Product expected = createProduct(1L, "후라이드", BigDecimal.valueOf(16_000));
+        given(productDao.save(request)).willReturn(expected);
+
+        // when
+        final Product actual = productService.create(request);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("상품 가격이 null 이거나 0보다 작은 경우 상품을 등록할 수 없다.")
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"-16000"})
+    void create_fail_invalidPrice(final BigDecimal price) {
+        // given
+        final Product request = createProductRequest("후라이드", price);
+
+        // when
+        ThrowableAssert.ThrowingCallable actual = () -> productService.create(request);
+
+        // then
+        assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 목록을 조회할 수 있다.")
+    @Test
+    void list() {
+        // given
+        final List<Product> expected = Arrays.asList(
+            createProduct(1L, "후라이드", BigDecimal.valueOf(16_000)),
+            createProduct(2L, "양념치킨", BigDecimal.valueOf(16_000))
+        );
+        given(productDao.findAll()).willReturn(expected);
+
+        // when
+        final List<Product> actual = productService.list();
+
+        // then
+        assertThat(actual).containsExactlyElementsOf(expected);
+    }
+
+    private Product createProductRequest(final String name, final BigDecimal price) {
+        final Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        return product;
+    }
+
+    private Product createProduct(final Long id, final String name, final BigDecimal price) {
+        final Product product = new Product();
+        product.setId(id);
+        product.setName(name);
+        product.setPrice(price);
+        return product;
+    }
+}
