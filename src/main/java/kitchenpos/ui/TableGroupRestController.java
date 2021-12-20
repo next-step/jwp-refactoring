794b28ddf8211a67dc75bb03a4de29ad5d685578
jerry92k <jens92.k@gmail.com>
2021-12-20 22:45:36 +0900

    refactor(TableGroupRestController): base url 클래스 레벨 지정 및 content 인코딩 설정

--- src/main/java/kitchenpos/ui/TableGroupRestController.java
+++ src/main/java/kitchenpos/ui/TableGroupRestController.java
@@ -2,33 +2,33 @@ package kitchenpos.ui;
 
 import kitchenpos.application.TableGroupService;
 import kitchenpos.domain.TableGroup;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
 @RestController
+@RequestMapping(value = "/api/table-groups", produces = "application/json; charset=utf8")
 public class TableGroupRestController {
-    private final TableGroupService tableGroupService;
+	private final TableGroupService tableGroupService;
 
-    public TableGroupRestController(final TableGroupService tableGroupService) {
-        this.tableGroupService = tableGroupService;
-    }
+	public TableGroupRestController(final TableGroupService tableGroupService) {
+		this.tableGroupService = tableGroupService;
+	}
 
-    @PostMapping("/api/table-groups")
-    public ResponseEntity<TableGroup> create(@RequestBody final TableGroup tableGroup) {
-        final TableGroup created = tableGroupService.create(tableGroup);
-        final URI uri = URI.create("/api/table-groups/" + created.getId());
-        return ResponseEntity.created(uri)
-                .body(created)
-                ;
-    }
+	@PostMapping
+	public ResponseEntity<TableGroup> create(@RequestBody final TableGroup tableGroup) {
+		final TableGroup created = tableGroupService.create(tableGroup);
+		final URI uri = URI.create("/api/table-groups/" + created.getId());
+		return ResponseEntity.created(uri)
+			.body(created);
+	}
 
-    @DeleteMapping("/api/table-groups/{tableGroupId}")
-    public ResponseEntity<Void> ungroup(@PathVariable final Long tableGroupId) {
-        tableGroupService.ungroup(tableGroupId);
-        return ResponseEntity.noContent()
-                .build()
-                ;
-    }
+	@DeleteMapping("/{tableGroupId}")
+	public ResponseEntity<Void> ungroup(@PathVariable final Long tableGroupId) {
+		tableGroupService.ungroup(tableGroupId);
+		return ResponseEntity.noContent()
+			.build();
+	}
 }
