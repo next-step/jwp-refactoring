2ed6ca8028533e912f3bb73d3dae2a9e3caa5705
HeeSeok Noh <nhs0912@nate.com>
2021-12-20 23:47:31 +0900

    test : product 인수테스트 작성

---
+++ src/test/java/kitchenpos/ui/ProductRestControllerTest.java
@@ -0,0 +1,18 @@
+package kitchenpos.ui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRestControllerTest {
+
+    @Test
+    void create() {
+
+    }
+
+    @Test
+    void list() {
+
+    }
+}
