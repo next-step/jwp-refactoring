d90fb2f9bcf55bd4f1a5b94daf7c92c43458cc06
shinmj <gkdlqkek@gmail.com>
2021-12-20 17:45:49 +0900

    chore: 텍스트 수정

--- src/test/java/kitchenpos/product/acceptance/ProductAcceptanceTest.java
+++ src/test/java/kitchenpos/product/acceptance/ProductAcceptanceTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+@DisplayName("상품 인수 테스트")
 public class ProductAcceptanceTest extends AcceptanceTest {
 
     private static final String PRODUCT_URL = "/api/products";
