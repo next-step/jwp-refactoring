9a4910860b89c013e49bc090f152042c8936ff32
jerry92k <jens92.k@gmail.com>
2021-12-21 08:30:51 +0900

    test: 불필요한 @Transactional 어노테이션 제거

--- src/test/java/kitchenpos/ui/IntegrationTest.java
+++ src/test/java/kitchenpos/ui/IntegrationTest.java
@@ -2,12 +2,10 @@ package kitchenpos.ui;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-@Transactional
 abstract public class IntegrationTest {
 
 }
