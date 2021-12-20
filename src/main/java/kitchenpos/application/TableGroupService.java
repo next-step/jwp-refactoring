af8864c8929ab74cddf0536e5f50835ae04d5489
jerry92k <jens92.k@gmail.com>
2021-12-20 11:03:40 +0900

    feat(TestGroupService): 생성자 추가, 오류메세지 작성 및 Business Object Test 작성

--- src/main/java/kitchenpos/application/TableGroupService.java
+++ src/main/java/kitchenpos/application/TableGroupService.java
@@ -18,6 +18,10 @@ import java.util.stream.Collectors;
 
 @Service
 public class TableGroupService {
+    private static final String ERROR_MESSAGE_NOT_ENOUGH_TABLES = "주문 테이블이 2개 이상일때 그룹화 가능 합니다.";
+    private static final String ERROR_MESSAGE_NOT_EMPTY_TABLE = "주문 테이블들은 빈 테이블이어야 합니다.";
+    private static final String ERROR_MESSAGE_TABLE_ALREADY_IN_GROUP = "테이블 그룹에 속해있는 주문테이블이 포함되면 안된다.";
+    private static final String ERROR_MESSAGE_ORDER_NOT_COMPLETE = "조리, 식사중인 주문이 존재하는 테이블이 있습니다.";
     private final OrderDao orderDao;
     private final OrderTableDao orderTableDao;
     private final TableGroupDao tableGroupDao;
@@ -33,7 +37,7 @@ public class TableGroupService {
         final List<OrderTable> orderTables = tableGroup.getOrderTables();
 
         if (CollectionUtils.isEmpty(orderTables) || orderTables.size() < 2) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_ENOUGH_TABLES);
         }
 
         final List<Long> orderTableIds = orderTables.stream()
@@ -47,8 +51,11 @@ public class TableGroupService {
         }
 
         for (final OrderTable savedOrderTable : savedOrderTables) {
-            if (!savedOrderTable.isEmpty() || Objects.nonNull(savedOrderTable.getTableGroupId())) {
-                throw new IllegalArgumentException();
+            if (!savedOrderTable.isEmpty()) {
+                throw new IllegalArgumentException(ERROR_MESSAGE_NOT_EMPTY_TABLE);
+            }
+            if(Objects.nonNull(savedOrderTable.getTableGroupId())){
+                throw new IllegalArgumentException(ERROR_MESSAGE_TABLE_ALREADY_IN_GROUP);
             }
         }
 
@@ -77,7 +84,7 @@ public class TableGroupService {
 
         if (orderDao.existsByOrderTableIdInAndOrderStatusIn(
                 orderTableIds, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE_ORDER_NOT_COMPLETE);
         }
 
         for (final OrderTable orderTable : orderTables) {
