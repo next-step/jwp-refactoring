23489a6ef3d4173fb9d824851e76dfa73866c904
jerry92k <jens92.k@gmail.com>
2021-12-20 17:42:18 +0900

    refactor: 통합테스트 환경 구성 추상화

---
+++ src/test/java/kitchenpos/ui/IntegrationTest.java
@@ -0,0 +1,12 @@
+package kitchenpos.ui;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(
+	webEnvironment=	SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@Transactional
+abstract public class IntegrationTest {
+}
--- src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
@@ -1,30 +1,21 @@
 package kitchenpos.ui;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -32,12 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import kitchenpos.application.MenuGroupService;
 import kitchenpos.domain.MenuGroup;
 
-
-@SpringBootTest(
-	webEnvironment=	SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
-@Transactional
-class MenuGroupRestControllerTest {
+class MenuGroupRestControllerTest extends IntegrationTest{
 
 	private static final String BASE_PATH="/api/menu-groups";
 
@@ -53,7 +39,7 @@ class MenuGroupRestControllerTest {
 	@Test
 	void create() throws Exception {
 		//given
-		Map<String,String> menuGroup = 메뉴그룹_정의("추천메뉴");
+		Map<String,String> menuGroup = 메뉴그룹_정보_생성("추천메뉴");
 		MenuGroup expectedMenuGroup = new MenuGroup(1L,menuGroup.get("name"));
 		given(service.create(any(MenuGroup.class)))
 			.willReturn(expectedMenuGroup);
@@ -73,8 +59,7 @@ class MenuGroupRestControllerTest {
 	@Test
 	void list() throws Exception {
 		//given
-		Map<String,String> menuGroup = 메뉴그룹_정의("추천메뉴");
-		MenuGroup expectedMenuGroup = new MenuGroup(1L,menuGroup.get("name"));
+		Map<String,String> menuGroup = 메뉴그룹_정보_생성("추천메뉴");
 
 		List<MenuGroup> expectedMenuGroups = Arrays.asList(
 			new MenuGroup(1L,menuGroup.get("추천메뉴")),
@@ -93,7 +78,7 @@ class MenuGroupRestControllerTest {
 		assertThat(findMenuGroups).containsAll(expectedMenuGroups);
 	}
 
-	private Map<String, String> 메뉴그룹_정의(String name) {
+	private Map<String, String> 메뉴그룹_정보_생성(String name) {
 		Map<String,String> params = new HashMap<>();
 		params.put("name",name);
 		return params;
