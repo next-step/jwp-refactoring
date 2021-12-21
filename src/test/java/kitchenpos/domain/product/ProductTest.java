bfc87661e5c67e5ca78df25487c41956fe54edfb
wooobo <irooori89@gmail.com>
2021-12-21 21:40:15 +0900

    test : `ProductTest`도메인 테스트 추가 및 `ProductServiceTest` 에러 발생하는 테스트 코드 수정

---
+++ src/test/java/kitchenpos/domain/product/ProductTest.java
@@ -0,0 +1,21 @@
+package kitchenpos.domain.product;
+
+import static kitchenpos.application.fixture.ProductFixture.마이너스_가격_상품;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+
+    @Test
+    @DisplayName("상품의 가격은 0원 미만(음수)이면 상품을 등록 할 수 없다.")
+    void 상품_가격이_음수인경우_실패한다() {
+        // when
+        ThrowableAssert.ThrowingCallable actual = () -> 마이너스_가격_상품();
+
+        // then
+        assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
+    }
+}
