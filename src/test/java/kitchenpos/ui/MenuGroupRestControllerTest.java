caa0bb400e23cbda5a3fa816861b56bca8c33677
jerry92k <jens92.k@gmail.com>
2021-12-21 10:33:39 +0900

    refactor: Controller 테스트 격리성 강화와 속도 향상을 위해 WebMvcTest 사용으로 변경

--- src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
@@ -17,11 +17,13 @@ import kitchenpos.ui.testfixture.CommonTestFixtures;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-class MenuGroupRestControllerTest extends IntegrationTest {
+@WebMvcTest(MenuGroupRestController.class)
+class MenuGroupRestControllerTest {
 
     private static final String BASE_PATH = "/api/menu-groups";
 
--- src/test/java/kitchenpos/ui/MenuRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuRestControllerTest.java
@@ -19,11 +19,13 @@ import kitchenpos.ui.testfixture.CommonTestFixtures;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-class MenuRestControllerTest extends IntegrationTest {
+@WebMvcTest(MenuRestController.class)
+class MenuRestControllerTest {
 
     private static final String BASE_PATH = "/api/menus";
 
