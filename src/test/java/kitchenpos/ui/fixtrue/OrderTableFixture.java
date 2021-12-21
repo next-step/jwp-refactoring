0eb3454f47527bfbf7e2f31dbafe1f4df450b552
donkey <rlaehdzlsla@gmail.com>
2021-12-21 17:52:58 +0900

    test: TableRestControllerTest, Fixture 추가

---
+++ src/test/java/kitchenpos/ui/fixtrue/OrderTableFixture.java
@@ -0,0 +1,19 @@
+package kitchenpos.ui.fixtrue;
+
+import kitchenpos.domain.OrderTable;
+
+public class OrderTableFixture {
+
+    private OrderTableFixture() {
+
+    }
+
+    public static OrderTable of(Long id, Long tableGroupId, int numberOfGuests, boolean empty) {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setId(id);
+        orderTable.setTableGroupId(tableGroupId);
+        orderTable.setNumberOfGuests(numberOfGuests);
+        orderTable.setEmpty(empty);
+        return orderTable;
+    }
+}
