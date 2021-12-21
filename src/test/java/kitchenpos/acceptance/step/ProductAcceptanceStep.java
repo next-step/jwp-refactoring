4b3a5261e42c3f73a1bc385a9eb7c9e7d7018203
wooobo <irooori89@gmail.com>
2021-12-21 19:04:02 +0900

    test : 수정된 `Service`에 맞추어 `Product` 인수테스트 수정

--- src/test/java/kitchenpos/acceptance/step/ProductAcceptanceStep.java
+++ src/test/java/kitchenpos/acceptance/step/ProductAcceptanceStep.java
@@ -6,8 +6,11 @@ import io.restassured.RestAssured;
 import io.restassured.mapper.TypeRef;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.math.BigDecimal;
 import java.util.List;
 import kitchenpos.domain.product.Product;
+import kitchenpos.dto.product.ProductRequest;
+import kitchenpos.dto.product.ProductResponse;
 import org.springframework.http.MediaType;
 
 public class ProductAcceptanceStep {
@@ -17,16 +20,16 @@ public class ProductAcceptanceStep {
     private ProductAcceptanceStep() {
     }
 
-    public static Product 상품_등록됨(Product product) {
-        ExtractableResponse<Response> 상품_등록_요청 = 상품_등록_요청(product);
+    public static Long 상품_등록됨(ProductRequest request) {
+        ExtractableResponse<Response> 상품_등록_요청 = 상품_등록_요청(request);
 
-        return 상품등록_검증(상품_등록_요청, product);
+        return 상품등록_검증(상품_등록_요청, request);
     }
 
-    public static ExtractableResponse<Response> 상품_등록_요청(Product product) {
+    public static ExtractableResponse<Response> 상품_등록_요청(ProductRequest request) {
         return RestAssured
             .given().log().all()
-            .body(product)
+            .body(request)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .when().post(API_URL)
             .then().log().all()
@@ -43,22 +46,28 @@ public class ProductAcceptanceStep {
     }
 
 
-    public static List<Product> 상품_목록조회_검증(ExtractableResponse<Response> response,
-        Product expected) {
-        List<Product> 조회된_상품목록 = response.as(new TypeRef<List<Product>>() {
+    public static List<ProductResponse> 상품_목록조회_검증(ExtractableResponse<Response> response,
+        Long expected) {
+        List<ProductResponse> 조회된_상품목록 = response.as(new TypeRef<List<ProductResponse>>() {
         });
-        assertThat(조회된_상품목록).contains(expected);
+
+        assertThat(조회된_상품목록).extracting("id").contains(expected);
 
         return 조회된_상품목록;
     }
 
 
-    public static Product 상품등록_검증(ExtractableResponse<Response> response, Product expected) {
-        Product 등록된_상품 = response.as(Product.class);
+    public static Long 상품등록_검증(ExtractableResponse<Response> response,
+        ProductRequest expected) {
+        ProductResponse 등록된_상품 = response.as(ProductResponse.class);
         assertThat(등록된_상품.getId()).isNotNull();
         assertThat(등록된_상품.getName()).isEqualTo(expected.getName());
         assertThat(등록된_상품.getPrice()).isEqualByComparingTo(expected.getPrice());
 
-        return 등록된_상품;
+        return 등록된_상품.getId();
+    }
+
+    public static ProductRequest 양념치킨() {
+        return new ProductRequest("양념치킨", BigDecimal.valueOf(16000));
     }
 }
