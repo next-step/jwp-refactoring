aa20d3926e8d6a892356e97beb308fe4c3d9a8f8
JiHun Hong <hongji3354@gmail.com>
2021-12-21 15:10:53 +0900

    test : 메뉴 그룹 관련 기능

---
+++ src/test/java/kitchenpos/domain/menugroup/MenuGroupAcceptanceTest.java
@@ -0,0 +1,44 @@
+package kitchenpos.domain.menugroup;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.AcceptanceTest;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("메뉴 그룹 관련 기능")
+public class MenuGroupAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    @DisplayName("메뉴 그룹을 등록할 수 있다.")
+    void createMenuGroup() {
+        // given
+        MenuGroup 추천메뉴 = new MenuGroup("추천메뉴");
+
+        // when
+        ExtractableResponse<Response> 메뉴_그룹_등록_요청_응답 = 메뉴_그룹_등록을_요청(추천메뉴);
+
+        // then
+        메뉴_그룹_등록됨(메뉴_그룹_등록_요청_응답);
+    }
+
+    private void 메뉴_그룹_등록됨(ExtractableResponse<Response> response) {
+        MenuGroup 등록된_메뉴_그룹 = response.as(MenuGroup.class);
+        assertThat(등록된_메뉴_그룹.getId()).isNotNull();
+        assertThat(등록된_메뉴_그룹.getName()).isEqualTo("추천메뉴");
+    }
+
+    public ExtractableResponse<Response> 메뉴_그룹_등록을_요청(MenuGroup menuGroup) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(menuGroup)
+                .when().post("/api/menu-groups")
+                .then().log().all()
+                .extract();
+    }
+}
---
+++ src/test/java/kitchenpos/domain/menugroup/MenuGroupTest.java
@@ -0,0 +1,68 @@
+package kitchenpos.domain.menugroup;
+
+import kitchenpos.application.MenuGroupService;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.anyOf;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("메뉴 그룹 관련 기능")
+class MenuGroupTest {
+
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    @Test
+    @DisplayName("메뉴 그룹 이름이 비어있을 경우 예외가 발생한다.")
+    void emptyMenuGroupName() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            menuGroupService.create(new MenuGroup(" "));
+        });
+    }
+
+    @Test
+    @DisplayName("메뉴 그룹을 등록할 수 있다.")
+    void createMenuGroup() {
+        // given
+        MenuGroup menuGroup = new MenuGroup(1L, "추천메뉴");
+        when(menuGroupDao.save(any())).thenReturn(menuGroup);
+
+        // when
+        MenuGroup savedMenuGroup = menuGroupService.create(menuGroup);
+
+        // then
+        assertThat(savedMenuGroup.getId()).isEqualTo(1L);
+        assertThat(savedMenuGroup.getName()).isEqualTo("추천메뉴");
+    }
+
+    @Test
+    @DisplayName("메뉴 그룹을 조회할 수 있다.")
+    void findMenuGroup() {
+        // given
+        List<MenuGroup> menuGroups = Arrays.asList(new MenuGroup(1L, "추천메뉴1"), new MenuGroup(2L, "추천메뉴2"));
+        when(menuGroupDao.findAll()).thenReturn(menuGroups);
+
+        // when
+        List<MenuGroup> findByMenuGroups = menuGroupService.list();
+
+        // then
+        assertThat(findByMenuGroups.size()).isEqualTo(2);
+    }
+}
