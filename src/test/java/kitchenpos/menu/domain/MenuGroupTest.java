85f2a57fee97d977cd9aa3b8d51735f5e3c7f4d1
Arasia <rlacndtjs261@gmail.com>
2021-12-21 23:14:25 +0900

    fix: 메뉴 그룹 생성 조건 추가

--- src/test/java/kitchenpos/menu/domain/MenuGroupTest.java
+++ src/test/java/kitchenpos/menu/domain/MenuGroupTest.java
@@ -2,10 +2,12 @@ package kitchenpos.menu.domain;
 
 import kitchenpos.common.domain.Name;
 import kitchenpos.common.domain.fixture.NameFixture;
-import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("메뉴 그룹 테스트")
 class MenuGroupTest {
@@ -19,6 +21,16 @@ class MenuGroupTest {
         ThrowableAssert.ThrowingCallable 생성_요청 = () -> MenuGroup.of(메뉴_그룹_이름);
 
         // then
-        Assertions.assertThatNoException().isThrownBy(생성_요청);
+        assertThatNoException().isThrownBy(생성_요청);
+    }
+
+    @DisplayName("메뉴 그룹 이름은 비어있을 수 없음")
+    @Test
+    void 메뉴_그룹_이름은_비어있을_수_없음() {
+        // when
+        ThrowableAssert.ThrowingCallable 생성_요청 = () -> MenuGroup.of(null);
+
+        // then
+        assertThatThrownBy(생성_요청).isInstanceOf(IllegalArgumentException.class);
     }
 }
