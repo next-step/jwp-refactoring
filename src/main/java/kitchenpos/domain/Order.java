d9f6c0bb1f30d4f3dee752bdc835b03b2f7879a9
jerry92k <jens92.k@gmail.com>
2021-12-21 09:58:27 +0900

    feat: 생성자 추가

--- src/main/java/kitchenpos/domain/Order.java
+++ src/main/java/kitchenpos/domain/Order.java
@@ -5,88 +5,93 @@ import java.util.List;
 import java.util.Objects;
 
 public class Order {
-	private Long id;
-	private Long orderTableId;
-	private String orderStatus;
-	private LocalDateTime orderedTime;
-	private List<OrderLineItem> orderLineItems;
 
-	public Long getId() {
-		return id;
-	}
+    private Long id;
+    private Long orderTableId;
+    private String orderStatus;
+    private LocalDateTime orderedTime;
+    private List<OrderLineItem> orderLineItems;
 
-	public void setId(final Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Order() {
-	}
+    public void setId(final Long id) {
+        this.id = id;
+    }
 
-	public Order(String orderStatus) {
-		this.orderStatus=orderStatus;
-	}
+    public Order() {
+    }
 
-	public Order(Long orderTableId, LocalDateTime orderedTime, List<OrderLineItem> orderLineItems) {
-		this(null, orderTableId, OrderStatus.COOKING.name(), orderedTime, orderLineItems);
-	}
+    public Order(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
 
-	public Order(Long id, Long orderTableId, String orderStatus, LocalDateTime orderedTime,
-		List<OrderLineItem> orderLineItems) {
-		this.id = id;
-		this.orderTableId = orderTableId;
-		this.orderStatus = orderStatus;
-		this.orderedTime = orderedTime;
-		this.orderLineItems = orderLineItems;
-	}
+    public Order(Long orderTableId, List<OrderLineItem> orderLineItems) {
+        this(orderTableId, null, orderLineItems);
+    }
 
-	public Long getOrderTableId() {
-		return orderTableId;
-	}
+    public Order(Long orderTableId, LocalDateTime orderedTime, List<OrderLineItem> orderLineItems) {
+        this(null, orderTableId, OrderStatus.COOKING.name(), orderedTime, orderLineItems);
+    }
 
-	public void setOrderTableId(final Long orderTableId) {
-		this.orderTableId = orderTableId;
-	}
+    public Order(Long id, Long orderTableId, String orderStatus, LocalDateTime orderedTime,
+        List<OrderLineItem> orderLineItems) {
+        this.id = id;
+        this.orderTableId = orderTableId;
+        this.orderStatus = orderStatus;
+        this.orderedTime = orderedTime;
+        this.orderLineItems = orderLineItems;
+    }
 
-	public String getOrderStatus() {
-		return orderStatus;
-	}
+    public Long getOrderTableId() {
+        return orderTableId;
+    }
 
-	public void setOrderStatus(final String orderStatus) {
-		this.orderStatus = orderStatus;
-	}
+    public void setOrderTableId(final Long orderTableId) {
+        this.orderTableId = orderTableId;
+    }
 
-	public LocalDateTime getOrderedTime() {
-		return orderedTime;
-	}
+    public String getOrderStatus() {
+        return orderStatus;
+    }
 
-	public void setOrderedTime(final LocalDateTime orderedTime) {
-		this.orderedTime = orderedTime;
-	}
+    public void setOrderStatus(final String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
 
-	public List<OrderLineItem> getOrderLineItems() {
-		return orderLineItems;
-	}
+    public LocalDateTime getOrderedTime() {
+        return orderedTime;
+    }
 
-	public void setOrderLineItems(final List<OrderLineItem> orderLineItems) {
-		this.orderLineItems = orderLineItems;
-	}
+    public void setOrderedTime(final LocalDateTime orderedTime) {
+        this.orderedTime = orderedTime;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
+    public List<OrderLineItem> getOrderLineItems() {
+        return orderLineItems;
+    }
 
-		if (!(o instanceof Order)) {
-			return false;
-		}
+    public void setOrderLineItems(final List<OrderLineItem> orderLineItems) {
+        this.orderLineItems = orderLineItems;
+    }
 
-		Order order = (Order)o;
-		return Objects.equals(getId(), order.getId());
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(getId());
-	}
+        if (!(o instanceof Order)) {
+            return false;
+        }
+
+        Order order = (Order) o;
+        return Objects.equals(getId(), order.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
