04ef6d7fb501d6f6756c649a341a54d0bedb9c64
Junhwan Kim <junhwan.tech@gmail.com>
2021-12-21 20:33:45 +0900

    refactor: TableFixture 생성시 OrderTable의 생성자를 호출하도록 수정

--- src/test/java/kitchenpos/application/fixture/TableFixture.java
+++ src/test/java/kitchenpos/application/fixture/TableFixture.java
@@ -7,13 +7,8 @@ public class TableFixture {
     private TableFixture() {
     }
 
-    public static OrderTable create(final long id, final Long tableGroupId,
+    public static OrderTable create(final Long id, final Long tableGroupId,
         final int numberOfGuests, final boolean empty) {
-        OrderTable orderTable = new OrderTable();
-        orderTable.setId(id);
-        orderTable.setTableGroupId(tableGroupId);
-        orderTable.setNumberOfGuests(numberOfGuests);
-        orderTable.setEmpty(empty);
-        return orderTable;
+        return new OrderTable(id, tableGroupId, numberOfGuests, empty);
     }
 }
