d43e406d687d78123a512ed619d72e72d71b1513
donkey <rlaehdzlsla@gmail.com>
2021-12-21 16:28:41 +0900

    test: MenuRestControllerTest, Fixture 추가

---
+++ src/test/java/kitchenpos/ui/fixtrue/MenuFixture.java
@@ -0,0 +1,24 @@
+package kitchenpos.ui.fixtrue;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+public class MenuFixture {
+
+    private MenuFixture() {
+
+    }
+
+    public static Menu of(long id, String name, BigDecimal price, Long menuGroupId, MenuProduct... menuProducts) {
+        Menu menu = new Menu();
+        menu.setId(id);
+        menu.setName(name);
+        menu.setPrice(price);
+        menu.setMenuGroupId(menuGroupId);
+        menu.setMenuProducts(Arrays.asList(menuProducts));
+        return menu;
+    }
+}
---
+++ src/test/java/kitchenpos/ui/fixtrue/MenuProductFixture.java
@@ -0,0 +1,18 @@
+package kitchenpos.ui.fixtrue;
+
+import kitchenpos.domain.MenuProduct;
+
+public class MenuProductFixture {
+
+    private MenuProductFixture() {
+    }
+
+    public static MenuProduct of(Long seq, Long menuId, Long productId, long quantity) {
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setSeq(seq);
+        menuProduct.setMenuId(menuId);
+        menuProduct.setProductId(productId);
+        menuProduct.setQuantity(quantity);
+        return menuProduct;
+    }
+}
