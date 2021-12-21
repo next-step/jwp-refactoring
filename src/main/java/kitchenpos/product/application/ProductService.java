d949a6baad9f1fd3139f76bf8036ea39350c1b74
songseongbin <ppsosqq@gmail.com>
2021-12-21 23:52:54 +0900

    refactor(ProductRestController, ProductService): 패키지이동
    - request, response 추가

--- src/main/java/kitchenpos/product/application/ProductService.java
+++ src/main/java/kitchenpos/product/application/ProductService.java
@@ -1,10 +1,11 @@
-package kitchenpos.application;
+package kitchenpos.product.application;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import kitchenpos.product.domain.Product;
 import kitchenpos.product.domain.ProductRepository;
+import kitchenpos.product.dto.ProductResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,14 +19,14 @@ public class ProductService {
     }
 
     @Transactional
-    public Product create(final Product product) {
+    public ProductResponse create(final Product product) {
         final BigDecimal price = product.getPrice();
 
         if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException();
         }
 
-        return productRepository.save(product);
+        return ProductResponse.from(productRepository.save(product));
     }
 
     public List<Product> list() {
