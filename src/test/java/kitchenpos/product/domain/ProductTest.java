0fe8f550a70ab1efa7d6f940da55e52d760c28a0
Arasia <rlacndtjs261@gmail.com>
2021-12-21 23:13:33 +0900

    fix: Product 생성 조건 추가

--- src/test/java/kitchenpos/product/domain/ProductTest.java
+++ src/test/java/kitchenpos/product/domain/ProductTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("상품 테스트")
 class ProductTest {
@@ -26,5 +27,31 @@ class ProductTest {
 
         // then
         assertThatNoException().isThrownBy(생성_요청);
+    }
+
+    @DisplayName("상품 이름은 비어있을 수 없음")
+    @Test
+    void 상품_이름은_비어있을_수_없음() {
+        // given
+        Price 상품_가격 = PriceFixture.of(BigDecimal.valueOf(17_000L));
+
+        // when
+        ThrowableAssert.ThrowingCallable 생성_요청 = () -> Product.of(null, 상품_가격);
+
+        // then
+        assertThatThrownBy(생성_요청).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 가격은 비어있을 수 없음")
+    @Test
+    void 상품_가격은_비어있을_수_없음() {
+        // given
+        Name 상품_이름 = NameFixture.of("강정치킨");
+
+        // when
+        ThrowableAssert.ThrowingCallable 생성_요청 = () -> Product.of(상품_이름, null);
+
+        // then
+        assertThatThrownBy(생성_요청).isInstanceOf(IllegalArgumentException.class);
     }
 }
