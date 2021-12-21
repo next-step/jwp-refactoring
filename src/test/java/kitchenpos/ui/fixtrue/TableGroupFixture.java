b1346c5ae5c37fb18ef2956292f9dabd6aded5d1
donkey <rlaehdzlsla@gmail.com>
2021-12-21 18:23:53 +0900

    test: TableGroupRestControllerTest, Fixture 추가

---
+++ src/test/java/kitchenpos/ui/fixtrue/TableGroupFixture.java
@@ -0,0 +1,20 @@
+package kitchenpos.ui.fixtrue;
+
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
+
+import java.util.Arrays;
+
+public class TableGroupFixture {
+
+    private TableGroupFixture() {
+    }
+
+    public static TableGroup of(Long id, OrderTable... orderTables) {
+        TableGroup tableGroup = new TableGroup();
+        tableGroup.setId(id);
+        tableGroup.setCreatedDate(null);
+        tableGroup.setOrderTables(Arrays.asList(orderTables));
+        return tableGroup;
+    }
+}
