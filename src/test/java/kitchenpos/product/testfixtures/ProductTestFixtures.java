12c66a563a9915952fc6b82799eac1a78800c1c6
jerry92k <jens92.k@gmail.com>
2021-12-22 18:09:29 +0900

    refactor: 테스트 픽쳐스 수정

--- src/test/java/kitchenpos/product/testfixtures/ProductTestFixtures.java
+++ src/test/java/kitchenpos/product/testfixtures/ProductTestFixtures.java
@@ -4,15 +4,15 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import java.util.List;
-import java.util.Optional;
+import kitchenpos.product.application.ProductService;
 import kitchenpos.product.domain.Product;
 import kitchenpos.product.domain.ProductDao;
 
 public class ProductTestFixtures {
 
-    public static void 상품_조회시_응답_모킹(ProductDao productDao, Product product) {
-        given(productDao.findById(product.getId()))
-            .willReturn(Optional.ofNullable(product));
+    public static void 상품_조회시_응답_모킹(ProductService productService, Product product) {
+        given(productService.findProduct(product.getId()))
+            .willReturn(product);
     }
 
     public static void 상품_생성_결과_모킹(ProductDao productDao, Product product) {
