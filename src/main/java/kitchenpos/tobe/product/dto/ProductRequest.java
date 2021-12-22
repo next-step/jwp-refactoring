835feb7e8e9f8e6ff64b79c2ed197ea4287255d1
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-12-22 21:43:39 +0900

    refactor(Product): 상품 어플리케이션 레이어 리팩토링

---
+++ src/main/java/kitchenpos/tobe/product/dto/ProductRequest.java
@@ -0,0 +1,30 @@
+package kitchenpos.tobe.product.dto;
+
+import java.math.BigDecimal;
+import kitchenpos.tobe.product.domain.Name;
+import kitchenpos.tobe.product.domain.Price;
+import kitchenpos.tobe.product.domain.Product;
+
+public class ProductRequest {
+
+    private final String name;
+
+    private final BigDecimal price;
+
+    public ProductRequest(final String name, final BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product toProduct() {
+        return new Product(new Name(name), new Price(price));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
--- src/test/java/kitchenpos/tobe/fixture/ProductFixture.java
+++ src/test/java/kitchenpos/tobe/fixture/ProductFixture.java
@@ -1,8 +1,10 @@
 package kitchenpos.tobe.fixture;
 
+import java.math.BigDecimal;
 import kitchenpos.tobe.product.domain.Name;
 import kitchenpos.tobe.product.domain.Price;
 import kitchenpos.tobe.product.domain.Product;
+import kitchenpos.tobe.product.dto.ProductRequest;
 
 public class ProductFixture {
 
@@ -15,5 +17,13 @@ public class ProductFixture {
 
     public static Product of(final Name name, final Price price) {
         return of(null, name, price);
+    }
+
+    public static Product of(final Long id, final String name, final BigDecimal price) {
+        return of(id, new Name(name), new Price(price));
+    }
+
+    public static ProductRequest ofRequest(final String name, final BigDecimal price) {
+        return new ProductRequest(name, price);
     }
 }
