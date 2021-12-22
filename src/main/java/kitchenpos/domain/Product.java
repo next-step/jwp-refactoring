180cc0db83c2d4d0ac3446f998545af8d9041fb4
lewisseo91 <togokr@naver.com>
2021-12-23 01:34:03 +0900

    feat(Product): mock 테스트 과도기 - 상품 서비스 관련 entity 추가, dao -> repo로 변경
    - mock 테스트로 과도기 설정 (아직 mock으로 테스트 살아있게 함)

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -1,13 +1,29 @@
 package kitchenpos.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Objects;
 
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private BigDecimal price;
 
     public Product() {
+    }
+
+    public Product(String name, BigDecimal price) {
+        validatePrice(price);
+        this.name = name;
+        this.price = price;
     }
 
     public Product(long id, String name, int price) {
@@ -15,9 +31,16 @@ public class Product {
     }
 
     public Product(long id, String name, BigDecimal price) {
+        validatePrice(price);
         this.id = id;
         this.name = name;
         this.price = price;
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public Long getId() {
--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,12 +1,15 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Product;
+import kitchenpos.domain.ProductRepository;
+import kitchenpos.dto.ProductRequest;
+import kitchenpos.dto.ProductResponse;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -23,28 +26,33 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
     @Mock
-    private ProductDao productDao;
+    private ProductRepository productRepository;
 
+    @InjectMocks
     private ProductService productService;
+
     private Product product;
     private Product product2;
-    private Product productZeroPrice;
+
+    private ProductRequest productRequest;
+    private ProductRequest productRequestZeroPrice;
 
     @BeforeEach
     void setUp() {
-        productService = new ProductService(productDao);
         product = new Product(1L, "상품1", 3500);
         product2 = new Product(2L, "상품2", 1500);
-        productZeroPrice = new Product(3L, "상품3", null);
+
+        productRequest = new ProductRequest(1L, "상품1", 3500);
+        productRequestZeroPrice = new ProductRequest(3L, "상품3", null);
     }
 
     @DisplayName("상품 등록 테스트")
     @Test
     void createProductTest() {
-        when(productDao.save(any())).thenReturn(product);
+        when(productRepository.save(any())).thenReturn(product);
 
         // when
-        final Product createdProduct = productService.create(product);
+        final ProductResponse createdProduct = productService.create(productRequest);
 
         // then
         assertAll(
@@ -58,7 +66,7 @@ class ProductServiceTest {
     void createProductExceptionTest() {
         assertThatThrownBy(() -> {
             // when
-            final Product createdProduct = productService.create(productZeroPrice);
+            final ProductResponse createdProduct = productService.create(productRequestZeroPrice);
 
         // then
         }).isInstanceOf(IllegalArgumentException.class);
@@ -67,15 +75,15 @@ class ProductServiceTest {
     @DisplayName("상품 목록을 가져올 수 있다.")
     @Test
     void getProductsTest() {
-        when(productDao.findAll()).thenReturn(Lists.newArrayList(product, product2));
+        when(productRepository.findAll()).thenReturn(Lists.newArrayList(product, product2));
 
         // when
-        final List<Product> products = productService.list();
+        final List<ProductResponse> products = productService.list();
 
         // then
         assertAll(
-                () -> assertThat(products.get(0)).isEqualTo(product),
-                () -> assertThat(products.get(1)).isEqualTo(product2)
+                () -> assertThat(products.get(0).getName()).isEqualTo(product.getName()),
+                () -> assertThat(products.get(1).getName()).isEqualTo(product2.getName())
         );
     }
 }
