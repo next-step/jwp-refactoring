86f85ce32f7d3f09493b59823472188569436695
vicjed <vicjed@icloud.com>
2021-12-24 05:34:55 +0900

    test : 테스트 코드 추가

--- src/test/java/kitchenpos/menu/domain/MenuTest.java
+++ src/test/java/kitchenpos/menu/domain/MenuTest.java
@@ -4,17 +4,17 @@ import static common.MenuGroupFixture.메뉴그룹_한마리;
 import static common.MenuProductFixture.양념치킨_1개;
 import static common.MenuProductFixture.콜라_1개;
 import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.math.BigDecimal;
 import kitchenpos.product.domain.Amount;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MenuTest {
 
-
     /**
-     *  양념치킨 가격 16000
-     *  콜라 1개 가격 2000
+     * 양념치킨 가격 16000 콜라 1개 가격 2000
      */
     @Test
     void 메뉴생성시_등록한_상품의합보다_큰_금액을_입력시_예외() {
@@ -24,6 +24,27 @@ class MenuTest {
         Assertions.assertThatThrownBy(() -> {
             menu.withMenuProducts(asList(양념치킨_1개, 콜라_1개));
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴_정상등록() {
+        // given
+        MenuGroup 메뉴그룹_한마리 = 메뉴그룹_한마리();
+        Menu menu = Menu.of("양념치킨", Amount.of(18000), 메뉴그룹_한마리);
+        MenuProduct 양념치킨_1개 = 양념치킨_1개(menu);
+        MenuProduct 콜라_1개 = 콜라_1개(menu);
+
+        // when
+        menu.withMenuProducts(asList(양념치킨_1개, 콜라_1개));
+
+        // then
+        assertAll(() -> {
+            Assertions.assertThat(menu.getMenuGroup().getName()).isEqualTo(메뉴그룹_한마리.getName());
+            Assertions.assertThat(menu.getName()).isEqualTo("양념치킨");
+            Assertions.assertThat(menu.getPrice().getPrice()).isEqualTo(new BigDecimal("18000"));
+            Assertions.assertThat(menu.getMenuProducts().getMenuProducts())
+                .contains(양념치킨_1개, 콜라_1개);
+        });
     }
 
 }
--- src/test/java/kitchenpos/table/domain/OrderTablesTest.java
+++ src/test/java/kitchenpos/table/domain/OrderTablesTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.Arrays;
 import java.util.List;
 import kitchenpos.common.exception.Message;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class OrderTablesTest {
@@ -33,4 +34,17 @@ class OrderTablesTest {
         }).isInstanceOf(IllegalArgumentException.class)
             .hasMessage(Message.ORDER_TABLES_IS_SMALL_THAN_MIN_TABLE_SIZE.getMessage());
     }
+
+    @Test
+    void 주문테이블_생성() {
+        // given
+        List<OrderTable> orderTables = Arrays.asList(첫번째_주문테이블(),두번째_주문테이블());
+
+        // when
+        OrderTables orderTable = OrderTables.of(orderTables);
+
+        // then
+        Assertions.assertThat(orderTable).isEqualTo(orderTable);
+    }
+
 }
