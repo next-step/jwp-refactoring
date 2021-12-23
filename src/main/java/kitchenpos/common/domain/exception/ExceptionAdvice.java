8dc8ef3e402ce0448d856947306cbab59d3a6194
Junhwan Kim <junhwan.tech@gmail.com>
2021-12-23 16:53:38 +0900

    refactor: ExceptionAdvice 추가

---
+++ src/main/java/kitchenpos/common/domain/exception/ExceptionAdvice.java
@@ -0,0 +1,14 @@
+package kitchenpos.common.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity handleIllegalArgumentException(final IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
