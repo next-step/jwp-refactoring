c75e1217e6b446f40ecf2977e6542c5834672a2a
Junhwan Kim <junhwan.tech@gmail.com>
2021-12-24 02:56:22 +0900

    feat: Menu, MenuProduct dto 추가

---
+++ src/main/java/kitchenpos/menu/dto/MenuRequest.java
@@ -0,0 +1,39 @@
+package kitchenpos.menu.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class MenuRequest {
+
+    private String name;
+    private BigDecimal price;
+    private Long menuGroupId;
+    private List<MenuProductRequest> menuProductRequests;
+
+    public MenuRequest() {
+    }
+
+    public MenuRequest(final String name, final BigDecimal price, final Long menuGroupId,
+        final List<MenuProductRequest> menuProductRequests) {
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+        this.menuProductRequests = menuProductRequests;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Long getMenuGroupId() {
+        return menuGroupId;
+    }
+
+    public List<MenuProductRequest> getMenuProductRequests() {
+        return menuProductRequests;
+    }
+}
---
+++ src/main/java/kitchenpos/menu/dto/MenuResponse.java
@@ -0,0 +1,57 @@
+package kitchenpos.menu.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import kitchenpos.menu.domain.Menu;
+
+public class MenuResponse {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private Long menuGroupId;
+    private List<MenuProductResponse> menuProductResponses;
+
+    public MenuResponse() {
+    }
+
+    public MenuResponse(final Long id, final String name, final BigDecimal price,
+        final Long menuGroupId, final List<MenuProductResponse> menuProductResponses) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+        this.menuProductResponses = menuProductResponses;
+    }
+
+    public static MenuResponse of(final Menu menu) {
+        List<MenuProductResponse> menuProductResponses = menu.getMenuProducts().getMenuProducts().
+            stream().
+            map(MenuProductResponse::of).
+            collect(Collectors.toList());
+
+        return new MenuResponse(menu.getId(), menu.getName().getName(), menu.getPrice().getPrice(),
+            menu.getMenuGroup().getId(), menuProductResponses);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Long getMenuGroupId() {
+        return menuGroupId;
+    }
+
+    public List<MenuProductResponse> getMenuProductResponses() {
+        return menuProductResponses;
+    }
+}
