4571bf613aa7611b30156fcdcb376bbd967de922
songseongbin <ppsosqq@gmail.com>
2021-12-24 00:12:41 +0900

    fix(OrderRepository): exist 함수 쿼리 수정

--- src/main/java/kitchenpos/order/domain/OrderRepository.java
+++ src/main/java/kitchenpos/order/domain/OrderRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query("SELECT o FROM orders o WHERE o.orderTableId IN :orderTableIds AND o.orderStatus IN :orderStatus")
+    @Query("SELECT CASE WHEN COUNT(o) > 0 THEN true ELSE false END "
+        + "FROM Order o WHERE o.orderTableId IN :orderTableIds AND o.orderStatus IN :orderStatus")
     boolean existsByOrderTableIdInAndOrderStatusIn(@Param("orderTableIds") List<Long> orderTableIds,
         @Param("orderStatus") List<OrderStatus> orderStatuses);
 
-    @Query("SELECT o FROM orders o WHERE o.orderTableId IN :orderTableIds AND o.orderStatus IN :orderStatus")
+    @Query("SELECT CASE WHEN COUNT(o) > 0 THEN true ELSE false END"
+        + " FROM Order o WHERE o.orderTableId IN :orderTableIds AND o.orderStatus IN :orderStatus")
     boolean existsByOrderTableIdAndOrderStatusIn(@Param("orderTableId") Long orderTableId,
         @Param("orderStatus") List<OrderStatus> orderStatus);
 }
