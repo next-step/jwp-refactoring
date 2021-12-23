8131f26a5b3158c78adc935888d704180854a8db
lovethefeel <lovethefeel@gmail.com>
2021-12-24 03:53:56 +0900

    feat: 커스텀 익셉션 생성, 요청한 값이 기존재하여 등록 불가능할때

---
+++ src/main/java/kitchenpos/common/exceptions/NotExistRegisterException.java
@@ -0,0 +1,11 @@
+package kitchenpos.common.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class NotExistRegisterException extends CustomException {
+    private static final String EXIST_MENU_GROUP = "요청한 내용은 이미 등록되어있습니다.";
+
+    public NotExistRegisterException() {
+        super(HttpStatus.BAD_REQUEST, EXIST_MENU_GROUP);
+    }
+}
