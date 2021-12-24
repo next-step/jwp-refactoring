cb96ede01d4704036280fd811f5a4a96823e9f7b
songseongbin <ppsosqq@gmail.com>
2021-12-24 11:34:18 +0900

    fix(OrderRepository): 쿼리 수정
    - 단건의 id만 받는데 in 절로 쿼리를 하는 문제 수정

--- src/main/java/kitchenpos/order/domain/OrderRepository.java
+++ src/main/java/kitchenpos/order/domain/OrderRepository.java
@@ -13,7 +13,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
         @Param("orderStatus") List<OrderStatus> orderStatuses);
 
     @Query("SELECT CASE WHEN COUNT(o) > 0 THEN true ELSE false END"
-        + " FROM Order o WHERE o.orderTableId IN :orderTableIds AND o.orderStatus IN :orderStatus")
+        + " FROM Order o WHERE o.orderTableId = :orderTableId AND o.orderStatus IN :orderStatus")
     boolean existsByOrderTableIdAndOrderStatusIn(@Param("orderTableId") Long orderTableId,
         @Param("orderStatus") List<OrderStatus> orderStatus);
 }
