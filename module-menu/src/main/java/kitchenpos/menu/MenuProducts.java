9f3f4fd367405d8aa882203511c22e9d857fa883
shinmj <gkdlqkek@gmail.com>
2021-12-25 18:01:35 +0900

    feat: module-menu add
    - menu, product 도메인  모듈 추가

---
+++ module-menu/src/main/java/kitchenpos/menu/MenuProducts.java
@@ -0,0 +1,51 @@
+package kitchenpos.menu;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import kitchenpos.common.domain.Price;
+import kitchenpos.common.exception.InvalidArgumentException;
+
+@Embeddable
+public class MenuProducts {
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "menu_id", foreignKey = @ForeignKey(name = "fk_menu_product_menu"))
+    private final List<MenuProduct> menuProducts = new ArrayList<>();
+
+    protected MenuProducts() {
+    }
+
+    public List<MenuProduct> get() {
+        return this.menuProducts;
+    }
+
+    public void validatePrice(Price price) {
+        if (price.isGreaterThan(getTotalPrice())) {
+            throw new InvalidArgumentException("메뉴의 총 가격은 구성하는 상품의 총가격보다 작거나 같아야 합니다.");
+        }
+    }
+
+    protected void add(MenuProduct menuProduct) {
+        if (!contains(menuProduct)) {
+            menuProducts.add(menuProduct);
+        }
+    }
+
+    protected Price getTotalPrice() {
+        BigDecimal sum = menuProducts.stream()
+            .map(it -> it.getPrice().toBigDecimal())
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return Price.valueOf(sum);
+    }
+
+    private boolean contains(MenuProduct menuProduct) {
+        return menuProducts.stream()
+            .anyMatch(it -> it.equalMenuProduct(menuProduct));
+    }
+}
---
+++ module-menu/src/test/java/kitchenpos/menu/MenuGroupTest.java
@@ -0,0 +1,29 @@
+package kitchenpos.menu;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import kitchenpos.common.exception.InvalidArgumentException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("메뉴 그룹 도메인 테스트")
+class MenuGroupTest {
+
+    private static final String MENU_GROUP_NAME = "치킨류";
+
+    @Test
+    @DisplayName("메뉴 그룹 이름은 필수이다")
+    void createValidateName() {
+        assertThatThrownBy(() -> MenuGroup.from(null))
+            .isInstanceOf(InvalidArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("메뉴 그룹 생성")
+    void createMenuGroup() {
+        MenuGroup menuGroup = MenuGroup.from(MENU_GROUP_NAME);
+        assertTrue(menuGroup.equalName(MENU_GROUP_NAME));
+    }
+
+}
