c2ba3bdb0dcf7e0f4159c21d5a4c5ae0e6f6eef8
JiHun Hong <hongji3354@gmail.com>
2021-12-26 02:19:35 +0900

    test : Service Layer 통합 테스트

--- src/test/java/kitchenpos/menu/MenuServiceTest.java
+++ src/test/java/kitchenpos/menu/MenuServiceTest.java
@@ -4,21 +4,16 @@ import kitchenpos.AcceptanceTest;
 import kitchenpos.application.MenuService;
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.dao.ProductDao;
-import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuGroup;
-import kitchenpos.domain.MenuProduct;
-import kitchenpos.domain.Product;
+import kitchenpos.dto.MenuCreateRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("메뉴 관련 기능")
 class MenuServiceTest extends AcceptanceTest {
@@ -36,7 +31,7 @@ class MenuServiceTest extends AcceptanceTest {
     @DisplayName("메뉴 그룹이 존재하지 않을 경우 예외가 발생한다.")
     void nonExistMenuGroup() {
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            menuService.create(new Menu("후라이드+후라이드", BigDecimal.valueOf(18000)));
+            menuService.create(new MenuCreateRequest(1L));
         });
     }
 
@@ -44,62 +39,11 @@ class MenuServiceTest extends AcceptanceTest {
     @DisplayName("메뉴에 등록하고자 하는 상품이 존재하지 않을 경우 예외가 발생한다.")
     void nonExistProduct() {
         // given
-        final MenuGroup savedMenuGroup = menuGroupDao.save(new MenuGroup("추천메뉴"));
+        final MenuGroup savedMenuGroup = menuGroupDao.save(MenuGroup.builder().name("추천메뉴").build());
 
         // when
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            menuService.create(new Menu("후라이드+후라이드", BigDecimal.valueOf(18000), savedMenuGroup.getId(), Arrays.asList(new MenuProduct())));
+            menuService.create(new MenuCreateRequest("후라이드+후라이드", BigDecimal.valueOf(18000), savedMenuGroup.getId(), Arrays.asList(new MenuCreateRequest.MenuProduct(1L, 1L))));
         });
-    }
-
-    @Test
-    @DisplayName("메뉴의 금액이 상품의 총 금액보다 크다면 예외가 발생한다.")
-    void menuPriceMoreThanProductPriceSum() {
-        // given
-        final MenuGroup savedMenuGroup = menuGroupDao.save(new MenuGroup("추천메뉴"));
-        final Product savedProduct = productDao.save(new Product("후라이드", BigDecimal.valueOf(8000)));
-
-        // when
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            menuService.create(new Menu("후라이드+후라이드", BigDecimal.valueOf(18000), savedMenuGroup.getId(), Arrays.asList(new MenuProduct(savedProduct.getId(), 2L))));
-        });
-    }
-
-    @Test
-    @DisplayName("메뉴를 등록할 수 있다.")
-    void createMenu() {
-        // given
-        final MenuGroup savedMenuGroup = menuGroupDao.save(new MenuGroup("추천메뉴"));
-        final Product savedProduct = productDao.save(new Product("후라이드", BigDecimal.valueOf(9000)));
-
-        // when
-        Menu menu = menuService.create(new Menu("후라이드+후라이드", BigDecimal.valueOf(18000), savedMenuGroup.getId(), Arrays.asList(new MenuProduct(savedProduct.getId(), 2L))));
-
-        // then
-        assertAll(
-                () -> assertThat(menu.getId()).isNotNull(),
-                () -> assertThat(menu.getName()).isEqualTo("후라이드+후라이드"),
-                () -> assertThat(menu.getPrice()).isEqualByComparingTo(BigDecimal.valueOf(18000)),
-                () -> assertThat(menu.getMenuGroupId()).isNotNull(),
-                () -> assertThat(menu.getMenuProducts()).extracting("seq").isNotNull(),
-                () -> assertThat(menu.getMenuProducts()).extracting("menuId").contains(menu.getId()),
-                () -> assertThat(menu.getMenuProducts()).extracting("productId").contains(savedProduct.getId()),
-                () -> assertThat(menu.getMenuProducts()).extracting("quantity").contains(2L)
-        );
-    }
-
-    @Test
-    @DisplayName("메뉴 목록을 조회할 수 있다.")
-    void findMenu() {
-        // given
-        final MenuGroup savedMenuGroup = menuGroupDao.save(new MenuGroup("추천메뉴"));
-        final Product savedProduct = productDao.save(new Product("후라이드", BigDecimal.valueOf(9000)));
-        menuService.create(new Menu("후라이드+후라이드", BigDecimal.valueOf(18000), savedMenuGroup.getId(), Arrays.asList(new MenuProduct(savedProduct.getId(), 2L))));
-
-        // when
-        List<Menu> findByMenus = menuService.list();
-
-        // then
-        assertThat(findByMenus.size()).isNotZero();
     }
 }
--- src/test/java/kitchenpos/tablegroup/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/tablegroup/TableGroupServiceTest.java
@@ -1,18 +1,15 @@
 package kitchenpos.tablegroup;
 
 import kitchenpos.AcceptanceTest;
-import kitchenpos.application.*;
-import kitchenpos.domain.*;
+import kitchenpos.application.TableGroupService;
+import kitchenpos.dto.TableGroupCreateRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("단체 지정 관련 기능")
 class TableGroupServiceTest extends AcceptanceTest {
@@ -20,109 +17,12 @@ class TableGroupServiceTest extends AcceptanceTest {
     @Autowired
     private TableGroupService tableGroupService;
 
-    @Autowired
-    private TableService tableService;
-
-    @Autowired
-    private ProductService productService;
-
-    @Autowired
-    private MenuService menuService;
-
-    @Autowired
-    private MenuGroupService menuGroupService;
-
-    @Autowired
-    private OrderService orderService;
-
     @Test
     @DisplayName("단체 지정 하고자 하는 테이블이 존재하지 않으면 예외가 발생한다.")
     void createTableGroupFailBecauseOfNotExistTable() {
         // when
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            tableGroupService.create(new TableGroup(Arrays.asList(new OrderTable(1L), new OrderTable(2L))));
+            tableGroupService.create(new TableGroupCreateRequest(Arrays.asList(new TableGroupCreateRequest.OrderTable(1L), new TableGroupCreateRequest.OrderTable(2L))));
         });
-    }
-
-
-    @Test
-    @DisplayName("단체 지정 하고자 하는 테이블의 상태가 사용중이라면 예외가 발생한다.")
-    void createTableGroupFailBecauseOfTableNotEmpty() {
-        // given
-        OrderTable firstOrderTable = tableService.create(new OrderTable(true));
-        OrderTable secondOrderTable = tableService.create(new OrderTable(false));
-
-        // when
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            tableGroupService.create(new TableGroup(Arrays.asList(new OrderTable(firstOrderTable.getId()), new OrderTable(secondOrderTable.getId()))));
-        });
-    }
-
-
-    @Test
-    @DisplayName("단체 지정 하고자 하는 테이블이 단체 지정이 되어 있다면 예외가 발생한다.")
-    void createTableGroupFailBecauseOfAlreadyTableGroup() {
-        // given
-        OrderTable firstOrderTable = tableService.create(new OrderTable(true));
-        OrderTable secondOrderTable = tableService.create(new OrderTable(true));
-        tableGroupService.create(new TableGroup(Arrays.asList(firstOrderTable, secondOrderTable)));
-
-        // when
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            tableGroupService.create(new TableGroup(Arrays.asList(firstOrderTable, secondOrderTable)));
-        });
-    }
-
-    @Test
-    @DisplayName("단체 지정을 할 수 있다.")
-    void createTableGroup() {
-        // given
-        OrderTable firstOrderTable = tableService.create(new OrderTable(true));
-        OrderTable secondOrderTable = tableService.create(new OrderTable(true));
-
-        // when
-        final TableGroup savedTableGroup = tableGroupService.create(new TableGroup(Arrays.asList(firstOrderTable, secondOrderTable)));
-
-        // then
-        assertAll(
-                () -> assertThat(savedTableGroup.getId()).isNotNull(),
-                () -> assertThat(savedTableGroup.getOrderTables().size()).isEqualTo(2)
-        );
-    }
-
-    @Test
-    @DisplayName("단체 지정 해제시 테이블의 주문 상태가 조리 또는 식사 변경이 불가능하다.")
-    void ungroupFailBecauseOfOrderStatusCookingOrMeal() {
-        // given
-        OrderTable firstOrderTable = tableService.create(new OrderTable(true));
-        OrderTable secondOrderTable = tableService.create(new OrderTable(true));
-        final TableGroup savedTableGroup = tableGroupService.create(new TableGroup(Arrays.asList(firstOrderTable, secondOrderTable)));
-        final MenuGroup savedMenuGroup = menuGroupService.create(new MenuGroup("추천메뉴"));
-        final Product savedProduct = productService.create(new Product("후라이드", BigDecimal.valueOf(9000)));
-        final Menu savedMenu = menuService.create(new Menu("후라이드+후라이드", BigDecimal.valueOf(18000), savedMenuGroup.getId(), Arrays.asList(new MenuProduct(savedProduct.getId(), 2L))));
-        orderService.create(new Order(firstOrderTable.getId(), Arrays.asList(new OrderLineItem(savedMenu.getId(), 2L))));
-
-        // when
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            tableGroupService.ungroup(savedTableGroup.getId());
-        });
-    }
-
-
-    @Test
-    @DisplayName("단체 지정을 해제할 수 있다.")
-    void ungroup() {
-        // given
-        OrderTable firstOrderTable = tableService.create(new OrderTable(true));
-        OrderTable secondOrderTable = tableService.create(new OrderTable(true));
-        final TableGroup savedTableGroup = tableGroupService.create(new TableGroup(Arrays.asList(firstOrderTable, secondOrderTable)));
-        final MenuGroup savedMenuGroup = menuGroupService.create(new MenuGroup("추천메뉴"));
-        final Product savedProduct = productService.create(new Product("후라이드", BigDecimal.valueOf(9000)));
-        final Menu savedMenu = menuService.create(new Menu("후라이드+후라이드", BigDecimal.valueOf(18000), savedMenuGroup.getId(), Arrays.asList(new MenuProduct(savedProduct.getId(), 2L))));
-        final Order order = orderService.create(new Order(firstOrderTable.getId(), Arrays.asList(new OrderLineItem(savedMenu.getId(), 2L))));
-        orderService.changeOrderStatus(order.getId(), new Order(OrderStatus.COMPLETION.name()));
-
-        // when
-        tableGroupService.ungroup(savedTableGroup.getId());
     }
 }
