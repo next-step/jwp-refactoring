1fd82c36692f05396111eae6b0bca1167446b1bb
shinmj <gkdlqkek@gmail.com>
2021-12-25 17:54:37 +0900

    feat: module-common add
    - 공통 관련 모듈 추가

---
+++ module-common/src/main/java/kitchenpos/common/exception/CannotCreateException.java
@@ -0,0 +1,8 @@
+package kitchenpos.common.exception;
+
+public class CannotCreateException extends BusinessException {
+
+    public CannotCreateException(String message) {
+        super(message);
+    }
+}
---
+++ module-common/src/main/java/kitchenpos/common/exception/dto/ErrorResponse.java
@@ -0,0 +1,19 @@
+package kitchenpos.common.exception.dto;
+
+public class ErrorResponse {
+
+    private final String message;
+
+    private ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public static ErrorResponse valueOf(String message) {
+        return new ErrorResponse(message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
