1c319a9767fa9c605af24addc08078b7b1e5d30c
Arasia <rlacndtjs261@gmail.com>
2021-12-25 15:55:45 +0900

    feat: Menu와 Order 모듈로 분리

---
+++ order/src/test/java/kitchenpos/AcceptanceTest.java
@@ -0,0 +1,23 @@
+package kitchenpos;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Sql("/truncate.sql")
+public class AcceptanceTest {
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+        }
+    }
+}
