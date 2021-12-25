7cb028931666e3b259d0de64ee729464bba6f617
Kelicia91 <kelicia91@gmail.com>
2021-12-26 03:31:29 +0900

    refactor: remove unused import

--- src/main/java/kitchenpos/tablegroup/domain/TableGroup.java
+++ src/main/java/kitchenpos/tablegroup/domain/TableGroup.java
@@ -9,10 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import org.springframework.util.CollectionUtils;
 
 import kitchenpos.ordertable.domain.OrderTable;
 
