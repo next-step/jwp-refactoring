785b1e71e1482c163782168ea3810611d4db9045
ch3224bin <ahrocksil@gmail.com>
2021-12-25 21:28:29 +0900

    refactor(Table): TableGroupManagerImpl -> TableGroupManagerService 이름, 패키지 변경
    - 다른 패키지와의 통합, 연계하는 작업을 하는 클래스임을 분명히 하기 위해

--- src/main/java/kitchenpos/table/application/TableGroupManagerService.java
+++ src/main/java/kitchenpos/table/application/TableGroupManagerService.java
@@ -1,16 +1,20 @@
-package kitchenpos.table.domain;
+package kitchenpos.table.application;
 
+import kitchenpos.table.domain.OrderTable;
+import kitchenpos.table.domain.OrderTableRepository;
+import kitchenpos.table.domain.OrderTableValidator;
+import kitchenpos.table.domain.OrderTables;
 import kitchenpos.tablegroup.domain.TableGroupManager;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class TableGroupManagerImpl implements TableGroupManager {
+public class TableGroupManagerService implements TableGroupManager {
     private final OrderTableRepository orderTableRepository;
     private final OrderTableValidator orderTableValidator;
 
-    public TableGroupManagerImpl(OrderTableRepository orderTableRepository, OrderTableValidator orderTableValidator) {
+    public TableGroupManagerService(OrderTableRepository orderTableRepository, OrderTableValidator orderTableValidator) {
         this.orderTableRepository = orderTableRepository;
         this.orderTableValidator = orderTableValidator;
     }
