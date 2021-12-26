03161e0ea7e973cb44eaabd436845d87e502f974
minho <lights8615@gmail.com>
2021-12-26 23:07:56 +0900

    feat(step2): 메뉴 그룹 jpa 적용

--- src/main/java/kitchenpos/dao/MenuGroupDao.java
+++ src/main/java/kitchenpos/dao/MenuGroupDao.java
@@ -1,16 +1,8 @@
 package kitchenpos.dao;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import kitchenpos.domain.MenuGroup;
 
-public interface MenuGroupDao {
-    MenuGroup save(MenuGroup entity);
-
-    Optional<MenuGroup> findById(Long id);
-
-    List<MenuGroup> findAll();
-
-    boolean existsById(Long id);
+public interface MenuGroupDao extends JpaRepository<MenuGroup, Long> {
 }
