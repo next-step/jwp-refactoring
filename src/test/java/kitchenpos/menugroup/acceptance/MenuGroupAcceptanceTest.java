f39fd1d6f9d13f552eb7b7bd6e338979e9cc5dba
박창혁 <changchang73@pionnet.co.kr>
2021-12-26 23:34:52 +0900

    test: 메뉴그룹 테스트 추가

--- src/test/java/kitchenpos/menugroup/acceptance/MenuGroupAcceptanceTest.java
+++ src/test/java/kitchenpos/menugroup/acceptance/MenuGroupAcceptanceTest.java
@@ -1,4 +1,60 @@
 package kitchenpos.menugroup.acceptance;
 
-public class MenuGroupAcceptanceTest {
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.AcceptanceTest;
+import kitchenpos.domain.MenuGroup;
+
+@DisplayName("메뉴 그룹 관리")
+public class MenuGroupAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("메뉴 그룹을 등록한다.")
+    @Test
+    void create() {
+        ExtractableResponse 메뉴_그룹_등록_요청_응답 = 메뉴_그룹_등록_요청("분식");
+
+        메뉴_그룹_등록됨(메뉴_그룹_등록_요청_응답);
+    }
+
+    @DisplayName("메뉴 그룹 목록을 조회한다.")
+    @Test
+    void list() {
+        ExtractableResponse 메뉴_그룹_목록_요청_응답 = 메뉴_그룹_목록_요청();
+
+        메뉴_그룹_조회됨(메뉴_그룹_목록_요청_응답);
+    }
+
+    private ExtractableResponse 메뉴_그룹_목록_요청() {
+        return RestAssured
+            .given().log().all()
+            .when().get("/api/menu-groups")
+            .then().log().all().extract();
+    }
+
+    private void 메뉴_그룹_조회됨(ExtractableResponse response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private ExtractableResponse<Response> 메뉴_그룹_등록_요청(String name) {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName(name);
+        return RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(menuGroup)
+            .when().post("/api/menu-groups")
+            .then().log().all().extract();
+    }
+
+    private void 메뉴_그룹_등록됨(ExtractableResponse response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
 }
--- src/test/java/kitchenpos/menugroup/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/menugroup/application/MenuGroupServiceTest.java
@@ -1,4 +1,67 @@
 package kitchenpos.menugroup.application;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kitchenpos.application.MenuGroupService;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+
+@ExtendWith(MockitoExtension.class)
 public class MenuGroupServiceTest {
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    private MenuGroup menuGroup;
+
+    @BeforeEach
+    void setUp() {
+        menuGroup = 메뉴_그룹_정보(1L, "제육볶음");
+    }
+
+    @DisplayName("메뉴그룹을 등록한다.")
+    @Test
+    void create() {
+        // given
+        given(menuGroupDao.save(any(MenuGroup.class))).willReturn(menuGroup);
+        MenuGroup expected = menuGroupService.create(menuGroup);
+
+        //then
+        assertThat(expected).isNotNull();
+        assertThat(expected.getName()).isEqualTo("제육볶음");
+    }
+
+    @DisplayName("메뉴그룹 목록을 조회한다.")
+    @Test
+    void list() {
+        // given
+        given(menuGroupDao.findAll()).willReturn(Arrays.asList(menuGroup));
+        List<MenuGroup> expected = menuGroupService.list();
+
+        // then
+        assertThat(expected).isNotNull();
+        assertThat(expected.size()).isEqualTo(1);
+    }
+
+    private MenuGroup 메뉴_그룹_정보(Long id, String name) {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setId(id);
+        menuGroup.setName(name);
+        return menuGroup;
+    }
 }
