3ecae51024e24173864d3c85d637734ce29b20ae
ChangSubKwak <paradise1732@gmail.com>
2021-12-27 18:18:49 +0900

    feat: 커스텀 예외 클래스 작성 추가

---
+++ src/main/java/common/WrongValueException.java
@@ -0,0 +1,9 @@
+package common;
+
+public class WrongValueException extends RuntimeException {
+    public static final String WRONG_VALUE_EXCEPTION_STATEMENT = "%s 은(는) 잘못되었습니다.";
+
+    public WrongValueException(String target) {
+        super(String.format(WRONG_VALUE_EXCEPTION_STATEMENT, target));
+    }
+}
