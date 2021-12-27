971e386a10e43b40be8fb5e71bd1cf3538080ae8
ChangSubKwak <paradise1732@gmail.com>
2021-12-27 18:45:34 +0900

    feat: 테이블 그룹(단체 지정) 도메인 / 서비스 / DTO / Repository 작성

---
+++ src/main/java/table/application/TableGroupService.java
@@ -0,0 +1,50 @@
+package table.application;
+
+import java.util.*;
+
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+
+import common.*;
+import table.domain.*;
+import table.dto.*;
+import table.repository.*;
+
+@Service
+@Transactional
+public class TableGroupService {
+    private static final String TABLE_GROUP = "테이블 그룹";
+    private static final String INSUFFICIENT_ORDER_TABLE_EXCEPTION_STATEMENT = "주문 테이블이 부족할 수 있습니다.";
+
+    private final OrderTableRepository orderTableRepository;
+    private final TableGroupRepository tableGroupRepository;
+
+    public TableGroupService(OrderTableRepository orderTableRepository, TableGroupRepository tableGroupRepository) {
+        this.orderTableRepository = orderTableRepository;
+        this.tableGroupRepository = tableGroupRepository;
+    }
+
+    public TableGroupResponse saveTableGroup(TableGroupRequest tableGroupRequest) {
+        TableGroup saveTableGroup = new TableGroup();
+        List<OrderTable> orderTables = orderTableRepository.findAllById(tableGroupRequest.getOrderTableIds());
+
+        validateOrderTable(tableGroupRequest, orderTables);
+        orderTables.stream()
+            .forEach(saveTableGroup::addOrderTable);
+
+        return TableGroupResponse.of(tableGroupRepository.save(saveTableGroup));
+    }
+
+    private void validateOrderTable(TableGroupRequest tableGroupRequest, List<OrderTable> orderTables) {
+        if (tableGroupRequest.getOrderTableIds().size() != orderTables.size()) {
+            throw new IllegalArgumentException(INSUFFICIENT_ORDER_TABLE_EXCEPTION_STATEMENT);
+        }
+    }
+
+    public void ungroup(final Long tableGroupId) {
+        TableGroup savedTableGroup = tableGroupRepository.findById(tableGroupId)
+                .orElseThrow(() -> new NotFoundException(TABLE_GROUP));
+        savedTableGroup.ungroup();
+        tableGroupRepository.delete(savedTableGroup);
+    }
+}
---
+++ src/main/java/table/dto/TableGroupResponse.java
@@ -0,0 +1,30 @@
+package table.dto;
+
+import java.time.*;
+import table.domain.*;
+
+public class TableGroupResponse {
+
+    private Long id;
+    private LocalDateTime createdDate;
+
+    public TableGroupResponse() {
+    }
+
+    private TableGroupResponse(TableGroup tableGroup) {
+        this.id = tableGroup.getId();
+        this.createdDate = tableGroup.getCreatedDate();
+    }
+
+    public static TableGroupResponse of(TableGroup tableGroup) {
+        return new TableGroupResponse(tableGroup);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+}
