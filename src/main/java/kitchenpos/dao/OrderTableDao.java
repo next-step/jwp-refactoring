9011c040d18dacdbf333651a65a2282c937905bd
minho <lights8615@gmail.com>
2021-12-27 11:52:29 +0900

    feat(step2): 주문 테이블 jpa 적용

--- src/main/java/kitchenpos/dao/OrderTableDao.java
+++ src/main/java/kitchenpos/dao/OrderTableDao.java
@@ -3,15 +3,11 @@ package kitchenpos.dao;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import kitchenpos.domain.OrderTable;
 
-public interface OrderTableDao {
-    OrderTable save(OrderTable entity);
-
-    Optional<OrderTable> findById(Long id);
-
-    List<OrderTable> findAll();
-
+public interface OrderTableDao extends JpaRepository<OrderTable, Long> {
     List<OrderTable> findAllByIdIn(List<Long> ids);
 
     List<OrderTable> findAllByTableGroupId(Long tableGroupId);
