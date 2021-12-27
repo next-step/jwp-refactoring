12613a08c286a67304b6a6174dd61bc561d2d520
minho <lights8615@gmail.com>
2021-12-28 03:22:04 +0900

    feat(step2): setter 제거

--- src/main/java/kitchenpos/dto/ProductRequest.java
+++ src/main/java/kitchenpos/dto/ProductRequest.java
@@ -17,16 +17,8 @@ public class ProductRequest {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
     }
 
     public Product toEntity() {
