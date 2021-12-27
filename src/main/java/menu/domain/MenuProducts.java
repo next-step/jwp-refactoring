3cb83c2b4c18d46aafd4570dbbcfd885bc7c3676
ChangSubKwak <paradise1732@gmail.com>
2021-12-27 18:33:01 +0900

    feat: 메뉴 상품 도메인 / DTO / Repository 작성

---
+++ src/main/java/menu/domain/MenuProducts.java
@@ -0,0 +1,20 @@
+package menu.domain;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+@Embeddable
+public class MenuProducts {
+
+    @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MenuProduct> menuProducts = new ArrayList<>();
+
+    public void add(Menu menu, Product product, Long quantity) {
+        menuProducts.add(new MenuProduct(menu, product, quantity));
+    }
+
+    public List<MenuProduct> getMenuProducts() {
+        return menuProducts;
+    }
+}
---
+++ src/main/java/menu/dto/MenuProductRequest.java
@@ -0,0 +1,23 @@
+package menu.dto;
+
+public class MenuProductRequest {
+    private Long productId;
+    private long quantity;
+
+    private MenuProductRequest(Long productId, long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public static MenuProductRequest of(Long productId, long quantity) {
+        return new MenuProductRequest(productId, quantity);
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+}
