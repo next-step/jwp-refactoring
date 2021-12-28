1f3ab6d514085625135b2c0f56e88c9c726c1511
ChangSubKwak <paradise1732@gmail.com>
2021-12-28 15:25:02 +0900

    refactor: 미사용 서비스, 도메인, 콘트롤러, DAO, 픽스쳐 제거

--- src/test/java/kitchenpos/fixture/OrderTableFixture.java
+++
@@ -1,8 +0,0 @@
-package kitchenpos.fixture;
-
-import kitchenpos.domain.OrderTable;
-
-public class OrderTableFixture {
-    public static final OrderTable orderTable1 = OrderTable.of(1L, null, 4, true);
-    public static final OrderTable orderTable2 = OrderTable.of(2L, null, 6, true);
-}
