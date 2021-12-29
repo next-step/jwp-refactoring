8753f27d690e19def0b3401ef675b7f7a882a06f
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:42:37 +0900

    refactor: 사용하지 않는 파일 제거

--- src/main/java/kitchenpos/common/exceptions/NotEmptyOrderLineItemException.java
+++
@@ -1,9 +0,0 @@
-package kitchenpos.common.exceptions;
-
-public class NotEmptyOrderLineItemException extends CustomException {
-    public static final String NOT_EMPTY_ORDER_LINE_ITEM_EXCEPTION = "주문 항목이 있어야 합니다.";
-
-    public NotEmptyOrderLineItemException() {
-        super(NOT_EMPTY_ORDER_LINE_ITEM_EXCEPTION);
-    }
-}
