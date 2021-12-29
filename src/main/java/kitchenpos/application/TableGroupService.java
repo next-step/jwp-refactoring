c09b6e5ab11bcb1f2ced3fbbe3d11959bfa4a6ad
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:43:26 +0900

    refactor: 테이블 그룹 서비스 생성자 변경, 응답 타입 변경, 주문 객체 로직 이동으로 전반적인 로직 수정

--- src/main/java/kitchenpos/application/TableGroupService.java
+++ src/main/java/kitchenpos/application/TableGroupService.java
@@ -1,94 +1,50 @@
 package kitchenpos.application;
 
-import kitchenpos.common.exceptions.EmptyException;
-import kitchenpos.common.exceptions.NotEqualsException;
-import kitchenpos.common.exceptions.NotExistOrDisableStatusException;
-import kitchenpos.common.exceptions.NotExistRegisterException;
-import kitchenpos.dao.OrderDao;
-import kitchenpos.dao.OrderTableDao;
-import kitchenpos.dao.TableGroupDao;
-import kitchenpos.domain.OrderStatus;
+import kitchenpos.common.exceptions.NotFoundEntityException;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.TableGroup;
-import org.springframework.http.HttpStatus;
+import kitchenpos.domain.TableGroupRepository;
+import kitchenpos.dto.tablegroup.TableGroupRequest;
+import kitchenpos.dto.tablegroup.TableGroupResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 public class TableGroupService {
-    private final OrderDao orderDao;
-    private final OrderTableDao orderTableDao;
-    private final TableGroupDao tableGroupDao;
+    private final OrderService orderService;
+    private final TableService tableService;
+    private final TableGroupRepository tableGroupRepository;
 
-    public TableGroupService(final OrderDao orderDao, final OrderTableDao orderTableDao, final TableGroupDao tableGroupDao) {
-        this.orderDao = orderDao;
-        this.orderTableDao = orderTableDao;
-        this.tableGroupDao = tableGroupDao;
+    public TableGroupService(final OrderService orderService, final TableService tableService, final TableGroupRepository tableGroupRepository) {
+        this.orderService = orderService;
+        this.tableService = tableService;
+        this.tableGroupRepository = tableGroupRepository;
     }
 
     @Transactional
-    public TableGroup create(final TableGroup tableGroup) {
-        final List<OrderTable> orderTables = tableGroup.getOrderTables();
+    public TableGroupResponse create(final TableGroupRequest request) {
+        final List<OrderTable> tables = getOrderTables(request.getOrderTableIds());
+        final TableGroup tableGroup = TableGroup.from(tables);
+        final TableGroup savedTableGroup = tableGroupRepository.save(tableGroup);
+        return TableGroupResponse.from(savedTableGroup);
+    }
 
-        if (CollectionUtils.isEmpty(orderTables) || orderTables.size() < 2) {
-            throw new EmptyException(HttpStatus.BAD_REQUEST);
-        }
-
-        final List<Long> orderTableIds = orderTables.stream()
-                .map(OrderTable::getId)
-                .collect(Collectors.toList());
-
-        final List<OrderTable> savedOrderTables = orderTableDao.findAllByIdIn(orderTableIds);
-
-        if (orderTables.size() != savedOrderTables.size()) {
-            throw new NotEqualsException(HttpStatus.BAD_REQUEST);
-        }
-
-        for (final OrderTable savedOrderTable : savedOrderTables) {
-            if (!savedOrderTable.isEmpty() || Objects.nonNull(savedOrderTable.getTableGroupId())) {
-                throw new NotExistRegisterException(HttpStatus.BAD_REQUEST);
-            }
-        }
-
-        tableGroup.setCreatedDate(LocalDateTime.now());
-
-        final TableGroup savedTableGroup = tableGroupDao.save(tableGroup);
-
-        final Long tableGroupId = savedTableGroup.getId();
-        for (final OrderTable savedOrderTable : savedOrderTables) {
-            savedOrderTable.setTableGroupId(tableGroupId);
-            savedOrderTable.setEmpty(false);
-            orderTableDao.save(savedOrderTable);
-        }
-        savedTableGroup.setOrderTables(savedOrderTables);
-
-        return savedTableGroup;
+    private List<OrderTable> getOrderTables(final List<Long> tableIds) {
+        return tableService.findAllById(tableIds);
     }
 
     @Transactional
     public void ungroup(final Long tableGroupId) {
-        final List<OrderTable> orderTables = orderTableDao.findAllByTableGroupId(tableGroupId);
+        final TableGroup tableGroup = findByTableGroupId(tableGroupId);
+        orderService.existsByOrderTableInAndOrderStatusIn(tableGroup.getOrderTables().toList());
+        tableGroup.unGroup();
+    }
 
-        final List<Long> orderTableIds = orderTables.stream()
-                .map(OrderTable::getId)
-                .collect(Collectors.toList());
-
-        if (orderDao.existsByOrderTableIdInAndOrderStatusIn(
-                orderTableIds, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))) {
-            throw new NotExistOrDisableStatusException(HttpStatus.BAD_REQUEST);
-        }
-
-        for (final OrderTable orderTable : orderTables) {
-            orderTable.setTableGroupId(null);
-            orderTableDao.save(orderTable);
-        }
+    private TableGroup findByTableGroupId(final Long id) {
+        return tableGroupRepository.findById(id)
+                .orElseThrow(NotFoundEntityException::new);
     }
 }
