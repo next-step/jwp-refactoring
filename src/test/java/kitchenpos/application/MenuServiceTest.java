9802e80b2af97fc7db8bebf9f8ef635056a89e45
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:50:45 +0900

    test: 메뉴 서비스 리팩토링으로 인한 테스트 소스 수정

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -1,12 +1,11 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.MenuDao;
-import kitchenpos.dao.MenuGroupDao;
-import kitchenpos.dao.MenuProductDao;
-import kitchenpos.dao.ProductDao;
-import kitchenpos.domain.Menu;
-import kitchenpos.domain.MenuProduct;
-import kitchenpos.domain.Product;
+import kitchenpos.domain.*;
+import kitchenpos.dto.menu.MenuProductRequest;
+import kitchenpos.dto.menu.MenuRequest;
+import kitchenpos.dto.menu.MenuResponse;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,10 +14,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -29,54 +26,71 @@ import static org.mockito.BDDMockito.given;
 @ExtendWith(MockitoExtension.class)
 class MenuServiceTest {
     @Mock
-    private MenuGroupDao menuGroupDao;
+    private MenuRepository menuRepository;
 
     @Mock
-    private MenuDao menuDao;
+    private MenuGroupService menuGroupService;
 
     @Mock
-    private ProductDao productDao;
-
-    @Mock
-    private MenuProductDao menuProductDao;
+    private ProductService productService;
 
     @InjectMocks
     private MenuService menuService;
 
+    private MenuGroup 메뉴그룹;
+    private Product 상품1;
+    private Menu 메뉴;
+    private List<Menu> 메뉴_목록;
+    private MenuProduct 메뉴상품;
+    private List<MenuProductRequest> 메뉴_상품_목록_요청;
+    private MenuRequest 메뉴_요청;
+
+    @BeforeEach
+    void setUp() {
+        메뉴그룹 = MenuGroup.of("메뉴그룹");
+        상품1 = Product.of(1L,"상품",new BigDecimal("10000"));
+        메뉴 = Menu.of(1L,Name.from("메뉴"),Price.from(new BigDecimal("50000")),MenuGroup.of("메뉴그룹"));
+        메뉴_목록 = Collections.singletonList(메뉴);
+        메뉴상품 = MenuProduct.of(1L, 메뉴, 상품1,2L);
+        메뉴_상품_목록_요청 = Lists.newArrayList(new MenuProductRequest(1L, 2L));
+        메뉴_요청 = MenuRequest.of("메뉴", new BigDecimal("10000"), 1L, 메뉴_상품_목록_요청);
+    }
+
     @DisplayName("메뉴를 등록한다.")
     @Test
     void saveMenu() {
-        final Product product1 = new Product(1L, "상품", new BigDecimal("10000"));
-        final Product product2 = new Product(2L, "상품2", new BigDecimal("10000"));
-        final MenuProduct menuProduct = new MenuProduct(1L, 1L, 1L, 2);
-        final Menu menu = new Menu(1L, "메뉴", new BigDecimal("10000"), 1L, Collections.singletonList(menuProduct));
+        given(menuGroupService.findMenuGroupById(anyLong())).willReturn(메뉴그룹);
+        given(productService.getById(any())).willReturn(상품1);
+        given(menuRepository.save(any())).willReturn(메뉴);
 
-        given(menuGroupDao.existsById(anyLong())).willReturn(true);
-        given(productDao.findById(anyLong())).willReturn(Optional.of(product1));
-        given(menuDao.save(any())).willReturn(menu);
-        given(menuProductDao.save(any())).willReturn(menuProduct);
+        final MenuResponse actual = menuService.create(메뉴_요청);
 
-        final Menu actual = menuService.create(menu);
+        assertAll(
+                () -> assertThat(actual.getId()).isEqualTo(메뉴.getId()),
+                () -> assertThat(actual.getName()).isEqualTo(메뉴.getName().toName()),
+                () -> assertThat(actual.getPrice()).isEqualTo(메뉴.getPrice().toBigDecimal()),
+                () -> assertThat(actual.getMenuGroupResponse().getName()).isEqualTo(메뉴.getMenuGroup().getName()),
+                () -> assertThat(actual.getMenuProductResponses()).hasSize(메뉴.getMenuProducts().toList().size())
 
-        assertThat(menu).isEqualTo(actual);
+        );
+        assertThat(actual).isNotNull();
     }
 
     @DisplayName("메뉴들을 조회한다.")
     @Test
     void findMenus() {
-        final MenuProduct menuProduct1 = new MenuProduct(1L, 1L, 1L, 2);
-        final MenuProduct menuProduct2 = new MenuProduct(2L, 1L, 2L, 3);
-        final List<MenuProduct> menuProducts = Arrays.asList(menuProduct1, menuProduct2);
-        final Menu menu = new Menu(1L, "메뉴", new BigDecimal("10000"), 1L, menuProducts);
+        given(menuRepository.findAll()).willReturn(Collections.singletonList(메뉴));
 
-        given(menuDao.findAll()).willReturn(Collections.singletonList(menu));
-        given(menuProductDao.findAllByMenuId(anyLong())).willReturn(menuProducts);
-
-        final List<Menu> actual = menuService.list();
+        final List<MenuResponse> actual = menuService.list();
 
         assertAll(
-                () -> assertThat(actual).hasSize(1),
-                () -> assertThat(actual.get(0).getMenuProducts()).hasSize(2)
+                () -> assertThat(actual).hasSize(메뉴_목록.size()),
+                () -> assertThat(actual.get(0)).isNotNull(),
+                () -> assertThat(actual.get(0).getId()).isEqualTo(메뉴_목록.get(0).getId()),
+                () -> assertThat(actual.get(0).getName()).isEqualTo(메뉴_목록.get(0).getName().toName()),
+                () -> assertThat(actual.get(0).getPrice()).isEqualTo(메뉴_목록.get(0).getPrice().toBigDecimal()),
+                () -> assertThat(actual.get(0).getMenuGroupResponse().getName()).isEqualTo(메뉴_목록.get(0).getMenuGroup().getName()),
+                () -> assertThat(actual.get(0).getMenuProductResponses()).hasSize(메뉴_목록.get(0).getMenuProducts().toList().size())
         );
     }
 }
