1f13bd00cd9032966398786af1602821e64606e1
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:50:56 +0900

    test: 메뉴 그룹 서비스 리팩토링으로 인한 테스트 소스 수정

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -1,7 +1,11 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.dto.menugroup.MenuGroupRequest;
+import kitchenpos.dto.menugroup.MenuGroupResponse;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,42 +13,53 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
+@DisplayName("메뉴 그룹 관련 서비스 테스트")
 @ExtendWith(MockitoExtension.class)
-class MenuGroupServiceTest {
-    @Mock
-    private MenuGroupDao menuGroupDao;
-
+public class MenuGroupServiceTest {
     @InjectMocks
     private MenuGroupService menuGroupService;
 
-    @DisplayName("메뉴 그룹을 등록한다.")
-    @Test
-    void SaveMenuGroup() {
-        final MenuGroup expected = new MenuGroup(1L, "메뉴그룹");
-        given(menuGroupDao.save(any())).willReturn(expected);
+    @Mock
+    private MenuGroupRepository menuGroupRepository;
 
-        final MenuGroup actual = menuGroupService.create(expected);
+    private MenuGroupRequest 메뉴그룹_요청;
+    private MenuGroup 메뉴그룹;
+    private List<MenuGroup> 메뉴그룹_목록;
 
-        assertThat(actual).isEqualTo(expected);
+    @BeforeEach
+    void setUp() {
+        메뉴그룹_요청 = MenuGroupRequest.from("추천메뉴");
+        메뉴그룹 = MenuGroup.of(1L, "추천메뉴");
+        메뉴그룹_목록 = Collections.singletonList(메뉴그룹);
     }
 
-    @DisplayName("등록한 메뉴들을 조회한다.")
+    @DisplayName("메뉴 그룹을 생성한다")
     @Test
-    void findMenuGroup() {
-        final MenuGroup menuGroup1 = new MenuGroup(1L, "메뉴그룹1");
-        final MenuGroup menuGroup2 = new MenuGroup(2L, "메뉴그룹2");
-        final List<MenuGroup> menuGroups = Arrays.asList(menuGroup1, menuGroup2);
-        given(menuGroupDao.findAll()).willReturn(menuGroups);
+    void createTest() {
+        given(menuGroupRepository.save(any())).willReturn(메뉴그룹);
 
-        final List<MenuGroup> actual = menuGroupService.list();
+        final MenuGroupResponse actual = menuGroupService.create(메뉴그룹_요청);
 
-        assertThat(actual).hasSize(2);
+        assertThat(actual.getId()).isEqualTo(메뉴그룹.getId());
+        assertThat(actual.getName()).isEqualTo(메뉴그룹.getName());
+    }
+
+    @DisplayName("메뉴 그룹 목록을 조회한다")
+    @Test
+    void listTest() {
+        given(menuGroupRepository.findAll()).willReturn(메뉴그룹_목록);
+
+        final List<MenuGroupResponse> actual = menuGroupService.list();
+
+        assertThat(actual.size()).isEqualTo(메뉴그룹_목록.size());
     }
 }
