03f6b7f19af236403e75acd79894fbd99a676c50
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:13:36 +0900

    feat: 메뉴 그룹 요청 / 응답 객체 생성

---
+++ src/main/java/kitchenpos/dto/menugroup/MenuGroupRequest.java
@@ -0,0 +1,26 @@
+package kitchenpos.dto.menugroup;
+
+import kitchenpos.domain.MenuGroup;
+
+public class MenuGroupRequest {
+    private String name;
+
+    protected MenuGroupRequest() {
+    }
+
+    public MenuGroupRequest(final String name) {
+        this.name = name;
+    }
+
+    public static MenuGroupRequest from(final String name) {
+        return new MenuGroupRequest(name);
+    }
+
+    public MenuGroup toMenuGroup() {
+        return MenuGroup.from(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
