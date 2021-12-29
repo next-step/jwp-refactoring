f965b654b05acbdd122ef472d3c9fbf092ae2a21
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:27:46 +0900

    feat: 공통 이름 객체 생성

---
+++ src/main/java/kitchenpos/domain/Name.java
@@ -0,0 +1,49 @@
+package kitchenpos.domain;
+
+import io.micrometer.core.instrument.util.StringUtils;
+import kitchenpos.common.exceptions.NoRequiredNameException;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class Name {
+    @Column
+    private String name;
+
+    protected Name() {
+    }
+
+    private Name(final String name) {
+        this.name = name;
+    }
+
+    public static Name from(final String valueOf) {
+        validate(valueOf);
+        return new Name(valueOf);
+    }
+
+    private static void validate(final String valueOf) {
+        if (StringUtils.isEmpty(valueOf) || Objects.isNull(valueOf)) {
+            throw new NoRequiredNameException();
+        }
+    }
+
+    public String toName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
