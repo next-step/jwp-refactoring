d4dc0708991e33dd07d961f7d1a0f0b802d2c6e9
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:51:52 +0900

    test: 메뉴 그룹 서비스 리팩토링으로 인한 테스트 소스 수정

--- src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
@@ -2,7 +2,9 @@ package kitchenpos.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kitchenpos.application.MenuGroupService;
-import kitchenpos.domain.MenuGroup;
+import kitchenpos.dto.menugroup.MenuGroupRequest;
+import kitchenpos.dto.menugroup.MenuGroupResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,7 +18,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
@@ -38,51 +39,57 @@ class MenuGroupRestControllerTest {
     @MockBean
     private MenuGroupService menuGroupService;
 
+    private MenuGroupRequest 메뉴그룹_요청;
+    private MenuGroupResponse 메뉴그룹_응답;
+    private MenuGroupResponse 메뉴그룹_응답2;
+    private List<MenuGroupResponse> 메뉴그룹목록_응답;
+
     @BeforeEach
     void setup() {
         this.mvc = MockMvcBuilders.webAppContextSetup(ctx)
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))
                 .alwaysDo(print())
                 .build();
+
+        메뉴그룹_요청 = MenuGroupRequest.from("메뉴그룹");
+        메뉴그룹_응답 = MenuGroupResponse.of(1L, "메뉴그룹");
+        메뉴그룹_응답2 = MenuGroupResponse.of(2L, "메뉴그룹2");
+        메뉴그룹목록_응답 = Lists.newArrayList(메뉴그룹_응답, 메뉴그룹_응답2);
     }
 
     @DisplayName("메뉴 그룹을 등록한다.")
     @Test
     void create() throws Exception {
-        // given
-        final MenuGroup menuGroup = new MenuGroup(1L, "한식");
-        given(menuGroupService.create(any())).willReturn(menuGroup);
+        
+        given(menuGroupService.create(any())).willReturn(메뉴그룹_응답);
 
-        // when
+        
         final ResultActions actions = mvc.perform(post("/api/menu-groups")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .accept(MediaType.APPLICATION_JSON_VALUE)
-                        .content(new ObjectMapper().writeValueAsString(menuGroup)))
+                        .content(new ObjectMapper().writeValueAsString(메뉴그룹_요청)))
                 .andDo(print());
-        //then
+        
         actions.andExpect(status().isCreated())
-                .andExpect(jsonPath("id").value(1L))
-                .andExpect(jsonPath("name").value("한식"));
+                .andExpect(jsonPath("id").value(메뉴그룹_응답.getId()))
+                .andExpect(jsonPath("name").value(메뉴그룹_요청.getName()));
     }
 
     @DisplayName("메뉴 그룹 목록을 조회한다.")
     @Test
     void list() throws Exception {
-        // given
-        final MenuGroup menuGroup1 = new MenuGroup(1L, "한식");
-        final MenuGroup menuGroup2 = new MenuGroup(2L, "중식");
-        final List<MenuGroup> menuGroups = Arrays.asList(menuGroup1, menuGroup2);
-        given(menuGroupService.list()).willReturn(menuGroups);
+        
+        given(menuGroupService.list()).willReturn(메뉴그룹목록_응답);
 
-        // when
+        
         final ResultActions actions = mvc.perform(get("/api/menu-groups")
                 .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andDo(print());
 
-        //then
+        
         actions.andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/json;charset=UTF-8")))
-                .andExpect(content().string(containsString("한식")))
-                .andExpect(content().string(containsString("중식")));
+                .andExpect(content().string(containsString("메뉴그룹")))
+                .andExpect(content().string(containsString("메뉴그룹2")));
     }
 }
