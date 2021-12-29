965a631d0fdf488e52f6ec56a90d3a979d053e94
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:38:41 +0900

    refactor: 메뉴 객체 JPA로 변경

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -1,65 +1,108 @@
 package kitchenpos.domain;
 
+import kitchenpos.common.exceptions.MenuGroupRequiredException;
+import kitchenpos.common.exceptions.MenuProductSumPriceException;
+import kitchenpos.common.exceptions.NoRequiredInputPriceException;
+import kitchenpos.common.exceptions.NoRequiredNameException;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
+@Entity
 public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private BigDecimal price;
-    private Long menuGroupId;
-    private List<MenuProduct> menuProducts;
+    @Embedded
+    @AttributeOverride(name = "name", column = @Column(name = "name", nullable = false))
+    private Name name;
+    @Enumerated
+    @AttributeOverride(name = "price", column = @Column(name = "price", nullable = false))
+    private Price price;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_group_id", foreignKey = @ForeignKey(name = "fk_menu_menu_group"), nullable = false)
+    private MenuGroup menuGroup;
+    @Embedded
+    private MenuProducts menuProducts = MenuProducts.empty();
 
-    public Menu() {}
+    public Menu() {
+    }
 
-    public Menu(final Long id, final String name, final BigDecimal price, final Long menuGroupId, final List<MenuProduct> menuProducts) {
+    private Menu(final Long id, final Name name, final Price price, final MenuGroup menuGroup) {
         this.id = id;
         this.name = name;
         this.price = price;
-        this.menuGroupId = menuGroupId;
-        this.menuProducts = menuProducts;
+        this.menuGroup = menuGroup;
     }
-    public static Menu of(final Long id, final String name, final BigDecimal price, final Long menuGroupId, final List<MenuProduct> menuProducts) {
-        return new Menu(id, name, price, menuGroupId, menuProducts);
+
+    public static Menu of(final Long id, final Name name, final Price price, final MenuGroup menuGroup) {
+        validate(name, price, menuGroup);
+        return new Menu(id, name, price, menuGroup);
+    }
+
+    public static Menu of(final String name, final BigDecimal price, final MenuGroup menuGroup) {
+        return of(null, Name.from(name), Price.from(price), menuGroup);
+    }
+
+    private static void validate(final Name name, final Price price, final MenuGroup menuGroup) {
+        if (Objects.isNull(name)) {
+            throw new NoRequiredNameException();
+        }
+        if (Objects.isNull(price)) {
+            throw new NoRequiredInputPriceException();
+        }
+        if (Objects.isNull(menuGroup)) {
+            throw new MenuGroupRequiredException();
+        }
+    }
+
+    public void addMenuProducts(final List<MenuProduct> menuProductList) {
+        menuProductList.forEach(this::addMenuProduct);
+    }
+
+    private void addMenuProduct(final MenuProduct menuProduct) {
+        menuProduct.decideMenu(this);
+        menuProducts.add(menuProduct);
+    }
+
+    public void checkOverPrice() {
+        if (menuProducts.isOverPrice(price)) {
+            throw new MenuProductSumPriceException();
+        }
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
+    public Name getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
+    public Price getPrice() {
         return price;
     }
 
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
+    public MenuGroup getMenuGroup() {
+        return menuGroup;
     }
 
-    public Long getMenuGroupId() {
-        return menuGroupId;
-    }
-
-    public void setMenuGroupId(final Long menuGroupId) {
-        this.menuGroupId = menuGroupId;
-    }
-
-    public List<MenuProduct> getMenuProducts() {
+    public MenuProducts getMenuProducts() {
         return menuProducts;
     }
 
-    public void setMenuProducts(final List<MenuProduct> menuProducts) {
-        this.menuProducts = menuProducts;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Menu menu = (Menu) o;
+        return Objects.equals(id, menu.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
