0ce9a79c4240de858d1d1e65853904d5f204b29d
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:36:29 +0900

    feat: 손님수 객체 생성

---
+++ src/main/java/kitchenpos/domain/NumberOfGuests.java
@@ -0,0 +1,50 @@
+package kitchenpos.domain;
+
+import kitchenpos.common.exceptions.NegativeNumberOfGuestsException;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class NumberOfGuests {
+    private static final int MINIMUM = 0;
+
+    @Column
+    private int numberOfGuests;
+
+    protected NumberOfGuests() {
+    }
+
+    private NumberOfGuests(final int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public static NumberOfGuests from(final Integer numberOfGuests) {
+        validate(numberOfGuests);
+        return new NumberOfGuests(numberOfGuests);
+    }
+
+    private static void validate(final Integer quantity) {
+        if (Objects.isNull(quantity) || quantity < MINIMUM) {
+            throw new NegativeNumberOfGuestsException();
+        }
+    }
+
+    public int toInt() {
+        return numberOfGuests;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NumberOfGuests that = (NumberOfGuests) o;
+        return numberOfGuests == that.numberOfGuests;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numberOfGuests);
+    }
+}
