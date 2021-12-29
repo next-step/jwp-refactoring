745dd2cbab45788ff856808cef5901944fb8d378
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:50:35 +0900

    test: 주문 서비스 리팩토링으로 인한 테스트 소스 수정

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -1,13 +1,10 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.MenuDao;
-import kitchenpos.dao.OrderDao;
-import kitchenpos.dao.OrderLineItemDao;
-import kitchenpos.dao.OrderTableDao;
-import kitchenpos.domain.Order;
-import kitchenpos.domain.OrderLineItem;
-import kitchenpos.domain.OrderStatus;
-import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.*;
+import kitchenpos.dto.menu.OrderLineItemRequest;
+import kitchenpos.dto.order.OrderRequest;
+import kitchenpos.dto.order.OrderResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,93 +12,98 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
     @Mock
-    private MenuDao menuDao;
-
+    private OrderRepository orderRepository;
     @Mock
-    private OrderDao orderDao;
-
+    private TableService tableService;
     @Mock
-    private OrderLineItemDao orderLineItemDao;
-
-    @Mock
-    private OrderTableDao orderTableDao;
-
+    private MenuService menuService;
     @InjectMocks
     private OrderService orderService;
+
+    private Menu 메뉴;
+    private MenuGroup 메뉴그룹;
+    private Order 주문;
+    private List<Order> 주문_목록;
+    private OrderTable 주문테이블;
+    private OrderLineItem 주문상품;
+
+    private OrderLineItemRequest 주문상품_요청;
+    private OrderRequest 주문_요청;
+    private OrderRequest 주문_완료_요청;
+
+    @BeforeEach
+    void setUp() {
+        메뉴그룹 = MenuGroup.of(1L, "메뉴그룹");
+        메뉴 = Menu.of(1L, Name.from("메뉴"), Price.from(new BigDecimal("15000")), 메뉴그룹);
+        주문테이블 = OrderTable.of(1L, 1, false);
+        주문상품 = OrderLineItem.of(메뉴, 1L);
+        주문 = Order.of(1L, 주문테이블, OrderStatus.COOKING);
+        주문.addOrderLineItems(Collections.singletonList(주문상품));
+        주문_목록 = Collections.singletonList(주문);
+
+        주문상품_요청 = OrderLineItemRequest.of(1L, 1L);
+        주문_요청 = OrderRequest.of(1L, Collections.singletonList(주문상품_요청));
+        주문_완료_요청 = OrderRequest.from(OrderStatus.COMPLETION);
+    }
 
     @DisplayName("주문을 등록한다.")
     @Test
     void saveOrder() {
-        final OrderLineItem orderLineItem1 = new OrderLineItem(1L, 1L, null, 5);
-        final OrderLineItem orderLineItem2 = new OrderLineItem(1L, 1L, null, 5);
-        final List<OrderLineItem> orderLineItems = Arrays.asList(orderLineItem1, orderLineItem2);
-        final Order order = new Order(1L, 1L, null, null, orderLineItems);
-        final OrderTable orderTable = new OrderTable(1L, null, 0, false);
-        given(menuDao.countByIdIn(anyList())).willReturn(2L);
-        given(orderTableDao.findById(anyLong())).willReturn(Optional.of(orderTable));
-        given(orderDao.save(any())).willReturn(order);
-        given(orderLineItemDao.save(any())).willReturn(orderLineItem1);
-        given(orderLineItemDao.save(any())).willReturn(orderLineItem2);
+        given(tableService.findOrderTableById(anyLong())).willReturn(주문테이블);
+        given(menuService.getMenuById(anyLong())).willReturn(메뉴);
+        given(orderRepository.save(any())).willReturn(주문);
 
-        final Order actual = orderService.create(order);
+        final OrderResponse actual = orderService.create(주문_요청);
 
         assertAll(
-                () -> assertThat(actual).isEqualTo(order),
-                () -> assertThat(actual.getOrderStatus()).isEqualTo(OrderStatus.COOKING.name()),
-                () -> assertThat(actual.getOrderLineItems()).hasSize(2)
+                () -> assertThat(actual).isNotNull(),
+                () -> assertThat(actual.getId()).isEqualTo(주문.getId()),
+                () -> assertThat(actual.getOrderStatus()).isEqualTo(주문.getOrderStatus()),
+                () -> assertThat(actual.getOrderLineItems()).hasSize(주문.getOrderLineItems().toList().size()),
+                () -> assertThat(actual.getOrderTableResponse().getId()).isEqualTo(주문.getOrderTable().getId()),
+                () -> assertThat(actual.getOrderTableResponse().getNumberOfGuests()).isEqualTo(주문.getOrderTable().getNumberOfGuests().toInt())
+
         );
     }
 
     @DisplayName("등록된 주문을 조회한다.")
     @Test
     void findOrders() {
-        final OrderLineItem orderLineItem1 = new OrderLineItem(1L, 1L, null, 5);
-        final OrderLineItem orderLineItem2 = new OrderLineItem(1L, 1L, null, 5);
-        final List<OrderLineItem> orderLineItems = Arrays.asList(orderLineItem1, orderLineItem2);
-        final Order order = new Order(1L, 1L, null, null, orderLineItems);
+        given(orderRepository.findAll()).willReturn(주문_목록);
 
-        given(orderDao.findAll()).willReturn(Collections.singletonList(order));
-        given(orderLineItemDao.findAllByOrderId(anyLong())).willReturn(orderLineItems);
-
-        final List<Order> actual = orderService.list();
+        final List<OrderResponse> actual = orderService.list();
 
         assertAll(
-                () -> assertThat(actual).isNotEmpty(),
-                () -> assertThat(actual.get(0)).isEqualTo(order),
-                () -> assertThat(actual.get(0).getOrderLineItems()).hasSize(2)
+                () -> assertThat(actual).isNotNull(),
+                () -> assertThat(actual.get(0).getId()).isEqualTo(주문_목록.get(0).getId()),
+                () -> assertThat(actual.get(0).getOrderStatus()).isEqualTo(주문_목록.get(0).getOrderStatus()),
+                () -> assertThat(actual.get(0).getOrderLineItems()).hasSize(주문_목록.get(0).getOrderLineItems().toList().size()),
+                () -> assertThat(actual.get(0).getOrderTableResponse().getId()).isEqualTo(주문_목록.get(0).getOrderTable().getId()),
+                () -> assertThat(actual.get(0).getOrderTableResponse().getNumberOfGuests()).isEqualTo(주문_목록.get(0).getOrderTable().getNumberOfGuests().toInt())
         );
     }
 
     @DisplayName("주문 상태를 완료로 변경한다.")
     @Test
     void changeOrderStatus() {
-        final OrderLineItem orderLineItem1 = new OrderLineItem(1L, 1L, null, 5);
-        final OrderLineItem orderLineItem2 = new OrderLineItem(1L, 1L, null, 5);
-        final List<OrderLineItem> orderLineItems = Arrays.asList(orderLineItem1, orderLineItem2);
-        final Order order = new Order(1L, 1L, OrderStatus.COOKING.name(), LocalDateTime.now(), orderLineItems);
-        final Order saveOrder = new Order(1L, 1L, OrderStatus.COMPLETION.name(), LocalDateTime.now(), orderLineItems);
+        given(orderRepository.findById(anyLong())).willReturn(Optional.of(주문));
 
-        given(orderDao.findById(anyLong())).willReturn(Optional.of(order));
-        given(orderDao.save(any())).willReturn(saveOrder);
-        given(orderLineItemDao.findAllByOrderId(anyLong())).willReturn(orderLineItems);
+        final OrderResponse actual = orderService.changeOrderStatus(1L, 주문_완료_요청);
 
-        final Order changeOrderStatus = new Order(null, null, OrderStatus.COMPLETION.name(), null, null);
-        final Order actual = orderService.changeOrderStatus(1L, changeOrderStatus);
-
-        assertThat(actual.getOrderStatus()).isEqualTo(changeOrderStatus.getOrderStatus());
+        assertThat(actual.getOrderStatus()).isEqualTo(주문_완료_요청.getOrderStatus());
     }
 }
