f42f3f08d12bc14073c4c906a414aa3397d21c65
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:51:42 +0900

    test: 메뉴 서비스 리팩토링으로 인한 테스트 소스 수정

--- src/test/java/kitchenpos/ui/MenuRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuRestControllerTest.java
@@ -1,11 +1,13 @@
 package kitchenpos.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import kitchenpos.application.MenuGroupService;
 import kitchenpos.application.MenuService;
-import kitchenpos.domain.Menu;
-import kitchenpos.domain.MenuGroup;
-import kitchenpos.domain.MenuProduct;
+import kitchenpos.dto.menu.MenuProductRequest;
+import kitchenpos.dto.menu.MenuProductResponse;
+import kitchenpos.dto.menu.MenuRequest;
+import kitchenpos.dto.menu.MenuResponse;
+import kitchenpos.dto.menugroup.MenuGroupResponse;
+import kitchenpos.dto.product.ProductResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,19 +22,15 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @WebMvcTest(MenuRestController.class)
 class MenuRestControllerTest {
@@ -45,54 +43,61 @@ class MenuRestControllerTest {
     @MockBean
     private MenuService menuService;
 
+    private MenuProductRequest 메뉴상품_요청;
+    private MenuRequest 메뉴_요청;
+    private MenuProductResponse 메뉴상품_응답;
+    private MenuGroupResponse 메뉴그룹_응답;
+    private MenuResponse 메뉴_응답;
+
     @BeforeEach
     void setup() {
         this.mvc = MockMvcBuilders.webAppContextSetup(ctx)
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))
                 .alwaysDo(print())
                 .build();
+
+        메뉴상품_요청 = MenuProductRequest.of(1L, 1L);
+        메뉴_요청 = MenuRequest.of("후라이드", new BigDecimal("50000"), 1L, Collections.singletonList(메뉴상품_요청));
+        메뉴상품_응답 = MenuProductResponse.of(ProductResponse.of(1L, "후라이드", new BigDecimal("10000")), 1L);
+        메뉴그룹_응답 = MenuGroupResponse.of(1L, "추천메뉴");
+        메뉴_응답 = MenuResponse.of(1L, "후라이드", new BigDecimal("50000"), 메뉴그룹_응답, Collections.singletonList(메뉴상품_응답));
     }
 
     @DisplayName("메뉴를 등록한다.")
     @Test
     void create() throws Exception {
-        // given
-        final MenuProduct menuProduct = new MenuProduct(1L, 1L, 1L, 2);
-        final Menu menu = new Menu(1L, "메뉴", new BigDecimal("10000"), 1L, Collections.singletonList(menuProduct));
-        given(menuService.create(any())).willReturn(menu);
+        given(menuService.create(any())).willReturn(메뉴_응답);
 
-        // when
+        
         final ResultActions actions = mvc.perform(post("/api/menus")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .accept(MediaType.APPLICATION_JSON_VALUE)
-                        .content(new ObjectMapper().writeValueAsString(menu)))
+                        .content(new ObjectMapper().writeValueAsString(메뉴_요청)))
                 .andDo(print());
-        //then
+        
         actions.andExpect(status().isCreated())
                 .andExpect(header().stringValues("Location", "/api/menus/1"))
                 .andExpect(jsonPath("id").value(1L))
-                .andExpect(jsonPath("name").value("메뉴"))
-                .andExpect(jsonPath("price").value(new BigDecimal("10000")))
-                .andExpect(jsonPath("menuGroupId").value(1L))
-                .andExpect(jsonPath("menuProducts").isArray());
+                .andExpect(jsonPath("name").value("후라이드"))
+                .andExpect(jsonPath("price").value(new BigDecimal("50000")))
+                .andExpect(jsonPath("menuGroupResponse").hasJsonPath())
+                .andExpect(jsonPath("menuProductResponses").isArray());
     }
 
     @DisplayName("메뉴를 조회한다.")
     @Test
     void list() throws Exception {
-        // given
-        final MenuProduct menuProduct = new MenuProduct(1L, 1L, 1L, 2);
-        final Menu menu = new Menu(1L, "메뉴", new BigDecimal("10000"), 1L, Collections.singletonList(menuProduct));
-        given(menuService.list()).willReturn(Collections.singletonList(menu));
+        
+        given(menuService.list()).willReturn(Collections.singletonList(메뉴_응답));
 
-        // when
+        
         final ResultActions actions = mvc.perform(get("/api/menus")
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print());
 
-        //then
+        
         actions.andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/json;charset=UTF-8")))
-                .andExpect(content().string(containsString("메뉴")));
+                .andExpect(content().string(containsString("후라이드")));
     }
 }
