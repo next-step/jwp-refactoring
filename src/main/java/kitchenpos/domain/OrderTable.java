643616ab3d6ed0354265af5eb6c5908456b368e0
lovethefeel <lovethefeel@gmail.com>
2021-12-30 03:39:10 +0900

    refactor: 주문 테이블 상품 객체 JPA로 변경

--- src/main/java/kitchenpos/domain/OrderTable.java
+++ src/main/java/kitchenpos/domain/OrderTable.java
@@ -1,53 +1,103 @@
 package kitchenpos.domain;
 
+import kitchenpos.common.exceptions.NegativeNumberOfGuestsException;
+import kitchenpos.common.exceptions.NotEmptyOrderTableGroupException;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class OrderTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long tableGroupId;
-    private int numberOfGuests;
-    private boolean empty;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "table_group_id", foreignKey = @ForeignKey(name = "fk_order_table_table_group"))
+    private TableGroup tableGroup;
+    @Embedded
+    @Column(nullable = false)
+    private NumberOfGuests numberOfGuests;
+    @Embedded
+    private Empty empty;
 
-    public OrderTable() {}
+    protected OrderTable() {}
 
-    public OrderTable(final long id, final Long tableGroupId, final int numberOfGuests, final boolean empty) {
+    private OrderTable(final Long id, final TableGroup tableGroup, final NumberOfGuests numberOfGuests, final Empty empty) {
         this.id = id;
-        this.tableGroupId = tableGroupId;
+        this.tableGroup = tableGroup;
         this.numberOfGuests = numberOfGuests;
         this.empty = empty;
     }
 
-    public static OrderTable of(final long id, final Long tableGroupId, final int numberOfGuests, final boolean empty) {
-        return new OrderTable(id, tableGroupId, numberOfGuests, empty);
+
+    public static OrderTable of(final Long id, final TableGroup tableGroup, final NumberOfGuests numberOfGuests, final Empty empty) {
+        return new OrderTable(id, tableGroup, numberOfGuests, empty);
+    }
+
+    public static OrderTable of(final NumberOfGuests numberOfGuests, final Empty empty) {
+        return of(null, null, numberOfGuests, empty);
+    }
+
+    public static OrderTable of(final int numberOfGuests, final boolean empty) {
+        return of(null, null, NumberOfGuests.from(numberOfGuests), Empty.from(empty));
+    }
+
+    public static OrderTable of(final TableGroup tableGroup, final int numberOfGuests, final boolean empty) {
+        return of(null, tableGroup, NumberOfGuests.from(numberOfGuests), Empty.from(empty));
+    }
+
+    public static OrderTable of(final Long id, final int numberOfGuests, final boolean empty) {
+        return new OrderTable(id, null, NumberOfGuests.from(numberOfGuests), Empty.from(empty));
+    }
+
+    public void changeEmptyStatus(final boolean empty) {
+        if (Objects.nonNull(tableGroup)) {
+            throw new NotEmptyOrderTableGroupException();
+        }
+        this.empty = Empty.from(empty);
+    }
+
+    public void changeNumberOfGuests(final int numbers) {
+        if (this.getEmpty().isEmpty()) {
+            throw new NegativeNumberOfGuestsException();
+        }
+        this.numberOfGuests = NumberOfGuests.from(numbers);
+    }
+
+    public void unGroup() {
+        this.tableGroup = null;
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
+    public TableGroup getTableGroup() {
+        return tableGroup;
     }
 
-    public Long getTableGroupId() {
-        return tableGroupId;
-    }
-
-    public void setTableGroupId(final Long tableGroupId) {
-        this.tableGroupId = tableGroupId;
-    }
-
-    public int getNumberOfGuests() {
+    public NumberOfGuests getNumberOfGuests() {
         return numberOfGuests;
     }
 
-    public void setNumberOfGuests(final int numberOfGuests) {
-        this.numberOfGuests = numberOfGuests;
-    }
-
-    public boolean isEmpty() {
+    public Empty getEmpty() {
         return empty;
     }
 
-    public void setEmpty(final boolean empty) {
-        this.empty = empty;
+    public boolean isEmpty() {
+        return empty.isEmpty();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderTable that = (OrderTable) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
