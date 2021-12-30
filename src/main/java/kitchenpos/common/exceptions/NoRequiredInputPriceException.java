94c1ee69b985b82824100c57d9e2729b7e9a10fe
lovethefeel <lovethefeel@gmail.com>
2021-12-31 01:14:41 +0900

    refactor: 사용하지 않는 익셉션 클래스 삭제

--- src/main/java/kitchenpos/common/exceptions/NoRequiredInputPriceException.java
+++
@@ -1,9 +0,0 @@
-package kitchenpos.common.exceptions;
-
-public class NoRequiredInputPriceException extends CustomException {
-    private static final String NO_REQUIRED_INPUT_PRICE = "가격이 입력되지 않았습니다.";
-
-    public NoRequiredInputPriceException() {
-        super(NO_REQUIRED_INPUT_PRICE);
-    }
-}
