bcff870f52ab12483a7b0f06ea1e05856b9c6e16
lovethefeel <lovethefeel@gmail.com>
2021-12-31 23:42:32 +0900

    refactor: 익셉션 체크 로직 추가

--- src/main/java/kitchenpos/application/OrderValidator.java
+++ src/main/java/kitchenpos/application/OrderValidator.java
@@ -1,11 +1,15 @@
 package kitchenpos.application;
 
+import kitchenpos.common.exceptions.EmptyMenuException;
+import kitchenpos.common.exceptions.EmptyOrderTableException;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.dto.order.OrderLineItemRequest;
 import kitchenpos.dto.order.OrderRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 @Component
 @Transactional(readOnly = true)
@@ -14,18 +18,24 @@ public class OrderValidator {
     private final MenuService menuService;
     private final TableService tableService;
 
-    public OrderValidator(MenuService menuService, TableService tableService) {
+    public OrderValidator(final MenuService menuService, final TableService tableService) {
         this.menuService = menuService;
         this.tableService = tableService;
     }
 
     public Long findOrderTableById(final OrderRequest request) {
         final OrderTable orderTable = tableService.findOrderTableById(request.getOrderTableId());
+        if (Objects.isNull(orderTable)) {
+            throw new EmptyOrderTableException();
+        }
         return orderTable.getId();
     }
 
     public Long findMenuById(final OrderLineItemRequest request) {
         final Menu menu = menuService.getMenuById(request.getMenuId());
+        if (Objects.isNull(menu)) {
+            throw new EmptyMenuException();
+        }
         return menu.getId();
     }
 }
