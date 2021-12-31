dff5f0b4cdbd4367377dce2ef2759f75a53f2e9f
lovethefeel <lovethefeel@gmail.com>
2021-12-31 23:49:00 +0900

    refactor: 쿼리 비용을 줄이기 위해서 변경

--- src/main/java/kitchenpos/domain/OrderRepository.java
+++ src/main/java/kitchenpos/domain/OrderRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByOrderTableIdIn(List<OrderTable> orderTables);
 
-    boolean existsByOrderTableIdAndOrderStatusIn(OrderTable orderTable, List<OrderStatus> statusList);
+    Order findByOrderTableId(Long orderTableId);
 }
