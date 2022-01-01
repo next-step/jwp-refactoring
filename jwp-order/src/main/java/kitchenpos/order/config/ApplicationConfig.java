a782b7dba514a9886e5a1d6deb34709afe4d3b26
lovethefeel <lovethefeel@gmail.com>
2022-01-02 03:43:07 +0900

    refactor: warning 제거

--- jwp-order/src/main/java/kitchenpos/order/config/ApplicationConfig.java
+++ jwp-order/src/main/java/kitchenpos/order/config/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package kitchenpos.order.config;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
