d93cbcfa7bb03718f93129c74aab9758d47a5182
lovethefeel <lovethefeel@gmail.com>
2022-01-02 00:52:29 +0900

    refactor: 주문연결상품 등록 메소드 추가

--- src/main/java/kitchenpos/dto/order/OrderLineItemRequest.java
+++ src/main/java/kitchenpos/dto/order/OrderLineItemRequest.java
@@ -1,5 +1,7 @@
 package kitchenpos.dto.order;
 
+import kitchenpos.domain.OrderLineItem;
+
 public class OrderLineItemRequest {
     private Long menuId;
     private long quantity;
@@ -14,6 +16,10 @@ public class OrderLineItemRequest {
 
     public static OrderLineItemRequest of(final Long menuId, final long quantity) {
         return new OrderLineItemRequest(menuId, quantity);
+    }
+
+    public OrderLineItem toOrderLineItem() {
+        return OrderLineItem.of(this.menuId, this.quantity);
     }
 
     public Long getMenuId() {
