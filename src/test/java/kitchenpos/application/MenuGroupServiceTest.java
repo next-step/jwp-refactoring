45e5f245758e17b51b75711bdb352a4ef9aebe96
joy.hj <joy.hj@kakaocorp.com>
2022-06-15 19:23:19 +0900

    test: MenuGroup -> business, controller 테스트 생성

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,51 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class MenuGroupServiceTest {
+    @Mock
+    private MenuGroupDao menuGroupDao;
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    @Test
+    @DisplayName("메뉴 그룹을 생성할 수 있다.")
+    void create() {
+        //given
+        given(menuGroupDao.save(any())).willReturn(new MenuGroup());
+
+        //when
+        MenuGroup menuGroup = menuGroupService.create(new MenuGroup());
+
+        //then
+        assertThat(menuGroup).isNotNull();
+    }
+
+    @Test
+    @DisplayName("전체 메뉴 그룹을 조회할 수 있다.")
+    void list() {
+        //given
+        given(menuGroupDao.findAll()).willReturn(Arrays.asList(new MenuGroup(), new MenuGroup()));
+
+        //when
+        List<MenuGroup> menuGroupList = menuGroupService.list();
+
+        //then
+        assertThat(menuGroupList).isNotEmpty();
+    }
+}
---
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -0,0 +1,10 @@
+package kitchenpos.application;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class TableServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
