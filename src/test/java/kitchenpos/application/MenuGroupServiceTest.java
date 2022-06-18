d585a0fe01f57349e967b33123a8320a20c206e5
limwoobin <drogba02@naver.com>
2022-06-18 23:16:47 +0900

    test: 메뉴그룹의 서비스 로직에 대한 테스트 코드를 작성하라

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,67 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@DisplayName("상품 서비스에 대한 테스트")
+@ExtendWith(MockitoExtension.class)
+class MenuGroupServiceTest {
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    private MenuGroup 치킨_그룹;
+    private MenuGroup 피자_그룹;
+
+    @BeforeEach
+    void setUp() {
+        치킨_그룹 = MenuGroup.of(1L, "치킨 메뉴 그룹");
+        피자_그룹 = MenuGroup.of(2L, "피자 메뉴 그룹");
+    }
+
+    @DisplayName("메뉴그룹을 등록하면 정상적으로 등록되어야 한다")
+    @Test
+    void create_test() {
+        // given
+        when(menuGroupDao.save(치킨_그룹))
+            .thenReturn(치킨_그룹);
+
+        // when
+        MenuGroup result = menuGroupService.create(치킨_그룹);
+
+        // then
+        assertAll(
+            () -> assertThat(result.getId()).isEqualTo(치킨_그룹.getId()),
+            () -> assertThat(result.getName()).isEqualTo(치킨_그룹.getName())
+        );
+    }
+
+    @DisplayName("메뉴그룹의 목록을 조회한다")
+    @Test
+    void findAll_test() {
+        // given
+        when(menuGroupDao.findAll())
+            .thenReturn(Arrays.asList(치킨_그룹, 피자_그룹));
+
+        // when
+        List<MenuGroup> result = menuGroupService.list();
+
+        // then
+        assertThat(result).hasSize(2);
+    }
+}
