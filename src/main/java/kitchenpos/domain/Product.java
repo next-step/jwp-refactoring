473759ba6e559fbc209d460cb3f585d2524b55e3
chj1768 <chj1768@gmail.com>
2022-06-20 18:42:36 +0900

    feat: Product entity 설정

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -1,11 +1,24 @@
 package kitchenpos.domain;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private BigDecimal price;
+
+    public Product() {}
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
 
     public Long getId() {
         return id;
--- src/main/java/kitchenpos/domain/ProductRepository.java
+++ src/main/java/kitchenpos/domain/ProductRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MenuGroupRepository extends JpaRepository<MenuGroup, Long> {
-    boolean existsById(Long id);
-}
+public interface ProductRepository extends JpaRepository<Product, Long> {}
