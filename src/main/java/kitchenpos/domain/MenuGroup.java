db05e02630f5093d0e8f92c0c2f420ff265c8b88
chj1768 <chj1768@gmail.com>
2022-06-20 18:07:30 +0900

    feat: MenuGroupRepository 생성, Entity 설정 적용

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,8 +1,20 @@
 package kitchenpos.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class MenuGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+
+    public MenuGroup() {}
+
+    public MenuGroup(String name) {
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
