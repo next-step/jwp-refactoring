8604e206e9e4207d23c8d31ef08a3de8a5ac0032
limwoobin <drogba02@naver.com>
2022-06-20 21:18:44 +0900

    test: 상품에 대해 인수테스트를 작성하라

--- src/test/java/kitchenpos/acceptance/ProductAcceptanceSupport.java
+++ src/test/java/kitchenpos/acceptance/ProductAcceptanceSupport.java
@@ -1,9 +1,13 @@
 package kitchenpos.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.List;
 import kitchenpos.domain.Product;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 public class ProductAcceptanceSupport {
@@ -16,5 +20,27 @@ public class ProductAcceptanceSupport {
             .when().post("/api/products")
             .then().log().all()
             .extract();
+    }
+
+    public static void 상품_등록됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
+    }
+
+    public static ExtractableResponse<Response> 상품목록_조회요청() {
+        return RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().get("/api/products")
+            .then().log().all().
+            extract();
+    }
+
+    public static void 상품목록_조회됨(ExtractableResponse<Response> response, int size) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        List<Product> result = response.jsonPath().getList(".", Product.class);
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(size);
     }
 }
---
+++ src/test/java/kitchenpos/acceptance/ProductAcceptanceTest.java
@@ -0,0 +1,52 @@
+package kitchenpos.acceptance;
+
+import static kitchenpos.acceptance.ProductAcceptanceSupport.상품_등록됨;
+import static kitchenpos.acceptance.ProductAcceptanceSupport.상품_등록요청;
+import static kitchenpos.acceptance.ProductAcceptanceSupport.상품목록_조회됨;
+import static kitchenpos.acceptance.ProductAcceptanceSupport.상품목록_조회요청;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.math.BigDecimal;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("상품에 관한 인수 테스트")
+class ProductAcceptanceTest extends AcceptanceTest {
+    private Product 상품;
+    private Product 상품2;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        상품 = Product.of(null, "후라이드치킨", BigDecimal.valueOf(15000L));
+        상품2 = Product.of(null, "불고기피자", BigDecimal.valueOf(13000L));
+    }
+
+    @DisplayName("상품을 등록한다")
+    @Test
+    void create_test() {
+        // when
+        ExtractableResponse<Response> response = 상품_등록요청(상품);
+
+        // then
+        상품_등록됨(response);
+    }
+
+    @DisplayName("상품목록을 조회한다")
+    @Test
+    void find_test() {
+        // given
+        상품_등록요청(상품);
+        상품_등록요청(상품2);
+
+        // when
+        ExtractableResponse<Response> response = 상품목록_조회요청();
+
+        // then
+        상품목록_조회됨(response, 2);
+    }
+}
