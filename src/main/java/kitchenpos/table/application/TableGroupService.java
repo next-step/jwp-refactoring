634611cba703712404e0e9f7c360ced1f578baa7
limwoobin <drogba02@naver.com>
2022-06-21 21:59:46 +0900

    refactor: 인수테스트를 기반으로 TableGroup 의 copy 본을 생성하라

--- src/main/java/kitchenpos/table/application/TableGroupService.java
+++ src/main/java/kitchenpos/table/application/TableGroupService.java
@@ -5,7 +5,15 @@ import kitchenpos.table.dao.OrderTableDao;
 import kitchenpos.table.dao.TableGroupDao;
 import kitchenpos.order.domain.OrderStatus;
 import kitchenpos.table.domain.OrderTable;
+import kitchenpos.table.domain.OrderTableEntity;
+import kitchenpos.table.domain.OrderTableRepository;
+import kitchenpos.table.domain.OrderTables;
 import kitchenpos.table.domain.TableGroup;
+import kitchenpos.table.domain.TableGroupEntity;
+import kitchenpos.table.domain.TableGroupRepository;
+import kitchenpos.table.domain.request.OrderTableRequest;
+import kitchenpos.table.domain.request.TableGroupRequest;
+import kitchenpos.table.domain.response.TableGroupResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
@@ -21,11 +29,15 @@ public class TableGroupService {
     private final OrderDao orderDao;
     private final OrderTableDao orderTableDao;
     private final TableGroupDao tableGroupDao;
+    private final OrderTableRepository orderTableRepository;
+    private final TableGroupRepository tableGroupRepository;
 
-    public TableGroupService(final OrderDao orderDao, final OrderTableDao orderTableDao, final TableGroupDao tableGroupDao) {
+    public TableGroupService(final OrderDao orderDao, final OrderTableDao orderTableDao, final TableGroupDao tableGroupDao, OrderTableRepository orderTableRepository, TableGroupRepository tableGroupRepository) {
         this.orderDao = orderDao;
         this.orderTableDao = orderTableDao;
         this.tableGroupDao = tableGroupDao;
+        this.orderTableRepository = orderTableRepository;
+        this.tableGroupRepository = tableGroupRepository;
     }
 
     @Transactional
@@ -65,6 +77,69 @@ public class TableGroupService {
         savedTableGroup.setOrderTables(savedOrderTables);
 
         return savedTableGroup;
+    }
+
+    @Transactional
+    public TableGroupResponse createCopy(final TableGroupRequest request) {
+        final List<OrderTable> orderTables = tableGroup.getOrderTables();
+
+        if (CollectionUtils.isEmpty(orderTables) || orderTables.size() < 2) {
+            throw new IllegalArgumentException();
+        }
+
+        final List<Long> orderTableIds = orderTables.stream()
+            .map(OrderTable::getId)
+            .collect(Collectors.toList());
+
+        final List<OrderTable> savedOrderTables = orderTableDao.findAllByIdIn(orderTableIds);
+
+        if (orderTables.size() != savedOrderTables.size()) {
+            throw new IllegalArgumentException();
+        }
+
+        for (final OrderTable savedOrderTable : savedOrderTables) {
+            if (!savedOrderTable.isEmpty() || Objects.nonNull(savedOrderTable.getTableGroupId())) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        tableGroup.setCreatedDate(LocalDateTime.now());
+
+        final TableGroup savedTableGroup = tableGroupDao.save(tableGroup);
+
+        final Long tableGroupId = savedTableGroup.getId();
+        for (final OrderTable savedOrderTable : savedOrderTables) {
+            savedOrderTable.setTableGroupId(tableGroupId);
+            savedOrderTable.setEmpty(false);
+            orderTableDao.save(savedOrderTable);
+        }
+        savedTableGroup.setOrderTables(savedOrderTables);
+
+        return savedTableGroup;
+    }
+
+    private List<OrderTableEntity> validateOrderTables(List<OrderTableRequest> orderTableRequest) {
+        validateOrderTableSize(orderTableRequest);
+
+        final List<Long> orderTableIds = orderTableRequest.stream()
+            .map(OrderTableRequest::getId)
+            .collect(Collectors.toList());
+
+        final List<OrderTableEntity> savedOrderTables = orderTableRepository.findAllByIdIn(orderTableIds);
+        validateOrderTableEqualsSize(savedOrderTables, orderTableIds);
+        return savedOrderTables;
+    }
+
+    private void validateOrderTableSize(List<OrderTableRequest> orderTableRequest) {
+        if (CollectionUtils.isEmpty(orderTableRequest) || orderTableRequest.size() < 2) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateOrderTableEqualsSize(List<OrderTableEntity> savedOrderTables, List<Long> orderTableIds)  {
+        if (savedOrderTables.size() != orderTableIds.size()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Transactional
--- src/main/java/kitchenpos/table/ui/TableGroupRestController.java
+++ src/main/java/kitchenpos/table/ui/TableGroupRestController.java
@@ -2,6 +2,9 @@ package kitchenpos.table.ui;
 
 import kitchenpos.table.application.TableGroupService;
 import kitchenpos.table.domain.TableGroup;
+import kitchenpos.table.domain.TableGroupRepository;
+import kitchenpos.table.domain.request.TableGroupRequest;
+import kitchenpos.table.domain.response.TableGroupResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +23,13 @@ public class TableGroupRestController {
     @PostMapping
     public ResponseEntity<TableGroup> create(@RequestBody final TableGroup tableGroup) {
         final TableGroup created = tableGroupService.create(tableGroup);
+        final URI uri = URI.create("/api/table-groups/" + created.getId());
+        return ResponseEntity.created(uri).body(created);
+    }
+
+    @PostMapping
+    public ResponseEntity<TableGroupResponse> createCopy(@RequestBody final TableGroupRequest request) {
+        final TableGroupResponse created = tableGroupService.createCopy(request);
         final URI uri = URI.create("/api/table-groups/" + created.getId());
         return ResponseEntity.created(uri).body(created);
     }
