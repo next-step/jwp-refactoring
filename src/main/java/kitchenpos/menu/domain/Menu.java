aa8cade459e284282cf043952c8a98ef77015e40
KangSeongHyo <abc9023@naver.com>
2022-06-21 17:58:10 +0900

    feat : menu 관련 클래스 패키지 이동 및 menu 엔티티, 레포지토리 추가

---
+++ src/main/java/kitchenpos/menu/domain/Menu.java
@@ -0,0 +1,23 @@
+package kitchenpos.menu.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Integer price;
+    @ManyToOne
+    @JoinColumn(name = "menu_group_id")
+    private MenuGroup menuGroup;
+}
