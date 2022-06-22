6c81087bee0a6858c2ddaf261113e65b5293fd33
limwoobin <drogba02@naver.com>
2022-06-23 06:18:01 +0900

    test: 서비스 테스트코드에 대해 도메인로직으로 분리된 부분을 적용하라

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -13,12 +14,21 @@ import java.util.Optional;
 import kitchenpos.menu.dao.MenuDao;
 import kitchenpos.order.dao.OrderDao;
 import kitchenpos.order.dao.OrderLineItemDao;
+import kitchenpos.order.domain.OrderEntity;
+import kitchenpos.order.domain.OrderLineItemEntity;
+import kitchenpos.order.domain.OrderLineItemRepository;
+import kitchenpos.order.domain.OrderRepository;
+import kitchenpos.order.domain.request.OrderRequest;
+import kitchenpos.order.domain.response.OrderResponse;
 import kitchenpos.table.dao.OrderTableDao;
 import kitchenpos.order.domain.Order;
 import kitchenpos.order.domain.OrderLineItem;
 import kitchenpos.order.domain.OrderStatus;
 import kitchenpos.table.domain.OrderTable;
 import kitchenpos.order.application.OrderService;
+import kitchenpos.table.domain.OrderTableEntity;
+import kitchenpos.table.domain.OrderTableRepository;
+import kitchenpos.table.domain.request.OrderLineItemRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -39,6 +49,12 @@ class OrderServiceTest {
     private OrderLineItemDao orderLineItemDao;
     @Mock
     private OrderTableDao orderTableDao;
+    @Mock
+    private OrderRepository orderRepository;
+    @Mock
+    private OrderLineItemRepository orderLineItemRepository;
+    @Mock
+    private OrderTableRepository orderTableRepository;
 
     @InjectMocks
     private OrderService orderService;
@@ -50,6 +66,16 @@ class OrderServiceTest {
     private Order 주문_1;
     private Order 주문_2;
 
+    private OrderTableEntity 주문_테이블_entity;
+
+    private OrderLineItemRequest 주문_항목_request;
+    private OrderLineItemRequest 주문_항목_request2;
+    private OrderRequest 주문_request;
+
+    private OrderLineItemEntity 주문_항목;
+    private OrderLineItemEntity 주문_항목2;
+    private OrderEntity 주문;
+
     @BeforeEach
     void setUp() {
         주문항목_1 = OrderLineItem.of(1L, 1L, 1L, 1);
@@ -59,6 +85,16 @@ class OrderServiceTest {
         주문_1 = Order.of(1L, 주문_테이블.getId(), null, null,
             Arrays.asList(주문항목_1, 주문항목_2, 주문항목_3));
         주문_2 = Order.of(2L, 1L, null, null, null);
+
+        주문_테이블_entity = OrderTableEntity.of(1L, null, 3, false);
+
+        주문_항목_request = new OrderLineItemRequest(1L, 1);
+        주문_항목_request2 = new OrderLineItemRequest(2L, 1);
+        주문_request = new OrderRequest(1L, null, null, Arrays.asList(주문_항목_request, 주문_항목_request2));
+
+        주문_항목 = OrderLineItemEntity.of(null, null, 주문_항목_request.getMenuId(), 주문_항목_request.getQuantity());
+        주문_항목2 = OrderLineItemEntity.of(null, null, 주문_항목_request2.getMenuId(), 주문_항목_request2.getQuantity());
+        주문 = OrderEntity.of(1L, 주문_테이블_entity);
     }
 
     @DisplayName("주문을 등록하면 정상적으로 등록되어야 한다")
@@ -87,6 +123,28 @@ class OrderServiceTest {
             () -> assertThat(result.getOrderStatus()).isEqualTo(OrderStatus.COOKING.name()),
             () -> assertNotNull(result.getOrderedTime()),
             () -> assertThat(result.getOrderLineItems()).hasSize(3)
+        );
+    }
+
+    @DisplayName("주문을 등록하면 정상적으로 등록되어야 한다")
+    @Test
+    void create_test_copy() {
+        // given
+        when(orderLineItemRepository.findAllByIdIn(Arrays.asList(주문_항목_request.getMenuId(), 주문_항목_request2.getMenuId())))
+            .thenReturn(Arrays.asList(주문_항목, 주문_항목2));
+        when(orderTableRepository.findById(주문_request.getOrderTableId()))
+            .thenReturn(Optional.of(주문_테이블_entity));
+        when(orderRepository.save(any()))
+            .thenReturn(주문);
+
+        // when
+        OrderResponse result = orderService.createCopy(주문_request);
+
+        // then
+        assertAll(
+            () -> assertThat(result.getOrderStatus()).isEqualTo(OrderStatus.COOKING),
+            () -> assertNotNull(result.getOrderedTime()),
+            () -> assertThat(result.getOrderLineItems()).hasSize(2)
         );
     }
 
@@ -170,6 +228,24 @@ class OrderServiceTest {
         assertThat(result.getOrderStatus()).isEqualTo(OrderStatus.COOKING.name());
     }
 
+    @DisplayName("주문상태를 변경하면 정상적으로 변경되어야 한다")
+    @Test
+    void change_order_test_copy() {
+        // given
+        주문_request = new OrderRequest(null, OrderStatus.MEAL, null, null);
+        when(orderRepository.findById(주문.getId()))
+            .thenReturn(Optional.of(주문));
+        when(orderRepository.findAllOrderAndItemsByOrder(주문))
+            .thenReturn(주문);
+
+        // when
+        OrderResponse result = orderService.changeOrderStatusCopy(주문.getId(), 주문_request);
+
+        // then
+        assertThat(result.getId()).isEqualTo(주문.getId());
+        assertThat(result.getOrderStatus()).isEqualTo(주문_request.getOrderStatus());
+    }
+
     @DisplayName("주문상태를 변경할 주문이 없다면 예외가 발생한다")
     @Test
     void change_order_exception_test() {
@@ -211,5 +287,19 @@ class OrderServiceTest {
 
         // then
         assertThat(result).hasSize(2);
+    }
+
+    @DisplayName("주문목록을 조회한다")
+    @Test
+    void findAll_test_copy() {
+        // given
+        when(orderRepository.findAllOrderAndItems())
+            .thenReturn(Arrays.asList(주문));
+
+        // when
+        List<OrderResponse> result = orderService.listCopy();
+
+        // then
+        assertThat(result).hasSize(1);
     }
 }
