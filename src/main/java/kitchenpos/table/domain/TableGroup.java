ac086c4d5d6055f5431b0cc06e71edf01c30c364
june <kimjy4536@hotmail.com>
2022-06-23 11:37:05 +0900

    feat: TableGroup entity, repository추가

---
+++ src/main/java/kitchenpos/table/domain/TableGroup.java
@@ -0,0 +1,50 @@
+package kitchenpos.table.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class TableGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime createdDate;
+
+    public TableGroup() {
+    }
+
+    public TableGroup(Long id) {
+        this.id = id;
+    }
+
+    private TableGroup(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public TableGroup(Long id, LocalDateTime createdDate) {
+        this.id = id;
+        this.createdDate = createdDate;
+    }
+
+    public static TableGroup of(Long id) {
+        return new TableGroup(id);
+    }
+
+    public static TableGroup create() {
+        return new TableGroup(LocalDateTime.now());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+}
