696906d7cc0ab52611b681ee3e5d89c33c974ca8
june <kimjy4536@hotmail.com>
2022-06-23 13:37:28 +0900

    fix: native query 오류 수정

--- src/main/java/kitchenpos/table/domain/OrderTableRepository.java
+++ src/main/java/kitchenpos/table/domain/OrderTableRepository.java
@@ -11,7 +11,6 @@ public interface OrderTableRepository extends JpaRepository<OrderTable, Long> {
     @Query("SELECT ot FROM OrderTable ot WHERE ot.tableGroupId = :tableGroupId")
     List<OrderTable> findAllByTableGroupId(@Param("tableGroupId") Long tableGroupId);
 
-    @Query("SELECT ot FROM OrderTable ot WHERE ot.id IN :ids")
+    @Query("SELECT ot FROM OrderTable ot WHERE ot.id IN :orderTableIds")
     List<OrderTable> findAllByIdIn(@Param("orderTableIds") List<Long> orderTableIds);
-
 }
