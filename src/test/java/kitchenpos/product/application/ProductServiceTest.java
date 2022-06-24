1ccc675e68efca883583186b9b77a0d926941edc
KangSeongHyo <abc9023@naver.com>
2022-06-24 15:58:35 +0900

    test : (refactor) 상푼, 단체 지정 관련 Service 기능테스트 @SpringBootTest -> @DataJpaTest 로 변경

--- src/test/java/kitchenpos/product/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/product/application/ProductServiceTest.java
@@ -1,8 +1,8 @@
 package kitchenpos.product.application;
 
-import static kitchenpos.helper.ProductFixtures.제육덮밥_요청;
 import static kitchenpos.helper.ProductFixtures.제육덮밥_가격NULL_요청;
 import static kitchenpos.helper.ProductFixtures.제육덮밥_가격마이너스_요청;
+import static kitchenpos.helper.ProductFixtures.제육덮밥_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -11,23 +11,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 import kitchenpos.product.dto.ProductResponse;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @DisplayName("상품 관련 Service 기능 테스트")
-@TestMethodOrder(OrderAnnotation.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DataJpaTest
+@Import(ProductService.class)
 class ProductServiceTest {
 
     @Autowired
     private ProductService productService;
 
-    @Order(1)
     @DisplayName("상품을 등록한다.")
     @Test
     void create() {
@@ -39,7 +35,6 @@ class ProductServiceTest {
         assertThat(result.getPrice()).isEqualTo(제육덮밥_요청.getPrice());
     }
 
-    @Order(2)
     @DisplayName("상품 가격이 null 이거나 0원 미만이면 등록 할 수 없다.")
     @Test
     void create_price_null_or_less_then_zero() {
@@ -50,7 +45,6 @@ class ProductServiceTest {
         );
     }
 
-    @Order(3)
     @DisplayName("상품 목록을 조회한다.")
     @Test
     void list() {
@@ -59,6 +53,6 @@ class ProductServiceTest {
 
         //then
         assertThat(result.stream().map(ProductResponse::getName).collect(Collectors.toList()))
-                .contains(제육덮밥_요청.getName());
+                .contains("후라이드", "양념치킨", "반반치킨", "통구이", "간장치킨", "순살치킨");
     }
 }
