1aae75a127649a3ec6385aa95d85b7a37100a1f3
tasklet1579 <tasklet1579@gmail.com>
2022-06-24 14:10:09 +0900

    test : 테이블 그룹 서비스 테스트 설명

--- src/test/java/kitchenpos/application/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -6,6 +6,7 @@ import kitchenpos.dao.TableGroupDao;
 import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.TableGroup;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,12 +17,13 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.times;
 
+@DisplayName("테이블 그룹 서비스 테스트")
 @ExtendWith(MockitoExtension.class)
 class TableGroupServiceTest {
     @Mock
@@ -33,14 +35,14 @@ class TableGroupServiceTest {
     @InjectMocks
     private TableGroupService tableGroupService;
 
+    @DisplayName("2개 이상의 개별 주문 테이블을 하나의 단체 지정 테이블로 생성한다.")
     @Test
     void create() {
         // given
-        TableGroup tableGroup = new TableGroup.Builder().orderTables(Arrays.asList(
-                                                                new OrderTable.Builder(0, true).id(1L).build(),
-                                                                new OrderTable.Builder(0, true).id(2L).build(),
-                                                                new OrderTable.Builder(0, true).id(3L).build()))
-                                                        .build();
+        TableGroup tableGroup = new TableGroup.Builder().orderTables(
+                Arrays.asList(new OrderTable.Builder().id(1L).numberOfGuests(0).empty(true).build(),
+                        new OrderTable.Builder().id(2L).numberOfGuests(0).empty(true).build(),
+                        new OrderTable.Builder().id(3L).numberOfGuests(0).empty(true).build())).build();
 
         given(orderTableDao.findAllByIdIn(anyList())).willReturn(tableGroup.getOrderTables());
         given(tableGroupDao.save(any(TableGroup.class))).willReturn(new TableGroup.Builder().id(1L).build());
@@ -57,29 +59,29 @@ class TableGroupServiceTest {
         then(orderTableDao).should(times(3)).save(any(OrderTable.class));
     }
 
+    @DisplayName("1개의 개별 주문 테이블로는 단체 지정 테이블을 생성할 수 없다.")
     @Test
     void create_throwException_ifIncorrectSize() {
         // given
-        TableGroup tableGroup = new TableGroup.Builder().orderTables(Collections.singletonList(
-                                                                new OrderTable.Builder(0, true).id(1L).build()))
-                                                        .build();
+        TableGroup tableGroup = new TableGroup.Builder().orderTables(
+                Collections.singletonList(new OrderTable.Builder().id(1L).numberOfGuests(0).empty(true).build())).build();
 
         // when
         // then
-        assertThatThrownBy(() -> tableGroupService.create(tableGroup))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatIllegalArgumentException().isThrownBy(() -> tableGroupService.create(tableGroup));
 
         // verify
         then(orderTableDao).should(never()).findAllByIdIn(anyList());
     }
 
+    @DisplayName("단체 지정 테이블을 개별 주문 테이블로 변경한다.")
     @Test
     void ungroup() {
         // given
-        given(orderTableDao.findAllByTableGroupId(1L)).willReturn(Arrays.asList(
-                new OrderTable.Builder(4, false).id(1L).build(),
-                new OrderTable.Builder(2, false).id(2L).build())
-        );
+        given(orderTableDao.findAllByTableGroupId(1L))
+                .willReturn(
+                        Arrays.asList(new OrderTable.Builder().id(1L).numberOfGuests(4).empty(false).build(),
+                                new OrderTable.Builder().id(2L).numberOfGuests(0).empty(false).build()));
         given(orderDao.existsByOrderTableIdInAndOrderStatusIn(
                 Arrays.asList(1L, 2L),
                 Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))).willReturn(false);
@@ -93,21 +95,21 @@ class TableGroupServiceTest {
         then(orderTableDao).should(times(2)).save(any(OrderTable.class));
     }
 
+    @DisplayName("주문 상태가 조리 또는 식사인 경우 개별 주문 테이블로 변경할 수 없다.")
     @Test
     void ungroup_throwException_ifOrderStatusInCookingAndMeal() {
         // given
-        given(orderTableDao.findAllByTableGroupId(1L)).willReturn(Arrays.asList(
-                new OrderTable.Builder(4, false).id(1L).build(),
-                new OrderTable.Builder(2, false).id(2L).build())
-        );
+        given(orderTableDao.findAllByTableGroupId(1L))
+                .willReturn(
+                        Arrays.asList(new OrderTable.Builder(4, false).id(1L).build(),
+                                new OrderTable.Builder(0, false).id(2L).build()));
         given(orderDao.existsByOrderTableIdInAndOrderStatusIn(
                 Arrays.asList(1L, 2L),
                 Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))).willReturn(true);
 
         // when
         // then
-        assertThatThrownBy(() -> tableGroupService.ungroup(1L))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatIllegalArgumentException().isThrownBy(() -> tableGroupService.ungroup(1L));
 
         // then
 
