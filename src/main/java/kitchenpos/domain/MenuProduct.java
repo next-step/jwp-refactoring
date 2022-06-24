3bf1f495b0084d04132ae7a9a66b679c3fa8e67e
toughchb <ggang2000@gmail.com>
2022-06-24 15:48:53 +0900

    feat: MenuProduct 엔티티 매핑

--- src/main/java/kitchenpos/domain/MenuProduct.java
+++ src/main/java/kitchenpos/domain/MenuProduct.java
@@ -1,11 +1,24 @@
 package kitchenpos.domain;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import kitchenpos.domain.common.Quantity;
 
+@Entity
 public class MenuProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long seq;
+
     private Long menuId;
+
     private Long productId;
+
+    @Embedded
     private Quantity quantity;
 
     public MenuProduct() {
