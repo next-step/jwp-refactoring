97728fe8479bf2bfe7067505d11a698deed33d32
toughchb <ggang2000@gmail.com>
2022-06-24 12:17:16 +0900

    feat(MenuGroup): 엔티티 매핑

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,12 +1,23 @@
 package kitchenpos.domain;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import kitchenpos.domain.common.Name;
 
+@Entity
 public class MenuGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Embedded
     private Name name;
 
-    public MenuGroup() {
+    protected MenuGroup() {
     }
 
     public MenuGroup(String name) {
