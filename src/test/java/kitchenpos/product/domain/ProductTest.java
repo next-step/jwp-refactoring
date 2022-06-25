92f71c25b531d9a27282ea26cfbedde4ba6e138d
mond <revive29@gmail.com>
2022-06-25 13:23:27 +0900

    test(ProductTest): Product 테스트 코드 추가

---
+++ src/test/java/kitchenpos/product/domain/ProductTest.java
@@ -0,0 +1,17 @@
+package kitchenpos.product.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+
+    @Test
+    @DisplayName("제품이 동일한지 검증")
+    void verifyEqualsProduct() {
+        final Product product = new Product(1L, "상품이름", 1_000L);
+
+        assertThat(product).isEqualTo(new Product(1L, "상품이름", 1_000L));
+    }
+}
