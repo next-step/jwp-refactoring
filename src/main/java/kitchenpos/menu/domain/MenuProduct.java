efd8066c20f0e1cc9fad2c69afdd2d26087a957f
mond <revive29@gmail.com>
2022-06-25 12:41:23 +0900

    refactor(MenuProduct, MenuGroupRepository): MenuProduct Entity, Repository 추가

---
+++ src/main/java/kitchenpos/menu/domain/MenuProduct.java
@@ -0,0 +1,53 @@
+package kitchenpos.menu.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MenuProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;
+
+    @Column(nullable = false)
+    private Long menuId;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private Long quantity;
+
+    protected MenuProduct() {
+    }
+
+    public MenuProduct(Long seq, Long menuId, Long productId, Long quantity) {
+        this.seq = seq;
+        this.menuId = menuId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        MenuProduct that = (MenuProduct) o;
+        return Objects.equals(seq, that.seq) && Objects.equals(menuId, that.menuId)
+                && Objects.equals(productId, that.productId) && Objects.equals(quantity, that.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seq, menuId, productId, quantity);
+    }
+}
---
+++ src/test/java/kitchenpos/menu/domain/MenuProductTest.java
@@ -0,0 +1,17 @@
+package kitchenpos.menu.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuProductTest {
+
+    @Test
+    @DisplayName("메뉴 상품이 같은지 검증")
+    void verifyEqualsMenuProduct() {
+        final MenuProduct menuProduct = new MenuProduct(1L, 1L, 1L, 1L);
+
+        assertThat(menuProduct).isEqualTo(new MenuProduct(1L, 1L, 1L, 1L));
+    }
+}
