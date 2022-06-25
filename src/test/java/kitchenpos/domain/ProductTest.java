4fff5e022ac37e11b5f775be8a9c4d9e12a1180b
Sung-jin <osj4872@gmail.com>
2022-06-25 14:47:31 +0900

    style: 상품 엔티티 적용

--- src/test/java/kitchenpos/domain/ProductTest.java
+++ src/test/java/kitchenpos/domain/ProductTest.java
@@ -7,11 +7,6 @@ import java.math.BigDecimal;
 public class ProductTest {
 
     public static Product 상품_생성(String name, int price) {
-        Product product = new Product();
-
-        product.setName(name);
-        product.setPrice(new BigDecimal(price));
-
-        return product;
+        return new Product(name, new BigDecimal(price));
     }
 }
