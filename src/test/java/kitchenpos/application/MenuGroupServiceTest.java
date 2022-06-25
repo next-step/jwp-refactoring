3320a92ea402d02ec891012bd01cde8fc1ab56cf
toughchb <ggang2000@gmail.com>
2022-06-25 21:28:02 +0900

    style(MenuGroupServiceTest): 잘못되어 있던 파일명 수정

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -20,7 +20,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class 용 {
+class MenuGroupServiceTest {
 
     @Mock
     private MenuGroupRepository menuGroupRepository;
