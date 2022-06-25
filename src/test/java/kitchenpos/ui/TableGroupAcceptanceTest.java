d1ef173f6f04b6a044cd802f93d1d8979b116fb5
jhsong2580 <thdwmdgns@naver.com>
2022-06-25 17:02:51 +0900

    test(style)

--- src/test/java/kitchenpos/ui/TableGroupAcceptanceTest.java
+++ src/test/java/kitchenpos/ui/TableGroupAcceptanceTest.java
@@ -64,7 +64,10 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * background given : 빈테이블 2개를 생성하고 when : 테이블 2개를 단체로 설정시 then : 정상적으로 등록된다.
+     * background
+        * given : 빈테이블 2개를 생성하고
+     * when : 테이블 2개를 단체로 설정시
+     * then : 정상적으로 등록된다.
      */
     @Test
     public void 테이블그룹_생성하기_테스트() {
@@ -77,8 +80,11 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * background given : 빈테이블 2개를 생성하고 given : 테이블 2개를 단체로 설정하고 when : 단체테이블 삭제시 then : 정상적으로
-     * 삭제된다.
+     * background
+        * given : 빈테이블 2개를 생성하고
+     * given : 테이블 2개를 단체로 설정하고
+     * when : 단체테이블 삭제시
+     * then : 정상적으로 삭제된다.
      */
     @Test
     public void 테이블그룹_삭제하기_테스트() {
@@ -94,8 +100,11 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * background given : 빈테이블 2개를 생성하고 given : 사용중인 테이블 1개를 생성후 when : 테이블 3개를 단체로 설정시 then : 에러가
-     * 발생한다
+     * background
+        * given : 빈테이블 2개를 생성하고
+     * given : 사용중인 테이블 1개를 생성후
+     * when : 테이블 3개를 단체로 설정시
+     * then : 에러가 발생한다
      */
     @Test
     public void 테이블그룹_사용중인테이블있을시_에러발생() {
@@ -108,7 +117,10 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * background given : 빈테이블 1개를 생성하고 when : 테이블 1개를 단체로 설정시 then : 에러가 발생한다
+     * background
+        * given : 빈테이블 1개를 생성하고
+     * when : 테이블 1개를 단체로 설정시
+     * then : 에러가 발생한다
      */
     @Test
     public void 테이블그룹_1개이하테이블_등록시_에러발생() {
@@ -121,7 +133,9 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * given : 없는 테이블 2개 생성후 when : 없는 테이블을 단체로 설정시 then : 에러가 발생한다
+     * given : 없는 테이블 2개 생성후
+     * when : 없는 테이블을 단체로 설정시
+     * then : 에러가 발생한다
      */
     @Test
     public void 테이블그룹_없는테이블_등록시_에러발생() {
@@ -138,8 +152,10 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * given : 테이블 2개 그룹으로 생성하고 given: 한개의 테이블은 주문완료, 한개의 테이블은 주문요리중으로 설정후 when : 테이블 그룹 삭제시 then :
-     * 에러가 발생한다
+     * given : 테이블 2개 그룹으로 생성하고
+     * given: 한개의 테이블은 주문완료, 한개의 테이블은 주문요리중으로 설정후
+     * when : 테이블 그룹 삭제시
+     * then : 에러가 발생한다
      */
     @Test
     public void 테이블그룹_다먹지않은테이블_삭제시_에러발생() {
@@ -159,12 +175,5 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
         //then
         단체_테이블_에러발생(단체_테이블_삭제하기_response);
     }
-//       - 제약사항
-//       - 등록할 테이블중 사용중인 테이블이 있으면 설정 불가
-//       - 등록할 테이블이 1개 이하라면 설정 불가
-//       - 등록할 테이블이 저장되어있지 않다면 설정 불가
-//   - 단체 계산 후 테이블 그룹 삭제
-//     - 제약사항
-//       - 그룹의 테이블중 주문상태가 먹고있거나(MEAL), 요리중일때(COOKING) 그룹 테이블 삭제 불가
 
 }
