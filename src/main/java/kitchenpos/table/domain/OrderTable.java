86a416644bf511d21ef39d42eb24a62915d0ecc0
mond <revive29@gmail.com>
2022-06-25 13:27:36 +0900

    refactor(OrderTable, OrderTableRepository): OrderTable Entity, Repository 추가

---
+++ src/main/java/kitchenpos/table/domain/OrderTable.java
@@ -0,0 +1,53 @@
+package kitchenpos.table.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class OrderTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long tableGroupId;
+
+    @Column
+    private int numberOfGuests;
+
+    @Column
+    private boolean empty;
+
+    protected OrderTable(){
+    }
+
+    public OrderTable(Long id, Long tableGroupId, int numberOfGuests, boolean empty) {
+        this.id = id;
+        this.tableGroupId = tableGroupId;
+        this.numberOfGuests = numberOfGuests;
+        this.empty = empty;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OrderTable that = (OrderTable) o;
+        return numberOfGuests == that.numberOfGuests && empty == that.empty && Objects.equals(id, that.id)
+                && Objects.equals(tableGroupId, that.tableGroupId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, tableGroupId, numberOfGuests, empty);
+    }
+}
---
+++ src/test/java/kitchenpos/table/domain/OrderTableTest.java
@@ -0,0 +1,17 @@
+package kitchenpos.table.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrderTableTest {
+
+    @Test
+    @DisplayName("주문 테이블 객체가 같은지 검증")
+    void verifyEqualsOrderTable() {
+        final OrderTable orderTable = new OrderTable(1L, 1L, 5, false);
+
+        assertThat(orderTable).isEqualTo(new OrderTable(1L, 1L, 5, false));
+    }
+}
