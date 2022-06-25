627104047e9ecba653d7a0442d180022122c73ee
toughchb <ggang2000@gmail.com>
2022-06-25 21:25:32 +0900

    test: table group 단체 해제 인수테스트 추가

--- src/test/java/kitchenpos/acceptance/TableGroupAcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/TableGroupAcceptanceTest.java
@@ -2,6 +2,7 @@ package kitchenpos.acceptance;
 
 import static kitchenpos.acceptance.TableAcceptanceTest.테이블_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -9,12 +10,15 @@ import io.restassured.response.Response;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import kitchenpos.dto.IdOfOrderTableRequest;
 import kitchenpos.dto.OrderTableResponse;
 import kitchenpos.dto.TableGroupRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -26,18 +30,22 @@ public class TableGroupAcceptanceTest extends AcceptanceTest{
     OrderTableResponse 테이블1;
     OrderTableResponse 테이블2;
 
+    @TestFactory
+    Stream<DynamicTest> menuGroupTest() {
+        return Stream.of(
+                dynamicTest("테이블들을 단체 지정 할 수 있다.", () -> {
+                    테이블1 = 테이블_생성_요청(0, true).as(OrderTableResponse.class);
+                    테이블2 = 테이블_생성_요청(0, true).as(OrderTableResponse.class);
+                    ExtractableResponse<Response> 단체_지정_요청_응답 = 단체_지정_요청(Arrays.asList(테이블1, 테이블2));
+                    단체가_지정됨(단체_지정_요청_응답);
+                }),
+                dynamicTest("테이블들을 단체 해제 할 수 있다.", ()->{
+                    ExtractableResponse<Response> 단체_지정_요청_응답 = 단체_지정_요청(Arrays.asList(테이블1, 테이블2));
 
-    @BeforeEach
-    void before() {
-        super.setUp();
-        테이블1 = 테이블_생성_요청(0, true).as(OrderTableResponse.class);
-        테이블2 = 테이블_생성_요청(0, true).as(OrderTableResponse.class);
-    }
-
-    @Test
-    void createTableGroup() {
-        ExtractableResponse<Response> 단체_지정_요청_응답 = 단체_지정_요청(Arrays.asList(테이블1, 테이블2));
-        단체가_지정됨(단체_지정_요청_응답);
+                    ExtractableResponse<Response> 단체_해제_요청_응답 = 단체_해제_요청(단체_지정_요청_응답);
+                    단체가_해제됨(단체_해제_요청_응답);
+                })
+        );
     }
 
     public static ExtractableResponse<Response> 단체_지정_요청(List<OrderTableResponse> orderTables) {
@@ -54,7 +62,21 @@ public class TableGroupAcceptanceTest extends AcceptanceTest{
                 extract();
     }
 
+    public static ExtractableResponse<Response> 단체_해제_요청(ExtractableResponse<Response> response) {
+        String uri = response.header("Location");
+
+        return RestAssured
+                .given().log().all()
+                .when().delete(uri)
+                .then().log().all()
+                .extract();
+    }
+
     public static void 단체가_지정됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    public static void 단체가_해제됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 }
