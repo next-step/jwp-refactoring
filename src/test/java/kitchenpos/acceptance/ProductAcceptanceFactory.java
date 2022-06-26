32e1f0a1039453708bb89e58ca3ba096db065ce1
seungin yang <rhfpdk92@naver.com>
2022-06-27 01:17:04 +0900

    test: 상품 인수테스트 추가

---
+++ src/test/java/kitchenpos/acceptance/ProductAcceptanceFactory.java
@@ -0,0 +1,65 @@
+package kitchenpos.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.domain.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class ProductAcceptanceFactory {
+
+    public static ExtractableResponse<Response> 상품_등록_요청(String name, int price) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(BigDecimal.valueOf(price));
+
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(product)
+                .when()
+                .post("/api/products")
+                .then().log().all()
+                .extract();
+    }
+    public static ExtractableResponse<Response> 상품_조회_요청() {
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/api/products")
+                .then().log().all()
+                .extract();
+    }
+
+
+
+    public static void 상품_등록_성공(ExtractableResponse<Response> 상품등록_결과, String 상품명) {
+        Product productResponse = 상품등록_결과.as(Product.class);
+        assertAll(
+                () -> assertThat(상품등록_결과.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(productResponse.getName()).isEqualTo(상품명)
+        );
+    }
+
+    public static void 상품_등록_실패(ExtractableResponse<Response> 상품등록_결과) {
+        assertThat(상품등록_결과.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    public static void 상품_조회_성공(ExtractableResponse<Response> 상품조회_결과, List<Product> 예상조회결과) {
+        List<Product> productList = 상품조회_결과.as(List.class);
+        assertAll(
+                () -> assertThat(상품조회_결과.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(productList.size()).isEqualTo(예상조회결과.size())
+        );
+    }
+}
---
+++ src/test/java/kitchenpos/acceptance/ProductAcceptanceTest.java
@@ -0,0 +1,45 @@
+package kitchenpos.acceptance;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static kitchenpos.acceptance.ProductAcceptanceFactory.상품_등록_실패;
+import static kitchenpos.acceptance.ProductAcceptanceFactory.상품_등록_요청;
+import static kitchenpos.acceptance.ProductAcceptanceFactory.상품_조회_성공;
+import static kitchenpos.acceptance.ProductAcceptanceFactory.상품_조회_요청;
+
+@DisplayName("상품관련")
+
+public class ProductAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    void 상품_등록_성공() {
+        ExtractableResponse<Response> 상품등록_결과 = 상품_등록_요청("후라이드", 16000);
+
+        ProductAcceptanceFactory.상품_등록_성공(상품등록_결과, "후라이드");
+    }
+
+    @Test
+    void 상품_등록_실패_상품가격_음수() {
+        ExtractableResponse<Response> 상품등록_결과 = 상품_등록_요청("후라이드", -16000);
+
+        상품_등록_실패(상품등록_결과);
+    }
+
+    @Test
+    void 상품_조회() {
+        Product 후라이드 = 상품_등록_요청("후라이드", 16000).as(Product.class);
+        Product 양념치킨 = 상품_등록_요청("양념치킨", 16000).as(Product.class);
+        Product 간장치킨 = 상품_등록_요청("간장치킨", 17000).as(Product.class);
+
+        ExtractableResponse<Response> 상품조회_결과 = 상품_조회_요청();
+
+        상품_조회_성공(상품조회_결과, Arrays.asList(후라이드, 양념치킨, 간장치킨));
+    }
+
+}
