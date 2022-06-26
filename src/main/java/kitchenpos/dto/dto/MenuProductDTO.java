33e1e6d3853bbfa972180137dcd9547b560877e2
jhsong2580 <thdwmdgns@naver.com>
2022-06-26 19:37:12 +0900

    fix: DTO 기본 생성자 생성

--- src/main/java/kitchenpos/dto/dto/MenuProductDTO.java
+++ src/main/java/kitchenpos/dto/dto/MenuProductDTO.java
@@ -6,7 +6,10 @@ public class MenuProductDTO {
     private Long productId;
     private Long quantity;
 
-    public MenuProductDTO(Long productId, Long quantity) {
+    public MenuProductDTO() {
+    }
+
+    protected MenuProductDTO(Long productId, Long quantity) {
         this.productId = productId;
         this.quantity = quantity;
     }
