7c7958255d235040acf58331240323b8ef7f1c20
jhsong2580 <thdwmdgns@naver.com>
2022-06-26 17:43:35 +0900

    refactor: OrderRestController와 Domain간 의존성 삭제를 위해 Request/Response DTO 생성 및 적용

--- src/main/java/kitchenpos/application/OrderService.java
+++ src/main/java/kitchenpos/application/OrderService.java
@@ -1,21 +1,21 @@
 package kitchenpos.application;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderLineItem;
 import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
+import kitchenpos.dto.dto.OrderLineItemDTO;
+import kitchenpos.dto.request.OrderRequest;
+import kitchenpos.dto.response.OrderResponse;
 import kitchenpos.repository.MenuRepository;
 import kitchenpos.repository.OrderRepository;
 import kitchenpos.repository.OrderTableRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
@@ -32,52 +32,58 @@ public class OrderService {
     }
 
     @Transactional
-    public Order create(final Order order) {
-        final List<OrderLineItem> orderLineItems = order.getOrderLineItems();
+    public OrderResponse create(final OrderRequest orderRequest) {
+        final List<OrderLineItemDTO> orderLineItems = orderRequest.getOrderLineItems();
 
         if (CollectionUtils.isEmpty(orderLineItems)) {
             throw new IllegalArgumentException();
         }
 
         final List<Long> menuIds = orderLineItems.stream()
-                .map(OrderLineItem::getMenuId)
-                .collect(Collectors.toList());
+            .map(OrderLineItemDTO::getMenuId)
+            .collect(Collectors.toList());
 
         if (orderLineItems.size() != menuRepository.countByIdIn(menuIds)) {
             throw new IllegalArgumentException();
         }
 
-        final OrderTable orderTable = orderTableRepository.findById(order.getOrderTableId())
-                .orElseThrow(IllegalArgumentException::new);
+        final OrderTable orderTable = orderTableRepository.findById(orderRequest.getOrderTableId())
+            .orElseThrow(IllegalArgumentException::new);
 
         if (orderTable.isEmpty()) {
             throw new IllegalArgumentException();
         }
+
+        Order order = new Order();
         order.mapToTable(orderTable.getId());
         order.startCooking();
 
-        for (final OrderLineItem orderLineItem : orderLineItems) {
-            order.mapOrderLineItem(orderLineItem);
+        for (final OrderLineItemDTO orderLineItem : orderLineItems) {
+            order.mapOrderLineItem(
+                new OrderLineItem(order, orderLineItem.getMenuId(), orderLineItem.getQuantity()));
         }
 
-        return orderRepository.save(order);
+        return OrderResponse.of(orderRepository.save(order));
     }
 
-    public List<Order> list() {
-        return orderRepository.findAll();
+    public List<OrderResponse> list() {
+        return orderRepository.findAll()
+            .stream()
+            .map(OrderResponse::of)
+            .collect(Collectors.toList());
     }
 
     @Transactional
-    public Order changeOrderStatus(final Long orderId, final Order order) {
+    public OrderResponse changeOrderStatus(final Long orderId, final OrderRequest orderRequest) {
         final Order savedOrder = orderRepository.findById(orderId)
-                .orElseThrow(IllegalArgumentException::new);
+            .orElseThrow(IllegalArgumentException::new);
 
         if (Objects.equals(OrderStatus.COMPLETION, savedOrder.getOrderStatus())) {
             throw new IllegalArgumentException();
         }
 
-        savedOrder.changeOrderStatus(order.getOrderStatus());
+        savedOrder.changeOrderStatus(orderRequest.getOrderStatus());
 
-        return orderRepository.save(savedOrder);
+        return OrderResponse.of(orderRepository.save(savedOrder));
     }
 }
---
+++ src/main/java/kitchenpos/dto/request/OrderRequest.java
@@ -0,0 +1,40 @@
+package kitchenpos.dto.request;
+
+import java.util.LinkedList;
+import java.util.List;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.dto.dto.OrderLineItemDTO;
+
+public class OrderRequest {
+
+    private Long orderTableId;
+    private List<OrderLineItemDTO> orderLineItems = new LinkedList<>();
+    private OrderStatus orderStatus;
+
+    public OrderRequest() {
+    }
+
+    public Long getOrderTableId() {
+        return orderTableId;
+    }
+
+    public void setOrderTableId(Long orderTableId) {
+        this.orderTableId = orderTableId;
+    }
+
+    public List<OrderLineItemDTO> getOrderLineItems() {
+        return orderLineItems;
+    }
+
+    public void setOrderLineItems(List<OrderLineItemDTO> orderLineItems) {
+        this.orderLineItems = orderLineItems;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
