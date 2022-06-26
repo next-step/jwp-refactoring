dbc01f512fed5878745f8a0f80ef4aa76efc8985
Yul Jeon <notkhaki@gmail.com>
2022-06-26 22:02:05 +0900

    refactor(TableGroup, OrderTables): use first class collection for the list of OrderTable

---
+++ src/main/java/kitchenpos/domain/OrderTables.java
@@ -0,0 +1,51 @@
+package kitchenpos.domain;
+
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class OrderTables {
+    private static final int MINIMUM_GROUP_TABLE_SIZE = 2;
+
+    @OneToMany(mappedBy = "tableGroup")
+    private final List<OrderTable> elements = new ArrayList<>();
+
+    protected OrderTables() {
+    }
+
+    public OrderTables(final TableGroup tableGroup, final List<OrderTable> orderTables) {
+        addAll(tableGroup, orderTables);
+    }
+
+    public List<OrderTable> getElements() {
+        return elements;
+    }
+
+    public void addAll(final TableGroup tableGroup, final List<OrderTable> orderTables) {
+        validate(orderTables);
+        for (OrderTable orderTable : orderTables) {
+            elements.add(orderTable);
+            orderTable.setTableGroup(tableGroup);
+        }
+    }
+
+    private void validate(final List<OrderTable> orderTables) {
+        if (orderTables.size() < MINIMUM_GROUP_TABLE_SIZE) {
+            throw new IllegalArgumentException("테이블 수가 2개 이상이어야 단체 지정할 수 있습니다");
+        }
+
+        if (orderTables.stream().anyMatch(table -> !table.isEmpty())) {
+            throw new IllegalArgumentException("빈 테이블들만 단체 지정할 수 있습니다.");
+        }
+
+        if (orderTables.stream().anyMatch(OrderTable::hasGroup)) {
+            throw new IllegalArgumentException("이미 단체 지정된 테이블이 있어서 단체 지정할 수 없습니다.");
+        }
+    }
+
+    public void ungroup() {
+        elements.forEach(table -> table.setTableGroup(null));
+    }
+}
--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -3,21 +3,18 @@ package kitchenpos.domain;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @EntityListeners(AuditingEntityListener.class)
 @Entity
 public class TableGroup {
-    private static final int MINIMUM_GROUP_TABLE_SIZE = 2;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,39 +22,20 @@ public class TableGroup {
     @CreatedDate
     private LocalDateTime createdDate;
 
-    @OneToMany(mappedBy = "tableGroup")
-    private List<OrderTable> orderTables = new ArrayList<>();
+    @Embedded
+    private OrderTables orderTables = new OrderTables();
 
     protected TableGroup() {
     }
 
     public TableGroup(final List<OrderTable> orderTables) {
-        validateTables(orderTables);
-        orderTables.forEach(table -> addOrderTable(table));
+        this.orderTables.addAll(this, orderTables);
     }
 
+    // for test
     public TableGroup(final Long id, final List<OrderTable> orderTables) {
         this.id = id;
-        this.orderTables = orderTables;
-    }
-
-    private void validateTables(final List<OrderTable> orderTables) {
-        if (orderTables.size() < MINIMUM_GROUP_TABLE_SIZE) {
-            throw new IllegalArgumentException("테이블 수가 2개 이상이어야 단체 지정할 수 있습니다");
-        }
-
-        if (orderTables.stream().anyMatch(table -> !table.isEmpty())) {
-            throw new IllegalArgumentException("빈 테이블들만 단체 지정할 수 있습니다.");
-        }
-
-        if (orderTables.stream().anyMatch(OrderTable::hasGroup)) {
-            throw new IllegalArgumentException("이미 단체 지정된 테이블이 있어서 단체 지정할 수 없습니다.");
-        }
-    }
-
-    private void addOrderTable(final OrderTable table) {
-        this.orderTables.add(table);
-        table.setTableGroup(this);
+        this.orderTables = new OrderTables(this, orderTables);
     }
 
     public Long getId() {
@@ -69,10 +47,10 @@ public class TableGroup {
     }
 
     public List<OrderTable> getOrderTables() {
-        return orderTables;
+        return orderTables.getElements();
     }
 
     public void ungroup() {
-        orderTables.forEach(table -> table.setTableGroup(null));
+        orderTables.ungroup();
     }
 }
