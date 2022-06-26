5e7b1d827a46d541a145f956fb09d13e37814c73
jhsong2580 <thdwmdgns@naver.com>
2022-06-26 19:08:13 +0900

    feat: MenuGroupResponse name getter/setter생성

--- src/main/java/kitchenpos/dto/response/MenuGroupResponse.java
+++ src/main/java/kitchenpos/dto/response/MenuGroupResponse.java
@@ -24,4 +24,11 @@ public class MenuGroupResponse {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
