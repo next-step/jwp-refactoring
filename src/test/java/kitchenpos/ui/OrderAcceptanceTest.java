d5513b8a916a20ad19511424187146ace7de19d0
jhsong2580 <thdwmdgns@naver.com>
2022-06-26 19:40:32 +0900

    test(fix) OrderAcceptanceTest Request/Response를 통한 API처리

--- src/test/java/kitchenpos/ui/OrderAcceptanceTest.java
+++ src/test/java/kitchenpos/ui/OrderAcceptanceTest.java
@@ -25,6 +25,13 @@ import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderLineItem;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.Product;
+import kitchenpos.dto.dto.MenuProductDTO;
+import kitchenpos.dto.dto.OrderLineItemDTO;
+import kitchenpos.dto.response.MenuGroupResponse;
+import kitchenpos.dto.response.MenuResponse;
+import kitchenpos.dto.response.OrderResponse;
+import kitchenpos.dto.response.OrderTableResponse;
+import kitchenpos.dto.response.ProductResponse;
 import kitchenpos.helper.AcceptanceApiHelper.TableApiHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,11 +43,11 @@ class OrderAcceptanceTest extends AcceptanceTest {
     public final static String 먹는중 = "MEAL";
     public static final String 요리중 = "COOKING";
 
-    private OrderTable 테이블_1;
-    private OrderTable 테이블_2;
-    private OrderTable 빈테이블;
-    private Menu 양념두마리_메뉴;
-    private OrderLineItem 주문;
+    private OrderTableResponse 테이블_1;
+    private OrderTableResponse 테이블_2;
+    private OrderTableResponse 빈테이블;
+    private MenuResponse 양념두마리_메뉴;
+    private OrderLineItemDTO 주문;
     @BeforeEach
     public void init() {
         테이블_설정하기();
@@ -49,24 +56,25 @@ class OrderAcceptanceTest extends AcceptanceTest {
     }
 
     private void 주문_설정하기(){
-        주문 = new OrderLineItem();
+        주문 = new OrderLineItemDTO();
         주문.setMenuId(양념두마리_메뉴.getId());
-        주문.setQuantity(2);
+        주문.setQuantity(2L);
     }
 
     private void 메뉴_설정하기() {
-        Product 양념 = 상품_등록하기("양념", 15000).as(Product.class);
-        MenuGroup 두마리메뉴 = 메뉴그룹_등록하기("두마리메뉴").as(MenuGroup.class);
-        MenuProduct 양념_한마리 = new MenuProduct();
+        ProductResponse 양념 = 상품_등록하기("양념", 15000).as(ProductResponse.class);
+        MenuGroupResponse 두마리메뉴 = 메뉴그룹_등록하기("두마리메뉴").as(MenuGroupResponse.class);
+        MenuProductDTO 양념_한마리 = new MenuProductDTO();
         양념_한마리.setProductId(양념.getId());
-        양념_한마리.setQuantity(2);
-        양념두마리_메뉴 = 메뉴_추가하기("양념두마리", 25000, 두마리메뉴.getId(), Arrays.asList(양념_한마리)).as(Menu.class);
+        양념_한마리.setQuantity(2L);
+
+        양념두마리_메뉴 = 메뉴_추가하기("양념두마리", 25000, 두마리메뉴.getId(), Arrays.asList(양념_한마리)).as(MenuResponse.class);
     }
 
     private void 테이블_설정하기() {
-        테이블_1 = TableApiHelper.빈테이블_생성하기().as(OrderTable.class);
-        테이블_2 = TableApiHelper.빈테이블_생성하기().as(OrderTable.class);
-        빈테이블 = TableApiHelper.빈테이블_생성하기().as(OrderTable.class);
+        테이블_1 = TableApiHelper.빈테이블_생성하기().as(OrderTableResponse.class);
+        테이블_2 = TableApiHelper.빈테이블_생성하기().as(OrderTableResponse.class);
+        빈테이블 = TableApiHelper.빈테이블_생성하기().as(OrderTableResponse.class);
 
         유휴테이블_여부_설정하기(사용중, 테이블_1.getId());
         유휴테이블_여부_설정하기(사용중, 테이블_2.getId());
@@ -106,9 +114,9 @@ class OrderAcceptanceTest extends AcceptanceTest {
     @Test
     public void 주문_리스트_조회하기_테스트() {
         //given
-        Order 주문_1 = 주문_생성하기(테이블_1.getId(), Arrays.asList(주문)).as(Order.class);
-        Order 주문_2 = 주문_생성하기(테이블_1.getId(), Arrays.asList(주문)).as(Order.class);
-        Order 주문_3 = 주문_생성하기(테이블_2.getId(), Arrays.asList(주문)).as(Order.class);
+        OrderResponse 주문_1 = 주문_생성하기(테이블_1.getId(), Arrays.asList(주문)).as(OrderResponse.class);
+        OrderResponse 주문_2 = 주문_생성하기(테이블_1.getId(), Arrays.asList(주문)).as(OrderResponse.class);
+        OrderResponse 주문_3 = 주문_생성하기(테이블_2.getId(), Arrays.asList(주문)).as(OrderResponse.class);
 
         //when
         ExtractableResponse<Response> 오더_리스트_조회하기_response = 오더_리스트_조회하기();
@@ -129,7 +137,7 @@ class OrderAcceptanceTest extends AcceptanceTest {
     @Test
     public void 주문_상태_변경하기_MEAL() {
         //given
-        Order 주문_1 = 주문_생성하기(테이블_1.getId(), Arrays.asList(주문)).as(Order.class);
+        OrderResponse 주문_1 = 주문_생성하기(테이블_1.getId(), Arrays.asList(주문)).as(OrderResponse.class);
 
         //when
         ExtractableResponse<Response> 주문_상태_변경하기_response = 주문_상태_변경하기(먹는중, 주문_1.getId());
@@ -150,7 +158,7 @@ class OrderAcceptanceTest extends AcceptanceTest {
     @Test
     public void 주문_상태_변경하기_COMPLETION() {
         //given
-        Order 주문_1 = 주문_생성하기(테이블_1.getId(), Arrays.asList(주문)).as(Order.class);
+        OrderResponse 주문_1 = 주문_생성하기(테이블_1.getId(), Arrays.asList(주문)).as(OrderResponse.class);
 
         //when
         ExtractableResponse<Response> 주문_상태_변경하기_response = 주문_상태_변경하기("COMPLETION", 주문_1.getId());
@@ -166,9 +174,9 @@ class OrderAcceptanceTest extends AcceptanceTest {
     @Test
     public void 없는_메뉴로_주문할때_에러발생() {
         //given
-        OrderLineItem 주문 = new OrderLineItem();
+        OrderLineItemDTO 주문 = new OrderLineItemDTO();
         주문.setMenuId(-1L);
-        주문.setQuantity(2);
+        주문.setQuantity(2L);
 
         //when
         ExtractableResponse<Response> 오더_생성하기_response = 주문_생성하기(테이블_1.getId(),
