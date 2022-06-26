4e541cfd3ef5061c953638d37a63dc5ce10d0f2d
toughchb <ggang2000@gmail.com>
2022-06-26 15:37:32 +0900

    test(OderTest): 주문 항목 도메인 테스트 추가

---
+++ src/test/java/kitchenpos/domain/OrderLineItemTest.java
@@ -0,0 +1,27 @@
+package kitchenpos.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrderLineItemTest {
+
+    @Test
+    @DisplayName("주문 항목에 주문 등록 테스트")
+    void registerOrderTest() {
+
+        //given
+        OrderTable orderTable = new OrderTable(10, false);
+        Order order = new Order(orderTable.getId());
+
+        OrderLineItem orderLineItem = new OrderLineItem(1L, 10);
+
+        //when
+        orderLineItem.registerOrder(order);
+
+        //then
+        assertThat(orderLineItem.getOrder()).isNotNull();
+    }
+}
