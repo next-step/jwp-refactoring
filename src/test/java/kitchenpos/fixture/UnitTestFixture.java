9e208a86d52c8c0c8e2662f3007132a689b577be
Ttungga <torb2002@gmail.com>
2022-06-27 01:01:07 +0900

    test: add tests for MenuGroup

---
+++ src/test/java/kitchenpos/fixture/UnitTestFixture.java
@@ -0,0 +1,8 @@
+package kitchenpos.fixture;
+
+import kitchenpos.domain.MenuGroup;
+
+public class UnitTestFixture {
+    public final MenuGroup 구이류 = new MenuGroup(1L, "구이류");
+    public final MenuGroup 식사류 = new MenuGroup(2L, "식사류");
+}
