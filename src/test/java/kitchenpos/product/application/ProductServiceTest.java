6d0713f8580fa20bee42244e843e14615104e582
Donghee Han <handh0413@gmail.com>
2022-06-26 21:27:37 +0900

    test: ProductService 테스트 추가

--- src/test/java/kitchenpos/product/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/product/application/ProductServiceTest.java
@@ -11,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -20,8 +22,11 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
-    public static final String PRODUCT_NAME = "바베큐치킨";
-    public static final BigDecimal PRODUCT_PRICE = new BigDecimal(30000);
+    public static final String PRODUCT_NAME01 = "바베큐치킨";
+    public static final BigDecimal PRODUCT_PRICE01 = new BigDecimal(30000);
+
+    public static final String PRODUCT_NAME02 = "치즈볼";
+    public static final BigDecimal PRODUCT_PRICE02 = new BigDecimal(5000);
 
     private ProductService productService;
 
@@ -36,10 +41,10 @@ public class ProductServiceTest {
     @DisplayName("상품을 생성한다.")
     @Test
     void create() {
-        when(productDao.save(any())).thenReturn(new Product(1L, PRODUCT_NAME, PRODUCT_PRICE));
+        when(productDao.save(any())).thenReturn(createProduct01());
 
         // when
-        Product created = productService.create(new Product(PRODUCT_NAME, PRODUCT_PRICE));
+        Product created = productService.create(new Product(PRODUCT_NAME01, PRODUCT_PRICE01));
 
         // then
         assertThat(created).isNotNull();
@@ -50,7 +55,7 @@ public class ProductServiceTest {
     @Test
     void create_price_null() {
         assertThatThrownBy(() -> {
-            productService.create(new Product(PRODUCT_NAME, null));
+            productService.create(new Product(PRODUCT_NAME01, null));
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -58,7 +63,31 @@ public class ProductServiceTest {
     @Test
     void create_price_under_zero() {
         assertThatThrownBy(() -> {
-            productService.create(new Product(PRODUCT_NAME, new BigDecimal(-100)));
+            productService.create(new Product(PRODUCT_NAME01, new BigDecimal(-100)));
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 목록을 조회한다.")
+    @Test
+    void list() {
+        when(productService.list()).thenReturn(createProductList());
+
+        // when
+        List<Product> list = productService.list();
+
+        // then
+        assertThat(list).isNotNull();
+    }
+    
+    public static Product createProduct01() {
+        return new Product(1L, PRODUCT_NAME01, PRODUCT_PRICE01);
+    }
+
+    public static Product createProduct02() {
+        return new Product(1L, PRODUCT_NAME02, PRODUCT_PRICE02);
+    }
+
+    public static List<Product> createProductList() {
+        return Arrays.asList(createProduct01(), createProduct02());
     }
 }
