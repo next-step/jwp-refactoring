e302fec08ee4120c4847fd9d42336970dbdd8350
jhsong2580 <thdwmdgns@naver.com>
2022-06-26 17:35:07 +0900

    refactor: price 원시값 객체화

---
+++ src/main/java/kitchenpos/domain/Price.java
@@ -0,0 +1,29 @@
+package kitchenpos.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Price {
+    private BigDecimal price;
+
+    protected Price() {
+    }
+
+    public Price(BigDecimal price) {
+        validatePrice(price);
+        this.price = price;
+    }
+
+    private void validatePrice(BigDecimal source){
+        if(Objects.isNull(source) || source.compareTo(BigDecimal.ZERO) < 0){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean bigger(BigDecimal target){
+        return price.compareTo(target) > 0 ;
+    }
+
+}
