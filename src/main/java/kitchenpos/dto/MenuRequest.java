ff992ddba1b3b12b3d115f1bdedabb3fc7338a77
tasklet1579 <tasklet1579@gmail.com>
2022-06-27 23:51:33 +0900

    feat : 메뉴, 메뉴 상품 DTO

---
+++ src/main/java/kitchenpos/dto/MenuRequest.java
@@ -0,0 +1,51 @@
+package kitchenpos.dto;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuRequest {
+    private String name;
+    private BigDecimal price;
+    private Long menuGroupId;
+    private List<MenuProductRequest> menuProducts = new ArrayList<>();
+
+    public MenuRequest(String name, BigDecimal price, Long menuGroupId, List<MenuProductRequest> menuProducts) {
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+        this.menuProducts = menuProducts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getMenuGroupId() {
+        return menuGroupId;
+    }
+
+    public void setMenuGroupId(Long menuGroupId) {
+        this.menuGroupId = menuGroupId;
+    }
+
+    public List<MenuProductRequest> getMenuProducts() {
+        return menuProducts;
+    }
+
+    public void setMenuProducts(List<MenuProductRequest> menuProducts) {
+        this.menuProducts = menuProducts;
+    }
+}
