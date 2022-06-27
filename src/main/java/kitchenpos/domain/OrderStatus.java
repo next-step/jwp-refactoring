4f3a042df3de329d4abc5e05476f5da0698769c6
toughchb <ggang2000@gmail.com>
2022-06-28 08:55:41 +0900

    refactor: 불필요한 로직 삭제

--- src/main/java/kitchenpos/domain/OrderStatus.java
+++ src/main/java/kitchenpos/domain/OrderStatus.java
@@ -1,9 +1,5 @@
 package kitchenpos.domain;
 
 public enum OrderStatus {
-    COOKING, MEAL, COMPLETION;
-
-    public boolean isNotCompletion() {
-        return !this.equals(COMPLETION);
-    }
+    COOKING, MEAL, COMPLETION
 }
