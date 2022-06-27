f72829ce679b7c3559f9cabc0b0cce4f930dfdd1
jhsong2580 <thdwmdgns@naver.com>
2022-06-27 17:40:51 +0900

    test(fix): 잘못된 값으로 변환하는 부분 수정(OrderTableRequest -> OrderTableResponse)

--- src/test/java/kitchenpos/ui/TableGroupAcceptanceTest.java
+++ src/test/java/kitchenpos/ui/TableGroupAcceptanceTest.java
@@ -7,7 +7,6 @@ import static kitchenpos.helper.AcceptanceApiHelper.OrderApiHelper.주문_생성
 import static kitchenpos.helper.AcceptanceApiHelper.ProductApiHelper.상품_등록하기;
 import static kitchenpos.helper.AcceptanceApiHelper.TableApiHelper.빈테이블_생성하기;
 import static kitchenpos.helper.AcceptanceApiHelper.TableApiHelper.유휴테이블_여부_설정하기;
-import static kitchenpos.helper.AcceptanceApiHelper.TableApiHelper.테이블_손님_인원_설정하기;
 import static kitchenpos.helper.AcceptanceApiHelper.TableGroupApiHelper.단체_테이블_등록하기;
 import static kitchenpos.helper.AcceptanceApiHelper.TableGroupApiHelper.단체_테이블_삭제하기;
 import static kitchenpos.helper.AcceptanceAssertionHelper.TableGroupAssertionHelper.단체_테이블_등록되어있음;
@@ -19,21 +18,12 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.Arrays;
 import kitchenpos.AcceptanceTest;
-import kitchenpos.domain.Menu;
-import kitchenpos.domain.MenuGroup;
-import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Order;
-import kitchenpos.domain.OrderLineItem;
-import kitchenpos.domain.OrderTable;
-import kitchenpos.domain.Product;
-import kitchenpos.domain.TableGroup;
 import kitchenpos.dto.dto.MenuProductDTO;
 import kitchenpos.dto.dto.OrderLineItemDTO;
 import kitchenpos.dto.request.OrderTableRequest;
-import kitchenpos.dto.request.TableGroupRequest;
 import kitchenpos.dto.response.MenuGroupResponse;
 import kitchenpos.dto.response.MenuResponse;
-import kitchenpos.dto.response.OrderResponse;
 import kitchenpos.dto.response.OrderTableResponse;
 import kitchenpos.dto.response.ProductResponse;
 import kitchenpos.dto.response.TableGroupResponse;
@@ -42,14 +32,13 @@ import kitchenpos.helper.AcceptanceApiHelper.TableGroupApiHelper;
 import kitchenpos.helper.AcceptanceAssertionHelper.TableGroupAssertionHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.jdbc.Sql;
 
 
 class TableGroupAcceptanceTest extends AcceptanceTest {
 
-    private OrderTableRequest 빈테이블_1;
-    private OrderTableRequest 빈테이블_2;
-    private OrderTableRequest 사용중인테이블;
+    private OrderTableResponse 빈테이블_1;
+    private OrderTableResponse 빈테이블_2;
+    private OrderTableResponse 사용중인테이블;
     private MenuResponse 양념두마리_메뉴;
     private OrderLineItemDTO 주문;
 
@@ -60,7 +49,7 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
         주문_설정하기();
     }
 
-    private void 주문_설정하기(){
+    private void 주문_설정하기() {
         주문 = new OrderLineItemDTO();
         주문.setMenuId(양념두마리_메뉴.getId());
         주문.setQuantity(2L);
@@ -73,13 +62,14 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
         양념_한마리.setProductId(양념.getId());
         양념_한마리.setQuantity(2L);
 
-        양념두마리_메뉴 = 메뉴_추가하기("양념두마리", 25000, 두마리메뉴.getId(), Arrays.asList(양념_한마리)).as(MenuResponse.class);
+        양념두마리_메뉴 = 메뉴_추가하기("양념두마리", 25000, 두마리메뉴.getId(), Arrays.asList(양념_한마리))
+            .as(MenuResponse.class);
     }
 
     private void 테이블_설정하기() {
-        빈테이블_1 = 빈테이블_생성하기().as(OrderTableRequest.class);
-        빈테이블_2 = 빈테이블_생성하기().as(OrderTableRequest.class);
-        사용중인테이블 = 빈테이블_생성하기().as(OrderTableRequest.class);
+        빈테이블_1 = 빈테이블_생성하기().as(OrderTableResponse.class);
+        빈테이블_2 = 빈테이블_생성하기().as(OrderTableResponse.class);
+        사용중인테이블 = 빈테이블_생성하기().as(OrderTableResponse.class);
 
         유휴테이블_여부_설정하기(사용중, 사용중인테이블.getId());
         TableApiHelper.테이블_손님_인원_설정하기(2, 사용중인테이블.getId());
@@ -93,9 +83,13 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
      */
     @Test
     public void 테이블그룹_생성하기_테스트() {
+        //given
+        OrderTableRequest 빈테이블_1_request = getOrderTableRequestFromResponse(빈테이블_1);
+        OrderTableRequest 빈테이블_2_request = getOrderTableRequestFromResponse(빈테이블_2);
+
         //when
         ExtractableResponse<Response> 단체_테이블_등록하기_response = 단체_테이블_등록하기(
-            Arrays.asList(빈테이블_1, 빈테이블_2));
+            Arrays.asList(빈테이블_1_request, 빈테이블_2_request));
 
         //then
         단체_테이블_등록되어있음(단체_테이블_등록하기_response);
@@ -111,7 +105,11 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
     @Test
     public void 테이블그룹_삭제하기_테스트() {
         //given
-        TableGroupResponse 단체테이블 = 단체_테이블_등록하기(Arrays.asList(빈테이블_1, 빈테이블_2)).as(TableGroupResponse.class);
+        OrderTableRequest 빈테이블_1_request = getOrderTableRequestFromResponse(빈테이블_1);
+        OrderTableRequest 빈테이블_2_request = getOrderTableRequestFromResponse(빈테이블_2);
+
+        TableGroupResponse 단체테이블 = 단체_테이블_등록하기(Arrays.asList(빈테이블_1_request, 빈테이블_2_request)).as(
+            TableGroupResponse.class);
 
         //when
         ExtractableResponse<Response> 단체_테이블_삭제하기_response = TableGroupApiHelper.단체_테이블_삭제하기(
@@ -130,9 +128,14 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
      */
     @Test
     public void 테이블그룹_사용중인테이블있을시_에러발생() {
+        //given
+        OrderTableRequest 빈테이블_1_request = getOrderTableRequestFromResponse(빈테이블_1);
+        OrderTableRequest 빈테이블_2_request = getOrderTableRequestFromResponse(빈테이블_2);
+        OrderTableRequest 사용중인테이블_request = getOrderTableRequestFromResponse(사용중인테이블);
+
         //when
         ExtractableResponse<Response> 단체_테이블_등록하기_response = 단체_테이블_등록하기(
-            Arrays.asList(빈테이블_1, 빈테이블_2, 사용중인테이블));
+            Arrays.asList(빈테이블_1_request, 빈테이블_2_request, 사용중인테이블_request));
 
         //then
         단체_테이블_에러발생(단체_테이블_등록하기_response);
@@ -146,9 +149,12 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
      */
     @Test
     public void 테이블그룹_1개이하테이블_등록시_에러발생() {
+        //given
+        OrderTableRequest 빈테이블_1_request = getOrderTableRequestFromResponse(빈테이블_1);
+
         //when
         ExtractableResponse<Response> 단체_테이블_등록하기_response = 단체_테이블_등록하기(
-            Arrays.asList(빈테이블_1));
+            Arrays.asList(빈테이블_1_request));
 
         //then
         단체_테이블_에러발생(단체_테이블_등록하기_response);
@@ -181,8 +187,13 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
      */
     @Test
     public void 테이블그룹_다먹지않은테이블_삭제시_에러발생() {
+
         //given
-        TableGroupResponse 단체테이블 = 단체_테이블_등록하기(Arrays.asList(빈테이블_1, 빈테이블_2)).as(TableGroupResponse.class);
+        OrderTableRequest 빈테이블_1_request = getOrderTableRequestFromResponse(빈테이블_1);
+        OrderTableRequest 빈테이블_2_request = getOrderTableRequestFromResponse(빈테이블_2);
+
+        TableGroupResponse 단체테이블 = 단체_테이블_등록하기(Arrays.asList(빈테이블_1_request, 빈테이블_2_request)).as(
+            TableGroupResponse.class);
         주문_생성하기(빈테이블_1.getId(), Arrays.asList(주문)).as(Order.class);
         주문_생성하기(빈테이블_2.getId(), Arrays.asList(주문)).as(Order.class);
         주문_상태_변경하기(먹는중, 빈테이블_1.getId());
@@ -194,4 +205,12 @@ class TableGroupAcceptanceTest extends AcceptanceTest {
         단체_테이블_에러발생(단체_테이블_삭제하기_response);
     }
 
+    private OrderTableRequest getOrderTableRequestFromResponse(
+        OrderTableResponse orderTableResponse) {
+        OrderTableRequest orderTableRequest = new OrderTableRequest();
+        orderTableRequest.setNumberOfGuests(orderTableResponse.getNumberOfGuests());
+        orderTableRequest.setEmpty(orderTableResponse.isEmpty());
+        orderTableRequest.setId(orderTableResponse.getId());
+        return orderTableRequest;
+    }
 }
