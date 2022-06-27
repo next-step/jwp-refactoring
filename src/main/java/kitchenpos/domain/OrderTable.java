6c46046bb04b975af8912f551a1b329bb564f0ba
toughchb <ggang2000@gmail.com>
2022-06-27 14:57:43 +0900

    refactor: 리뷰반영 empty 원시 객체화

--- src/main/java/kitchenpos/domain/OrderTable.java
+++ src/main/java/kitchenpos/domain/OrderTable.java
@@ -1,5 +1,6 @@
 package kitchenpos.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
@@ -9,7 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import kitchenpos.domain.common.Empty;
 import kitchenpos.domain.common.NumberOfGuests;
 
 @Entity
@@ -27,38 +27,38 @@ public class OrderTable {
     @Embedded
     private NumberOfGuests numberOfGuests;
 
-    @Embedded
-    private Empty empty;
+    @Column(nullable = false)
+    private boolean empty;
 
     protected OrderTable() {
     }
 
     public OrderTable(Long id, boolean empty) {
         this.id = id;
-        this.empty = new Empty(empty);
+        this.empty = empty;
     }
 
     public OrderTable(Long id, TableGroup tableGroup, boolean empty) {
         this.id = id;
         this.tableGroup = tableGroup;
-        this.empty = new Empty(empty);
+        this.empty = empty;
     }
 
     public OrderTable(TableGroup tableGroup, int numberOfGuests, boolean empty) {
         this.tableGroup = tableGroup;
         this.numberOfGuests = new NumberOfGuests(numberOfGuests);
-        this.empty = new Empty(empty);
+        this.empty = empty;
     }
 
     public OrderTable(Long id, int numberOfGuests, boolean empty) {
         this.id = id;
         this.numberOfGuests = new NumberOfGuests(numberOfGuests);
-        this.empty = new Empty(empty);
+        this.empty = empty;
     }
 
     public OrderTable(int numberOfGuests, boolean empty) {
         this.numberOfGuests = new NumberOfGuests(numberOfGuests);
-        this.empty = new Empty(empty);
+        this.empty = empty;
     }
 
     public Long getId() {
@@ -85,14 +85,14 @@ public class OrderTable {
     }
 
     public boolean isEmpty() {
-        return empty.isTrue();
+        return empty;
     }
 
     public void switchEmpty(boolean empty) {
         if (isInTableGroup()) {
             throw new IllegalArgumentException("빈테이블로 지정 할 수 없습니다. 단체 지정이 되어 있는 주문 테이블 입니다.");
         }
-        this.empty = new Empty(empty);
+        this.empty = empty;
     }
 
     public boolean isInTableGroup() {
