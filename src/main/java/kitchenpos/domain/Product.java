60087bdfdcd226f5240ce495463664fa3e01dd51
kbh0581 <kbh0581@gmail.com>
2022-06-27 13:32:40 +0900

    test : 상품 도메인 테스트 코드 작성
    - 인수 정의
       - 상품관리
         * given 등록할 상품을 생성한다.
         * when 상품을 등록한다.
         * then 상품이 등록됨
         * when 상품목록을 조회한다.
         * then 등록한 상품이 조회 된다.

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -7,6 +7,21 @@ public class Product {
     private String name;
     private BigDecimal price;
 
+    public Product() {
+
+    }
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
     public Long getId() {
         return id;
     }
@@ -30,4 +45,5 @@ public class Product {
     public void setPrice(final BigDecimal price) {
         this.price = price;
     }
+
 }
