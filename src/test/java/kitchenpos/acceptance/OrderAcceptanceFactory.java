7b8f80df7a7e662a9d21e1a9094507055a57b094
seungin yang <rhfpdk92@naver.com>
2022-06-28 01:09:42 +0900

    test: 주문 인수테스트 추가

---
+++ src/test/java/kitchenpos/acceptance/OrderAcceptanceFactory.java
@@ -0,0 +1,82 @@
+package kitchenpos.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class OrderAcceptanceFactory {
+
+    public static ExtractableResponse<Response> 주문_등록_요청(Long orderTableId, List<OrderLineItem> orderLineItems) {
+        Order order = new Order();
+        order.setOrderTableId(orderTableId);
+        order.setOrderStatus("");
+        order.setOrderLineItems(orderLineItems);
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(order)
+                .when()
+                .post("/api/orders")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 주문_조회_요청() {
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/api/orders")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 주문_상태_변경_요청(Order 주문, OrderStatus 주문상태) {
+        주문.setOrderStatus(주문상태.name());
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(주문)
+                .when()
+                .put("/api/orders/{orderId}/order-status",주문.getId())
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 주문_등록성공(ExtractableResponse<Response> 주문_등록_결과) {
+        assertThat(주문_등록_결과.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    public static void 주문_등록실패(ExtractableResponse<Response> 주문_등록_결과) {
+        assertThat(주문_등록_결과.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    public static void 주문_조회성공(ExtractableResponse<Response> 주문_조회_결과) {
+        assertThat(주문_조회_결과.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static void 주문_변경성공(ExtractableResponse<Response> 주문_변경_결과, OrderStatus 기대주문상태) {
+        Order 변경된주문 = 주문_변경_결과.as(Order.class);
+        assertAll(
+                () -> assertThat(주문_변경_결과.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(변경된주문.getOrderStatus()).isEqualTo(기대주문상태.name())
+        );
+    }
+    public static void 주문_변경실패(ExtractableResponse<Response> 주문_변경_결과) {
+        assertThat(주문_변경_결과.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+}
---
+++ src/test/java/kitchenpos/acceptance/OrderAcceptanceTest.java
@@ -0,0 +1,154 @@
+package kitchenpos.acceptance;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static kitchenpos.acceptance.MenuAcceptanceFactory.메뉴_등록_요청;
+import static kitchenpos.acceptance.MenuGroupAcceptanceFactory.메뉴그룹_등록_요청;
+import static kitchenpos.acceptance.OrderAcceptanceFactory.주문_등록_요청;
+import static kitchenpos.acceptance.OrderAcceptanceFactory.주문_등록성공;
+import static kitchenpos.acceptance.OrderAcceptanceFactory.주문_등록실패;
+import static kitchenpos.acceptance.OrderAcceptanceFactory.주문_변경성공;
+import static kitchenpos.acceptance.OrderAcceptanceFactory.주문_변경실패;
+import static kitchenpos.acceptance.OrderAcceptanceFactory.주문_상태_변경_요청;
+import static kitchenpos.acceptance.OrderAcceptanceFactory.주문_조회_요청;
+import static kitchenpos.acceptance.OrderAcceptanceFactory.주문_조회성공;
+import static kitchenpos.acceptance.OrderTableAcceptanceFactory.주문테이블_등록_요청;
+import static kitchenpos.acceptance.ProductAcceptanceFactory.상품_등록_요청;
+
+@DisplayName("주문 관련")
+public class OrderAcceptanceTest extends AcceptanceTest {
+    private OrderTable 주문테이블1;
+    private OrderTable 주문테이블2;
+
+    private Menu 후라이드메뉴;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        //테이블
+        주문테이블1 = 주문테이블_등록_요청(false, 5).as(OrderTable.class);
+        주문테이블2 = 주문테이블_등록_요청(false, 5).as(OrderTable.class);
+
+        //메뉴
+        Product 후라이드 = 상품_등록_요청("후라이드", 16000).as(Product.class);
+        MenuGroup 두마리메뉴 = 메뉴그룹_등록_요청("두마리메뉴").as(MenuGroup.class);
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProductId(후라이드.getId());
+        menuProduct.setQuantity(1);
+        후라이드메뉴 = 메뉴_등록_요청(후라이드.getName(), 후라이드.getPrice(), 두마리메뉴.getId(), Arrays.asList(menuProduct)).as(Menu.class);
+    }
+
+    @Test
+    void 주문_등록_성공() {
+        OrderLineItem 주문항목 = new OrderLineItem();
+        주문항목.setMenuId(후라이드메뉴.getId());
+        주문항목.setQuantity(1);
+
+        ExtractableResponse<Response> 주문_등록_결과 = 주문_등록_요청(주문테이블1.getId(), Arrays.asList(주문항목));
+
+        주문_등록성공(주문_등록_결과);
+    }
+
+    @Test
+    void 주문_등록_실패_주문항목이_비어있음() {
+        ExtractableResponse<Response> 주문_등록_결과 = 주문_등록_요청(주문테이블1.getId(), Arrays.asList());
+
+        주문_등록실패(주문_등록_결과);
+    }
+
+    @Test
+    void 주문_등록_실패_등록되지않은_메뉴() {
+        long 등록되지않은_메뉴아이디 = 999L;
+        OrderLineItem 주문항목 = new OrderLineItem();
+        주문항목.setMenuId(등록되지않은_메뉴아이디);
+        주문항목.setQuantity(1);
+
+        ExtractableResponse<Response> 주문_등록_결과 = 주문_등록_요청(주문테이블1.getId(), Arrays.asList(주문항목));
+
+        주문_등록실패(주문_등록_결과);
+    }
+
+    @Test
+    void 주문_등록_실패_존재하지않는_주문테이블() {
+        Long 존재하지않는_주문테이블 = 999L;
+        OrderLineItem 주문항목 = new OrderLineItem();
+        주문항목.setMenuId(후라이드메뉴.getId());
+        주문항목.setQuantity(1);
+
+        ExtractableResponse<Response> 주문_등록_결과 = 주문_등록_요청(존재하지않는_주문테이블, Arrays.asList(주문항목));
+
+        주문_등록실패(주문_등록_결과);
+    }
+
+    @Test
+    void 주문_등록_실패_빈테이블() {
+        OrderTable 빈테이블 = 주문테이블_등록_요청(true, 5).as(OrderTable.class);
+        OrderLineItem 주문항목 = new OrderLineItem();
+        주문항목.setMenuId(후라이드메뉴.getId());
+        주문항목.setQuantity(1);
+
+        ExtractableResponse<Response> 주문_등록_결과 = 주문_등록_요청(빈테이블.getId(), Arrays.asList(주문항목));
+
+        주문_등록실패(주문_등록_결과);
+    }
+
+
+    @Test
+    void 주문_조회_성공() {
+        ExtractableResponse<Response> 주문_조회_결과 = 주문_조회_요청();
+
+        주문_조회성공(주문_조회_결과);
+    }
+
+    @Test
+    void 주문_상태_변경_성공() {
+        OrderStatus 주문상태_요리중 = OrderStatus.COOKING;
+        OrderLineItem 주문항목 = new OrderLineItem();
+        주문항목.setMenuId(후라이드메뉴.getId());
+        주문항목.setQuantity(1);
+        Order 주문 = 주문_등록_요청(주문테이블1.getId(), Arrays.asList(주문항목)).as(Order.class);
+
+        ExtractableResponse<Response> 주문_변경_결과 = 주문_상태_변경_요청(주문, 주문상태_요리중);
+
+        주문_변경성공(주문_변경_결과, 주문상태_요리중);
+    }
+
+    @Test
+    void 주문_상태_변경_실패_주문이_존재하지않음() {
+        Order 존재하지않는_주문 = new Order();
+        존재하지않는_주문.setId(999L);
+        OrderStatus 주문상태_요리중 = OrderStatus.COOKING;
+
+        ExtractableResponse<Response> 주문_변경_결과 = 주문_상태_변경_요청(존재하지않는_주문, 주문상태_요리중);
+
+        주문_변경실패(주문_변경_결과);
+    }
+
+    @Test
+    void 주문_상태_변경_실패_이미완료된_주문() {
+        OrderStatus 주문상태_완료 = OrderStatus.COMPLETION;
+        OrderLineItem 주문항목 = new OrderLineItem();
+        주문항목.setMenuId(후라이드메뉴.getId());
+        주문항목.setQuantity(1);
+        Order 주문 = 주문_등록_요청(주문테이블1.getId(), Arrays.asList(주문항목)).as(Order.class);
+        ExtractableResponse<Response> 완료상태로_변경 = 주문_상태_변경_요청(주문, 주문상태_완료);
+
+        ExtractableResponse<Response> 주문_변경_결과 = 주문_상태_변경_요청(주문, 주문상태_완료);
+
+        주문_변경실패(주문_변경_결과);
+    }
+}
