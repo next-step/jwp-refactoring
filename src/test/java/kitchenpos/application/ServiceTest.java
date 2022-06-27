d0978a48cb8d6d5944dcbaf6181ba5bdbf3bd85a
toughchb <ggang2000@gmail.com>
2022-06-27 14:41:12 +0900

    refactor(ServiceTest): 리뷰 반영 ServiceTest abstract class 화

--- src/test/java/kitchenpos/application/ServiceTest.java
+++ src/test/java/kitchenpos/application/ServiceTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ServiceTest {
+public abstract class ServiceTest {
 
     @Autowired
     private DatabaseCleanup databaseCleanup;
