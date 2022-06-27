74cc951da0c68363da4f2cc90220ecbc3427b341
limwoobin <drogba02@naver.com>
2022-06-27 22:02:52 +0900

    refactor: 메뉴 도메인의 테스트코드를 수정하라

--- src/test/java/kitchenpos/domain/MenuProductsTest.java
+++ src/test/java/kitchenpos/domain/MenuProductsTest.java
@@ -1,56 +1,40 @@
 package kitchenpos.domain;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
-import kitchenpos.core.exception.CannotCreateException;
-import kitchenpos.core.exception.ExceptionType;
+import kitchenpos.menu.domain.Menu;
 import kitchenpos.menu.domain.MenuProduct;
 import kitchenpos.menu.domain.MenuProducts;
-import kitchenpos.core.domain.Price;
-import kitchenpos.product.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("메뉴 상품 목록에 대한 단위 테스트")
 class MenuProductsTest {
-    private Product 상품;
-    private Product 상품2;
+
+    private Menu menu;
 
     @BeforeEach
     void setUp() {
-        상품 = Product.of("test", BigDecimal.valueOf(500L));
-        상품2 = Product.of("test", BigDecimal.valueOf(300L));
+        menu = mock(Menu.class);
     }
 
-    @DisplayName("메뉴 상품중 상품값을 안가지고 있는 값이 있으면 예외가 발생한다")
+    @DisplayName("메뉴 상품목록에 메뉴를 매핑하면 정상적으로 매핑된다")
     @Test
-    void exception_test() {
-        // given
-        MenuProduct menuProduct = MenuProduct.of(1L, 3);
-        MenuProduct menuProduct2 = MenuProduct.of(null, 3);
-
-        // then
-        assertThatThrownBy(() -> {
-            new MenuProducts(Arrays.asList(menuProduct, menuProduct2), null, null);
-        }).isInstanceOf(CannotCreateException.class)
-            .hasMessageContaining(ExceptionType.CONTAINS_NOT_EXIST_PRODUCT.getMessage());
-    }
-
-    @DisplayName("메뉴 상품들의 가격의 합보다 메뉴 가격이 높으면 예외가 발생한다")
-    @Test
-    void exception_test2() {
+    void create_test() {
         // given
         MenuProduct menuProduct = MenuProduct.of(1L, 3);
         MenuProduct menuProduct2 = MenuProduct.of(2L, 3);
+        when(menu.getId()).thenReturn(1L);
+
+        // given
+        MenuProducts menuProducts = new MenuProducts(Arrays.asList(menuProduct, menuProduct2), menu);
 
         // then
-        Price menuPrice = new Price(BigDecimal.valueOf(2410L));
-        assertThatThrownBy(() -> {
-            new MenuProducts(Arrays.asList(menuProduct, menuProduct2), menuPrice, null);
-        }).isInstanceOf(CannotCreateException.class)
-            .hasMessageContaining(ExceptionType.IS_NOT_OVER_THAN_MENU_PRICE.getMessage());
+        assertNotNull(menuProducts.getItems().get(0).getMenu());
+        assertNotNull(menuProducts.getItems().get(1).getMenu());
     }
 }
