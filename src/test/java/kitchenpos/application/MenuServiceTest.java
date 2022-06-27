510a9eccffef673238b028440944cd1fb7da6529
wenodev <logicalstark1105@gmail.com>
2022-06-28 08:30:15 +0900

    test: 메뉴 목록 조회 예외 테스트코드 작성한다

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -9,6 +9,7 @@ import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -25,6 +26,7 @@ import static kitchenpos.factory.fixture.MenuGroupFixtureFactory.createMenuGroup
 import static kitchenpos.factory.fixture.MenuProductFixtureFactory.createMenuProduct;
 import static kitchenpos.factory.fixture.ProductFixtureFactory.createProduct;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -48,9 +50,9 @@ class MenuServiceTest {
     Long 양념_치킨_ID;
     Long 치킨_그룹_ID;
 
-    private Product 밀가루;
+    private Product 콜라;
     private Product 닭;
-    private MenuProduct 양념_치킨_밀가루;
+    private MenuProduct 양념_치킨_콜라;
     private MenuProduct 양념_치킨_닭;
     private MenuGroup 치킨_그룹;
     private Menu 양념_치킨;
@@ -60,18 +62,19 @@ class MenuServiceTest {
         양념_치킨_ID = 1L;
         치킨_그룹_ID = 1L;
 
-        밀가루 = createProduct(1L, "밀가루", new BigDecimal("5000"));
-        닭 = createProduct(2L, "닭", new BigDecimal("10000"));
-        양념_치킨_밀가루 = createMenuProduct(1L, 양념_치킨_ID, 밀가루.getId(), 3);
+        콜라 = createProduct(1L, "밀가루", BigDecimal.valueOf(5000L));
+        닭 = createProduct(2L, "닭", BigDecimal.valueOf(10000L));
+        양념_치킨_콜라 = createMenuProduct(1L, 양념_치킨_ID, 콜라.getId(), 2);
         양념_치킨_닭 = createMenuProduct(2L, 양념_치킨_ID, 닭.getId(), 1);
         치킨_그룹 = createMenuGroup(치킨_그룹_ID, "치킨");
-        양념_치킨 = createMenu(양념_치킨_ID, "양념치킨", new BigDecimal("20000"), 치킨_그룹.getId(), Arrays.asList(양념_치킨_밀가루, 양념_치킨_닭));
+        양념_치킨 = createMenu(양념_치킨_ID, "양념치킨", BigDecimal.valueOf(20000L), 치킨_그룹.getId(), Arrays.asList(양념_치킨_콜라, 양념_치킨_닭));
     }
 
+    @DisplayName("메뉴를 생성한다.")
     @Test
     void create() {
         given(menuGroupDao.existsById(치킨_그룹.getId())).willReturn(Boolean.TRUE);
-        given(productDao.findById(양념_치킨_밀가루.getProductId())).willReturn(Optional.of(밀가루));
+        given(productDao.findById(양념_치킨_콜라.getProductId())).willReturn(Optional.of(콜라));
         given(productDao.findById(양념_치킨_닭.getProductId())).willReturn(Optional.of(닭));
         given(menuDao.save(any(Menu.class))).willReturn(양념_치킨);
 
@@ -80,12 +83,60 @@ class MenuServiceTest {
         assertThat(menu.getName()).isEqualTo(양념_치킨.getName());
     }
 
+    @DisplayName("메뉴의 가격이 0원이면 예외를 던진다.")
+    @Test
+    void createWithInvalidPrice() {
+        MenuGroup menuGroup = createMenuGroup(1000L, "테스트");
+        Menu menu = createMenu(1000L, null, BigDecimal.ZERO, menuGroup.getId(), null);
+
+        given(menuGroupDao.existsById(menuGroup.getId())).willReturn(Boolean.FALSE);
+
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴 그룹에 등록되지 않았다면 예외를 던진다.")
+    @Test
+    void createWihInvalidMeuGroupId() {
+        MenuGroup menuGroup = createMenuGroup(1000L, "테스트");
+        Menu menu = createMenu(1000L, null, BigDecimal.valueOf(1000L), menuGroup.getId(), null);
+
+        given(menuGroupDao.existsById(menuGroup.getId())).willReturn(Boolean.FALSE);
+
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품이 존재하지 않는다면 예외를 던진다.")
+    @Test
+    void createWihInvalidProductId() {
+        given(menuGroupDao.existsById(치킨_그룹.getId())).willReturn(Boolean.TRUE);
+        given(productDao.findById(양념_치킨_콜라.getProductId())).willReturn(Optional.empty());
+
+        assertThatThrownBy(() -> menuService.create(양념_치킨))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 금액의 합보다 메뉴 금액이 더 크면 예외를 던진다.")
+    @Test
+    void createWithInvalidSum() {
+        닭.setPrice(BigDecimal.valueOf(10000L));
+        콜라.setPrice(BigDecimal.valueOf(4000L));
+
+        given(menuGroupDao.existsById(양념_치킨.getMenuGroupId())).willReturn(true);
+        given(productDao.findById(양념_치킨_닭.getProductId())).willReturn(Optional.of(닭));
+        given(productDao.findById(양념_치킨_콜라.getProductId())).willReturn(Optional.of(콜라));
+
+        assertThatThrownBy(() -> menuService.create(양념_치킨))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Test
     void list() {
-        Menu 양념_치킨 = createMenu(양념_치킨_ID, "양념치킨", new BigDecimal("20000"), 치킨_그룹.getId(), Arrays.asList(양념_치킨_밀가루, 양념_치킨_닭));
+        Menu 양념_치킨 = createMenu(양념_치킨_ID, "양념치킨", BigDecimal.valueOf(20000L), 치킨_그룹.getId(), Arrays.asList(양념_치킨_콜라, 양념_치킨_닭));
         given(menuDao.findAll()).willReturn(Arrays.asList(양념_치킨));
         given(menuProductDao.findAllByMenuId(양념_치킨.getId()))
-                .willReturn(Arrays.asList(양념_치킨_밀가루, 양념_치킨_닭));
+                .willReturn(Arrays.asList(양념_치킨_콜라, 양념_치킨_닭));
 
         List<Menu> list = menuService.list();
 
