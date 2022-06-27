9dfec3dc56e4443596b59e179d127debd611b500
14km <k.connor614@gmail.com>
2022-06-28 02:02:34 +0900

    feat: Messages 추가

---
+++ src/main/java/kitchenpos/common/Messages.java
@@ -0,0 +1,8 @@
+package kitchenpos.common;
+
+public class Messages {
+    public static final String PRICE_CANNOT_ZERO = "[ERROR] 가격은 0원 이상 입력해야 합니다.";
+    public static final String NAME_CANNOT_EMPTY = "[ERROR] 이름은 필수로 입력해야 합니다.";
+    public static final String QUANTITY_CANNOT_EMPTY = "[ERROR] 수량은 0개 이상 입력해야 합니다.";
+
+}
