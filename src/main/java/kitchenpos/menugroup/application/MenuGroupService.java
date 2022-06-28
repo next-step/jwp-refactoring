16cfcf99fa4abff614a2276b18c637bd3bf06756
14km <k.connor614@gmail.com>
2022-06-29 02:46:07 +0900

    refactor: MenuGroup Domain refactor

--- src/main/java/kitchenpos/menugroup/application/MenuGroupService.java
+++ src/main/java/kitchenpos/menugroup/application/MenuGroupService.java
@@ -1,26 +1,39 @@
 package kitchenpos.menugroup.application;
 
-import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.common.domain.Name;
 import kitchenpos.menugroup.domain.MenuGroup;
+import kitchenpos.menugroup.domain.MenuGroupRepository;
+import kitchenpos.menugroup.dto.MenuGroupRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
+@Transactional(readOnly = true)
 public class MenuGroupService {
-    private final MenuGroupDao menuGroupDao;
 
-    public MenuGroupService(final MenuGroupDao menuGroupDao) {
-        this.menuGroupDao = menuGroupDao;
+    private final MenuGroupRepository menuGroupRepository;
+
+    public MenuGroupService(MenuGroupRepository menuGroupRepository) {
+        this.menuGroupRepository = menuGroupRepository;
     }
 
-    @Transactional
-    public MenuGroup create(final MenuGroup menuGroup) {
-        return menuGroupDao.save(menuGroup);
+    public MenuGroup findById(Long id) {
+        return menuGroupRepository.findById(id)
+                .orElseThrow(NoSuchElementException::new);
     }
 
     public List<MenuGroup> list() {
-        return menuGroupDao.findAll();
+        return menuGroupRepository.findAll();
+    }
+
+    @Transactional
+    public MenuGroup create(MenuGroupRequest menuGroupRequest) {
+        Name name = Name.of(menuGroupRequest.getName());
+        MenuGroup menuGroup = MenuGroup.of(name);
+
+        return menuGroupRepository.save(menuGroup);
     }
 }
