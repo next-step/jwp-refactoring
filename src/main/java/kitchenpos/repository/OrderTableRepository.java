edfb343b23297645426b08a86133c9980306f27f
gunan <power4889@gmail.com>
2022-06-29 02:25:31 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/kitchenpos/repository/OrderTableRepository.java
+++ src/main/java/kitchenpos/repository/OrderTableRepository.java
@@ -4,9 +4,6 @@ import kitchenpos.domain.OrderTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface OrderTableRepository extends JpaRepository<OrderTable, Long> {
-    List<OrderTable> findAllByTableGroupId(Long tableGroupId);
 }
