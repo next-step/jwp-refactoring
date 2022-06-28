67a87e2efcf01f33131717cbdaedaf78584ad209
Donghee Han <handh0413@gmail.com>
2022-06-29 00:34:59 +0900

    fix: 테이블명 수정

--- src/main/java/kitchenpos/table/domain/OrderTableRepository.java
+++ src/main/java/kitchenpos/table/domain/OrderTableRepository.java
@@ -2,5 +2,5 @@ package kitchenpos.table.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderTableRepository extends JpaRepository<OrderTableRepository, Long> {
+public interface OrderTableRepository extends JpaRepository<OrderTable, Long> {
 }
