92360fcf5fb519a19ad65fdeab69b0a5de994da7
jiyeonghwang <gyp0288@naver.com>
2022-06-28 23:56:15 +0900

    feat: Order, OrderLineItem, OrderTable 생성자 추가

--- src/main/java/kitchenpos/domain/OrderLineItem.java
+++ src/main/java/kitchenpos/domain/OrderLineItem.java
@@ -6,6 +6,20 @@ public class OrderLineItem {
     private Long menuId;
     private long quantity;
 
+    public OrderLineItem() {
+    }
+
+    private OrderLineItem(Long seq, Long orderId, Long menuId, Long quantity) {
+        this.seq = seq;
+        this.orderId = orderId;
+        this.menuId = menuId;
+        this.quantity = quantity;
+    }
+
+    public static OrderLineItem of(Long seq, Long orderId, Long menuId, Long quantity) {
+        return new OrderLineItem(seq, orderId, menuId, quantity);
+    }
+
     public Long getSeq() {
         return seq;
     }
