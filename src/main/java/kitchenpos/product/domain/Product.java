eededc33531a69d341af477a3017dbe74627faf2
Donghee Han <handh0413@gmail.com>
2022-06-28 23:43:47 +0900

    feat: Product JPA 엔티티 추가

---
+++ src/main/java/kitchenpos/product/domain/Product.java
@@ -0,0 +1,44 @@
+package kitchenpos.product.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column(unique = true)
+    private BigDecimal price;
+
+    protected Product() {
+    }
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
