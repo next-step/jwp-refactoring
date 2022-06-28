56f6ae2fc2e6f8b515c83b66b66babf2dc1b0262
Donghee Han <handh0413@gmail.com>
2022-06-29 00:17:22 +0900

    fix: Menu 테이블명 수정

--- src/main/java/kitchenpos/menu/domain/Menu.java
+++ src/main/java/kitchenpos/menu/domain/Menu.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "menu_product")
+@Table(name = "menu")
 public class Menu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
