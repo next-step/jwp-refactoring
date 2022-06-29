ba6e1a8eec835c9ccfe433a11f6bbab51e1b6bea
Lisc <qqwer153@gmail.com>
2022-06-30 00:11:22 +0900

    test: MenuGroupService 테스트 코드 작성

--- src/test/java/kitchenpos/application/CommonTestFixture.java
+++ src/test/java/kitchenpos/application/CommonTestFixture.java
@@ -8,4 +8,8 @@ public class CommonTestFixture {
     public static Product createProduct(Long id, String name, int price) {
         return new Product(id, name, BigDecimal.valueOf(price));
     }
+
+    public static MenuGroup createMenuGroup(Long id, String name) {
+        return new MenuGroup(id, name);
+    }
 }
