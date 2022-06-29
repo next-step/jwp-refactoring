a9e755178df5f2bbf71905f73debc7055877e32b
jhsong2580 <thdwmdgns@naver.com>
2022-06-29 21:52:22 +0900

    test: eventListener Test 구현

---
+++ src/test/java/kitchenpos/event/listener/MenuCreateEventListenerInProductTest.java
@@ -0,0 +1,81 @@
+package kitchenpos.event.listener;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import kitchenpos.domain.Product;
+import kitchenpos.dto.event.MenuCreateEventDTO;
+import kitchenpos.event.customEvent.MenuCreateEvent;
+import kitchenpos.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MenuCreateEventListenerInProductTest {
+
+    private MenuCreateEventListenerInProduct eventListener;
+
+    @Mock
+    private ProductRepository productRepository;
+    private Product pizza;
+    private Product chicken;
+
+    @BeforeEach
+    public void init() {
+        eventListener = new MenuCreateEventListenerInProduct(productRepository);
+
+        chicken = new Product("chicken", BigDecimal.valueOf(500));
+        pizza = new Product("pizza", BigDecimal.valueOf(1000));
+    }
+
+    @Test
+    @DisplayName("Menu에 포함된 Product의 합의 총합은 Menu Price보다 커야한다.")
+    public void validatePriceSmallThenSumTest() {
+        //given
+        Map<Long, Long> quantityPerProduct = new HashMap<>();
+        quantityPerProduct.put(1L, 1L);
+        quantityPerProduct.put(2L, 1L);
+        MenuCreateEventDTO eventDTO = new MenuCreateEventDTO(quantityPerProduct,
+            BigDecimal.valueOf(3000));
+        MenuCreateEvent event = new MenuCreateEvent(eventDTO);
+
+        when(productRepository.findById(1L)).thenReturn(
+            Optional.of(chicken));
+        when(productRepository.findById(2L)).thenReturn(
+            Optional.of(pizza));
+
+        //when & then
+        assertThatThrownBy(() -> eventListener.onApplicationEvent(event))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("메뉴 가격은 상품 가격 총합보다 작아야합니다");
+    }
+
+
+    @Test
+    @DisplayName("저장된 상품이 아니면 에러를 발생한다.")
+    public void validateSavedProduct() {
+        //given
+        Map<Long, Long> quantityPerProduct = new HashMap<>();
+        quantityPerProduct.put(1L, 1L);
+        quantityPerProduct.put(2L, 1L);
+        MenuCreateEventDTO eventDTO = new MenuCreateEventDTO(quantityPerProduct,
+            BigDecimal.valueOf(3000));
+        MenuCreateEvent event = new MenuCreateEvent(eventDTO);
+
+        when(productRepository.findById(1L)).thenReturn(
+            Optional.of(chicken));
+
+        //when & then
+        assertThatThrownBy(() -> eventListener.onApplicationEvent(event))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
