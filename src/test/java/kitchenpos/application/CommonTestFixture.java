3e89ea905f06cb647010a449dc687b197babfe63
Lisc <qqwer153@gmail.com>
2022-06-30 00:14:13 +0900

    test: MenuService 테스트 코드 작성

--- src/test/java/kitchenpos/application/CommonTestFixture.java
+++ src/test/java/kitchenpos/application/CommonTestFixture.java
@@ -3,6 +3,7 @@ package kitchenpos.application;
 import kitchenpos.domain.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CommonTestFixture {
     public static Product createProduct(Long id, String name, int price) {
@@ -11,5 +12,13 @@ public class CommonTestFixture {
 
     public static MenuGroup createMenuGroup(Long id, String name) {
         return new MenuGroup(id, name);
+    }
+
+    public static Menu createMenu(Long id, Long menuGroupId, String name, int price, List<MenuProduct> menuProducts) {
+        return new Menu(id, name, BigDecimal.valueOf(price), menuGroupId, menuProducts);
+    }
+
+    public static MenuProduct createMenuProduct(int seq, Long menuId, Long productId, int quantity) {
+        return new MenuProduct((long) seq, menuId, productId, (long) quantity);
     }
 }
