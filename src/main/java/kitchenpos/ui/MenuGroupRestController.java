7dffae00b9569589cb908d4a4a0161f7e6785ca4
Donghee Han <handh0413@gmail.com>
2022-06-29 18:23:00 +0900

    refactor: MeneGroupResponse 추가

--- src/main/java/kitchenpos/ui/MenuGroupRestController.java
+++ src/main/java/kitchenpos/ui/MenuGroupRestController.java
@@ -2,6 +2,7 @@ package kitchenpos.ui;
 
 import kitchenpos.application.MenuGroupService;
 import kitchenpos.menugroup.domain.MenuGroup;
+import kitchenpos.menugroup.dto.MenuGroupResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class MenuGroupRestController {
@@ -20,18 +22,17 @@ public class MenuGroupRestController {
     }
 
     @PostMapping("/api/menu-groups")
-    public ResponseEntity<MenuGroup> create(@RequestBody final MenuGroup menuGroup) {
-        final MenuGroup created = menuGroupService.create(menuGroup);
+    public ResponseEntity<MenuGroupResponse> create(@RequestBody final MenuGroup menuGroup) {
+        final MenuGroupResponse created = MenuGroupResponse.of(menuGroupService.create(menuGroup));
         final URI uri = URI.create("/api/menu-groups/" + created.getId());
-        return ResponseEntity.created(uri)
-                .body(created)
-                ;
+        return ResponseEntity.created(uri).body(created);
     }
 
     @GetMapping("/api/menu-groups")
-    public ResponseEntity<List<MenuGroup>> list() {
-        return ResponseEntity.ok()
-                .body(menuGroupService.list())
-                ;
+    public ResponseEntity<List<MenuGroupResponse>> list() {
+        List<MenuGroupResponse> list = menuGroupService.list().stream()
+                .map(MenuGroupResponse::of)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(list);
     }
 }
