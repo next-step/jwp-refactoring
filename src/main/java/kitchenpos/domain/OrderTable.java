6ae1b40a7e30cd580700b85143dd99d74fe02700
Ttungga <torb2002@gmail.com>
2022-06-30 23:06:15 +0900

    refactor: make OrderTable JPA entity and add OrderTableRepository

--- src/main/java/kitchenpos/domain/OrderTable.java
+++ src/main/java/kitchenpos/domain/OrderTable.java
@@ -1,9 +1,27 @@
 package kitchenpos.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "order_table")
 public class OrderTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "table_group_id")
     private Long tableGroupId;
+
+    @Column(name = "number_of_guests")
     private int numberOfGuests;
+
+    @Column(name = "empty")
     private boolean empty;
 
     public OrderTable() {
@@ -20,31 +38,15 @@ public class OrderTable {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public Long getTableGroupId() {
         return tableGroupId;
-    }
-
-    public void setTableGroupId(final Long tableGroupId) {
-        this.tableGroupId = tableGroupId;
     }
 
     public int getNumberOfGuests() {
         return numberOfGuests;
     }
 
-    public void setNumberOfGuests(final int numberOfGuests) {
-        this.numberOfGuests = numberOfGuests;
-    }
-
     public boolean isEmpty() {
         return empty;
-    }
-
-    public void setEmpty(final boolean empty) {
-        this.empty = empty;
     }
 }
