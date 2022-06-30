fa9a28efcf7a9edea29e238acb4b11cc542637fc
jhsong2580 <thdwmdgns@naver.com>
2022-06-30 20:18:55 +0900

    test: auditing 기능 사용하기 위해 @JpaAuditing 설정

--- module-table/src/test/java/kitchenpos/ModuleCommonApplicationTests.java
+++ module-table/src/test/java/kitchenpos/ModuleCommonApplicationTests.java
@@ -2,8 +2,10 @@ package kitchenpos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ModuleCommonApplicationTests {
 
     @Test
