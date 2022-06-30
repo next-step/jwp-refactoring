3b2821b4547fad18d69a47500c8b020d2247190a
Yul Jeon <notkhaki@gmail.com>
2022-07-01 04:15:26 +0900

    feat(Menu): add @Version annotation

--- module-menu/src/main/java/kitchenpos/menu/domain/Menu.java
+++ module-menu/src/main/java/kitchenpos/menu/domain/Menu.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Version;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -32,6 +33,7 @@ public class Menu {
     @JoinColumn(nullable = false)
     private MenuGroup menuGroup;
 
+    @Version
     private Long version;
 
     @Embedded
