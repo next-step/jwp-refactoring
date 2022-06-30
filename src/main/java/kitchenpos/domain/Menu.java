fd90161679df6a9da42a27ef384486fb1a93c56e
Ttungga <torb2002@gmail.com>
2022-06-30 22:48:57 +0900

    feat: add MenuProducts to Menu

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -1,15 +1,11 @@
 package kitchenpos.domain;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -29,27 +25,36 @@ public class Menu {
     @Column(name = "menu_group_id")
     private Long menuGroupId;
 
-    @OneToMany(mappedBy = "menu", cascade = {CascadeType.ALL}, orphanRemoval = true)
-    private List<MenuProduct> menuProducts = new ArrayList<>();
+    @Embedded
+    private MenuProducts menuProducts = new MenuProducts();
 
     public Menu() {
     }
 
-    public Menu(Long id) {
-        this.id = id;
-    }
-
-    public Menu(String name, Price price, Long menuGroupId, List<MenuProduct> menuProducts) {
+    public Menu(String name, Price price, Long menuGroupId) {
         this.name = name;
         this.price = price;
         this.menuGroupId = menuGroupId;
-        this.menuProducts = menuProducts;
     }
 
-    public Menu(final Long id, final String name, final BigDecimal price, final Long menuGroupId, final List<MenuProduct> menuProducts) {
+    public Menu(final Long id,
+                final String name,
+                final Price price,
+                final Long menuGroupId) {
         this.id = id;
         this.name = name;
-        this.price = new Price(price.longValue());
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+    }
+
+    public Menu(final Long id,
+                final String name,
+                final Price price,
+                final Long menuGroupId,
+                final MenuProducts menuProducts) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
         this.menuGroupId = menuGroupId;
         this.menuProducts = menuProducts;
     }
@@ -70,11 +75,7 @@ public class Menu {
         return menuGroupId;
     }
 
-    public List<MenuProduct> getMenuProducts() {
+    public MenuProducts getMenuProducts() {
         return menuProducts;
-    }
-
-    public void setMenuProducts(final List<MenuProduct> menuProducts) {
-        this.menuProducts = menuProducts;
     }
 }
