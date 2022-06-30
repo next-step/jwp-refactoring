5280e94a1b61f2e6178fb64db29927e3e89d15d9
jhsong2580 <thdwmdgns@naver.com>
2022-06-30 21:30:39 +0900

    refactor: VO를 각 모듈로 내려, 모듈간 참조를 방지한다

---
+++ module-product/src/main/java/kitchenpos/domain/Price.java
@@ -0,0 +1,63 @@
+package kitchenpos.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Price {
+
+    @Column
+    private BigDecimal price;
+
+    protected Price() {
+    }
+
+    public Price(BigDecimal price) {
+        validatePrice(price);
+        this.price = price;
+    }
+
+    private void validatePrice(BigDecimal source) {
+        if (Objects.isNull(source) || source.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean bigger(BigDecimal target) {
+        return price.compareTo(target) > 0;
+    }
+
+    public BigDecimal multiply(BigDecimal source) {
+        return price.multiply(source);
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void changePrice(BigDecimal price) {
+        validatePrice(price);
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Price)) {
+            return false;
+        }
+        Price price1 = (Price) o;
+        return Objects.equals(price, price1.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
+    }
+
+
+}
