e241ffb4555cfba3f72dff3dad7b770291659f96
kwonyongil <ksgkms23@likelion.org>
2022-07-01 18:08:17 +0900

    feat(kitchenpos) module-product 생성 분리

---
+++ module-product/src/test/java/kitchenpos/product/ModuleProductApplicationTests.java
@@ -0,0 +1,8 @@
+package kitchenpos.product;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleProductApplicationTests {
+    public void contextLoads() {}
+}
