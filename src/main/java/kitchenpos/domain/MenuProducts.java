8c1782bda8172d808ba1c2031e652b97d6580c87
minzzang <minjjang1117@gmail.com>
2022-07-01 20:43:11 +0900

    feat: 메뉴의 가격은 메뉴 상품들 가격의 총합보다 높을 수 없다

--- src/main/java/kitchenpos/domain/MenuProducts.java
+++ src/main/java/kitchenpos/domain/MenuProducts.java
@@ -1,5 +1,6 @@
 package kitchenpos.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
@@ -9,7 +10,7 @@ import java.util.List;
 
 @Embeddable
 public class MenuProducts {
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "menu")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "menu", cascade = CascadeType.PERSIST)
     private List<MenuProduct> elements = new ArrayList<>();
 
     protected MenuProducts() {
@@ -21,5 +22,24 @@ public class MenuProducts {
 
     public List<MenuProduct> elements() {
         return Collections.unmodifiableList(elements);
+    }
+
+    public void addMenu(Menu menu) {
+        elements
+                .forEach(menuProduct -> menuProduct.updateMenu(menu));
+    }
+
+    public void validatePrice(Price menuPrice) {
+        if (menuPrice.isExpensive(sum())) {
+            throw new IllegalArgumentException("메뉴의 가격은 메뉴 상품 가격의 총합보다 높을 수 없습니다.");
+        }
+    }
+
+    private Price sum() {
+        Price price = new Price();
+        for (MenuProduct element : elements) {
+            price = price.add(element.price());
+        }
+        return price;
     }
 }
