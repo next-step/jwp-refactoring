a192f72db30e55f2d314655249b480e3994ca843
choi-ys <project.log.062@gmail.com>
2022-07-02 23:22:42 +0900

    feat: 상품 서비스 계층 리팩터링
    상품 생성 로직 리팩터링
    - 상품 가격에 대한 유효성 검증을 상품 도메인 객체 생성 시점에 적용
    - 상품 가격의 최소 기준을 상수로 추출
    - 상품 가격에 대한 오류 문구 정의
    
    TODO : 생성자 추가에 따라 테스트 코드에 사용된 기본 생성자 추가 정의
      - JPA 적용 시 프록시 사용을 위해 접근 제한제를 `public`에서 `protected`로 변경
    
    구현에 따른 TODO List 갱신

--- src/main/java/kitchenpos/domain/product/Product.java
+++ src/main/java/kitchenpos/domain/product/Product.java
@@ -1,11 +1,35 @@
 package kitchenpos.domain.product;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
+    private static final int MINIMUM_PRICE = 0;
+    public static final String INVALID_PRODUCT_PRICE_ERROR_MESSAGE = "상품 가격이 올바르지 않습니다. 0원 이상의 가격을 입력해주세요.";
+
     private Long id;
     private String name;
     private BigDecimal price;
+
+    public Product() {
+
+    }
+
+    private Product(String name, BigDecimal price) {
+        validatePrice(price);
+        this.name = name;
+        this.price = price;
+    }
+
+    public static Product of(String name, BigDecimal price) {
+        return new Product(name, price);
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < MINIMUM_PRICE) {
+            throw new IllegalArgumentException(INVALID_PRODUCT_PRICE_ERROR_MESSAGE);
+        }
+    }
 
     public Long getId() {
         return id;
