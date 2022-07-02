184c868c0b3b8b3a24f1914f87032ee397b96091
minzzang <minjjang1117@gmail.com>
2022-07-02 10:37:53 +0900

    feat: 주문 테이블 아이디 목록을 조회한다

--- src/main/java/kitchenpos/domain/OrderTables.java
+++ src/main/java/kitchenpos/domain/OrderTables.java
@@ -2,15 +2,20 @@ package kitchenpos.domain;
 
 import org.springframework.util.CollectionUtils;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Embeddable
 public class OrderTables {
     public static final int MINIMUM_GROUP_NUMBER = 2;
-    
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "tableGroup", cascade = CascadeType.PERSIST)
     private List<OrderTable> elements = new ArrayList<>();
 
     protected OrderTables() {
@@ -25,6 +30,12 @@ public class OrderTables {
         if (CollectionUtils.isEmpty(orderTables) || orderTables.size() < MINIMUM_GROUP_NUMBER) {
             throw new IllegalArgumentException("2개 이상의 테이블을 단체 지정할 수 있습니다.");
         }
+    }
+
+    public List<Long> getOrderTableIds() {
+        return elements.stream()
+                .map(OrderTable::getId)
+                .collect(Collectors.toList());
     }
 
     public List<OrderTable> elements() {
--- src/test/java/kitchenpos/domain/OrderTablesTest.java
+++ src/test/java/kitchenpos/domain/OrderTablesTest.java
@@ -2,9 +2,11 @@ package kitchenpos.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OrderTablesTest {
@@ -22,4 +24,27 @@ class OrderTablesTest {
         ).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("2개 이상의 테이블을 단체 지정할 수 있습니다.");
     }
+
+    @Test
+    void 주문_테이블_아이디_목록을_조회한다() {
+        // given
+        OrderTables orderTables = createOrderTables();
+
+        // when
+        List<Long> result = orderTables.getOrderTableIds();
+
+        // then
+        assertThat(result).containsExactly(1L, 2L);
+    }
+
+    private OrderTables createOrderTables() {
+        return new OrderTables(
+                Arrays.asList(
+                        new OrderTable(1L),
+                        new OrderTable(2L)
+                )
+        );
+    }
+
+
 }
