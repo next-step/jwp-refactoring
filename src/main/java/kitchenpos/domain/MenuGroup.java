d29395be47cb6d97e2074b355c1311d051ba59a9
boss.back <boss.back@kakaocorp.com>
2022-07-03 01:28:42 +0900

    refactor: MenuGroup Repository로 수정

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,8 +1,22 @@
 package kitchenpos.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "menu_group")
 public class MenuGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    protected MenuGroup() {
+
+    }
 
     public Long getId() {
         return id;
