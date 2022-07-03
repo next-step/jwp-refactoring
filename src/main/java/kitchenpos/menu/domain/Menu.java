47dd033e7f605fcf71d2cfe6e76a706a496a74dc
jiyeonghwang <gyp0288@naver.com>
2022-07-04 07:46:22 +0900

    feat: Menu, MenuProduct 도메인 생성

---
+++ src/main/java/kitchenpos/menu/domain/Menu.java
@@ -0,0 +1,67 @@
+package kitchenpos.menu.domain;
+
+import kitchenpos.domain.Name;
+import kitchenpos.domain.Price;
+import kitchenpos.menuGroup.domain.MenuGroup;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Name name;
+
+    @Embedded
+    private Price price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_group_id")
+    private MenuGroup menuGroup;
+
+    @Embedded
+    private MenuProducts menuProducts;
+
+    public Menu(){
+    }
+
+    private Menu(String name, BigDecimal price, MenuGroup menuGroup, List<MenuProduct> menuProducts) {
+        this.name = new Name(name);
+        this.price = new Price(price);
+        this.menuGroup = menuGroup;
+        this.menuProducts = new MenuProducts(menuProducts, this.price, this);
+    }
+
+    public static Menu of(String name, BigDecimal price, MenuGroup menuGroup, List<MenuProduct> menuProducts) {
+        return new Menu(name, price, menuGroup, menuProducts);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name.getName();
+    }
+
+    public BigDecimal getPrice() {
+        return price.getPrice();
+    }
+
+    public Long getMenuGroupId() {
+        if (menuGroup != null) {
+            return menuGroup.getId();
+        }
+
+        return null;
+    }
+
+    public List<MenuProduct> getMenuProducts() {
+        return menuProducts.getMenuProducts();
+    }
+}
