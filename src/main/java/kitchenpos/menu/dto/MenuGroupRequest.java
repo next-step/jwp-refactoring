962c2d88d6dae16c7bbc8cd1befb70673ffd5572
piopoi <kangsw1988@gmail.com>
2022-07-03 19:20:44 +0900

    refactor: 2단계 피드백 반영 - request DTO에 Entity 변환 메서드 추가

--- src/main/java/kitchenpos/menu/dto/MenuGroupRequest.java
+++ src/main/java/kitchenpos/menu/dto/MenuGroupRequest.java
@@ -1,10 +1,16 @@
 package kitchenpos.menu.dto;
 
+import kitchenpos.menu.domain.MenuGroup;
+
 public class MenuGroupRequest {
     private final String name;
 
     public MenuGroupRequest(String name) {
         this.name = name;
+    }
+
+    public MenuGroup toMenuGroup() {
+        return new MenuGroup(name);
     }
 
     public String getName() {
