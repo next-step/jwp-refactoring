8c82488285c21d591b36ba49928ab4d08be80e01
Woody <yuncheol92@naver.com>
2022-07-03 17:59:57 +0900

    refactor : 사용하지 않는 메서드 삭제

--- src/main/java/kitchenpos/menu/domain/MenuRepository.java
+++ src/main/java/kitchenpos/menu/domain/MenuRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-    int countByIdIn(List<Long> menuIds);
-
     List<Menu> findAllByIdIn(List<Long> menuIds);
 }
