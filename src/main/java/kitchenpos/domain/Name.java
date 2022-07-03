b8a9f1157effa6d88311c3a006b4eb0c8a941c07
jiyeonghwang <gyp0288@naver.com>
2022-07-03 21:05:56 +0900

    feat: `Name` 도메인 생성

---
+++ src/main/java/kitchenpos/domain/Name.java
@@ -0,0 +1,24 @@
+package kitchenpos.domain;
+
+import org.springframework.util.StringUtils;
+
+import javax.persistence.Column;
+
+public class Name {
+    @Column(nullable = false)
+    private String name;
+
+    protected Name() {
+    }
+
+    public Name(String name) {
+        if (!StringUtils.hasText(name)) {
+            throw new IllegalArgumentException("잘못된 이름을 입력하였습니다.");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
