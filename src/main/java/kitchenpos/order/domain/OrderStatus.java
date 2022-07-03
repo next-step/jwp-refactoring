163ed13afaeda7e66f2ccfff83adb5dd5698be7a
minzzang <minjjang1117@gmail.com>
2022-07-04 00:44:49 +0900

    refactor: 주문 완료 전 상태 목록 조회 로직 수정

--- src/main/java/kitchenpos/order/domain/OrderStatus.java
+++ src/main/java/kitchenpos/order/domain/OrderStatus.java
@@ -2,18 +2,17 @@ package kitchenpos.order.domain;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public enum OrderStatus {
     COOKING, MEAL, COMPLETION;
+
+    private static final List<OrderStatus> STATUS_BEFORE_COMPLETION = Arrays.asList(COOKING, MEAL);
 
     public boolean isCompletion() {
         return this == COMPLETION;
     }
 
     public static List<OrderStatus> findNotCompletionStatus() {
-        return Arrays.stream(values())
-                .filter(status -> !status.isCompletion())
-                .collect(Collectors.toList());
+        return STATUS_BEFORE_COMPLETION;
     }
 }
