777efc5dc39655ac3ab5d1f7c80d61696c2356ca
SANGIK KIM <sangik.kim@enel.com>
2022-07-03 18:33:56 +0900

    test(OrderTable): add tests for OrderTable

--- src/main/java/kitchenpos/domain/OrderTable.java
+++ src/main/java/kitchenpos/domain/OrderTable.java
@@ -9,6 +9,10 @@ public class OrderTable {
     public OrderTable() {
     }
 
+    public OrderTable(int numberOfGuests, boolean empty) {
+        this(null, null, numberOfGuests, empty);
+    }
+
     public OrderTable(Long id, int numberOfGuests) {
         this(id, numberOfGuests, false);
     }
