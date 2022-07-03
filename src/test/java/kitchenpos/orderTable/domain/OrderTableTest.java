8fb1f3599f8990a5821912d30b2f7716b9958578
Woody <yuncheol92@naver.com>
2022-07-03 17:57:00 +0900

    test : 주문_테이블 생성 메서드추가

--- src/test/java/kitchenpos/orderTable/domain/OrderTableTest.java
+++ src/test/java/kitchenpos/orderTable/domain/OrderTableTest.java
@@ -1,4 +1,4 @@
-package kitchenpos.table.domain;
+package kitchenpos.orderTable.domain;
 
 import kitchenpos.tableGroup.domain.TableGroup;
 
@@ -6,6 +6,10 @@ public class OrderTableTest {
 
     public static OrderTable 주문_태이블_생성(TableGroup tableGroup, int numberOfGuests, boolean empty) {
         return new OrderTable(tableGroup, numberOfGuests, empty);
+    }
+
+    public static OrderTable 주문_태이블_생성(int numberOfGuests, boolean empty) {
+        return new OrderTable(numberOfGuests, empty);
     }
 
     public static OrderTable 주문_태이블_생성(Long id, TableGroup tableGroup, int numberOfGuests, boolean empty) {
