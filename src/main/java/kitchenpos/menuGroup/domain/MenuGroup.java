3ba69a6447e3a32c471c6e3579b3f06cbee9663f
jiyeonghwang <gyp0288@naver.com>
2022-07-03 22:14:24 +0900

    refactor: MenuGroup domain 리팩토링

---
+++ src/main/java/kitchenpos/menuGroup/domain/MenuGroup.java
@@ -0,0 +1,34 @@
+package kitchenpos.menuGroup.domain;
+
+import kitchenpos.domain.Name;
+
+import javax.persistence.*;
+
+@Entity
+public class MenuGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Name name;
+
+    public MenuGroup() {
+    }
+
+    private MenuGroup(String name) {
+        this.name = new Name(name);
+    }
+
+    public static MenuGroup of(String name) {
+        return new MenuGroup(name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name.getName();
+    }
+}
