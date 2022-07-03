41893a65bee55a9bc5f927a4f24727e04acd4b96
Lisc <qqwer153@gmail.com>
2022-07-03 19:40:16 +0900

    refactor: TableService 리팩토링

---
+++ src/main/java/kitchenpos/order/application/TableService.java
@@ -0,0 +1,82 @@
+package kitchenpos.order.application;
+
+import kitchenpos.order.domain.OrderRepository;
+import kitchenpos.order.domain.OrderStatus;
+import kitchenpos.order.domain.OrderTable;
+import kitchenpos.order.domain.OrderTableRepository;
+import kitchenpos.order.dto.OrderTableRequest;
+import kitchenpos.order.dto.OrderTableResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@Service
+public class TableService {
+    private static final String ERROR_MESSAGE_INVALID_ORDER_STATUS = "주문 상태가 '조리' 또는 '식사'가 아니어야 합니다.";
+    private static final String ERROR_MESSAGE_INVALID_NUMBER_OF_GUESTS = "방문객 수는 0 명 이상이어야 합니다.";
+
+    private final OrderTableRepository orderTableRepository;
+    private final OrderRepository orderRepository;
+
+    public TableService(OrderTableRepository orderTableRepository, OrderRepository orderRepository) {
+        this.orderTableRepository = orderTableRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    @Transactional
+    public OrderTableResponse create(final OrderTableRequest orderTableRequest) {
+        OrderTable orderTable = new OrderTable(orderTableRequest.getNumberOfGuests(), orderTableRequest.isEmpty());
+        orderTable = orderTableRepository.save(orderTable);
+        return OrderTableResponse.of(orderTable);
+    }
+
+    @Transactional(readOnly = true)
+    public List<OrderTableResponse> list() {
+        List<OrderTable> orderTableEntities = orderTableRepository.findAll();
+        return orderTableEntities.stream()
+                .map(OrderTableResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderTableResponse changeEmpty(final Long orderTableId) {
+        OrderTable orderTable = getOrderTableById(orderTableId);
+        orderTable.validateHasTableGroupId();
+
+        if (orderRepository.existsByOrderTableIdAndOrderStatusIn(
+                orderTable.getId(), Arrays.asList(OrderStatus.COOKING, OrderStatus.MEAL))) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_ORDER_STATUS);
+        }
+
+        orderTable.updateEmpty();
+        orderTable = orderTableRepository.save(orderTable);
+        return OrderTableResponse.of(orderTable);
+    }
+
+    @Transactional
+    public OrderTableResponse changeNumberOfGuests(final Long orderTableId, final OrderTableRequest orderTableRequest) {
+        final int numberOfGuests = orderTableRequest.getNumberOfGuests();
+        validateNumberOfGuests(numberOfGuests);
+
+        OrderTable savedOrderTable = getOrderTableById(orderTableId);
+        savedOrderTable.validateIsEmpty();
+
+        savedOrderTable.changeNumberOfGuests(numberOfGuests);
+        return OrderTableResponse.of(orderTableRepository.save(savedOrderTable));
+    }
+
+    private void validateNumberOfGuests(int numberOfGuests) {
+        if (numberOfGuests < 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_NUMBER_OF_GUESTS);
+        }
+    }
+
+    private OrderTable getOrderTableById(Long orderTableId) {
+        return orderTableRepository.findById(orderTableId)
+                .orElseThrow(NoSuchElementException::new);
+    }
+}
