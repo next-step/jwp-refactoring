6d7db52802cb8b10160954a260b4b8c3d2dca95f
14km <k.connor614@gmail.com>
2022-07-05 00:23:57 +0900

    feat: 연관 관계 수정

--- src/main/java/kitchenpos/menu/application/MenuService.java
+++ src/main/java/kitchenpos/menu/application/MenuService.java
@@ -10,42 +10,34 @@ import kitchenpos.menu.domain.MenuRepository;
 import kitchenpos.menu.dto.MenuProductRequest;
 import kitchenpos.menu.dto.MenuRequest;
 import kitchenpos.menu.dto.MenuResponse;
-import kitchenpos.menugroup.application.MenuGroupService;
-import kitchenpos.menugroup.domain.MenuGroup;
-import kitchenpos.product.appliaction.ProductService;
-import kitchenpos.product.domain.Product;
+import kitchenpos.menu.validator.MenuValidator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
-
-import static kitchenpos.common.Messages.PRODUCT_FIND_IN_NO_SUCH;
 
 @Service
 @Transactional(readOnly = true)
 public class MenuService {
+    private final MenuValidator menuValidator;
     private final MenuRepository menuRepository;
-    private final MenuGroupService menuGroupService;
-    private final ProductService productService;
 
-    public MenuService(MenuRepository menuRepository, MenuGroupService menuGroupService, ProductService productService) {
+    public MenuService(MenuValidator menuValidator, MenuRepository menuRepository) {
+        this.menuValidator = menuValidator;
         this.menuRepository = menuRepository;
-        this.menuGroupService = menuGroupService;
-        this.productService = productService;
     }
 
     @Transactional
     public MenuResponse create(final MenuRequest menuRequest) {
+        menuValidator.validateCreateMenu(menuRequest);
+
         final Name name = Name.of(menuRequest.getName());
         final Price price = Price.of(menuRequest.getPrice());
-        final MenuGroup menuGroup = menuGroupService.findById(menuRequest.getMenuGroupId());
         final MenuProducts menuProducts = MenuProducts.of(convertToMenuProducts(menuRequest.getMenuProducts()));
 
-        Menu menu = Menu.of(name, price, menuGroup, menuProducts);
+        Menu menu = Menu.of(name, price, menuRequest.getMenuGroupId(), menuProducts);
         return MenuResponse.of(menuRepository.save(menu));
     }
 
@@ -55,21 +47,8 @@ public class MenuService {
     }
 
     private List<MenuProduct> convertToMenuProducts(List<MenuProductRequest> menuProducts) {
-        Map<Long, Long> menuProductRequests = menuProducts.stream()
-                .collect(Collectors.toMap(
-                        MenuProductRequest::getProductId,
-                        MenuProductRequest::getQuantity
-                ));
-
-        List<Long> productsIds = new ArrayList<>(menuProductRequests.keySet());
-        List<Product> products = productService.findByIdIn(productsIds);
-
-        if (products.size() != menuProducts.size()) {
-            throw new NoSuchElementException(PRODUCT_FIND_IN_NO_SUCH);
-        }
-
-        return products.stream()
-                .map(product -> MenuProduct.of(product, Quantity.of(menuProductRequests.get(product.getId()))))
+        return menuProducts.stream().map(menuProductRequest ->
+                        MenuProduct.of(menuProductRequest.getProductId(), Quantity.of(menuProductRequest.getQuantity())))
                 .collect(Collectors.toList());
     }
 
--- src/test/java/kitchenpos/menu/domain/MenuProductTest.java
+++ src/test/java/kitchenpos/menu/domain/MenuProductTest.java
@@ -22,26 +22,13 @@ class MenuProductTest {
         Quantity 수량 = Quantity.of(1);
 
         // when
-        MenuProduct 메뉴_상품 = MenuProduct.of(피자, 수량);
+        MenuProduct 메뉴_상품 = MenuProduct.of(피자.getId(), 수량);
 
         // then
         assertAll(
                 () -> assertThat(메뉴_상품).isNotNull(),
-                () -> assertThat(메뉴_상품.getProduct()).isEqualTo(피자),
+                () -> assertThat(메뉴_상품.getProductId()).isEqualTo(피자.getId()),
                 () -> assertThat(메뉴_상품.getQuantity()).isEqualTo(수량.getQuantity())
         );
-    }
-
-    @Test
-    @DisplayName("메뉴 상품 합계 테스트")
-    void getTotalPrice() {
-        // given
-        Product 피자 = Product.of(Name.of("피자"), Price.of(BigDecimal.valueOf(17_000)));
-        Quantity 수량 = Quantity.of(10);
-
-        MenuProduct 메뉴_상품 = MenuProduct.of(피자, 수량);
-
-        // when & then
-        assertThat(메뉴_상품.getProductPrice()).isEqualTo(BigDecimal.valueOf(170_000));
     }
 }
