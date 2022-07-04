7c5a144d484e3fa3e97c4164afc10e0228fb05c1
jiyeonghwang <gyp0288@naver.com>
2022-07-04 11:15:28 +0900

    fix: OrderTableRepository 사용하지 않는 메소드 제거

--- src/main/java/kitchenpos/table/domain/OrderTableRepository.java
+++ src/main/java/kitchenpos/table/domain/OrderTableRepository.java
@@ -1,14 +1,11 @@
 package kitchenpos.table.domain;
 
-import kitchenpos.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrderTableRepository extends JpaRepository<OrderTable, Long> {
-    boolean existsByOrderTableIdAndOrderStatusIn(OrderTable orderTable, List<OrderStatus> orderStatusList);
-
-    List<OrderTable> findAllByTableGroupIds(List<Long> orderTableIds);
+    List<OrderTable> findAllByIdIn(List<Long> orderTableIds);
 
     List<OrderTable> findAllByTableGroup(TableGroup tableGroup);
 }
