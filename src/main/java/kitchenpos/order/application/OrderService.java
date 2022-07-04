ebc3813874d17b1b73495d84f8e76b16b6870dc1
jiyeonghwang <gyp0288@naver.com>
2022-07-04 11:14:41 +0900

    refactor: OrderService 리팩터링

---
+++ src/main/java/kitchenpos/order/application/OrderService.java
@@ -0,0 +1,83 @@
+package kitchenpos.order.application;
+
+import kitchenpos.menu.domain.MenuRepository;
+import kitchenpos.order.domain.Order;
+import kitchenpos.order.domain.OrderLineItem;
+import kitchenpos.order.domain.OrderRepository;
+import kitchenpos.order.dto.OrderLineItemRequest;
+import kitchenpos.order.dto.OrderRequest;
+import kitchenpos.order.dto.OrderResponse;
+import kitchenpos.table.domain.OrderTable;
+import kitchenpos.table.domain.OrderTableRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final OrderTableRepository orderTableRepository;
+    private final MenuRepository menuRepository;
+
+    public OrderService(OrderRepository orderRepository, OrderTableRepository orderTableRepository, MenuRepository menuRepository) {
+        this.orderRepository = orderRepository;
+        this.orderTableRepository = orderTableRepository;
+        this.menuRepository = menuRepository;
+    }
+
+    @Transactional
+    public OrderResponse create(final OrderRequest request) {
+        final List<OrderLineItemRequest> orderLineItemRequests = request.getOrderLineItemRequests();
+        validOrderLineItems(orderLineItemRequests);
+        validMenu(orderLineItemRequests);
+        List<OrderLineItem> orderLineItems = orderLineItemRequests.stream()
+                .map(OrderLineItemRequest::toEntity)
+                .collect(Collectors.toList());
+
+        OrderTable orderTable = orderTableRepository.findById(request.getOrderTableId())
+                .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 주문 테이블입니다."));
+
+        Order order = Order.of(orderTable, orderLineItems);
+        order = orderRepository.save(order);
+
+        return OrderResponse.of(order);
+    }
+
+    private void validOrderLineItems(List<OrderLineItemRequest> orderLineItemRequests) {
+        if (CollectionUtils.isEmpty(orderLineItemRequests)) {
+            throw new IllegalArgumentException("주문항목이 없습니다.");
+        }
+    }
+
+    public void validMenu(List<OrderLineItemRequest> orderLineItemRequests) {
+        final List<Long> menuIds = orderLineItemRequests.stream()
+                .map(OrderLineItemRequest::getMenuId)
+                .collect(Collectors.toList());
+
+        if (orderLineItemRequests.size() != menuRepository.countByIdIn(menuIds)) {
+            throw new IllegalArgumentException("등록되지 않은 메뉴가 포함되어 있습니다.");
+        }
+    }
+
+    public List<OrderResponse> list() {
+        List<Order> list = orderRepository.findAllOrderAndItems();
+        return list.stream()
+                .map(OrderResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderResponse changeOrderStatus(final Long orderId, final OrderRequest request) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 주문입니다."));
+
+        order.changeOrderStatus(request.getOrderStatus());
+        orderRepository.save(order);
+        order = orderRepository.findAllOrderAndItemsByOrder(order);
+
+        return OrderResponse.of(order);
+    }
+}
