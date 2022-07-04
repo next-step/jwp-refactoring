877f23a9ab94ae755f80f210d9f1f46d3549556d
jiyeonghwang <gyp0288@naver.com>
2022-07-04 11:12:59 +0900

    feat: OrderRepository 생성

---
+++ src/main/java/kitchenpos/order/domain/OrderRepository.java
@@ -0,0 +1,24 @@
+package kitchenpos.order.domain;
+
+import kitchenpos.table.domain.OrderTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query(value = "select o from Order o "
+
+
+            + "join fetch o.orderLineItems")
+    List<Order> findAllOrderAndItems();
+
+    @Query(value = "select o from Order o "
+            + "join fetch o.orderLineItems oli "
+            + "where o = :order")
+    Order findAllOrderAndItemsByOrder(Order order);
+
+    boolean existsByOrderTableAndOrderStatusIn(OrderTable orderTable, List<OrderStatus> orderStatuses);
+
+    boolean existsByOrderTableInAndOrderStatusIn(List<OrderTable> orderTableEntities, List<OrderStatus> orderStatuses);
+}
