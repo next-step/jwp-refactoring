09cff3dac80bafe49dffff3e6274b71f7dcd5390
mmtos <js223kr92@naver.com>
2022-07-05 22:20:05 +0900

    review-feedback: 테스트 코드에서 Reflection으로 Dto 생성하는 부분 수정

--- src/test/java/kitchenpos/menu/application/fixture/MenuDtoFixtureFactory.java
+++ src/test/java/kitchenpos/menu/application/fixture/MenuDtoFixtureFactory.java
@@ -1,12 +1,16 @@
 package kitchenpos.menu.application.fixture;
 
+import static java.util.stream.Collectors.toList;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
+import kitchenpos.menu.domain.Menu;
 import kitchenpos.menu.domain.MenuGroup;
+import kitchenpos.menu.domain.MenuProduct;
 import kitchenpos.menu.dto.MenuDto;
 import kitchenpos.menu.dto.MenuProductDto;
 
@@ -16,24 +20,9 @@ public class MenuDtoFixtureFactory {
 
     public static MenuDto createMenu(MenuGroup menuGroup, String menuName, int menuPrice,
                                      List<MenuProductDto> menuProductDtos) {
-        MenuDto menuDto = getNewInstance(MenuDto.class);
-        setField(menuDto, "name", menuName);
-        setField(menuDto, "price", BigDecimal.valueOf(menuPrice));
-        setField(menuDto, "menuGroup", menuGroup);
-        setField(menuDto, "menuProductDtos", menuProductDtos);
-        return menuDto;
-    }
-
-    private static <T> T getNewInstance(Class<?> clazz) {
-        try {
-            Constructor<?> constructor = clazz.getDeclaredConstructor();
-            if (!constructor.isAccessible()) {
-                constructor.setAccessible(true);
-            }
-
-            return (T) constructor.newInstance();
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException | IllegalArgumentException e) {
-            throw new RuntimeException("Can't generate no args constructor. Class : " + clazz.getName(), e);
-        }
+        List<MenuProduct> menuProducts = menuProductDtos.stream()
+                .map(menuProductDto -> new MenuProduct(menuProductDto.getProductId(),menuProductDto.getQuantity()))
+                .collect(toList());
+        return MenuDto.of(new Menu(menuName,new BigDecimal(menuPrice),menuGroup, menuProducts));
     }
 }
--- src/test/java/kitchenpos/menu/application/fixture/MenuProductDtoFixtureFactory.java
+++ src/test/java/kitchenpos/menu/application/fixture/MenuProductDtoFixtureFactory.java
@@ -4,6 +4,7 @@ import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import kitchenpos.menu.domain.MenuProduct;
 import kitchenpos.menu.dto.MenuProductDto;
 
 public class MenuProductDtoFixtureFactory {
@@ -11,29 +12,6 @@ public class MenuProductDtoFixtureFactory {
     }
 
     public static MenuProductDto createMenuProduct(Long productId, long quantity) {
-        MenuProductDto menuProductDto = getNewInstance(MenuProductDto.class);
-        setField(menuProductDto, "productId", productId);
-        setField(menuProductDto, "quantity", quantity);
-        return menuProductDto;
+        return MenuProductDto.of(new MenuProduct(productId,quantity));
     }
-
-    public static MenuProductDto createMenuProduct(Long seq, Long productId, long quantity) {
-        MenuProductDto menuProductDto = createMenuProduct(productId, quantity);
-        setField(menuProductDto, "seq", seq);
-        return menuProductDto;
-    }
-
-    private static <T> T getNewInstance(Class<?> clazz) {
-        try {
-            Constructor<?> constructor = clazz.getDeclaredConstructor();
-            if (!constructor.isAccessible()) {
-                constructor.setAccessible(true);
-            }
-
-            return (T) constructor.newInstance();
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException | IllegalArgumentException e) {
-            throw new RuntimeException("Can't generate no args constructor. Class : " + clazz.getName(), e);
-        }
-    }
-
 }
