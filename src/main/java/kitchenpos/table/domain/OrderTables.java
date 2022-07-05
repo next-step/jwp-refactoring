b007e5ee3868bb1e80ce95a6e91589a8bbea2cd6
14km <k.connor614@gmail.com>
2022-07-06 01:39:04 +0900

    refactor: Table Domain 연관관계 수정 및 이벤트 기반 처리

--- src/main/java/kitchenpos/table/domain/OrderTables.java
+++ src/main/java/kitchenpos/table/domain/OrderTables.java
@@ -5,15 +5,11 @@ import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
-import static kitchenpos.common.Messages.TABLE_GROUP_ORDER_IDS_FIND_IN_NO_SUCH;
-import static kitchenpos.common.Messages.TABLE_GROUP_ORDER_NOT_EMPTY;
 
 @Embeddable
 public class OrderTables {
 
-    @OneToMany(mappedBy = "tableGroup", fetch = FetchType.LAZY, orphanRemoval = true)
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true)
     private List<OrderTable> orderTables = new ArrayList<>();
 
     protected OrderTables() {
@@ -35,23 +31,11 @@ public class OrderTables {
         return orderTables;
     }
 
-    public void validateOrderTableGroup(List<Long> requestOrderTablesIds) {
-        if (requestOrderTablesIds.size() != orderTables.size()) {
-            throw new IllegalArgumentException(TABLE_GROUP_ORDER_IDS_FIND_IN_NO_SUCH);
-        }
-
-        for (final OrderTable orderTable : orderTables) {
-            if (orderTable.isNotEmpty() || Objects.nonNull(orderTable.getTableGroupId())) {
-                throw new IllegalArgumentException(TABLE_GROUP_ORDER_NOT_EMPTY);
-            }
-        }
-    }
-
     public void add(OrderTable orderTable) {
         orderTables.add(orderTable);
     }
 
     public void ungroup() {
-        orderTables.forEach(orderTable -> orderTable.setTableGroup(null));
+        orderTables.forEach(orderTable -> orderTable.setTableGroupId(null));
     }
 }
