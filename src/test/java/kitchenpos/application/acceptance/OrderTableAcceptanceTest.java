cbdc5cba71478a844b47f1058b874f81843e5311
breezeunn <je7689@gmail.com>
2022-07-06 01:39:14 +0900

    refactor(ORDERTABLE): 주문 테이블 테스트 가독성 개선

--- src/test/java/kitchenpos/application/acceptance/OrderTableAcceptanceTest.java
+++ src/test/java/kitchenpos/application/acceptance/OrderTableAcceptanceTest.java
@@ -26,9 +26,15 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("주문 테이블 관련 기능")
 class OrderTableAcceptanceTest extends BaseAcceptanceTest {
 
+    private OrderTable 주문_테이블_1;
+
+    private Menu 메뉴;
+
     @BeforeEach
     public void setUp() {
         super.setUp();
+
+        주문_테이블_1 = 주문_테이블_생성_요청(3, false).as(OrderTable.class);
     }
 
     /**
@@ -58,16 +64,12 @@ class OrderTableAcceptanceTest extends BaseAcceptanceTest {
     }
 
     /**
-     * Given 주문 테이블 1건을 등록한 후
      * When 주문 테이블 목록을 조회하면
      * Then 1건이 조회된다
      */
     @DisplayName("주문 테이블 목록 조회")
     @Test
     void list() {
-        // given
-        주문_테이블_생성_요청(3, true);
-
         // when
         ExtractableResponse<Response> 주문_테이블_목록_조회_요청_응답 = 주문_테이블_목록_조회_요청();
 
@@ -76,107 +78,92 @@ class OrderTableAcceptanceTest extends BaseAcceptanceTest {
     }
 
     /**
-     * Given 주문 테이블 1건을 등록한 후
      * When 주문 테이블을 빈 테이블로 변경하면
      * Then 주문 테이블이 빈 테이블로 변경된다.
      */
     @DisplayName("주문 테이블을 빈 테이블로 변경")
     @Test
     void changeEmpty() {
-        // given
-        OrderTable orderTable = 주문_테이블_생성_요청(3, false).as(OrderTable.class);
-        orderTable.setEmpty(true);
-
         // when
+        주문_테이블_1.setEmpty(true);
         ExtractableResponse<Response> 주문_테이블_빈_테이블로_변경_요청_응답
-                = 주문_테이블_빈_테이블로_변경_요청(orderTable.getId(), orderTable);
+                = 주문_테이블_빈_테이블로_변경_요청(주문_테이블_1.getId(), 주문_테이블_1);
 
         // then
         assertTrue(주문_테이블_빈_테이블로_변경_요청_응답.as(OrderTable.class).isEmpty());
     }
 
     /**
-     * Given 주문 테이블 1건을 등록한 후
      * When 주문 테이블의 손님 수를 변경하면
      * Then 주문 테이블의 손님 수가 변경된다.
      */
     @DisplayName("주문 테이블의 손님 수를 변경")
     @Test
     void changeNumberOfGuests() {
-        // given
-        OrderTable orderTable = 주문_테이블_생성_요청(3, false).as(OrderTable.class);
-        orderTable.setNumberOfGuests(2);
-
         // when
+        주문_테이블_1.setNumberOfGuests(2);
         ExtractableResponse<Response> 주문_테이블_손님_수_변경_요청_응답
-                = 주문_테이블_손님_수_변경_요청(orderTable.getId(), orderTable);
+                = 주문_테이블_손님_수_변경_요청(주문_테이블_1.getId(), 주문_테이블_1);
 
         // then
         assertEquals(2, 주문_테이블_손님_수_변경_요청_응답.as(OrderTable.class).getNumberOfGuests());
     }
 
     /**
-     * Given 주문 테이블 1건을 등록한 후
      * When 주문 테이블의 손님 수를 음수로 변경하면
      * Then 오류가 발생한다.
      */
     @DisplayName("주문 테이블의 손님 수를 음수로 변경")
     @Test
     void changeNumberOfGuestsToNegativeNumber() {
-        // given
-        OrderTable orderTable = 주문_테이블_생성_요청(3, false).as(OrderTable.class);
-        orderTable.setNumberOfGuests(-2);
-
         // when
+        주문_테이블_1.setNumberOfGuests(-2);
         ExtractableResponse<Response> 주문_테이블_손님_수_변경_요청_응답
-                = 주문_테이블_손님_수_변경_요청(orderTable.getId(), orderTable);
+                = 주문_테이블_손님_수_변경_요청(주문_테이블_1.getId(), 주문_테이블_1);
 
         // then
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), 주문_테이블_손님_수_변경_요청_응답.statusCode());
     }
 
     /**
-     * Given 주문 테이블 1건을 등록한 후
      * When 생성한 주문 테이블이 아닌 임의의 주문 테이블 ID 를 전송하면
      * Then 오류가 발생한다.
      */
     @DisplayName("주문 테이블의 손님 수를 음수로 변경")
     @Test
     void changeNumberOfGuestsWithInvalidOrderTableId() {
-        // given
-        OrderTable orderTable = 주문_테이블_생성_요청(3, false).as(OrderTable.class);
-
         // when
         ExtractableResponse<Response> 주문_테이블_손님_수_변경_요청_응답
-                = 주문_테이블_손님_수_변경_요청(100L, orderTable);
+                = 주문_테이블_손님_수_변경_요청(100L, 주문_테이블_1);
 
         // then
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), 주문_테이블_손님_수_변경_요청_응답.statusCode());
     }
 
     /**
-     * Given 주문 테이블 2건을 등록하고 단체 지정을 생성한 후
-     * When empty 로 상태를 변경하면
+     * Given 주문 테이블 2건을 등록하고
+     *       And 단체로 지정한 후
+     * When 빈 테이블로 상태를 변경하면
      * Then 오류가 발생한다.
      */
     @DisplayName("단체 지정 정보가 등록된 주문 테이블 상태 변경")
     @Test
     void changeEmptyOfNotEmptyTableGroupId() {
         // given
-        OrderTable 주문_테이블_1 = 주문_테이블_생성_요청(1L, 3, true).as(OrderTable.class);
-        OrderTable 주문_테이블_2 = 주문_테이블_생성_요청(2L, 2, true).as(OrderTable.class);
-        단체_지정_요청(Arrays.asList(주문_테이블_1, 주문_테이블_2));
+        OrderTable 주문_테이블_3 = 주문_테이블_생성_요청(1L, 3, true).as(OrderTable.class);
+        OrderTable 주문_테이블_4 = 주문_테이블_생성_요청(2L, 2, true).as(OrderTable.class);
+        단체_지정_요청(Arrays.asList(주문_테이블_3, 주문_테이블_4));
 
         // when
         ExtractableResponse<Response> 주문_테이블_빈_테이블로_변경_요청_응답
-                = 주문_테이블_빈_테이블로_변경_요청(주문_테이블_1.getId(), 주문_테이블_1);
+                = 주문_테이블_빈_테이블로_변경_요청(주문_테이블_3.getId(), 주문_테이블_3);
 
         // then
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), 주문_테이블_빈_테이블로_변경_요청_응답.statusCode());
     }
 
     /**
-     * Given 주문을 생성한 후 주문의 상태를 MEAL 로 변경하고
+     * Given 주문의 상태를 MEAL 로 변경하고
      * When 주문에 해당하는 주문 테이블을 빈 테이블로 변경할 경우
      * Then 오류가 발생한다
      */
@@ -184,17 +171,8 @@ class OrderTableAcceptanceTest extends BaseAcceptanceTest {
     @Test
     void changeMealStatusTableToEmptyStatus() {
         // given
-        OrderTable 주문_테이블_1 = 주문_테이블_생성_요청(1L, 3, true).as(OrderTable.class);
-        OrderTable 주문_테이블_2 = 주문_테이블_생성_요청(2L,2, true).as(OrderTable.class);
-        TableGroup 단체 = 단체_지정_요청(Arrays.asList(주문_테이블_1, 주문_테이블_2)).as(TableGroup.class);
+        메뉴_테스트_데이터_생성();
 
-        MenuGroup 메뉴_그룹 = 메뉴_그룹_생성_요청("신메뉴").as(MenuGroup.class);
-        Product 상품 = 상품_생성_요청("녹두빈대떡", new BigDecimal(7000)).as(Product.class);
-        MenuProduct 메뉴_상품 = new MenuProduct();
-        메뉴_상품.setProductId(상품.getId());
-        메뉴_상품.setQuantity(1);
-
-        Menu 메뉴 = 메뉴_생성_요청("녹두빈대떡", new BigDecimal(7000), 메뉴_그룹.getId(), Arrays.asList(메뉴_상품)).as(Menu.class);
         OrderLineItem 주문_항목 = new OrderLineItem();
         주문_항목.setMenuId(메뉴.getId());
         주문_항목.setQuantity(1);
@@ -208,6 +186,16 @@ class OrderTableAcceptanceTest extends BaseAcceptanceTest {
 
         // then
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), 주문_테이블_빈_테이블로_변경_요청_응답.statusCode());
+    }
+
+    private void 메뉴_테스트_데이터_생성() {
+        MenuGroup 메뉴_그룹 = 메뉴_그룹_생성_요청("신메뉴").as(MenuGroup.class);
+        Product 상품 = 상품_생성_요청("녹두빈대떡", new BigDecimal(7000)).as(Product.class);
+        MenuProduct 메뉴_상품 = new MenuProduct();
+        메뉴_상품.setProductId(상품.getId());
+        메뉴_상품.setQuantity(1);
+
+        메뉴 = 메뉴_생성_요청("녹두빈대떡", new BigDecimal(7000), 메뉴_그룹.getId(), Arrays.asList(메뉴_상품)).as(Menu.class);
     }
 
     public static ExtractableResponse<Response> 주문_테이블_목록_조회_요청() {
