7567624ed86ba435edf2dafa438eebf9c94308af
gunan <power4889@gmail.com>
2022-07-05 20:49:39 +0900

    refactor: MenuGroup 컨트롤러에서 도메인 제거

--- src/main/java/kitchenpos/menu_group/application/MenuGroupService.java
+++ src/main/java/kitchenpos/menu_group/application/MenuGroupService.java
@@ -1,11 +1,15 @@
 package kitchenpos.menu_group.application;
 
 import kitchenpos.menu_group.domain.MenuGroup;
+import kitchenpos.menu_group.dto.MenuGroupRequest;
+import kitchenpos.menu_group.dto.MenuGroupResponse;
 import kitchenpos.menu_group.repository.MenuGroupRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class MenuGroupService {
@@ -16,12 +20,15 @@ public class MenuGroupService {
     }
 
     @Transactional
-    public MenuGroup create(final MenuGroup menuGroup) {
-        return menuGroupRepository.save(menuGroup);
+    public MenuGroupResponse create(final MenuGroupRequest menuGroup) {
+        MenuGroup savedMenuGroup = menuGroupRepository.save(menuGroup.toEntity());
+        return new MenuGroupResponse(savedMenuGroup);
     }
 
     @Transactional(readOnly = true)
-    public List<MenuGroup> list() {
-        return menuGroupRepository.findAll();
+    public List<MenuGroupResponse> list() {
+        return menuGroupRepository.findAll().stream()
+                .map(MenuGroupResponse::new)
+                .collect(toList());
     }
 }
--- src/main/java/kitchenpos/menu_group/ui/MenuGroupRestController.java
+++ src/main/java/kitchenpos/menu_group/ui/MenuGroupRestController.java
@@ -1,7 +1,8 @@
 package kitchenpos.menu_group.ui;
 
 import kitchenpos.menu_group.application.MenuGroupService;
-import kitchenpos.menu_group.domain.MenuGroup;
+import kitchenpos.menu_group.dto.MenuGroupRequest;
+import kitchenpos.menu_group.dto.MenuGroupResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,59 +22,17 @@ public class MenuGroupRestController {
 
     @PostMapping("/api/menu-groups")
     public ResponseEntity<MenuGroupResponse> create(@RequestBody final MenuGroupRequest request) {
-        final MenuGroup created = menuGroupService.create(request.toEntity());
+        final MenuGroupResponse created = menuGroupService.create(request);
         final URI uri = URI.create("/api/menu-groups/" + created.getId());
         return ResponseEntity.created(uri)
-                .body(new MenuGroupResponse(created))
+                .body(created)
                 ;
     }
 
     @GetMapping("/api/menu-groups")
-    public ResponseEntity<List<MenuGroup>> list() {
+    public ResponseEntity<List<MenuGroupResponse>> list() {
         return ResponseEntity.ok()
                 .body(menuGroupService.list())
                 ;
-    }
-
-    static class MenuGroupRequest {
-        private String name;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public MenuGroup toEntity() {
-            return new MenuGroup(name);
-        }
-    }
-
-    static class MenuGroupResponse {
-        private Long id;
-        private String name;
-
-        public MenuGroupResponse(MenuGroup created) {
-            this.id = created.getId();
-            this.name = created.getName();
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
     }
 }
