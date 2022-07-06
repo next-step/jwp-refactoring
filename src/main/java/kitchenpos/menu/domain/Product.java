def272e38f95de050276cb9d04a29392113923b1
Philip <pch4623@gmail.com>
2022-07-06 19:34:34 +0900

    feat: 상품 엔티티 클래스로 전환

--- src/main/java/kitchenpos/menu/domain/Product.java
+++ src/main/java/kitchenpos/menu/domain/Product.java
@@ -2,6 +2,7 @@ package kitchenpos.menu.domain;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Table(name = "product")
 @Entity
@@ -13,15 +14,16 @@ public class Product {
     @Column(name = "name", nullable = false)
     private String name;
 
-    private BigDecimal price;
+    @Embedded
+    private Price price;
 
-    public Product() {
+    protected Product() {
     }
 
-    public Product(Long id, String name, BigDecimal price) {
+    public Product (Long id, String name, BigDecimal price) {
         this.id = id;
         this.name = name;
-        this.price = price;
+        this.price = Price.from(price);
     }
 
     public Product(String name, BigDecimal price) {
@@ -32,23 +34,24 @@ public class Product {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
-        return price;
+        return price.getValue();
     }
 
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
