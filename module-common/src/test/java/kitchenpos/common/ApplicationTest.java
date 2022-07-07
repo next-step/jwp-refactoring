00d39c26837337e0bf2f229a880615399c65680e
14km <k.connor614@gmail.com>
2022-07-08 01:36:46 +0900

    feat: 멀티모듈 설정 변경

---
+++ module-common/src/test/java/kitchenpos/common/ApplicationTest.java
@@ -0,0 +1,11 @@
+package kitchenpos.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
