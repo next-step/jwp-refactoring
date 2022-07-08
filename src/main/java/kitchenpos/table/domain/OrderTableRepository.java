7115a6b7629a5a462183c5e4513b175128adf94c
Philip <pch4623@gmail.com>
2022-07-08 22:43:16 +0900

    feat: 불필요한 메서드 삭제

--- src/main/java/kitchenpos/table/domain/OrderTableRepository.java
+++ src/main/java/kitchenpos/table/domain/OrderTableRepository.java
@@ -6,6 +6,4 @@ import java.util.List;
 
 public interface OrderTableRepository extends JpaRepository<OrderTable, Long> {
     List<OrderTable> findAllByIdIn(List<Long> orderTableIds);
-
-    List<OrderTable> findAllByTableGroupId(Long tableGroupId);
 }
