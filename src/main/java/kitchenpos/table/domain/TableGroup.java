1324e29003aae2c943ef93bdeedf6942732bafe9
Philip <pch4623@gmail.com>
2022-07-08 22:42:49 +0900

    feat: 단체 지정 예외 케이스 추가

--- src/main/java/kitchenpos/table/domain/TableGroup.java
+++ src/main/java/kitchenpos/table/domain/TableGroup.java
@@ -54,6 +54,9 @@ public class TableGroup {
         if (!orderTable.isEmptyTableGroup()) {
             throw new IllegalStateException("이미 단체 지정이 되어있는 테이블은 단체 지정을 할 수 없습니다.");
         }
+        if (!orderTable.isEmpty()) {
+            throw new IllegalStateException("비어있지 않은 테이블은 단체 지정을 할 수 없습니다.");
+        }
     }
 
     public void ungroup() {
