1ec2bf46a622d10f66777e36170590b39d7da11e
DESKTOP-IMQ723M\SKMG <lcjltj@gmail.com>
2022-07-08 18:03:05 +0900

    refactor : MenuProduct 연관 관계 제거 및 MenuValidator 추가

--- src/test/java/kitchenpos/menu/validator/MenuValidatorTest.java
+++ src/test/java/kitchenpos/menu/validator/MenuValidatorTest.java
@@ -2,77 +2,42 @@ package kitchenpos.menu.validator;
 
 import kitchenpos.menu.dto.CreateMenuRequest;
 import kitchenpos.menu.dto.MenuProductRequest;
-import kitchenpos.menu.exception.MenuException;
 import kitchenpos.product.domain.Product;
 import kitchenpos.product.domain.ProductRepository;
-import kitchenpos.product.exception.ProductException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
 public class MenuValidatorTest {
-    @Mock
+    @Autowired
     ProductRepository productRepository;
-    @InjectMocks
+    @Autowired
     MenuValidator menuValidator;
+
+    private Product 데리버거;
+    private Product 새우버거;
 
     @BeforeEach
     void setUp() {
+        데리버거 = productRepository.save(Product.of("데리버거", BigDecimal.valueOf(1_000)));
+        새우버거 = productRepository.save(Product.of("새우버거", BigDecimal.valueOf(1_000)));
+
     }
 
     @Test
-    @DisplayName("메뉴 상품 validation 정상")
     void validate() {
         // given
-        final Product 불고기버거 = new Product(1L, "불고기버거", BigDecimal.valueOf(1_500));
-        final Product 새우버거 = new Product(2L, "새우버거", BigDecimal.valueOf(2_000));
         final CreateMenuRequest 햄버거메뉴 = new CreateMenuRequest("햄버거메뉴", BigDecimal.valueOf(1_500), 1L,
-                Arrays.asList(new MenuProductRequest(불고기버거.getId(), 1), (new MenuProductRequest(새우버거.getId(), 1))));
-        given(productRepository.findAllById(any()))
-                .willReturn(Arrays.asList(불고기버거, 새우버거));
-        // when
+                Arrays.asList(new MenuProductRequest(데리버거.getId(), 1), (new MenuProductRequest(새우버거.getId(), 1))));
+        // then
         menuValidator.validate(햄버거메뉴);
-    }
 
-    @Test
-    @DisplayName("메뉴 상품 개수 상이 오류")
-    void exception1() {
-        // given
-        final Product 불고기버거 = new Product(1L, "불고기버거", BigDecimal.valueOf(1_500));
-        final Product 새우버거 = new Product(2L, "새우버거", BigDecimal.valueOf(2_000));
-        final CreateMenuRequest 햄버거메뉴 = new CreateMenuRequest("햄버거메뉴", BigDecimal.valueOf(1_500), 1L,
-                Arrays.asList(new MenuProductRequest(불고기버거.getId(), 1)));
-        given(productRepository.findAllById(any()))
-                .willReturn(Arrays.asList(불고기버거, 새우버거));
-        // when
-        assertThatThrownBy(() -> menuValidator.validate(햄버거메뉴))
-                .isInstanceOf(ProductException.class);
-    }
-
-    @Test
-    @DisplayName("메뉴 상품 개수 금액 초과 오류")
-    void exception2() {
-        // given
-        final Product 불고기버거 = new Product(1L, "불고기버거", BigDecimal.valueOf(1_500));
-        final Product 새우버거 = new Product(2L, "새우버거", BigDecimal.valueOf(2_000));
-        final CreateMenuRequest 햄버거메뉴 = new CreateMenuRequest("햄버거메뉴", BigDecimal.valueOf(5_500), 1L,
-                Arrays.asList(new MenuProductRequest(불고기버거.getId(), 1), (new MenuProductRequest(새우버거.getId(), 1))));
-        given(productRepository.findAllById(any()))
-                .willReturn(Arrays.asList(불고기버거, 새우버거));
-        // when
-        assertThatThrownBy(() -> menuValidator.validate(햄버거메뉴))
-                .isInstanceOf(MenuException.class);
     }
 }
