ec4b556d0d0550504dabf429d61eb3bf36b7702b
Philip <pch4623@gmail.com>
2022-07-08 20:23:32 +0900

    refactor: 주문 테이블 관련 도메인 리팩토링

--- src/main/java/kitchenpos/table/domain/OrderTable.java
+++ src/main/java/kitchenpos/table/domain/OrderTable.java
@@ -1,54 +1,97 @@
 package kitchenpos.table.domain;
 
+import org.springframework.util.ObjectUtils;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Table(name = "order_table")
+@Entity
 public class OrderTable {
+    private static final int MIN_NUMBER_OF_GUESTS = 0;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long tableGroupId;
+
+    @ManyToOne
+    @JoinColumn(name = "table_group_id")
+    private TableGroup tableGroup = new TableGroup();
+
     private int numberOfGuests;
+
     private boolean empty;
 
-    public OrderTable() {
+    protected OrderTable() {
     }
 
-    public OrderTable(Long id, Long tableGroupId, int numberOfGuests, boolean empty) {
+    public OrderTable(final int numberOfGuests, final boolean empty) {
+        this(null, numberOfGuests, empty);
+    }
+
+    public OrderTable(final Long id, final int numberOfGuests, final boolean empty) {
+        this(id, null, numberOfGuests, empty);
+    }
+
+    public OrderTable(final Long id, final TableGroup tableGroup,  final int numberOfGuests, final boolean empty) {
         this.id = id;
-        this.tableGroupId = tableGroupId;
+        this.tableGroup = tableGroup;
         this.numberOfGuests = numberOfGuests;
         this.empty = empty;
     }
 
-    public OrderTable(int numberOfGuests, boolean empty) {
-        this(null, null,numberOfGuests, empty);
+    public void addedBy(final TableGroup tableGroup) {
+        this.tableGroup = tableGroup;
+    }
+
+    public void changeEmpty(final boolean empty) {
+        if (!isEmptyTableGroup()) {
+            throw new IllegalStateException("단체 지정이 된 테이블은 빈 테이블 여부를 변경할 수 없습니다.");
+        }
+        this.empty = empty;
+    }
+
+    public void changeNumberOfGuests(final int numberOfGuests) {
+        if (numberOfGuests < MIN_NUMBER_OF_GUESTS) {
+            throw new IllegalArgumentException(String.format("손님 수는 최소 %d명 이상이어야 합니다.", MIN_NUMBER_OF_GUESTS));
+        }
+        if (isEmpty()) {
+            throw new IllegalStateException("빈 테이블의 손님 수는 변경할 수 없습니다.");
+        }
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public boolean isEmptyTableGroup() {
+        return ObjectUtils.isEmpty(tableGroup);
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getTableGroupId() {
-        return tableGroupId;
-    }
-
-    public void setTableGroupId(final Long tableGroupId) {
-        this.tableGroupId = tableGroupId;
+    public TableGroup getTableGroup() {
+        return tableGroup;
     }
 
     public int getNumberOfGuests() {
         return numberOfGuests;
     }
 
-    public void setNumberOfGuests(final int numberOfGuests) {
-        this.numberOfGuests = numberOfGuests;
-    }
-
     public boolean isEmpty() {
         return empty;
     }
 
-    public void setEmpty(final boolean empty) {
-        this.empty = empty;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderTable that = (OrderTable) o;
+        return Objects.equals(id, that.id)
+                && Objects.equals(tableGroup, that.tableGroup);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, tableGroup);
     }
 }
--- src/main/java/kitchenpos/table/domain/TableGroup.java
+++ src/main/java/kitchenpos/table/domain/TableGroup.java
@@ -1,47 +1,91 @@
 package kitchenpos.table.domain;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
+@Table(name = "table_group")
+@Entity
 public class TableGroup {
+    private static final int MIN_ORDER_TABLE_SIZE = 2;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private LocalDateTime createdDate;
-    private List<OrderTable> orderTables;
 
-    public TableGroup() {
+    @Embedded
+    private OrderTables orderTables = new OrderTables();
+
+    protected TableGroup() {
     }
 
-    public TableGroup(Long id, LocalDateTime createdDate, List<OrderTable> orderTables) {
+    public TableGroup(final List<OrderTable> orderTables) {
+        this(null, orderTables);
+    }
+
+    public TableGroup(final Long id, final List<OrderTable> orderTables) {
+        validateOrderTablesSize(orderTables);
         this.id = id;
-        this.createdDate = createdDate;
-        this.orderTables = orderTables;
+        orderTables.forEach(this::addOrderTable);
     }
 
-    public TableGroup(List<OrderTable> orderTables) {
-        this(null, null, orderTables);
+    private void validateOrderTablesSize(final List<OrderTable> orderTables) {
+        if (orderTables.size() < MIN_ORDER_TABLE_SIZE) {
+            throw new IllegalArgumentException(String.format("단체 지정시 테이블은 최소 %d개 이상이어야 합니다.", MIN_ORDER_TABLE_SIZE));
+        }
+    }
+
+    private void addOrderTable(final OrderTable orderTable) {
+        validateOrderTable(orderTable);
+        this.createdDate = LocalDateTime.now();
+        this.orderTables.add(orderTable);
+        orderTable.addedBy(this);
+    }
+
+    private void validateOrderTable(final OrderTable orderTable) {
+        if (!orderTable.isEmptyTableGroup()) {
+            throw new IllegalStateException("이미 단체 지정이 되어있는 테이블은 단체 지정을 할 수 없습니다.");
+        }
+    }
+
+    public void ungroup() {
+        orderTables.ungroup();
+    }
+
+    public List<Long> getOrderTableIds() {
+        return orderTables.getOrderTableIds();
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public LocalDateTime getCreatedDate() {
         return createdDate;
     }
 
-    public void setCreatedDate(final LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
     public List<OrderTable> getOrderTables() {
-        return orderTables;
+        return orderTables.getOrderTables();
     }
 
-    public void setOrderTables(final List<OrderTable> orderTables) {
-        this.orderTables = orderTables;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TableGroup that = (TableGroup) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
