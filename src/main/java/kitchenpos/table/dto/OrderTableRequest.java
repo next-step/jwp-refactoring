5f8dbe6da45c4647db7b78d926a26b55f053c99f
Philip <pch4623@gmail.com>
2022-07-08 20:21:22 +0900

    feat: 주문테이블 요청 DTO 추가

--- src/main/java/kitchenpos/table/dto/OrderTableRequest.java
+++ src/main/java/kitchenpos/table/dto/OrderTableRequest.java
@@ -6,6 +6,14 @@ public class OrderTableRequest {
 
     public OrderTableRequest() {}
 
+    public OrderTableRequest(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public OrderTableRequest(boolean empty) {
+        this.empty = empty;
+    }
+
     public OrderTableRequest(final int numberOfGuests, final boolean empty) {
         this.numberOfGuests = numberOfGuests;
         this.empty = empty;
