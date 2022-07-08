7d2b09a98ac7381b7a59a11d5b5fbe31f52fef51
Philip <pch4623@gmail.com>
2022-07-08 18:41:26 +0900

    refactor: 컨틀롤러 요청 및 응답 DTO로 변경

--- src/main/java/kitchenpos/order/ui/OrderRestController.java
+++ src/main/java/kitchenpos/order/ui/OrderRestController.java
@@ -1,13 +1,15 @@
 package kitchenpos.order.ui;
 
 import kitchenpos.order.application.OrderService;
-import kitchenpos.order.domain.Order;
+import kitchenpos.order.dto.OrderRequest;
+import kitchenpos.order.dto.OrderResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
 
+@RequestMapping("/api/orders")
 @RestController
 public class OrderRestController {
     private final OrderService orderService;
@@ -16,27 +18,19 @@ public class OrderRestController {
         this.orderService = orderService;
     }
 
-    @PostMapping("/api/orders")
-    public ResponseEntity<Order> create(@RequestBody final Order order) {
-        final Order created = orderService.create(order);
-        final URI uri = URI.create("/api/orders/" + created.getId());
-        return ResponseEntity.created(uri)
-                .body(created)
-                ;
+    @PostMapping
+    public ResponseEntity<OrderResponse> create(@RequestBody final OrderRequest orderRequest) {
+        final OrderResponse order = orderService.create(orderRequest);
+        return ResponseEntity.created(URI.create("/api/orders/" + order.getId())).body(order);
     }
 
-    @GetMapping("/api/orders")
-    public ResponseEntity<List<Order>> list() {
-        return ResponseEntity.ok()
-                .body(orderService.list())
-                ;
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> list() {
+        return ResponseEntity.ok().body(orderService.list());
     }
 
-    @PutMapping("/api/orders/{orderId}/order-status")
-    public ResponseEntity<Order> changeOrderStatus(
-            @PathVariable final Long orderId,
-            @RequestBody final Order order
-    ) {
-        return ResponseEntity.ok(orderService.changeOrderStatus(orderId, order));
+    @PutMapping("/{orderId}/order-status")
+    public ResponseEntity<OrderResponse> changeOrderStatus(@PathVariable final Long orderId, @RequestBody final OrderRequest orderRequest) {
+        return ResponseEntity.ok(orderService.changeOrderStatus(orderId, orderRequest));
     }
 }
