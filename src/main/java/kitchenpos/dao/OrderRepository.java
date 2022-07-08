7f0b47fc6118558517e109fc9da3f7f775b5f479
tasklet1579 <tasklet1579@gmail.com>
2022-07-08 22:41:13 +0900

    style : 주문 조회 파라미터 변경

--- src/main/java/kitchenpos/dao/OrderRepository.java
+++ src/main/java/kitchenpos/dao/OrderRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByOrderTableId(OrderTable orderTable);
+    List<Order> findByOrderTableId(Long id);
 }
