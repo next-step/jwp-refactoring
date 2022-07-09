7904fa070fb835266ded4cb42a752941f980a99b
kms <kmmin78@gmail.com>
2022-07-09 18:53:30 +0900

    refactor step4 멀티모듈 구조로 변경

---
+++ module-main/src/test/java/kitchenpos/main/ApplicationTest.java
@@ -0,0 +1,11 @@
+package kitchenpos.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
