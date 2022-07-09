f7d689b9f90520ce17e6646642b39be0f2d738ee
tasklet1579 <tasklet1579@gmail.com>
2022-07-10 04:32:37 +0900

    remove : 기존 메뉴 파일 삭제

--- src/main/java/kitchenpos/dao/MenuProductRepository.java
+++
@@ -1,9 +0,0 @@
-package kitchenpos.dao;
-
-import kitchenpos.domain.MenuProduct;
-import kitchenpos.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuProductRepository extends JpaRepository<MenuProduct, Long> {
-    boolean existsByProduct(Product product);
-}
