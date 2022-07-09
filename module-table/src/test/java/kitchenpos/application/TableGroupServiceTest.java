67e1fc6e57572ea3aa47c13aa56b5e13db6fc43b
lcjltj <lcjltj@gmail.com>
2022-07-09 23:26:19 +0900

    test: 모듈 분리로 인한 서비스 테스트 수정

--- module-table/src/test/java/kitchenpos/application/TableGroupServiceTest.java
+++ module-table/src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -1,82 +1,71 @@
-//package kichenpos.application;
-//
-//import kitchenpos.application.TableGroupService;
-//import kitchenpos.domain.OrderTable;
-//import kitchenpos.domain.TableGroup;
-//import kitchenpos.domain.repository.OrderTableRepository;
-//import kitchenpos.domain.repository.TableGroupRepository;
-//import kitchenpos.dto.TableGroupRequest;
-//import kitchenpos.dto.TableGroupResponse;
-//import kitchenpos.menu.domain.MenuTest;
-//import kitchenpos.order.domain.Order;
-//import kitchenpos.order.domain.OrderLineItem;
-//import kitchenpos.order.domain.OrderStatus;
-//import kitchenpos.table.domain.OrderTable;
-//import kitchenpos.table.domain.TableGroup;
-//import kitchenpos.table.domain.repository.OrderTableRepository;
-//import kitchenpos.table.domain.repository.TableGroupRepository;
-//import kitchenpos.table.dto.TableGroupRequest;
-//import kitchenpos.table.dto.TableGroupResponse;
-//import kitchenpos.table.validator.OrderTableValidator;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import java.util.Arrays;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.doNothing;
-//
-//@ExtendWith(MockitoExtension.class)
-//class TableGroupServiceTest {
-//    @Mock
-//    private OrderTableRepository orderTableRepository;
-//    @Mock
-//    private TableGroupRepository tableGroupRepository;
-//    @InjectMocks
-//    TableGroupService tableGroupService;
-//
-//    @Test
-//    @DisplayName("테이블 병합")
-//    void create() {
-//        // given
-//        OrderTable 일번_테이블 = OrderTable.of(0, true);
-//        OrderTable 이번_테이블 = OrderTable.of(0, true);
-//        given(orderTableRepository.findAllById(any()))
-//                .willReturn(Arrays.asList(일번_테이블, 이번_테이블));
-//        given(tableGroupRepository.save(any()))
-//                .willReturn(TableGroup.of(Arrays.asList(일번_테이블, 이번_테이블), 2));
-//
-//        // when
-//        final TableGroupResponse tableGroup = tableGroupService.create(new TableGroupRequest(Arrays.asList(일번_테이블, 이번_테이블)));
-//        // then
-//        assertThat(tableGroup)
-//                .isInstanceOf(TableGroupResponse.class);
-//    }
-//
-//    @Test
-//    @DisplayName("테이블 분리")
-//    void ungroup() {
-//        // given
-//        final OrderTable 일번_테이블 = OrderTable.of(0, true);
-//        final OrderTable 이번_테이블 = OrderTable.of(0, true);
-//        final Order order = Order.of(일번_테이블.getId(), Arrays.asList(OrderLineItem.of(MenuTest.햄버거메뉴, 1L)));
-//        order.updateOrderStatus(OrderStatus.COMPLETION);
-//
-//        given(orderTableRepository.findAllById(any()))
-//                .willReturn(Arrays.asList(일번_테이블, 이번_테이블));
-//        given(tableGroupRepository.findById(any()))
-//                .willReturn(Optional.of(TableGroup.of(Arrays.asList(일번_테이블, 이번_테이블), 2)));
-//        doNothing().when(orderTableValidator).validateTableSeparate(any());
-//        // when
-//        tableGroupService.ungroup(1L);
-//        // then
-//        assertThat(일번_테이블.getTableGroupId()).isNull();
-//    }
-//}
+package kichenpos.application;
+
+import kitchenpos.application.TableGroupService;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
+import kitchenpos.domain.repository.OrderTableRepository;
+import kitchenpos.domain.repository.TableGroupRepository;
+import kitchenpos.dto.TableGroupRequest;
+import kitchenpos.dto.TableGroupResponse;
+import kitchenpos.validator.OrderStatusValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+
+@ExtendWith(MockitoExtension.class)
+class TableGroupServiceTest {
+    @Mock
+    private OrderTableRepository orderTableRepository;
+    @Mock
+    private TableGroupRepository tableGroupRepository;
+    @Mock
+    OrderStatusValidator orderStatusValidator;
+    @InjectMocks
+    TableGroupService tableGroupService;
+
+    @Test
+    @DisplayName("테이블 병합")
+    void create() {
+        // given
+        OrderTable 일번_테이블 = OrderTable.of(0, true);
+        OrderTable 이번_테이블 = OrderTable.of(0, true);
+        given(orderTableRepository.findAllById(any()))
+                .willReturn(Arrays.asList(일번_테이블, 이번_테이블));
+        given(tableGroupRepository.save(any()))
+                .willReturn(TableGroup.of(Arrays.asList(일번_테이블, 이번_테이블), 2));
+
+        // when
+        final TableGroupResponse tableGroup = tableGroupService.create(new TableGroupRequest(Arrays.asList(일번_테이블, 이번_테이블)));
+        // then
+        assertThat(tableGroup)
+                .isInstanceOf(TableGroupResponse.class);
+    }
+
+    @Test
+    @DisplayName("테이블 분리")
+    void ungroup() {
+        // given
+        final OrderTable 일번_테이블 = OrderTable.of(0, true);
+        final OrderTable 이번_테이블 = OrderTable.of(0, true);
+        given(orderTableRepository.findAllById(any()))
+                .willReturn(Arrays.asList(일번_테이블, 이번_테이블));
+        given(tableGroupRepository.findById(any()))
+                .willReturn(Optional.of(TableGroup.of(Arrays.asList(일번_테이블, 이번_테이블), 2)));
+        doNothing().when(orderStatusValidator).validateTableSeparate(any());
+        // when
+        tableGroupService.ungroup(1L);
+        // then
+        assertThat(일번_테이블.getTableGroupId()).isNull();
+    }
+}
