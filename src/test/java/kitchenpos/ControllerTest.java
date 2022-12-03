4a36d677a5d2803a406ebb7b2194efc7bae4d321
jisu1211 <dbdnzl1211@gmail.com>
2022-12-04 01:50:09 +0900

    test: Controller 테스트 공통 부분 추출

---
+++ src/test/java/kitchenpos/ControllerTest.java
@@ -0,0 +1,29 @@
+package kitchenpos;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+public class ControllerTest {
+    @Autowired
+    protected MockMvc webMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    protected ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    public void setUp() {
+        this.webMvc = MockMvcBuilders.webAppContextSetup(context)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+    }
+}
