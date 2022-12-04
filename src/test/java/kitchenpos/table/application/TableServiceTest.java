8e7980d612c607f367409a6d1d141121b3217b00
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-04 13:13:29 +0900

    test(TableServiceTest.java) : 주문테이블이 없을 경우 손님수를 변경할 수 없다.

--- src/test/java/kitchenpos/table/application/TableServiceTest.java
+++ src/test/java/kitchenpos/table/application/TableServiceTest.java
@@ -42,4 +42,12 @@ class TableServiceTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(CHANGE_NUMBER_OF_GUESTS_MINIMUM_NUMBER_EXCEPTION_MESSAGE);
     }
+
+    @DisplayName("손님수를 변경한다. / 주문테이블이 없을 수 없다.")
+    @Test
+    void changeNumberOfGuests_fail_notExistOrderTable() {
+        ChangeNumberOfGuestsRequest changeNumberOfGuestsRequest = new ChangeNumberOfGuestsRequest(1);
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(100L, changeNumberOfGuestsRequest))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
