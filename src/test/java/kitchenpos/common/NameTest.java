df5b1a107092f95f78af509c541c6c73b49cdd97
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-06 21:11:29 +0900

    test(NameTest.java) : 이름을 생성한다.

--- src/test/java/kitchenpos/common/NameTest.java
+++ src/test/java/kitchenpos/common/NameTest.java
@@ -1,19 +1,22 @@
 package kitchenpos.common;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static kitchenpos.common.Name.NAME_NOT_EMPTY_EXCEPTION_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("이름")
 class NameTest {
 
     @DisplayName("이름을 생성한다.")
-    @Test
-    void constructor() {
+    @ParameterizedTest
+    @ValueSource(strings = "name")
+    void constructor(String name) {
+        assertThatNoException().isThrownBy(() -> new Name(name));
     }
 
     @DisplayName("이름은 null 일 수 없다. / 이름은 공백일 수 없다.")
