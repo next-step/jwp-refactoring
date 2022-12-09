de906995a4b7c51592f8ce4e28224fb09c9bf1f7
이동길 <akxkdldhtm@gmail.com>
2022-12-09 22:07:31 +0900

    test: 메뉴 테스트 작성

--- src/main/java/kitchenpos/domain/MenuProduct.java
+++ src/main/java/kitchenpos/domain/MenuProduct.java
@@ -1,10 +1,27 @@
 package kitchenpos.domain;
 
+import java.util.Objects;
+
 public class MenuProduct {
     private Long seq;
     private Long menuId;
     private Long productId;
     private long quantity;
+
+    public MenuProduct(Long seq, Long menuId, Long productId, long quantity) {
+        this.seq = seq;
+        this.menuId = menuId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public static MenuProduct of(Long menuId, Long productId, long quantity) {
+        return new MenuProduct(null, menuId, productId, quantity);
+    }
+
+    public static MenuProduct of(Long seq, Long menuId, Long productId, long quantity) {
+        return new MenuProduct(seq, menuId, productId, quantity);
+    }
 
     public Long getSeq() {
         return seq;
@@ -36,5 +53,18 @@ public class MenuProduct {
 
     public void setQuantity(final long quantity) {
         this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuProduct that = (MenuProduct) o;
+        return quantity == that.quantity && Objects.equals(seq, that.seq) && Objects.equals(menuId, that.menuId) && Objects.equals(productId, that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seq, menuId, productId, quantity);
     }
 }
