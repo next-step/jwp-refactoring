6fb0b0fc38dd8457411820d0820c094a5190f2af
jisu1211 <dbdnzl1211@gmail.com>
2022-12-10 04:20:26 +0900

    refactor: TableGroupService, TableGroupRestController 리팩터링 - TableGroupRepository 추가, OrderTableRepository, OrderRepository 사용하도록 수정 - dto 추가
    test: OrderRespositoryTest, OrderTableRepositoryTest, TableGroupServiceTest, TableGroupRestControllerTest 추가

---
+++ src/main/java/kitchenpos/order/domain/TableGroupRepository.java
@@ -0,0 +1,8 @@
+package kitchenpos.order.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TableGroupRepository extends JpaRepository<TableGroup, Long> {
+}
---
+++ src/test/java/kitchenpos/order/domain/OrderTableRepositoryTest.java
@@ -0,0 +1,30 @@
+package kitchenpos.order.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("OrderTableRepository 테스트")
+@DataJpaTest
+class OrderTableRepositoryTest {
+    @Autowired
+    private OrderTableRepository orderTableRepository;
+
+    @Test
+    void 주문_테이블_Id_목록을_입력받아_주문_테이블_목록_조회() {
+        OrderTable 주문테이블1 = orderTableRepository.save(new OrderTable(5, false));
+        OrderTable 주문테이블2 = orderTableRepository.save(new OrderTable(2, false));
+        OrderTable 주문테이블3 = orderTableRepository.save(new OrderTable(3, false));
+
+        List<OrderTable> orderTables = orderTableRepository.findAllById(
+                Arrays.asList(주문테이블1.getId(), 주문테이블2.getId(), 주문테이블3.getId()));
+
+        assertThat(orderTables).hasSize(3);
+    }
+}
