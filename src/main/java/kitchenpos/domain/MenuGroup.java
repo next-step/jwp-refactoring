34c44fb3be5ef3d1357d713b3d62f3445db43444
ilmare-cbk <cbk104325@gmail.com>
2022-12-10 01:59:38 +0900

    feat(MenuGroupName.java): 메뉴 그룹 이름을 wrapping한 MenuGroupName 클래스 생성
    - 메뉴 그룹 이름에 대한 유효성 검사하는 테스트 작성

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -4,31 +4,23 @@ import java.util.Objects;
 
 public class MenuGroup {
     private Long id;
-    private String name;
+    private MenuGroupName name;
 
-    private MenuGroup(Long id, String name) {
+    private MenuGroup(Long id, MenuGroupName name) {
         this.id = id;
         this.name = name;
     }
 
     public static MenuGroup of(Long id, String name) {
-        return new MenuGroup(id, name);
+        return new MenuGroup(id, MenuGroupName.from(name));
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
+        return name.getName();
     }
 
     @Override
---
+++ src/main/java/kitchenpos/exception/ExceptionMessage.java
@@ -0,0 +1,7 @@
+package kitchenpos.exception;
+
+public class ExceptionMessage {
+    public static String INVALID_MENU_GROUP_NAME_SIZE = "메뉴 그룹 이름은 1자 이상이어야 합니다.";
+    private ExceptionMessage() {
+    }
+}
