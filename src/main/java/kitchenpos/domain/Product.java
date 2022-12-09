77841df25d198c97b3c086c927707f4b5bea0a78
이동길 <akxkdldhtm@gmail.com>
2022-12-09 21:23:56 +0900

    test: 상품 테스트 작성

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -1,11 +1,22 @@
 package kitchenpos.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+
+    private Product(long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public static Product of(long id, String name, BigDecimal price) {
+        return new Product(id,name,price);
+    }
 
     public Long getId() {
         return id;
@@ -29,5 +40,18 @@ public class Product {
 
     public void setPrice(final BigDecimal price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
     }
 }
