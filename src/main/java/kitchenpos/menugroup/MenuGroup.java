2b38eec36528d38b6d1e93407d8d79921e8ddba4
chunhodong <yhms4432@naver.com>
2022-12-11 00:54:14 +0900

    refactor: MenuGroup 빌더로 수정

---
+++ src/main/java/kitchenpos/menugroup/MenuGroup.java
@@ -0,0 +1,43 @@
+package kitchenpos.menugroup;
+
+public class MenuGroup {
+    private Long id;
+    private String name;
+
+    private MenuGroup(MenuGroupBuilder builder){
+        this.id = builder.id;
+        this.name = builder.name;
+    }
+
+    public static MenuGroupBuilder builder() {
+        return new MenuGroupBuilder();
+    }
+
+    public static class MenuGroupBuilder {
+        private Long id;
+        private String name;
+
+        public MenuGroupBuilder id(Long id){
+            this.id = id;
+            return this;
+        }
+
+        public MenuGroupBuilder name(String name){
+            this.name = name;
+            return this;
+        }
+
+        public MenuGroup build(){
+            return new MenuGroup(this);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
