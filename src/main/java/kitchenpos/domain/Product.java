a2f167d3cce935f2060a3c7177a8d83695460677
seonghyeoklee <dltjdgur327@gmail.com>
2022-12-10 18:12:49 +0900

    test : Product (상품) 관련 비즈니스 테스트 작성

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -7,6 +7,18 @@ public class Product {
     private String name;
     private BigDecimal price;
 
+    private Product() {}
+
+    private Product(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public static Product of(Long id, String name, BigDecimal price) {
+        return new Product(id, name, price);
+    }
+
     public Long getId() {
         return id;
     }
