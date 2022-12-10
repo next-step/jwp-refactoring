ee14157b33e3b024b066b52aaa0c27ca111598a6
sjk5766 <wjdrnr6662@gmail.com>
2022-12-11 06:38:27 +0900

    test(domain): OrderTables 테스트 코드 추가

---
+++ src/test/java/kitchenpos/domain/OrderTablesTest.java
@@ -0,0 +1,38 @@
+package kitchenpos.domain;
+
+import kitchenpos.common.domain.Price;
+import kitchenpos.ordertable.domain.NumberOfGuests;
+import kitchenpos.ordertable.domain.OrderTable;
+import kitchenpos.ordertable.domain.OrderTables;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class OrderTablesTest {
+    @DisplayName("주문 테이블 목록은 비어있을 수 없다.")
+    @Test
+    void orderTablesEmptyException() {
+        assertThatThrownBy(() -> new OrderTables(new ArrayList<>()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("주문 테이블 목록에 주문 테이블이 없습니다.");
+    }
+
+    @DisplayName("주문 테이블은 2개 이상이야 한다.")
+    @Test
+    void orderTablesLessThanMinimumException() {
+        // given
+        OrderTable orderTable = new OrderTable(new NumberOfGuests(4), false);
+
+        // when & then
+        assertThatThrownBy(() -> new OrderTables(Arrays.asList(orderTable)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("주문 테이블은 2개 이상 존재해야 합니다.");
+    }
+}
