76bad9752da9bf836c7f253390703d0d88cb440d
ilmare-cbk <cbk104325@gmail.com>
2022-12-10 17:57:09 +0900

    refactor(OrderLineItem.java): equals & hashcode 메소드 오버라이딩
    - 동등성에 대한 테스트 작성

--- src/main/java/kitchenpos/domain/OrderLineItem.java
+++ src/main/java/kitchenpos/domain/OrderLineItem.java
@@ -1,10 +1,27 @@
 package kitchenpos.domain;
 
+import java.util.Objects;
+
 public class OrderLineItem {
     private Long seq;
     private Long orderId;
     private Long menuId;
     private long quantity;
+
+    public OrderLineItem() {
+    }
+
+    public OrderLineItem(Long menuId, long quantity) {
+        this.menuId = menuId;
+        this.quantity = quantity;
+    }
+
+    public OrderLineItem(Long seq, Long orderId, Long menuId, long quantity) {
+        this.seq = seq;
+        this.orderId = orderId;
+        this.menuId = menuId;
+        this.quantity = quantity;
+    }
 
     public Long getSeq() {
         return seq;
@@ -36,5 +53,22 @@ public class OrderLineItem {
 
     public void setQuantity(final long quantity) {
         this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OrderLineItem that = (OrderLineItem) o;
+        return seq.equals(that.seq);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seq);
     }
 }
