6c41703122ca3e9754c4f658ed65f45a5f199635
earth-h <earth.h.183@gmail.com>
2022-12-11 01:50:29 +0900

    refactor(repository): 사용하지 않는 repository 제거

--- src/main/java/kitchenpos/menu/domain/MenuRepository.java
+++ src/main/java/kitchenpos/menu/domain/MenuRepository.java
@@ -1,6 +1,5 @@
 package kitchenpos.menu.domain;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
