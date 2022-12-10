fa1f03d6f7fe30124c8e9f9fce6a4bc4610a6272
ilmare-cbk <cbk104325@gmail.com>
2022-12-10 13:37:51 +0900

    refactor(TableGroup.java): equals & hashcode 메소드 오버라이딩
    - 동등성에 대한 테스트 작성

--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -2,6 +2,7 @@ package kitchenpos.domain;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 public class TableGroup {
     private Long id;
@@ -39,5 +40,22 @@ public class TableGroup {
 
     public void setOrderTables(final List<OrderTable> orderTables) {
         this.orderTables = orderTables;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TableGroup that = (TableGroup) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
