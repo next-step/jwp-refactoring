6407a5ba91a904645df90a8060d55537acb89d77
chunhodong <yhms4432@naver.com>
2022-12-10 21:22:51 +0900

    test: TableGroupRestControllerTest 추가

---
+++ src/test/java/kitchenpos/tablegroup/ui/TableGroupRestControllerTest.java
@@ -0,0 +1,94 @@
+package kitchenpos.tablegroup.ui;
+
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import kitchenpos.ControllerTest;
+import kitchenpos.application.TableGroupService;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
+import kitchenpos.ui.TableGroupRestController;
+import net.jqwik.api.Arbitraries;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(TableGroupRestController.class)
+public class TableGroupRestControllerTest extends ControllerTest {
+    @MockBean
+    private TableGroupService tableGroupService;
+
+    @DisplayName("테이블그룹 생성을 요청하면 생성된 테이블그룹응답")
+    @Test
+    public void returnTableGroup() throws Exception {
+        TableGroup tableGroup = getTableGroup();
+        String tableGroupCreated = tableGroup.getCreatedDate().toString();
+        doReturn(tableGroup).when(tableGroupService).create(any(TableGroup.class));
+
+        webMvc.perform(post("/api/table-groups")
+                        .content(mapper.writeValueAsString(new TableGroup()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(tableGroup.getId().intValue())))
+                .andExpect(jsonPath("$.createdDate", is(tableGroupCreated.substring(0, tableGroupCreated.length() - 2))))
+                .andExpect(jsonPath("$.orderTables", hasSize(tableGroup.getOrderTables().size())))
+                .andExpect(status().isCreated());
+    }
+
+    @DisplayName("테이블그룹 생성을 요청하면 실패응답")
+    @Test
+    public void throwsExceptionWhenTableGroupCreate() throws Exception {
+        doThrow(new IllegalArgumentException()).when(tableGroupService).create(any(TableGroup.class));
+
+        webMvc.perform(post("/api/table-groups")
+                        .content(mapper.writeValueAsString(new TableGroup()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @DisplayName("테이블그룹 해재를 요청하면 성공응답")
+    @Test
+    public void unTableGroup() throws Exception {
+        TableGroup tableGroup = getTableGroup();
+        webMvc.perform(delete("/api/table-groups/" + tableGroup.getId()))
+                .andExpect(status().isNoContent());
+    }
+
+    @DisplayName("테이블그룹 해재를 요청하면 실패응답")
+    @Test
+    public void throwsExceptionWhenUnTableGroup() throws Exception {
+        TableGroup tableGroup = getTableGroup();
+        doThrow(new IllegalArgumentException()).when(tableGroupService).ungroup(tableGroup.getId());
+        webMvc.perform(delete("/api/table-groups/" + tableGroup.getId()))
+                .andExpect(status().isBadRequest());
+    }
+
+    private TableGroup getTableGroup() {
+        return FixtureMonkey.create()
+                .giveMeBuilder(TableGroup.class)
+                .set("id", Arbitraries.longs().between(1, 100))
+                .set("createdDate", LocalDateTime.now())
+                .set("orderTables", getTables())
+                .set("empty", true)
+                .sample();
+    }
+
+    private List<OrderTable> getTables() {
+        return FixtureMonkey.create()
+                .giveMeBuilder(OrderTable.class)
+                .sampleList(Arbitraries.integers().between(1, 3).sample());
+    }
+}
+
