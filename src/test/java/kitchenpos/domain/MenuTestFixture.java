e98430feea2a4a9c004759a3a7291600d8e8d29b
daegyeom.kim <daegyeom.kim@qcells.com>
2022-12-10 15:24:52 +0900

    test(order): 주문 관련 인수테스트 작성

--- src/test/java/kitchenpos/domain/MenuTestFixture.java
+++ src/test/java/kitchenpos/domain/MenuTestFixture.java
@@ -1,7 +1,15 @@
 package kitchenpos.domain;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
+
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class MenuTestFixture {
 
@@ -11,5 +19,30 @@ public class MenuTestFixture {
 
     public static Menu createMenu(String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
         return Menu.of(null, name, price, menuGroupId, menuProducts);
+    }
+
+    public static void 주문_목록_응답됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static void 주문_목록_포함됨(ExtractableResponse<Response> response, List<ExtractableResponse<Response>> createdResponses) {
+        List<Long> expectedOrderIds = createdResponses.stream()
+                .map(it -> Long.parseLong(it.header("Location").split("/")[3]))
+                .collect(Collectors.toList());
+
+        List<Long> resultOrderIds = response.jsonPath().getList(".", Order.class).stream()
+                .map(Order::getId)
+                .collect(Collectors.toList());
+
+        assertThat(resultOrderIds).containsAll(expectedOrderIds);
+    }
+
+    public static void 주문_상태_변경됨(ExtractableResponse<Response> response, String expectOrderStatus) {
+        String actualOrderStatus = response.jsonPath().getString("orderStatus");
+
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(actualOrderStatus).isEqualTo(expectOrderStatus)
+        );
     }
 }
