1a07095f7a58df7cf4dcf281748655922b06509b
ilmare-cbk <cbk104325@gmail.com>
2022-12-11 17:17:37 +0900

    test(OrderTablesTest.java): ungroup 에 대한 테스트 추가

--- src/test/java/kitchenpos/domain/OrderTablesTest.java
+++ src/test/java/kitchenpos/domain/OrderTablesTest.java
@@ -72,4 +72,35 @@ class OrderTablesTest {
                 () -> assertThat(orderTables.getOrderTables().get(1).isEmpty()).isFalse()
         );
     }
+
+    @DisplayName("주문 상태가 조리인 주문 테이블이 하나라도 있으면 단체 지정 취소 시 예외가 발생한다.")
+    @Test
+    void ungroupException() {
+        OrderTable 주문_테이블1 = OrderTable.of(10, true);
+        Order.order(주문_테이블1, Arrays.asList(OrderLineItem.of(1L, 2)));
+        OrderTable 주문_테이블2 = OrderTable.of(10, true);
+        Order.order(주문_테이블2, Arrays.asList(OrderLineItem.of(2L, 2)));
+
+        OrderTables orderTables = OrderTables.from(Arrays.asList(주문_테이블1, 주문_테이블2));
+
+        Assertions.assertThatThrownBy(orderTables::unGroup)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 상태가 식사인 주문 테이블이 하나라도 있으면 단체 지정 취소 시 예외가 발생한다.")
+    @Test
+    void ungroupException2() {
+        OrderTable 주문_테이블1 = OrderTable.of(10, true);
+        Order 주문1 = Order.order(주문_테이블1, Arrays.asList(OrderLineItem.of(1L, 2)));
+        주문1.setOrderStatus(OrderStatus.COMPLETION.name());
+
+        OrderTable 주문_테이블2 = OrderTable.of(10, true);
+        Order 주문2 = Order.order(주문_테이블2, Arrays.asList(OrderLineItem.of(2L, 2)));
+        주문2.setOrderStatus(OrderStatus.MEAL.name());
+
+        OrderTables orderTables = OrderTables.from(Arrays.asList(주문_테이블1, 주문_테이블2));
+
+        Assertions.assertThatThrownBy(orderTables::unGroup)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
