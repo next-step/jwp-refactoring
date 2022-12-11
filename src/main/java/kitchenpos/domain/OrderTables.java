94263780f099f3319997c36017a6c129630f8a7f
ilmare-cbk <cbk104325@gmail.com>
2022-12-11 16:06:59 +0900

    feat(OrderTables.java): 주문 테이블 일급 콜렉션 클래스 추가
    - 주문 테이블에 대한 테스트 추가

---
+++ src/main/java/kitchenpos/domain/OrderTables.java
@@ -0,0 +1,66 @@
+package kitchenpos.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+
+@Embeddable
+public class OrderTables {
+
+    private static final int MIN_ORDER_TABLE_SIZE = 2;
+
+    @OneToMany(mappedBy = "tableGroup")
+    private List<OrderTable> orderTables;
+
+    protected OrderTables() {
+    }
+
+    private OrderTables(List<OrderTable> orderTables) {
+        this.orderTables = orderTables;
+    }
+
+    public static OrderTables from(List<OrderTable> orderTables) {
+        return new OrderTables(orderTables);
+    }
+
+    public List<OrderTable> getOrderTables() {
+        return Collections.unmodifiableList(orderTables);
+    }
+
+    public void registerTableGroup(TableGroup tableGroup) {
+        checkOrderTableSizeGreaterThanMinSize();
+        checkAllOrderTablesAreEmpty();
+        checkAllOrderTablesNotYetRegisteredTableGroup();
+
+        orderTables.forEach(
+                it -> {
+                    it.registerTableGroup(tableGroup);
+                    it.changeEmpty(false);
+                }
+        );
+    }
+
+    private void checkOrderTableSizeGreaterThanMinSize() {
+        if (orderTables.isEmpty() || orderTables.size() < MIN_ORDER_TABLE_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkAllOrderTablesAreEmpty() {
+        boolean isNotEmpty = orderTables.stream().anyMatch(it -> !it.isEmpty());
+
+        if (isNotEmpty) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkAllOrderTablesNotYetRegisteredTableGroup() {
+        boolean alreadyRegistered = orderTables.stream().anyMatch(it -> Objects.nonNull(it.getTableGroupId()));
+
+        if (alreadyRegistered) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -4,16 +4,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class TableGroup {
-
-    private static final int MIN_ORDER_TABLE_SIZE = 2;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,8 +20,8 @@ public class TableGroup {
     @Column(nullable = false)
     private LocalDateTime createdDate;
 
-    @OneToMany(mappedBy = "tableGroup")
-    private List<OrderTable> orderTables;
+    @Embedded
+    private OrderTables orderTables;
 
     protected TableGroup() {
     }
@@ -33,32 +31,16 @@ public class TableGroup {
         this.createdDate = createdDate;
     }
 
-    private TableGroup(Long id, List<OrderTable> orderTables) {
+    private TableGroup(Long id, OrderTables orderTables) {
         this.id = id;
         this.createdDate = LocalDateTime.now();
         this.orderTables = orderTables;
     }
 
-    public TableGroup(List<OrderTable> orderTables) {
+    private TableGroup(OrderTables orderTables) {
         this.createdDate = LocalDateTime.now();
         this.orderTables = orderTables;
-
-        if (orderTables.isEmpty() || orderTables.size() < MIN_ORDER_TABLE_SIZE) {
-            throw new IllegalArgumentException();
-        }
-
-        for (final OrderTable orderTable : orderTables) {
-            if (!orderTable.isEmpty() || Objects.nonNull(orderTable.getTableGroupId())) {
-                throw new IllegalArgumentException();
-            }
-        }
-
-        orderTables.forEach(
-                it -> {
-                    it.registerTableGroup(this);
-                    it.changeEmpty(false);
-                }
-        );
+        orderTables.registerTableGroup(this);
     }
 
     public static TableGroup of(Long id, LocalDateTime createdDate) {
@@ -66,35 +48,27 @@ public class TableGroup {
     }
 
     public static TableGroup of(Long id, List<OrderTable> orderTables) {
-        return new TableGroup(id, orderTables);
+        return new TableGroup(id, OrderTables.from(orderTables));
     }
 
     public static TableGroup from(List<OrderTable> orderTables) {
-        return new TableGroup(orderTables);
+        return new TableGroup(OrderTables.from(orderTables));
+    }
+
+    public static TableGroup createEmpty() {
+        return new TableGroup();
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public LocalDateTime getCreatedDate() {
         return createdDate;
     }
 
-    public void setCreatedDate(final LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
     public List<OrderTable> getOrderTables() {
-        return orderTables;
-    }
-
-    public void setOrderTables(final List<OrderTable> orderTables) {
-        this.orderTables = orderTables;
+        return orderTables.getOrderTables();
     }
 
     @Override
