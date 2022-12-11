1b9e622d904d656e29acb9ffe1fea2317106f325
giraffelim <taeyang7704@naver.com>
2022-12-11 17:53:50 +0900

    refactor(MenuGroup): 기존 도메인 클래스를 엔티티로 변경

---
+++ src/test/java/kitchenpos/menugroup/domain/MenuGroupFixture.java
@@ -0,0 +1,6 @@
+package kitchenpos.menugroup.domain;
+
+public class MenuGroupFixture {
+    public static MenuGroup 추천메뉴 = new MenuGroup("추천메뉴");
+    public static MenuGroup 인기메뉴 = new MenuGroup("인기메뉴");
+}
