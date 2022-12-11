b4e7640a09f1ef8e9398808987143d5cc2d87043
민형준 <aksgudwns@gmail.com>
2022-12-11 19:22:10 +0900

    feat - MenuGroupServicetest 추가

--- src/test/java/kitchenpos/menu/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/menu/application/MenuServiceTest.java
@@ -12,7 +12,6 @@ import kitchenpos.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
--- src/test/java/kitchenpos/product/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/product/application/ProductServiceTest.java
@@ -6,8 +6,6 @@ import kitchenpos.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
