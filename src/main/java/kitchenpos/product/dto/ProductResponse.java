b6105816a00164b2c643d5d3f5fa794c9aa63a31
chunhodong <yhms4432@naver.com>
2022-12-11 20:55:04 +0900

    feat: Product응답용 ProductResponse크래스추가

---
+++ src/main/java/kitchenpos/product/dto/ProductResponse.java
@@ -0,0 +1,33 @@
+package kitchenpos.product.dto;
+
+import kitchenpos.product.domain.Product;
+
+import java.math.BigDecimal;
+
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private BigDecimal money;
+
+    private ProductResponse(Long id,String name,BigDecimal money){
+        this.id = id;
+        this.name = name;
+        this.money = money;
+    }
+
+    public static ProductResponse from(Product product){
+        return new ProductResponse(product.getId(),product.getName(),product.getPrice());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+}
