277833974500fa16744fd8126fc21faa414f922b
chunhodong <yhms4432@naver.com>
2022-12-11 18:47:32 +0900

    refactor: ControllerAdvice이름수정

--- src/main/java/kitchenpos/common/RestControllerExceptionAdvice.java
+++ src/main/java/kitchenpos/common/RestControllerExceptionAdvice.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class RestControllerAdvice {
+public class RestControllerExceptionAdvice {
 
     @ExceptionHandler({ IllegalArgumentException.class })
     public ResponseEntity handleException() {
