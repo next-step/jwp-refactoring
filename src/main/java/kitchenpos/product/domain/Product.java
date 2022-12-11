c5cab92034b745331fab4c3b33dfd6f142288b42
chunhodong <yhms4432@naver.com>
2022-12-11 20:41:15 +0900

    refactor: Product도메인 빌더로 생성자 수정

--- src/main/java/kitchenpos/product/domain/Product.java
+++ src/main/java/kitchenpos/product/domain/Product.java
@@ -5,7 +5,46 @@ import java.math.BigDecimal;
 public class Product {
     private Long id;
     private String name;
-    private BigDecimal price;
+    private Money money;
+
+    public Product() {
+
+    }
+
+    private Product(ProductBuilder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.money = builder.money;
+    }
+
+    public static ProductBuilder builder() {
+        return new ProductBuilder();
+    }
+
+    public static class ProductBuilder {
+        private Long id;
+        private String name;
+        private Money money;
+
+        public ProductBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public ProductBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public ProductBuilder money(Money money) {
+            this.money = money;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+    }
 
     public Long getId() {
         return id;
@@ -23,11 +62,9 @@ public class Product {
         this.name = name;
     }
 
-    public BigDecimal getPrice() {
-        return price;
+    public BigDecimal getPrice(){
+        return money.value();
     }
 
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
-    }
+
 }
