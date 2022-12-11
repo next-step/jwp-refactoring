1917fc067c1fceae5879ecfc788626990dcd097b
ilmare-cbk <cbk104325@gmail.com>
2022-12-11 13:58:28 +0900

    test(OrderTableTest.java): changeEmpty 메소드에 대한 테스트 추가

--- src/test/java/kitchenpos/domain/OrderTableTest.java
+++ src/test/java/kitchenpos/domain/OrderTableTest.java
@@ -1,5 +1,6 @@
 package kitchenpos.domain;
 
+import java.util.Arrays;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,5 +24,50 @@ class OrderTableTest {
         OrderTable table2 = OrderTable.of(2L, 1, false);
 
         Assertions.assertThat(table1).isNotEqualTo(table2);
+    }
+
+    @DisplayName("주문 테이블이 단체 지정이 되어있으면 주문 테이블의 empty 변경 시 예외가 발생한다.")
+    @Test
+    void changeEmptyException() {
+        OrderTable 단체_지정된_주문_테이블 = OrderTable.of(1L, 1L, 10, false);
+
+        boolean empty = !단체_지정된_주문_테이블.isEmpty();
+        Assertions.assertThatThrownBy(() -> 단체_지정된_주문_테이블.setEmpty(empty))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 테이블의 주문 상태가 조리이면 주문 테이블의 empty 변경 시 예외가 발생한다.")
+    @Test
+    void changeEmptyException2() {
+        OrderTable 조리상태_주문_테이블 = OrderTable.of(10, false);
+        Order.order(조리상태_주문_테이블, Arrays.asList(OrderLineItem.of(1L, 2)));
+
+        boolean empty = !조리상태_주문_테이블.isEmpty();
+        Assertions.assertThatThrownBy(() -> 조리상태_주문_테이블.setEmpty(empty))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 테이블의 주문 상태가 식사이면 주문 테이블의 empty 변경 시 예외가 발생한다.")
+    @Test
+    void changeEmptyException3() {
+        OrderTable 식사상태_주문_테이블 = OrderTable.of(10, false);
+        Order 주문 = Order.order(식사상태_주문_테이블, Arrays.asList(OrderLineItem.of(1L, 2)));
+        주문.setOrderStatus(OrderStatus.MEAL.name());
+
+        boolean empty = !식사상태_주문_테이블.isEmpty();
+        Assertions.assertThatThrownBy(() -> 식사상태_주문_테이블.setEmpty(empty))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 테이블의 empty 상태가 변경된다.")
+    @Test
+    void changeEmpty() {
+        OrderTable 주문_테이블 = OrderTable.of(10, false);
+        Order 주문 = Order.order(주문_테이블, Arrays.asList(OrderLineItem.of(1L, 2)));
+        주문.setOrderStatus(OrderStatus.COMPLETION.name());
+
+        주문_테이블.setEmpty(!주문_테이블.isEmpty());
+
+        Assertions.assertThat(주문_테이블.isEmpty()).isTrue();
     }
 }
