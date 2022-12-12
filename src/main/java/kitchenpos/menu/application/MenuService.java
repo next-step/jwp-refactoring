777c020cc92babbaef6b10b2b17af83fa71272e8
sonyoon7 <sonyoon7@gmail.com>
2022-12-13 02:25:27 +0900

    refactor (Menu) Service, Controller 리팩토링 및 테스트 코드 작성

--- src/main/java/kitchenpos/menu/application/MenuService.java
+++ src/main/java/kitchenpos/menu/application/MenuService.java
@@ -1,84 +1,73 @@
 package kitchenpos.menu.application;
 
-import kitchenpos.menu.dao.MenuDao;
-import kitchenpos.menu.dao.MenuGroupDao;
-import kitchenpos.menu.dao.MenuProductDao;
-import kitchenpos.product.dao.ProductDao;
 import kitchenpos.menu.domain.Menu;
+import kitchenpos.menu.domain.MenuGroupRepository;
 import kitchenpos.menu.domain.MenuProduct;
+import kitchenpos.menu.domain.MenuRepository;
+import kitchenpos.menu.dto.MenuProductRequest;
+import kitchenpos.menu.dto.MenuRequest;
+import kitchenpos.menu.dto.MenuResponse;
+import kitchenpos.menu.exception.NotFoundMenuGroupException;
 import kitchenpos.product.domain.Product;
+import kitchenpos.product.domain.ProductRepository;
+import kitchenpos.product.exception.NotFoundProductException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class MenuService {
-    private final MenuDao menuDao;
-    private final MenuGroupDao menuGroupDao;
-    private final MenuProductDao menuProductDao;
-    private final ProductDao productDao;
+    private final MenuRepository menuRepository;
+    private final MenuGroupRepository menuGroupRepository;
+    private final ProductRepository productRepository;
 
     public MenuService(
-            final MenuDao menuDao,
-            final MenuGroupDao menuGroupDao,
-            final MenuProductDao menuProductDao,
-            final ProductDao productDao
+            final MenuRepository menuRepository,
+            final MenuGroupRepository menuGroupRepository,
+            final ProductRepository productRepository
     ) {
-        this.menuDao = menuDao;
-        this.menuGroupDao = menuGroupDao;
-        this.menuProductDao = menuProductDao;
-        this.productDao = productDao;
+        this.menuRepository = menuRepository;
+        this.menuGroupRepository = menuGroupRepository;
+        this.productRepository = productRepository;
     }
 
     @Transactional
-    public Menu create(final Menu menu) {
-        final BigDecimal price = menu.getPrice();
-
-        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException();
-        }
-
-        if (!menuGroupDao.existsById(menu.getMenuGroupId())) {
-            throw new IllegalArgumentException();
-        }
-
-        final List<MenuProduct> menuProducts = menu.getMenuProducts();
-
-        BigDecimal sum = BigDecimal.ZERO;
-        for (final MenuProduct menuProduct : menuProducts) {
-            final Product product = productDao.findById(menuProduct.getProductId())
-                    .orElseThrow(IllegalArgumentException::new);
-            sum = sum.add(product.getPrice().multiply(BigDecimal.valueOf(menuProduct.getQuantity())));
-        }
-
-        if (price.compareTo(sum) > 0) {
-            throw new IllegalArgumentException();
-        }
-
-        final Menu savedMenu = menuDao.save(menu);
-
-        final Long menuId = savedMenu.getId();
-        final List<MenuProduct> savedMenuProducts = new ArrayList<>();
-        for (final MenuProduct menuProduct : menuProducts) {
-            menuProduct.setMenuId(menuId);
-            savedMenuProducts.add(menuProductDao.save(menuProduct));
-        }
-        savedMenu.setMenuProducts(savedMenuProducts);
-
-        return savedMenu;
+    public MenuResponse create(final MenuRequest request) {
+        validateExistMenuGroup(request);
+        Menu menu = createMenu(request);
+        return MenuResponse.of(menu);
     }
 
-    public List<Menu> list() {
-        final List<Menu> menus = menuDao.findAll();
+    private Menu createMenu(MenuRequest request) {
+        Menu menu = menuRepository.save(request.toMenu());
+        menu.addMenuProducts(toMenuProducts(request));
+        return menu;
+    }
 
-        for (final Menu menu : menus) {
-            menu.setMenuProducts(menuProductDao.findAllByMenuId(menu.getId()));
+    private List<MenuProduct> toMenuProducts(MenuRequest request) {
+        List<MenuProduct> menuProducts = new ArrayList<>(request.getMenuProducts().size());
+        for (MenuProductRequest menuProductRequest : request.getMenuProducts()) {
+            Product product = findProductById(menuProductRequest);
+            menuProducts.add(menuProductRequest.toMenuProduct(product));
         }
+        return menuProducts;
+    }
 
-        return menus;
+    private Product findProductById(MenuProductRequest menuProductRequest) {
+        return productRepository.findById(menuProductRequest.getProductId())
+                .orElseThrow(NotFoundProductException::new);
+    }
+
+    private void validateExistMenuGroup(MenuRequest request) {
+        if (!menuGroupRepository.existsById(request.getMenuGroupId())) {
+            throw new NotFoundMenuGroupException();
+        }
+    }
+
+    public List<MenuResponse> list() {
+        return MenuResponse.of(menuRepository.findAll());
     }
 }
+
--- src/test/java/kitchenpos/menu/MenuAcceptanceUtil.java
+++ src/test/java/kitchenpos/menu/MenuAcceptanceUtil.java
@@ -1,8 +1,9 @@
 package kitchenpos.menu;
 
-import kitchenpos.menu.domain.Menu;
-import kitchenpos.menu.domain.MenuGroup;
-import kitchenpos.product.domain.Product;
+
+import kitchenpos.menu.dto.MenuGroupResponse;
+import kitchenpos.menu.dto.MenuResponse;
+import kitchenpos.product.dto.ProductResponse;
 
 import java.math.BigDecimal;
 
@@ -15,9 +16,9 @@ public final class MenuAcceptanceUtil {
     private MenuAcceptanceUtil() {
     }
 
-    public static Menu 신메뉴_강정치킨_가져오기() {
-        Product 강정치킨 = 상품_등록됨("강정치킨", BigDecimal.valueOf(15_000L));
-        MenuGroup 신메뉴 = 메뉴_그룹_등록됨("신메뉴");
+    public static MenuResponse 신메뉴_강정치킨_가져오기() {
+        ProductResponse 강정치킨 = 상품_등록됨("강정치킨", BigDecimal.valueOf(15_000L));
+        MenuGroupResponse 신메뉴 = 메뉴_그룹_등록됨("신메뉴");
         return 메뉴_등록됨("강정치킨", BigDecimal.valueOf(15_000L), 신메뉴.getId(), 강정치킨);
     }
 }
