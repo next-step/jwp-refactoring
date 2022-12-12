2070a21de5c9f1ace670c518c93c6cff88fe0178
hybrid.simpler@gmail.com <hybrid.simpler@gmail.com>
2022-12-13 01:06:42 +0900

    refactor: 불필요한 출력 제거

--- src/test/java/kitchenpos/acceptance/TableAcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/TableAcceptanceTest.java
@@ -136,7 +136,7 @@ class TableAcceptanceTest extends AcceptanceTest {
                 .stream()
                 .map(OrderTable::getId)
                 .collect(Collectors.toList());
-        System.out.println(ids);
+
         assertAll(
                 () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(ids).containsAll(tableIds)
