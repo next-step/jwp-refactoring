50049660f2ee6a1b0829c6cbde9266a3eba3acc5
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-12 15:15:49 +0900

    test(ProductServiceTest.java) : 상품 생성

--- src/main/java/kitchenpos/product/dto/ProductCreateRequest.java
+++ src/main/java/kitchenpos/product/dto/ProductCreateRequest.java
@@ -9,7 +9,8 @@ public class ProductCreateRequest {
     private String name;
     private BigDecimal price;
 
-    public ProductCreateRequest(BigDecimal price) {
+    public ProductCreateRequest(String name, BigDecimal price) {
+        this.name = name;
         this.price = price;
     }
 
--- src/test/java/kitchenpos/product/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/product/application/ProductServiceTest.java
@@ -1,19 +1,25 @@
 package kitchenpos.product.application;
 
 import kitchenpos.product.dao.ProductDao;
+import kitchenpos.product.domain.Product;
 import kitchenpos.product.dto.ProductCreateRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 
 import static kitchenpos.common.Price.PRICE_MINIMUM_EXCEPTION_MESSAGE;
 import static kitchenpos.common.Price.PRICE_NOT_NULL_EXCEPTION_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("상품 서비스")
+@SpringBootTest
 class ProductServiceTest {
 
 
@@ -31,13 +37,17 @@ class ProductServiceTest {
     @DisplayName("상품 생성")
     @Test
     void create() {
-
+        Product product = productService.create(new ProductCreateRequest("상품A", BigDecimal.ONE));
+        assertAll(
+                () -> assertEquals(0, product.getPrice().compareTo(BigDecimal.ONE)),
+                () -> assertThat(product.getName()).isEqualTo("상품A")
+        );
     }
 
     @DisplayName("상품 생성 / 가격을 필수로 갖는다.")
     @Test
     void create_fail_priceNull() {
-        assertThatThrownBy(() -> productService.create(new ProductCreateRequest(null)))
+        assertThatThrownBy(() -> productService.create(new ProductCreateRequest("상품A", null)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(PRICE_NOT_NULL_EXCEPTION_MESSAGE);
     }
@@ -45,14 +55,9 @@ class ProductServiceTest {
     @DisplayName("상품 생성 / 가격은 0원보다 작을 수 없다.")
     @Test
     void create_fail_minimumPrice() {
-        assertThatThrownBy(() -> productService.create(new ProductCreateRequest(BigDecimal.valueOf(-1))))
+        assertThatThrownBy(() -> productService.create(new ProductCreateRequest("상품A", BigDecimal.valueOf(-1))))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(PRICE_MINIMUM_EXCEPTION_MESSAGE);
-    }
-
-    @DisplayName("상품 생성 / 이름을 필수로 갖는다.")
-    @Test
-    void create_fail_name() {
     }
 
     @DisplayName("상품 목록 조회")
