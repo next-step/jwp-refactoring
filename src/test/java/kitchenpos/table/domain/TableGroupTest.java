67b1a2d151ced0003a0f582ed93ca359bac9e1aa
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-12 11:32:08 +0900

    test(TableGroupTest.java) : 테이블 그룹을 생성한다.

--- src/test/java/kitchenpos/table/domain/TableGroupTest.java
+++ src/test/java/kitchenpos/table/domain/TableGroupTest.java
@@ -8,21 +8,24 @@ import java.util.List;
 
 import static kitchenpos.table.domain.TableGroup.ORDER_TABLE_MINIMUM_SIZE_EXCEPTION_MESSAGE;
 import static kitchenpos.table.domain.TableGroup.ORDER_TABLE_NOT_EMPTY_EXCEPTION_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("테이블 그룹")
 class TableGroupTest {
 
-//    @DisplayName("테이블 그룹을 생성한다.")
-//    @Test
-//    void create() {
-//        List<OrderTable> orderTables = new ArrayList<>();
-//        assertThatNoException().isThrownBy(() -> new TableGroup(orderTables));
-//    }
+    @DisplayName("테이블 그룹을 생성한다.")
+    @Test
+    void create() {
+        List<OrderTable> orderTables = new ArrayList<>();
+        orderTables.add(new OrderTable(1L, 1L, 1, true));
+        orderTables.add(new OrderTable(2L, 1L, 1, true));
+        assertThatNoException().isThrownBy(() -> new TableGroup(orderTables));
+    }
 
     @DisplayName("주문 테이블이 비어있을 수 없다.")
     @Test
-    void create() {
+    void create_fail_orderTable() {
         List<OrderTable> orderTables = new ArrayList<>();
         assertThatThrownBy(() -> new TableGroup(orderTables))
                 .isInstanceOf(IllegalArgumentException.class)
