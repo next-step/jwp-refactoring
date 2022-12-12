75e7407666d20872e73ec1c43c243c32e1caf572
jisu1211 <dbdnzl1211@gmail.com>
2022-12-13 05:37:37 +0900

    refactor: 사용되지 않는 클래스 제거

--- src/main/java/kitchenpos/menu/domain/MenuRepository.java
+++
@@ -1,8 +0,0 @@
-package kitchenpos.menu.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-}
