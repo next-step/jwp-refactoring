adb39a060207843f49dcc4d38d7f82b73ab7f71d
crongcm <ucm5459@kakao.com>
2022-12-13 01:21:00 +0900

    refactor: MenuGroup 리팩터링

---
+++ src/test/java/kitchenpos/domain/MenuGroupTest.java
@@ -0,0 +1,34 @@
+package kitchenpos.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import kitchenpos.common.exception.InvalidParameterException;
+import kitchenpos.menu.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuGroupTest {
+    @Test
+    @DisplayName("메뉴 그룹 생성")
+    void createMenuGroup() {
+        // when
+        MenuGroup actual = MenuGroup.from("면류");
+
+        // then
+        assertAll(
+                () -> assertThat(actual).isNotNull(),
+                () -> assertThat(actual).isInstanceOf(MenuGroup.class)
+        );
+    }
+
+    @Test
+    @DisplayName("메뉴 그룹명 필수")
+    void createMenuGroupByNameIsNotNull() {
+        // when & then
+        assertThatThrownBy(() -> MenuGroup.from(null))
+                .isInstanceOf(InvalidParameterException.class)
+                .hasMessage("이름은 필수입니다.");
+    }
+}
