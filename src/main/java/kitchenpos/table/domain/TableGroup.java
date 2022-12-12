59b593d740456a3b018a9728202ea9dbe28f17dd
sonyoon7 <sonyoon7@gmail.com>
2022-12-13 02:17:34 +0900

    refactor (Table Group Domain) Table Group domain 리팩토링 및 테스트 코드 작성

--- src/main/java/kitchenpos/table/domain/TableGroup.java
+++ src/main/java/kitchenpos/table/domain/TableGroup.java
@@ -1,36 +1,58 @@
 package kitchenpos.table.domain;
 
-import kitchenpos.order.domain.OrderTable;
-
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Entity
 public class TableGroup {
+
+    private static final int MIN_SIZE = 2;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private LocalDateTime createdDate;
-    private List<OrderTable> orderTables;
+    @Embedded
+    private OrderTables orderTables = new OrderTables();
+
+    public TableGroup() {
+        this.createdDate = LocalDateTime.now();
+    }
+
+    public void addOrderTables(List<OrderTable> orderTables) {
+        validateOrderTables(orderTables);
+        this.orderTables.addAll(this, orderTables);
+    }
+
+    private void validateOrderTables(List<OrderTable> orderTables) {
+        if (orderTables.size() < MIN_SIZE) {
+            throw new IllegalArgumentException("테이블 갯수가 적습니다.");
+        }
+        if (hasNotEmptyOrGrouped(orderTables)) {
+            throw new IllegalArgumentException(
+                    "테이블 중에 빈 테이블이 아니거나 다른 단체에 지정된 테이블이 있습니다.");
+        }
+    }
+
+    private boolean hasNotEmptyOrGrouped(List<OrderTable> orderTables) {
+        return orderTables.stream().anyMatch(it -> !it.isEmpty() || it.isGrouped());
+    }
+
+    public void ungroup() {
+        orderTables.clear();
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
     }
 
     public LocalDateTime getCreatedDate() {
         return createdDate;
     }
 
-    public void setCreatedDate(final LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
     public List<OrderTable> getOrderTables() {
-        return orderTables;
-    }
-
-    public void setOrderTables(final List<OrderTable> orderTables) {
-        this.orderTables = orderTables;
+        return orderTables.get();
     }
 }
