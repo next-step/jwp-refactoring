d585995d1adf70753bec10a96fb1fcc022e0a3ad
Minjae Kim <minjae.kim@mytrum.com>
2022-12-14 00:47:32 +0900

    test: 상품 ui 및 bo 테스트 작성

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -1,11 +1,20 @@
 package kitchenpos.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product() {
+    }
 
     public Long getId() {
         return id;
@@ -29,5 +38,22 @@ public class Product {
 
     public void setPrice(final BigDecimal price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && Objects.equals(price.intValue(), product.price.intValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
