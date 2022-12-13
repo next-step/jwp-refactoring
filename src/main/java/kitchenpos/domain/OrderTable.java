beb7f42605c7ecb56159d01116695dd9a04e937d
Minjae Kim <minjae.kim@mytrum.com>
2022-12-14 00:49:19 +0900

    test: 주문 테이블 ui 및 bo 테스트 작성

--- src/main/java/kitchenpos/domain/OrderTable.java
+++ src/main/java/kitchenpos/domain/OrderTable.java
@@ -1,10 +1,31 @@
 package kitchenpos.domain;
 
+import java.util.Objects;
+
 public class OrderTable {
     private Long id;
     private Long tableGroupId;
     private int numberOfGuests;
     private boolean empty;
+
+    private OrderTable(Long id, Long tableGroupId, int numberOfGuests, boolean empty) {
+        this(numberOfGuests, empty);
+        this.id = id;
+        this.tableGroupId = tableGroupId;
+        this.empty = empty;
+    }
+
+    private OrderTable(int numberOfGuests, boolean empty) {
+        this.numberOfGuests = numberOfGuests;
+        this.empty = empty;
+    }
+
+    public static OrderTable ofNumberOfGuestsAndEmpty(int numberOfGuests, boolean empty) {
+        return new OrderTable(numberOfGuests, empty);
+    }
+
+    public OrderTable() {
+    }
 
     public Long getId() {
         return id;
@@ -36,5 +57,31 @@ public class OrderTable {
 
     public void setEmpty(final boolean empty) {
         this.empty = empty;
+    }
+
+    public OrderTable changeEmpty(boolean empty) {
+        return new OrderTable(this.id, this.tableGroupId, this.numberOfGuests, empty);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OrderTable that = (OrderTable) o;
+        return numberOfGuests == that.numberOfGuests && empty == that.empty && Objects.equals(tableGroupId,
+                that.tableGroupId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tableGroupId, numberOfGuests, empty);
+    }
+
+    public OrderTable changeNumberOfGuest(int numberOfGuests) {
+        return new OrderTable(this.id, this.tableGroupId, numberOfGuests, this.empty);
     }
 }
