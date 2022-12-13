cc1fe22414915154af9a3c093d8a2ccc04c7469c
cwJohnPark <pierrotcw@gmail.com>
2022-12-13 20:45:28 +0900

    fix: Jackson LocalDateTime 직렬화 문제 해결

--- src/main/java/kitchenpos/ui/infra/ObjectMapperConfig.java
+++ src/main/java/kitchenpos/ui/infra/ObjectMapperConfig.java
@@ -1,6 +1,6 @@
 package kitchenpos.ui.infra;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import kitchenpos.domain.Money;
 
@@ -18,12 +20,17 @@ public class ObjectMapperConfig {
 	@Bean
 	public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
 		return Jackson2ObjectMapperBuilder.json()
+				.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 				.modules(getMoneyModule());
 	}
 
 	private List<Module> getMoneyModule() {
+		return Arrays.asList(new JavaTimeModule(), getMoneySerializerModule());
+	}
+
+	private static SimpleModule getMoneySerializerModule() {
 		SimpleModule module = new SimpleModule();
 		module.addSerializer(Money.class, new MoneySerializer());
-		return Collections.singletonList(module);
+		return module;
 	}
 }
