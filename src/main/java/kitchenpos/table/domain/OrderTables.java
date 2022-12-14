1ff675bb7daa9624b89b29e25a544443acd632dd
crongcm <ucm5459@kakao.com>
2022-12-15 04:42:03 +0900

    feat: OrderTables 주문 테이블 묶음 구현

---
+++ src/main/java/kitchenpos/table/domain/OrderTables.java
@@ -0,0 +1,74 @@
+package kitchenpos.table.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import kitchenpos.common.exception.InvalidParameterException;
+import org.springframework.util.CollectionUtils;
+
+@Embeddable
+public class OrderTables {
+    private static final String ERROR_MESSAGE_ORDER_TABLES_IS_EMPTY = "주문 테이블은 비어있을 수 없습니다.";
+    private static final String ERROR_MESSAGE_ORDER_TABLES_MIN_SIZE = "주문 테이블은 2석 이상이어야 합니다.";
+
+    private static final int TABLES_MIN_SIZE = 2;
+
+    @OneToMany(mappedBy = "tableGroupId", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<OrderTable> tables = new ArrayList<>();
+
+    protected OrderTables() {}
+
+    private OrderTables(List<OrderTable> tables) {
+        validate(tables);
+        this.tables = tables;
+    }
+
+    private void validate(List<OrderTable> tables) {
+        validateTableIsEmpty(tables);
+        validateTableSize(tables);
+        validateIsGrouped(tables);
+    }
+
+    private void validateTableIsEmpty(List<OrderTable> tables) {
+        if (CollectionUtils.isEmpty(tables)) {
+            throw new InvalidParameterException(ERROR_MESSAGE_ORDER_TABLES_IS_EMPTY);
+        }
+    }
+
+    private void validateTableSize(List<OrderTable> tables) {
+        if (tables.size() < TABLES_MIN_SIZE) {
+            throw new InvalidParameterException(ERROR_MESSAGE_ORDER_TABLES_MIN_SIZE);
+        }
+    }
+
+    private void validateIsGrouped(List<OrderTable> tables) {
+        tables.forEach(OrderTable::isGrouped);
+    }
+
+    public static OrderTables from(List<OrderTable> tables) {
+        return new OrderTables(tables);
+    }
+
+    public boolean validNotEmptyOrderTable() {
+        return !tables.stream().allMatch(OrderTable::isEmpty);
+    }
+
+    public void validAlreadyGroup() {
+        tables.forEach(OrderTable::isGrouped);
+    }
+
+    public void updateTableGroup(TableGroup tableGroup) {
+        tables.forEach(orderTable -> orderTable.updateTableGroup(tableGroup.id()));
+    }
+
+    public void ungroup() {
+        tables.forEach(OrderTable::ungroup);
+    }
+
+    public List<OrderTable> list() {
+        return Collections.unmodifiableList(tables);
+    }
+}
