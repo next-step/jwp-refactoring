1787465c70fc8c7d964aa3c1d99d8d9c1eab1391
jylim <jylim@12cm.co.kr>
2022-12-14 20:15:47 +0900

    feat(ProductName): 상품명 분리, exception 추가

---
+++ src/main/java/kitchenpos/exception/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package kitchenpos.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Void> handleIllegalArgsException() {
+        return badRequest();
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestException> handleBadRequestException(Exception e) {
+        return new ResponseEntity<>(new BadRequestException(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<Void> badRequest() {
+        return ResponseEntity.badRequest().build();
+    }
+}
