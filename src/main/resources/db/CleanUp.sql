e38602421b25755db44a5d65045e1f8faef1310b
sixthou <justyuk@naver.com>
2022-12-14 16:28:02 +0900

    feat : AcceptanceTest 생성

---
+++ src/main/resources/db/CleanUp.sql
@@ -0,0 +1,15 @@
+truncate table orders;
+
+truncate table order_line_item;
+
+truncate table menu;
+
+truncate table menu_group;
+
+truncate table menu_product;
+
+truncate table order_table;
+
+truncate table table_group;
+
+truncate table product;
