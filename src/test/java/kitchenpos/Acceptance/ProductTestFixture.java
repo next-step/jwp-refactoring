a95091de6520a777512adf4c0b6078c85111a7cd
sixthou <justyuk@naver.com>
2022-12-14 17:53:14 +0900

    test : product 인수 테스트 작성

---
+++ src/test/java/kitchenpos/Acceptance/ProductTestFixture.java
@@ -0,0 +1,40 @@
+package kitchenpos.Acceptance;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.List;
+import java.util.stream.Collectors;
+import kitchenpos.domain.Product;
+import org.assertj.core.api.Assertions;
+
+public class ProductTestFixture extends testFixture {
+
+    public static final String PRODUCT_BASE_URI = "/api/products";
+
+    public static ExtractableResponse<Response> 상품_생성_요청함(Product product) {
+        return post(PRODUCT_BASE_URI, product);
+    }
+
+    public static ExtractableResponse<Response> 상품_목록_조회_요청함() {
+        return get(PRODUCT_BASE_URI);
+    }
+
+    public static void 상품_조회_요청_응답됨(ExtractableResponse<Response> response) {
+        ok(response);
+    }
+
+    public static void 상품_생성됨(ExtractableResponse<Response> response) {
+        created(response);
+    }
+
+    public static void 상품_조회_포함됨(ExtractableResponse<Response> response, List<ExtractableResponse<Response>> productResponses) {
+        List<Product> actual = response.jsonPath()
+                .getList(".", Product.class);
+        List<Product> expect = productResponses.stream()
+                .map(r -> r.as(Product.class))
+                .collect(Collectors.toList());
+
+        Assertions.assertThat(actual).containsAll(expect);
+    }
+
+}
