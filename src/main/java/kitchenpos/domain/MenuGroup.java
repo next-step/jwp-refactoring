9baa24288f6563436bf796843be1bb11ba3295c1
hahoho87 <bbaing4564@gmail.com>
2022-12-15 01:10:27 +0900

    refactor(domain): 생성자 추가

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,22 +1,30 @@
 package kitchenpos.domain;
 
 public class MenuGroup {
-    private Long id;
-    private String name;
+	private Long id;
+	private String name;
 
-    public Long getId() {
-        return id;
-    }
+	public MenuGroup() {
+	}
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
+	public MenuGroup(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
 }
