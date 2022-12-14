9f88550a505ffddab75c862f964a2b86d04106b2
hahoho87 <bbaing4564@gmail.com>
2022-12-15 02:20:00 +0900

    test: truncate 방식 변경
    - sql -> DataSource 이용

--- src/test/java/kitchenpos/acceptance/AcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/AcceptanceTest.java
@@ -1,23 +1,29 @@
 package kitchenpos.acceptance;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 
 import io.restassured.RestAssured;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Sql("/truncate.sql")
 public abstract class AcceptanceTest {
 	@LocalServerPort
 	int port;
 
+	@Autowired
+	protected DatabaseCleanUp cleanUpUtils;
+
 
 	@BeforeEach
 	public void setUp() {
-		RestAssured.port = port;
+		if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+			RestAssured.port = port;
+		}
+		cleanUpUtils.cleanUp();
+
 	}
 }
