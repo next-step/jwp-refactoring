551be872d03d03c9be0c7e5bba006bc439f7b7ee
seogineer <dgseo8981@gmail.com>
2022-12-14 23:38:24 +0900

    refactor: 메뉴 그룹 Jpa repository로 전환

--- src/main/java/kitchenpos/application/MenuGroupService.java
+++ src/main/java/kitchenpos/application/MenuGroupService.java
@@ -5,27 +5,36 @@ import java.util.stream.Collectors;
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.dto.MenuGroupResponse;
+import kitchenpos.exception.ErrorCode;
+import kitchenpos.exception.KitchenposException;
+import kitchenpos.repository.MenuGroupRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MenuGroupService {
-    private final MenuGroupDao menuGroupDao;
+    private final MenuGroupRepository menuGroupRepository;
 
-    public MenuGroupService(final MenuGroupDao menuGroupDao) {
-        this.menuGroupDao = menuGroupDao;
+    public MenuGroupService(final MenuGroupRepository menuGroupRepository) {
+        this.menuGroupRepository = menuGroupRepository;
     }
 
     public MenuGroupResponse create(final MenuGroup entity) {
-        MenuGroup menuGroup = menuGroupDao.save(entity);
+        MenuGroup menuGroup = menuGroupRepository.save(entity);
         return MenuGroupResponse.of(menuGroup.getId(), menuGroup.getName());
     }
 
     @Transactional(readOnly = true)
     public List<MenuGroupResponse> list() {
-        return menuGroupDao.findAll().stream()
+        return menuGroupRepository.findAll().stream()
                 .map(MenuGroupResponse::of)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public MenuGroup findById(Long menuGroupId){
+        return menuGroupRepository.findById(menuGroupId)
+                .orElseThrow(() -> new KitchenposException(ErrorCode.NOT_EXISTS_MENU_GROUP));
     }
 }
--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -1,12 +1,17 @@
 package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.dto.MenuGroupResponse;
+import kitchenpos.repository.MenuGroupRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,22 +22,48 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class MenuGroupServiceTest {
     @Mock
-    private MenuGroupDao menuGroupDao;
+    private MenuGroupRepository menuGroupRepository;
     @InjectMocks
     private MenuGroupService menuGroupService;
-    private MenuGroup 메뉴_그룹;
+    private MenuGroup 메뉴_그룹_1;
+    private MenuGroup 메뉴_그룹_2;
+    private List<MenuGroup> 메뉴_그룹_목록;
 
     @BeforeEach
     void setUp() {
-        메뉴_그룹 = new MenuGroup("한마리메뉴");
+        메뉴_그룹_1 = new MenuGroup(1L, "한마리메뉴");
+        메뉴_그룹_2 = new MenuGroup(2L, "두마리메뉴");
+        메뉴_그룹_목록 = Arrays.asList(메뉴_그룹_1, 메뉴_그룹_2);
     }
 
     @Test
     void 생성() {
-        given(menuGroupDao.save(any())).willReturn(메뉴_그룹);
+        given(menuGroupRepository.save(any())).willReturn(메뉴_그룹_1);
 
-        MenuGroupResponse response = menuGroupService.create(메뉴_그룹);
+        MenuGroupResponse response = menuGroupService.create(메뉴_그룹_1);
 
         assertThat(response.getName()).isEqualTo("한마리메뉴");
+    }
+
+    @Test
+    void 조회() {
+        given(menuGroupRepository.findAll()).willReturn(메뉴_그룹_목록);
+
+        List<MenuGroupResponse> responses = menuGroupService.list();
+
+        assertThat(responses.size()).isEqualTo(2);
+    }
+
+    @Test
+    void 아이디로_조회() {
+        given(menuGroupRepository.findById(메뉴_그룹_1.getId())).willReturn(Optional.of(메뉴_그룹_1));
+
+        MenuGroup response = menuGroupService.findById(메뉴_그룹_1.getId());
+
+        assertAll(
+                () -> assertThat(response).isNotNull(),
+                () -> assertThat(response.getId()).isEqualTo(1L),
+                () -> assertThat(response.getName()).isEqualTo("한마리메뉴")
+        );
     }
 }
