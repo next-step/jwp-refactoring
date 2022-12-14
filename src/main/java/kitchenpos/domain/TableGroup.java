11fd6a862a70cc7039a903660b6d7f20e4472f63
jylim <jylim@12cm.co.kr>
2022-12-14 21:21:38 +0900

    refactor(TableGroup): TableGroup 리펙토링

--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -2,11 +2,34 @@ package kitchenpos.domain;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 public class TableGroup {
     private Long id;
     private LocalDateTime createdDate;
     private List<OrderTable> orderTables;
+
+    private TableGroup() {
+    }
+
+    private TableGroup(Long id, LocalDateTime createdDate) {
+        this.id = id;
+        this.createdDate = createdDate;
+    }
+
+    private TableGroup(Long id, List<OrderTable> orderTables) {
+        this.id = id;
+        this.createdDate = LocalDateTime.now();
+        this.orderTables = orderTables;
+    }
+
+    public static TableGroup of(Long id, LocalDateTime createdDate) {
+        return new TableGroup(id, createdDate);
+    }
+
+    public static TableGroup of(Long id, List<OrderTable> orderTables) {
+        return new TableGroup(id, orderTables);
+    }
 
     public Long getId() {
         return id;
@@ -30,5 +53,22 @@ public class TableGroup {
 
     public void setOrderTables(final List<OrderTable> orderTables) {
         this.orderTables = orderTables;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TableGroup that = (TableGroup) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
