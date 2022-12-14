25ce5acb7400a948699ed68bb302fb54e79a5a2c
seogineer <dgseo8981@gmail.com>
2022-12-14 14:52:24 +0900

    test: 추가된 유효성 검증 메소드에 대한 테스트 코드 추가

--- src/test/java/kitchenpos/domain/MenuTest.java
+++ src/test/java/kitchenpos/domain/MenuTest.java
@@ -1,11 +1,18 @@
 package kitchenpos.domain;
 
+import static kitchenpos.exception.ErrorCode.PRICE_GREATER_THAN_SUM;
+import static kitchenpos.exception.ErrorCode.PRICE_IS_NULL_OR_MINUS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import kitchenpos.exception.KitchenposException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MenuTest {
     @Test
@@ -24,5 +31,33 @@ class MenuTest {
                 () -> assertThat(menu.getMenuGroupId()).isEqualTo(1L),
                 () -> assertThat(menu.getMenuProducts().size()).isEqualTo(1)
         );
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void 가격이_NULL인_경우(BigDecimal price) {
+        Menu menu = new Menu(1L, "후라이드", price, null);
+        assertThatThrownBy(menu::validatePrice)
+                .isInstanceOf(KitchenposException.class)
+                .hasMessageContaining(PRICE_IS_NULL_OR_MINUS.getDetail());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -10, -100, -9999})
+    void 가격이_마이너스인_경우(int price) {
+        Menu menu = new Menu(1L, "후라이드치킨", BigDecimal.valueOf(price), null);
+
+        assertThatThrownBy(menu::validatePrice)
+                .isInstanceOf(KitchenposException.class)
+                .hasMessageContaining(PRICE_IS_NULL_OR_MINUS.getDetail());
+    }
+
+    @Test
+    void 가격과_메뉴_항목의_가격_합계와_일치하지_않는_경우() {
+        Menu menu = new Menu(1L, "후라이드치킨", BigDecimal.valueOf(16000), null);
+
+        assertThatThrownBy(() -> menu.validatePriceGreaterThanSum(BigDecimal.ZERO))
+                .isInstanceOf(KitchenposException.class)
+                .hasMessageContaining(PRICE_GREATER_THAN_SUM.getDetail());
     }
 }
