59fb766dfcba0a5f2d0e9b53dd37666fda4bcdfd
ilmare-cbk <cbk104325@gmail.com>
2022-12-16 01:32:24 +0900

    refactor(Orders.java): 사용하지 않는 메소드 및 설정 제거

--- src/main/java/kitchenpos/order/domain/Orders.java
+++ src/main/java/kitchenpos/order/domain/Orders.java
@@ -1,19 +1,10 @@
 package kitchenpos.order.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import javax.persistence.Embeddable;
-import javax.persistence.OneToMany;
 
-@Embeddable
 public class Orders {
 
-    @OneToMany(mappedBy = "orderTable")
-    private List<Order> orders = new ArrayList<>();
-
-    protected Orders() {
-    }
+    private List<Order> orders;
 
     private Orders(List<Order> orders) {
         this.orders = orders;
@@ -21,14 +12,6 @@ public class Orders {
 
     public static Orders from(List<Order> orders) {
         return new Orders(orders);
-    }
-
-    public static Orders createEmpty() {
-        return new Orders(new ArrayList<>());
-    }
-
-    public List<Order> getOrders() {
-        return Collections.unmodifiableList(orders);
     }
 
     public boolean anyMatchedIn(List<OrderStatus> orderStatuses) {
