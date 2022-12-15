4c51727af2096d6330d937e274f05fd3d81bcb03
yeoojin <only1year100@gmail.com>
2022-12-15 20:09:14 +0900

    feat(MenuGroupRespone): MenuGroupResponse 생성

---
+++ src/main/java/kitchenpos/dto/MenuGroupResponse.java
@@ -0,0 +1,41 @@
+package kitchenpos.dto;
+
+import java.util.Objects;
+
+public class MenuGroupResponse {
+    private Long id;
+    private String name;
+
+    private MenuGroupResponse() {
+    }
+
+    private MenuGroupResponse(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        MenuGroupResponse that = (MenuGroupResponse) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
