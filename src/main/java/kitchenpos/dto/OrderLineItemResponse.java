8150b5c4619bdc3d31a9e1f20a1d246fd645b15e
yeoojin <only1year100@gmail.com>
2022-12-15 20:18:37 +0900

    feat(OrderResponse): OrderResponse, OrderLineItemResponse 생성

---
+++ src/main/java/kitchenpos/dto/OrderLineItemResponse.java
@@ -0,0 +1,56 @@
+package kitchenpos.dto;
+
+import kitchenpos.domain.OrderLineItem;
+
+import java.util.Objects;
+
+public class OrderLineItemResponse {
+    private Long seq;
+    private Long orderId;
+    private Long menuId;
+    private long quantity;
+
+    private OrderLineItemResponse() {
+    }
+
+    private OrderLineItemResponse(Long seq, Long orderId, Long menuId, long quantity) {
+        this.seq = seq;
+        this.orderId = orderId;
+        this.menuId = menuId;
+        this.quantity = quantity;
+    }
+
+    public Long getSeq() {
+        return seq;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public Long getMenuId() {
+        return menuId;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OrderLineItemResponse that = (OrderLineItemResponse) o;
+        return quantity == that.quantity && Objects.equals(seq, that.seq) && Objects.equals(orderId,
+                that.orderId) && Objects.equals(menuId, that.menuId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seq, orderId, menuId, quantity);
+    }
+}
