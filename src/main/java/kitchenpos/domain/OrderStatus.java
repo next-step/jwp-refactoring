c68807f96cc9ea8ae16f2b1f44b9e112b22d6eac
daegyeom.kim <daegyeom.kim@qcells.com>
2022-12-15 22:43:50 +0900

    refactor(orderStatus): enum description 추가

--- src/main/java/kitchenpos/domain/OrderStatus.java
+++ src/main/java/kitchenpos/domain/OrderStatus.java
@@ -4,7 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum OrderStatus {
-    COOKING, MEAL, COMPLETION;
+    COOKING("조리"), MEAL("식사"), COMPLETION("계산완료");
+
+    private final String name;
+
+    OrderStatus(final String name) {
+        this.name = name;
+    }
 
     public static List<OrderStatus> onGoingOrderStatus() {
         return Arrays.asList(OrderStatus.COOKING, OrderStatus.MEAL);
