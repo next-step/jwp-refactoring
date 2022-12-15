0d5747a9fe1ff0f3a47ef0ef5e86e877a6018160
Minjae Kim <minjae.kim@mytrum.com>
2022-12-15 23:39:13 +0900

    refactor: 메뉴상품 hibernate entity 선언 및 테스트 생성

--- src/main/java/kitchenpos/domain/MenuProduct.java
+++ src/main/java/kitchenpos/domain/MenuProduct.java
@@ -1,20 +1,40 @@
 package kitchenpos.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
 import java.util.Objects;
 
+@Entity
+@Table(name = "menu_product")
 public class MenuProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long seq;
-    private Long menuId;
-    private Long productId;
+    @ManyToOne
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @Column
     private long quantity;
 
-    private MenuProduct(Long productId, long quantity) {
-        this.productId = productId;
+    private MenuProduct(Product product, long quantity) {
+        this.product = product;
         this.quantity = quantity;
     }
 
-    public static MenuProduct of(Long productId, long quantity) {
-        return new MenuProduct(productId, quantity);
+    public static MenuProduct of(Product product, long quantity) {
+        return new MenuProduct(product, quantity);
     }
 
     public MenuProduct() {
@@ -28,20 +48,25 @@ public class MenuProduct {
         this.seq = seq;
     }
 
-    public Long getMenuId() {
-        return menuId;
+    @JsonIgnore // infinite recursion을 피하기 위함
+    public Menu getMenu() {
+        return menu;
     }
 
-    public void setMenuId(final Long menuId) {
-        this.menuId = menuId;
+    public void setMenu(final Menu menu) {
+        this.menu = menu;
     }
 
-    public Long getProductId() {
-        return productId;
+    public Product getProduct() {
+        return product;
     }
 
-    public void setProductId(final Long productId) {
-        this.productId = productId;
+    public BigDecimal totalProductPrice() {
+        return this.product.getPrice().multiply(BigDecimal.valueOf(quantity));
+    }
+
+    public void setProduct(final Product product) {
+        this.product = product;
     }
 
     public long getQuantity() {
@@ -61,12 +86,12 @@ public class MenuProduct {
             return false;
         }
         MenuProduct that = (MenuProduct) o;
-        return quantity == that.quantity && Objects.equals(menuId, that.menuId) && Objects.equals(
-                productId, that.productId);
+        return quantity == that.quantity && Objects.equals(menu, that.menu) && Objects.equals(product,
+                that.product);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(menuId, productId, quantity);
+        return Objects.hash(menu, product, quantity);
     }
 }
