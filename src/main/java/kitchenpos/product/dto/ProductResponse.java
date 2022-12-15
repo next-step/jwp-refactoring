81476b26e150e0f6eda222bc9db1f80fd66f29d3
chunhodong <yhms4432@naver.com>
2022-12-16 01:24:08 +0900

    refactor: 안쓰는 get/set메소드제거

--- src/main/java/kitchenpos/product/dto/ProductResponse.java
+++ src/main/java/kitchenpos/product/dto/ProductResponse.java
@@ -32,26 +32,4 @@ public class ProductResponse {
     public BigDecimal getPrice() {
         return price;
     }
-/*
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-*/
-
 }
