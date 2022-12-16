ea1160b3ed263667d5953e91aae3d31461d0b802
yomni <yearsheep@gmail.com>
2022-12-16 14:11:23 +0900

    test: 도메인 테스트 케이스추가

--- src/test/java/kitchenpos/order/domain/OrderTableTest.java
+++ src/test/java/kitchenpos/order/domain/OrderTableTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("테이블 도메인 테스트")
 public class OrderTableTest extends JpaEntityTest {
@@ -24,5 +25,77 @@ public class OrderTableTest extends JpaEntityTest {
 
         // then
         assertThat(savedOrderTable).isNotNull();
+    }
+
+    @DisplayName("테이블의 손님 숫자 변경")
+    @Test
+    void changeOrderTableNumberOfGuests() {
+        // given
+        OrderTable savedOrderTable = orderTableRepository.save(new OrderTable(1, false));
+
+        // when
+        savedOrderTable.changeNumberOfGuests(2);
+
+        // then
+        assertThat(savedOrderTable.getNumberOfGuests()).isEqualTo(2);
+    }
+
+    @DisplayName("테이블의 손님 숫자 음수로 변경시 예외")
+    @Test
+    void changeOrderTableNumberOfGuestsException1() {
+        // given
+        OrderTable savedOrderTable = orderTableRepository.save(new OrderTable(1, false));
+
+        // when / then
+        assertThatThrownBy(() -> savedOrderTable.changeNumberOfGuests(-1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("빈 테이블의 손님 숫자 변경시 예외")
+    @Test
+    void changeOrderTableNumberOfGuestsException2() {
+        // given
+        OrderTable savedOrderTable = orderTableRepository.save(new OrderTable(1, true));
+
+        // when / then
+        assertThatThrownBy(() -> savedOrderTable.changeNumberOfGuests(2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("빈테이블 여부 변경 테스트")
+    @Test
+    void changeEmpty() {
+        // given
+        OrderTable savedOrderTable = orderTableRepository.save(new OrderTable(1, true));
+
+        // when
+        savedOrderTable.changeEmpty(false);
+
+        // then
+        assertThat(savedOrderTable.isEmpty()).isFalse();
+    }
+
+    @DisplayName("빈테이블 여부 변경 테스트 예외 - 단체테이블에 속해있는 경우")
+    @Test
+    void changeEmptyException1() {
+        // given
+        OrderTable savedOrderTable = orderTableRepository.save(new OrderTable(1, true));
+        // TODO : 단체테이블로 group
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("빈테이블 여부 변경 테스트 예외 - 속해있는 주문이 요리 / 식사 상태중일 경우")
+    @Test
+    void changeEmptyException2() {
+        // given
+        OrderTable savedOrderTable = orderTableRepository.save(new OrderTable(1, true));
+        // TODO : 단체테이블로 group
+
+        // when
+
+        // then
     }
 }
