1bab3fb1ed1cedcb78094055ad998afa823856ca
yomni <yearsheep@gmail.com>
2022-12-16 14:41:50 +0900

    refactor: OrderTableResponse 의 필요없는 property 제거

--- src/main/java/kitchenpos/order/dto/OrderTableResponse.java
+++ src/main/java/kitchenpos/order/dto/OrderTableResponse.java
@@ -5,32 +5,26 @@ import kitchenpos.order.domain.OrderTable;
 public class OrderTableResponse {
 
     private Long id;
-    private TableGroupResponse tableGroup;
     private int numberOfGuests;
     private boolean empty;
 
     private OrderTableResponse() {
     }
 
-    private OrderTableResponse(Long id, TableGroupResponse tableGroup, int numberOfGuests, boolean empty) {
+    private OrderTableResponse(Long id, int numberOfGuests, boolean empty) {
         this.id = id;
-        this.tableGroup = tableGroup;
         this.numberOfGuests = numberOfGuests;
         this.empty = empty;
     }
 
     public static OrderTableResponse of(OrderTable orderTable) {
-        return new OrderTableResponse(orderTable.getId(), TableGroupResponse.of(orderTable.getTableGroup()), orderTable.getNumberOfGuests(), orderTable.isEmpty());
+        return new OrderTableResponse(orderTable.getId(), orderTable.getNumberOfGuests(), orderTable.isEmpty());
     }
 
     public Long getId() {
         return id;
     }
-
-    public TableGroupResponse getTableGroup() {
-        return tableGroup;
-    }
-
+    
     public int getNumberOfGuests() {
         return numberOfGuests;
     }
