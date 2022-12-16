5849d1193f66f1ba4206d91dc3004d68d6111c16
Minjae Kim <minjae.kim@mytrum.com>
2022-12-16 14:26:22 +0900

    refactor: 이름, 가격 객체 패키지 이동

--- src/test/java/kitchenpos/product/domain/NameTest.java
+++ src/test/java/kitchenpos/product/domain/NameTest.java
@@ -1,5 +1,6 @@
-package kitchenpos.domain;
+package kitchenpos.product.domain;
 
+import kitchenpos.product.domain.Name;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,14 +15,18 @@ public class NameTest {
     @DisplayName("생성 성공")
     @Test
     void 생성_성공() {
+        //given:
         final String name = "아무 이름";
+        //when, then:
         assertThat(Name.from(name)).isEqualTo(Name.from(name));
     }
 
     @DisplayName("생성 예외 - 이름이 빈 값일 경우")
     @Test
     void 생성_예외_이름이_빈_값일_경우() {
+        //given:
         final String name = EMPTY_NAME;
+        //when, then:
         assertThatIllegalArgumentException().isThrownBy(() -> Name.from(name));
     }
 }
--- src/test/java/kitchenpos/product/domain/PriceTest.java
+++ src/test/java/kitchenpos/product/domain/PriceTest.java
@@ -1,5 +1,6 @@
-package kitchenpos.domain;
+package kitchenpos.product.domain;
 
+import kitchenpos.product.domain.Price;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,30 +18,38 @@ public class PriceTest {
     @DisplayName("생성 성공")
     @Test
     void 생성_성공() {
+        //given:
         final BigDecimal price = BigDecimal.ONE;
+        //when, then:
         assertThat(Price.from(price)).isEqualTo(Price.from(price));
     }
 
     @DisplayName("생성 예외 - 가격이 0보다 작은 경우")
     @Test
     void 생성_예외_가격이_0보다_작은_경우() {
+        //given:
         final BigDecimal price = MINUS_PRICE;
+        //when, then:
         assertThatIllegalArgumentException().isThrownBy(() -> Price.from(price));
     }
 
     @DisplayName("moreThan 메서드 테스트 성공")
     @Test
     void moreThan_메서드_성공() {
+        //given:
         final Price source = Price.from(EXPENSIVE_PRICE);
         final Price target = Price.from(BigDecimal.ZERO);
+        //when, then:
         assertThat(source.moreThan(target)).isTrue();
     }
 
     @DisplayName("multiply 메서드 테스트 성공")
     @Test
     void multiply_메서드_성공() {
+        //given:
         final Price source = Price.from(BigDecimal.valueOf(2));
         final BigDecimal target = BigDecimal.valueOf(3);
+        //when, then:
         assertThat(source.multiply(target)).isEqualTo(BigDecimal.valueOf(6));
     }
 }
