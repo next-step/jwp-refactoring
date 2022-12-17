cad2316c89774e0f8ef0be066031bc6926f9f385
jimbaemon <jimbae@kakao.com>
2022-12-17 14:13:52 +0900

    refactor: MenuService 함수 추출 후 정리.

--- src/main/java/kitchenpos/menu/application/MenuService.java
+++ src/main/java/kitchenpos/menu/application/MenuService.java
@@ -8,11 +8,11 @@ import java.util.stream.Collectors;
 import kitchenpos.menu.dao.MenuDao;
 import kitchenpos.menu.dao.MenuGroupDao;
 import kitchenpos.menu.dao.MenuProductDao;
-import kitchenpos.product.dao.ProductDao;
 import kitchenpos.menu.domain.Menu;
 import kitchenpos.menu.domain.MenuProduct;
-import kitchenpos.product.domain.Product;
 import kitchenpos.menu.dto.MenuResponse;
+import kitchenpos.product.dao.ProductDao;
+import kitchenpos.product.domain.Product;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,18 +38,35 @@ public class MenuService {
 
     @Transactional
     public MenuResponse create(final Menu menu) {
-        final BigDecimal price = menu.getPrice();
-
-        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException();
-        }
-
+        validatePrice(menu);
         if (!menuGroupDao.existsById(menu.getMenuGroupId())) {
             throw new IllegalArgumentException();
         }
+        validateMenuPrice(menu);
 
-        final List<MenuProduct> menuProducts = menu.getMenuProducts();
+        final Menu savedMenu = menuDao.save(menu);
+        savedMenu.setMenuProducts(saveMenuProducts(menu.getMenuProducts(), savedMenu.getId()));
 
+        return MenuResponse.from(savedMenu);
+    }
+
+    private List<MenuProduct> saveMenuProducts(List<MenuProduct> menuProducts, Long menuId) {
+        final List<MenuProduct> savedMenuProducts = new ArrayList<>();
+        for (final MenuProduct menuProduct : menuProducts) {
+            menuProduct.setMenuId(menuId);
+            savedMenuProducts.add(menuProductDao.save(menuProduct));
+        }
+        return savedMenuProducts;
+    }
+
+    private void validateMenuPrice(Menu menu) {
+        if (menu.getPrice().compareTo(sumOfProducts(menu)) > 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private BigDecimal sumOfProducts(Menu menu) {
+        List<MenuProduct> menuProducts = menu.getMenuProducts();
         BigDecimal sum = BigDecimal.ZERO;
         for (final MenuProduct menuProduct : menuProducts) {
             final Product product = productDao.findById(menuProduct.getProductId())
@@ -57,22 +74,14 @@ public class MenuService {
             sum = sum
                 .add(product.getPrice().multiply(BigDecimal.valueOf(menuProduct.getQuantity())));
         }
+        return sum;
+    }
 
-        if (price.compareTo(sum) > 0) {
+    private void validatePrice(Menu menu) {
+        BigDecimal price = menu.getPrice();
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException();
         }
-
-        final Menu savedMenu = menuDao.save(menu);
-
-        final Long menuId = savedMenu.getId();
-        final List<MenuProduct> savedMenuProducts = new ArrayList<>();
-        for (final MenuProduct menuProduct : menuProducts) {
-            menuProduct.setMenuId(menuId);
-            savedMenuProducts.add(menuProductDao.save(menuProduct));
-        }
-        savedMenu.setMenuProducts(savedMenuProducts);
-
-        return MenuResponse.from(savedMenu);
     }
 
     public List<MenuResponse> list() {
