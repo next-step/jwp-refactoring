a796fe68b4d5f6519990997e1fc3e0a8a94cd60d
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-12-17 20:57:17 +0900

    [test] 주문 테스트 코드 작성

---
+++ src/test/java/kitchenpos/domain/OrderTableFixture.java
@@ -0,0 +1,18 @@
+package kitchenpos.domain;
+
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class OrderTableFixture {
+
+    public static OrderTable 주문테이블(Long id, Long tableGroupId, int numberOfGuests, boolean empty) {
+        OrderTable orderTable = new OrderTable(tableGroupId, numberOfGuests, empty);
+        ReflectionTestUtils.setField(orderTable, "id", id);
+        return orderTable;
+    }
+
+    public static OrderTable 빈주문테이블(Long id) {
+        OrderTable orderTable = new OrderTable(null, 0, true);
+        ReflectionTestUtils.setField(orderTable, "id", id);
+        return orderTable;
+    }
+}
