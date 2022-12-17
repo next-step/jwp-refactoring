712c4ec01e8f760fd189213a2dac43f678e92cbe
anwjrrp33 <anwjrrp33@gmail.com>
2022-12-18 00:40:49 +0900

    test: MenuProduct 테스트 코드 작성

--- src/test/java/kitchenpos/menu/domain/MenuProductTest.java
+++ src/test/java/kitchenpos/menu/domain/MenuProductTest.java
@@ -3,12 +3,17 @@ package kitchenpos.menu.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
 import kitchenpos.menu.testfixture.MenuProductTestFixture;
+import kitchenpos.menu.testfixture.MenuTestFixture;
 import kitchenpos.menugroup.domain.MenuGroup;
 import kitchenpos.menugroup.testfixture.MenuGroupTestFixture;
 import kitchenpos.product.domain.Product;
 import kitchenpos.product.testfixture.ProductTestFixture;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class MenuProductTest {
 
@@ -21,5 +26,15 @@ class MenuProductTest {
         하와이안피자 = ProductTestFixture.create("하와이안피자", BigDecimal.valueOf(15_000));
         피자 = MenuGroupTestFixture.create("피자");
         하와이안피자상품 = MenuProductTestFixture.create(하와이안피자, 1);
+    }
+
+    @DisplayName("메뉴를 지정한다.")
+    @Test
+    void assignMenu() {
+        Menu menu = MenuTestFixture.create("하와이안피자세트", java.math.BigDecimal.valueOf(15_000), 피자, Arrays.asList(하와이안피자상품));
+
+        하와이안피자상품.assignMenu(menu);
+
+        assertThat(하와이안피자상품.getMenu()).isEqualTo(menu);
     }
 }
