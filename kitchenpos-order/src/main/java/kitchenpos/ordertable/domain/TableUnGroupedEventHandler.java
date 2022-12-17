09cf43fe4e455bf1b8f5c1d1f68e902330e3e3bf
giraffelim <taeyang7704@naver.com>
2022-12-18 02:55:47 +0900

    refactor(TableUnGroupedEventHandler): 기존 EventListener를 TransactionalEventListener로 변경

--- kitchenpos-order/src/main/java/kitchenpos/ordertable/domain/TableUnGroupedEventHandler.java
+++ kitchenpos-order/src/main/java/kitchenpos/ordertable/domain/TableUnGroupedEventHandler.java
@@ -2,9 +2,9 @@ package kitchenpos.ordertable.domain;
 
 import java.util.List;
 import kitchenpos.tablegroup.domain.TableUnGroupedEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class TableUnGroupedEventHandler {
@@ -16,7 +16,7 @@ public class TableUnGroupedEventHandler {
         this.orderTableValidator = orderTableValidator;
     }
 
-    @EventListener
+    @TransactionalEventListener
     @Transactional
     public void handle(TableUnGroupedEvent event) {
         List<OrderTable> tables = orderTableRepository.findAllByTableGroupId(event.getTableGroupId());
