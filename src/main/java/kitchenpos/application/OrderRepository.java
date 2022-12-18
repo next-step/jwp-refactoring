2a0ccb3647916e53fbb3fd439449ad5981b00f7a
deokmoon <deok2moon@gmail.com>
2022-12-19 02:02:39 +0900

    refactor: order service 리팩토링 적용

---
+++ src/main/java/kitchenpos/application/OrderRepository.java
@@ -0,0 +1,17 @@
+package kitchenpos.application;
+
+import kitchenpos.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("SELECT o FROM Order o WHERE o.orderTable.id = :orderTableId")
+    List<Order> findAllByOrderTableId(Long orderTableId);
+
+    @Query("SELECT o FROM Order o WHERE o.orderTable.id IN :orderTableIds")
+    List<Order> findAllByOrderTableIds(List<Long> orderTableIds);
+}
