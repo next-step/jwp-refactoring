956670039f055e6e88591dc58d4f8d394b3cea11
chunhodong <yhms4432@naver.com>
2022-12-18 19:38:33 +0900

    feat: Dbcleanup클래스로 대체

--- kitchenpos-common/src/test/java/kitchenpos/AcceptanceTest.java
+++ kitchenpos-common/src/test/java/kitchenpos/AcceptanceTest.java
@@ -2,18 +2,21 @@ package kitchenpos;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql("/db/truncate.sql")
 public abstract class AcceptanceTest {
     @LocalServerPort
     int port;
 
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
+        databaseCleanup.execute();
     }
 }
