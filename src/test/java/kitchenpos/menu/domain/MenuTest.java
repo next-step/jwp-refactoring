83897c2aff15aa04bc82eb998d8646cdf30fc55b
deokmoon <deok2moon@gmail.com>
2022-12-18 20:51:45 +0900

    Test: 메뉴 도메인에 대한 테스트코드 작성

--- src/test/java/kitchenpos/menu/domain/MenuTest.java
+++ src/test/java/kitchenpos/menu/domain/MenuTest.java
@@ -1,5 +1,6 @@
 package kitchenpos.menu.domain;
 
+import kitchenpos.common.ErrorCode;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuProduct;
@@ -7,9 +8,15 @@ import kitchenpos.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("메뉴 도메인 단위 테스트")
@@ -28,9 +35,74 @@ public class MenuTest {
         용포탕 = new MenuProduct(한정식_A세트, new Product("용포탕", new BigDecimal(35000)), 1L);
     }
 
+    @DisplayName("메뉴 이름이 같으면 동등하다.")
     @Test
-    void 동등성_테스트() {
+    void menuEquals() {
         assertEquals(new Menu("한정식 A세트", new BigDecimal(50000), 한식),
-                new Menu("한정식 A세트", new BigDecimal(50000), 한식));
+                new Menu("한정식 A세트", new BigDecimal(45000), 한식));
+    }
+
+    @DisplayName("이름을 null 또는 빈 값으로 입력하면 예외처리 된다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void makeExceptionWhenNameIsNullOrEmpty(String name) {
+        assertThatThrownBy(() -> {
+            new Menu(name, new BigDecimal(43000), 한식);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_FORMAT_MENU_NAME.getErrorMessage());
+    }
+
+    @DisplayName("메뉴그룹이 null이면 예외처리 된다.")
+    @Test
+    void makeExceptionWhenMenuGroupIsNull() {
+        assertThatThrownBy(() -> {
+            new Menu("한정식_A", new BigDecimal(43000), null);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_FORMAT_MENU_GROUP.getErrorMessage());
+    }
+
+    @DisplayName("메뉴가격이 null이면 예외처리 된다.")
+    @Test
+    void makeExceptionWhenPriceIsNull() {
+        assertThatThrownBy(() -> {
+            new Menu("한정식_A", null, 한식);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_FORMAT_PRICE.getErrorMessage());
+    }
+
+    @DisplayName("메뉴가격이 음수이면 예외처리 된다.")
+    @ParameterizedTest
+    @ValueSource(ints = { -1, -100, -500, -1000 })
+    void makeExceptionWhenPriceIsNegative(int price) {
+        assertThatThrownBy(() -> {
+            new Menu("한정식_A", new BigDecimal(price), 한식);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_FORMAT_PRICE_IS_NEGATIVE.getErrorMessage());
+    }
+
+    @DisplayName("메뉴상품을 추가하면 메뉴에 추가된다.")
+    @Test
+    void addMenuProductTest() {
+        한정식_A세트.create(Arrays.asList(불고기, 용포탕));
+        assertThat(한정식_A세트.getMenuProducts()).hasSize(2);
+    }
+
+    @DisplayName("이미 등록된 메뉴 상품은 추가 되지 않는다.")
+    @Test
+    void addSameMenuProductTest() {
+        한정식_A세트.create(Arrays.asList(불고기, 용포탕));
+        한정식_A세트.create(Arrays.asList(불고기, 용포탕));
+        assertThat(한정식_A세트.getMenuProducts()).hasSize(2);
+    }
+
+    @DisplayName("메뉴상품 추가 시 메뉴상품의 총 금액의 합보다 메뉴의 가격이 클 수 없다.")
+    @Test
+    void makeExceptionWhenMenuPriceGreaterThanMenuProductPrice() {
+        Menu 한정식_B세트 = new Menu("한정식 B세트", new BigDecimal(60000), 한식);
+
+        assertThatThrownBy(() -> {
+            한정식_B세트.create(Arrays.asList(불고기, 용포탕));
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_ADD_MENU_PRICE.getErrorMessage());
     }
 }
