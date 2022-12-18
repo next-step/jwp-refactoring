82dbf9a81ef707bb0f4acf76319181a3b968eb3f
yoonmin-kim <rladbsals23@gmail.com>
2022-12-18 17:03:26 +0900

    chore: 누락 된 어노테이션 추가

--- src/main/java/kitchenpos/event/TableGroupEventListener.java
+++ src/main/java/kitchenpos/event/TableGroupEventListener.java
@@ -6,10 +6,12 @@ import kitchenpos.ordertable.repository.OrderTableRepository;
 import kitchenpos.tablegroup.event.TableGroupedEvent;
 import kitchenpos.tablegroup.event.TableUnGroupedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
+@Transactional
 public class TableGroupEventListener {
 
     private final OrderTableRepository orderTableRepository;
@@ -19,6 +21,7 @@ public class TableGroupEventListener {
     }
 
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    @Transactional
     public void onGroupedEvent(TableGroupedEvent tableGroupedEvent) {
         List<OrderTable> orderTables = orderTableRepository.findAllByIdIn(tableGroupedEvent.getOrderTableIds())
                 .orElseThrow(() -> new IllegalArgumentException("등록 된 주문 테이블에 대해서만 단체 지정이 가능합니다"));
@@ -29,6 +32,7 @@ public class TableGroupEventListener {
     }
 
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    @Transactional
     public void onUnGroupedEvent(TableUnGroupedEvent tableUnGroupedEvent) {
         orderTableRepository.findListByTableGroupId(tableUnGroupedEvent
                 .getTableGroupId())
