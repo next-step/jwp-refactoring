cc14e76b3d9f749de9d66c4cab06fe497c6eec38
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-19 13:40:53 +0900

    test(NumberOfGuestsTest.java) : 손님수 도메인 테스트

--- src/test/java/kitchenpos/table/domain/NumberOfGuestsTest.java
+++ src/test/java/kitchenpos/table/domain/NumberOfGuestsTest.java
@@ -3,11 +3,22 @@ package kitchenpos.table.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 @DisplayName("손님수")
 class NumberOfGuestsTest {
 
-    @DisplayName("손님수 변경 0명보다 적을 수 없다.")
+    @DisplayName("손님수를 생성한다.")
     @Test
-    void name() {
+    void create() {
+        assertThatNoException().isThrownBy(() -> new NumberOfGuests(1));
+    }
+
+    @DisplayName("손님수가 0명보다 적을 수 없다.")
+    @Test
+    void create_fail_minimum() {
+        assertThatThrownBy(() -> new NumberOfGuests(-1))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
