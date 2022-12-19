bbec607ee31399b019b0abe700aa0db8c7927fbf
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-19 13:45:26 +0900

    test(OrderTablesTest.java) : 주문 테이블 일급 콜렉션 테스트 메서드 시그니처 작성

---
+++ src/test/java/kitchenpos/table/domain/OrderTablesTest.java
@@ -0,0 +1,46 @@
+package kitchenpos.table.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("주문 테이블 일급 콜렉션")
+class OrderTablesTest {
+
+    @DisplayName("주문 테이블 일급 콜렉션을 생성한다.")
+    @Test
+    void create() {
+
+    }
+
+    @DisplayName("주문 테이블들이 empty 일 수 없다.")
+    @Test
+    void create_notEmpty() {
+
+    }
+
+    @DisplayName("주문 테이블의 갯수가 2보다 작을 수 없다.")
+    @Test
+    void create_orderTableMinimumSize() {
+    }
+
+    @DisplayName("주문 테이블의 상태가 empty 가 아니면 안된다.")
+    @Test
+    void create_empty() {
+    }
+
+    @DisplayName("테이블 그룹이 없을 수 없다.")
+    @Test
+    void create_tableGroup() {
+    }
+
+    @DisplayName("테이블 그룹을 해제한다.")
+    @Test
+    void upGroup() {
+    }
+
+    @DisplayName("주문 테이블 아이디들을 반환한다.")
+    @Test
+    void orderTablesIds() {
+
+    }
+}
