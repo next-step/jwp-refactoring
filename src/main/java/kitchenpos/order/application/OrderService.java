87d9be9c5f8b623dfc299794b9c161cb98c85cc5
choiyongseok90 <badaelephant>
2022-12-20 01:17:47 +0900

    refactor: 주문서비스에 밸리데이터(인터페이스) 적용

--- src/main/java/kitchenpos/order/application/OrderService.java
+++ src/main/java/kitchenpos/order/application/OrderService.java
@@ -7,47 +7,44 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import kitchenpos.exception.EntityNotFoundException;
-import kitchenpos.exception.ErrorMessage;
 import kitchenpos.menu.domain.Menu;
-import kitchenpos.menu.repository.MenuRepository;
+import kitchenpos.menu.domain.MenuRepository;
 import kitchenpos.order.domain.Order;
 import kitchenpos.order.domain.OrderLineItem;
 import kitchenpos.order.domain.OrderLineItems;
+import kitchenpos.order.domain.OrderMenu;
+import kitchenpos.order.domain.OrderRepository;
 import kitchenpos.order.domain.OrderStatus;
-import kitchenpos.order.domain.Orders;
+import kitchenpos.order.domain.OrderValidator;
 import kitchenpos.order.dto.OrderLineItemRequest;
 import kitchenpos.order.dto.OrderRequest;
 import kitchenpos.order.dto.OrderResponse;
-import kitchenpos.order.repository.OrderRepository;
-import kitchenpos.table.domain.OrderTable;
-import kitchenpos.table.repository.OrderTableRepository;
 
 @Service
 public class OrderService {
 	private final MenuRepository menuRepository;
 	private final OrderRepository orderRepository;
-	private final OrderTableRepository orderTableRepository;
+	private final OrderValidator orderValidator;
 
 	public OrderService(
 		final MenuRepository menuRepository,
 		final OrderRepository orderRepository,
-		final OrderTableRepository orderTableRepository
+		final OrderValidator orderValidator
 	) {
 		this.menuRepository = menuRepository;
 		this.orderRepository = orderRepository;
-		this.orderTableRepository = orderTableRepository;
+		this.orderValidator = orderValidator;
 	}
 
 	@Transactional
 	public OrderResponse create(final OrderRequest orderRequest) {
-		OrderTable orderTable = orderTableRepository.findById(orderRequest.getOrderTableId()).orElseThrow(
-			() -> new EntityNotFoundException(ErrorMessage.notFoundEntity("주문테이블", orderRequest.getOrderTableId()))
-		);
+		orderValidator.orderTableExistValidate(orderRequest.getOrderTableId());
+		orderValidator.orderTableEmptyValidate(orderRequest.getOrderTableId());
 		List<OrderLineItem> orderLineItems = orderRequest.getOrderLineItemRequests()
 			.stream()
 			.map(this::createOrderLineItem)
 			.collect(Collectors.toList());
-		Order saved = orderRepository.save(Order.of(orderTable, OrderLineItems.of(orderLineItems)));
+		Order saved = orderRepository.save(Order.of(orderRequest.getOrderTableId(), OrderLineItems.of(orderLineItems)));
 		return OrderResponse.of(saved);
 	}
 
@@ -67,19 +64,15 @@ public class OrderService {
 
 	private OrderLineItem createOrderLineItem(OrderLineItemRequest orderLineItemRequest) {
 		Menu menu = menuRepository.findById(orderLineItemRequest.getMenuId()).orElseThrow(
-			() -> new EntityNotFoundException(ErrorMessage.notFoundEntity("메뉴", orderLineItemRequest.getMenuId()))
+			() -> new EntityNotFoundException(Menu.ENTITY_NAME, orderLineItemRequest.getMenuId())
 		);
-		return OrderLineItem.of(menu, orderLineItemRequest.getQuantity());
-	}
-
-	public boolean isAllOrderFinished(final Long orderTableId) {
-		List<Order> orders = orderRepository.findOrdersByOrderTableId(orderTableId);
-		return Orders.of(orders).isAllFinished();
+		OrderMenu orderMenu = OrderMenu.of(menu.getId(), menu.getName(), menu.getPrice());
+		return OrderLineItem.of(orderMenu, orderLineItemRequest.getQuantity());
 	}
 
 	private Order findOrderById(Long orderId) {
 		return orderRepository.findById(orderId).orElseThrow(
-			() -> new EntityNotFoundException(ErrorMessage.notFoundEntity("주문", orderId))
+			() -> new EntityNotFoundException(Order.ENTITY_NAME, orderId)
 		);
 	}
 
