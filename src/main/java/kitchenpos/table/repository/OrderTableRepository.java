c86db181d4e3ba197f53f42ab800390a3dfa03bc
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-19 15:26:09 +0900

    refactor(OrderTableRespotitory.java) : 미사용 메서드 삭제

--- src/main/java/kitchenpos/table/repository/OrderTableRepository.java
+++ src/main/java/kitchenpos/table/repository/OrderTableRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderTableRepository extends JpaRepository<OrderTable, Long> {
-    List<OrderTable> findAllByTableGroupId(Long tableGroupId);
 }
