b541de3d2a93db947448ed55bec583d47af00b2d
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-19 14:09:32 +0900

    test(MenuProductsTest.java) : 메뉴 상품 일급 콜렉션 도메인 테스트 메서드 시그니처 작성

---
+++ src/test/java/kitchenpos/menu/domain/MenuProductsTest.java
@@ -0,0 +1,29 @@
+package kitchenpos.menu.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("메뉴 상품 일급 콜렉션")
+class MenuProductsTest {
+
+    @DisplayName("메뉴 상품 일급 콜렉션을 생성한다.")
+    @Test
+    void create() {
+    }
+
+    @DisplayName("메뉴 상품 일급 콜렉션의 가격의 합을 구한다.")
+    @Test
+    void sum() {
+
+    }
+
+    @DisplayName("메뉴 상품 일급 콜렉션의 empty 여부를 반환한다.")
+    @Test
+    void isEmpty() {
+    }
+
+    @DisplayName("메뉴와 매핑한다.")
+    @Test
+    void mapMenu() {
+    }
+}
