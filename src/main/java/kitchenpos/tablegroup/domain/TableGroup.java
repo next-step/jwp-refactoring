84121a42e245600d45de98edae7f221a5af96e76
iamsojung <sojungkid@gmail.com>
2022-12-20 21:41:09 +0900

    refactor: TableGroup 도메인 수정 추가

---
+++ src/main/java/kitchenpos/tablegroup/domain/TableGroup.java
@@ -0,0 +1,71 @@
+package kitchenpos.tablegroup.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import kitchenpos.table.domain.OrderTable;
+import kitchenpos.table.domain.OrderTables;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.util.CollectionUtils;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class TableGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdDate;
+
+    @Embedded
+    private OrderTables orderTables = new OrderTables();
+
+    protected TableGroup() {
+    }
+
+    public static TableGroup of() {
+        return new TableGroup();
+    }
+
+    public TableGroup(List<OrderTable> orderTables) {
+        validateTableGroup(orderTables);
+
+        this.orderTables.addOrderTables(orderTables);
+        this.orderTables.includeToTableGroup(this);
+    }
+
+    private void validateTableGroup(List<OrderTable> orderTables) {
+        if (CollectionUtils.isEmpty(orderTables)) {
+            throw new IllegalArgumentException("테이블이 리스트가 없으면 테이블 그룹을 등록할 수 없습니다.");
+        }
+        if (orderTables.size() < 2) {
+            throw new IllegalArgumentException("테이블이 2개 미만인 경우 테이블 그룹을 등록할 수 없습니다.");
+        }
+    }
+
+    public void ungroup() {
+        orderTables.ungroup();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public List<OrderTable> getOrderTables() {
+        return orderTables.toList();
+    }
+}
---
+++ src/main/java/kitchenpos/tablegroup/ui/TableGroupRestController.java
@@ -0,0 +1,36 @@
+package kitchenpos.tablegroup.ui;
+
+import java.net.URI;
+import kitchenpos.tablegroup.application.TableGroupService;
+import kitchenpos.tablegroup.dto.TableGroupRequest;
+import kitchenpos.tablegroup.dto.TableGroupResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/table-groups")
+@RestController
+public class TableGroupRestController {
+    private final TableGroupService tableGroupService;
+
+    public TableGroupRestController(TableGroupService tableGroupService) {
+        this.tableGroupService = tableGroupService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TableGroupResponse> create(@RequestBody TableGroupRequest request) {
+        TableGroupResponse response = tableGroupService.create(request);
+        URI uri = URI.create("/api/table-groups/" + response.getId());
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    @DeleteMapping("/{tableGroupId}")
+    public ResponseEntity<Void> ungroup(@PathVariable Long tableGroupId) {
+        tableGroupService.ungroup(tableGroupId);
+        return ResponseEntity.noContent().build();
+    }
+}
