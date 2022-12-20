872c48aa10e744a203127dad05f7fd4faea88d77
diqksrk <diqksrk123@naver.com>
2022-12-21 04:29:02 +0900

    refactor : DTO Response 사용하도록 변경

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -3,6 +3,9 @@ package kitchenpos.application;
 import kitchenpos.product.domain.Product;
 import kitchenpos.product.application.ProductService;
 import kitchenpos.product.domain.ProductRepository;
+import kitchenpos.product.dto.ProductRequest;
+import kitchenpos.product.dto.ProductResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -29,18 +33,28 @@ public class ProductServiceTest {
     @InjectMocks
     private ProductService productService;
 
+    private Product 삼겹살;
+    private Product 소고기;
+
+    @BeforeEach
+    void setUp() {
+        삼겹살 = new Product("삼겹살", BigDecimal.valueOf(3000L));
+        소고기 = new Product("소고기", BigDecimal.valueOf(1500L));
+    }
+
     @DisplayName("상품 생성 테스트")
     @Test
     void createProductTest() {
-        // given
-        Product product = new Product(1L, "삼겹살", BigDecimal.valueOf(12_000));
-        when(productRepository.save(product)).thenReturn(product);
+        when(productRepository.save(삼겹살)).thenReturn(삼겹살);
 
         // when
-        Product result = productService.create(product);
+        ProductResponse saveProduct = productService.create(new ProductRequest(삼겹살.getName(), 삼겹살.getPrice()));
 
         // then
-        checkForCreateProduct(result, product);
+        assertAll(
+                () -> assertThat(saveProduct.getName()).isEqualTo(삼겹살.getName()),
+                () -> assertThat(saveProduct.getPrice()).isEqualTo(삼겹살.getPrice())
+        );
     }
 
     private void checkForCreateProduct(Product createProduct, Product sourceProduct) {
@@ -64,15 +78,18 @@ public class ProductServiceTest {
     @Test
     void findAllProductsTest() {
         // given
-        Product product = new Product(1L, "삼겹살", BigDecimal.valueOf(12_000));
-        when(productRepository.findAll()).thenReturn(Arrays.asList(product));
+        List<Product> products = Arrays.asList(삼겹살, 소고기);
+        when(productRepository.findAll()).thenReturn(products);
 
         // when
-        List<Product> result = productService.list();
+        List<ProductResponse> findProducts = productService.list();
 
         // then
-        assertThat(result).hasSize(1)
-                .containsExactly(product);
+        assertAll(
+                () -> assertThat(findProducts).hasSize(products.size()),
+                () -> assertThat(findProducts.stream().map(ProductResponse::getName).collect(Collectors.toList()))
+                        .containsExactly(삼겹살.getName(), 소고기.getName())
+        );
     }
 
 }
