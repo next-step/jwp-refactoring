d73bda29025ce58c3ade837ea2e94a19d40e5574
namhyun.sung <namhyun.sung@yanolja.com>
2022-12-20 23:00:06 +0900

    fix(MenuGroupRequest): 생성자가 한개면 JsonCreator를 붙여야한다..

--- src/main/java/kitchenpos/menu/dto/MenuGroupRequest.java
+++ src/main/java/kitchenpos/menu/dto/MenuGroupRequest.java
@@ -1,12 +1,18 @@
 package kitchenpos.menu.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import kitchenpos.menu.domain.MenuGroup;
 
 public class MenuGroupRequest {
     private String name;
 
+    @JsonCreator
     public MenuGroupRequest(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public MenuGroup toMenuGroup() {
