028bdac9442ae47ef1fe5b4250165b1cfba45b5b
jimbaemon <jimbae@kakao.com>
2022-12-22 23:55:27 +0900

    refactor: Product to JPA Entity.

--- src/main/java/kitchenpos/product/dao/ProductDao.java
+++ src/main/java/kitchenpos/product/dao/ProductDao.java
@@ -1,11 +1,12 @@
 package kitchenpos.product.dao;
 
-import kitchenpos.product.domain.Product;
-
 import java.util.List;
 import java.util.Optional;
+import kitchenpos.product.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductDao {
+public interface ProductDao extends JpaRepository<Product, Long> {
+
     Product save(Product entity);
 
     Optional<Product> findById(Long id);
