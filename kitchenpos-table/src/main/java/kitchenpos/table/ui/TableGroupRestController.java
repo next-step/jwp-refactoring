ec098e5fb9f9457a75bf5dc266483a0cf42b0af9
sixthou <justyuk@naver.com>
2022-12-23 18:19:59 +0900

    refactor : table 모듈분리

--- kitchenpos-table/src/main/java/kitchenpos/table/ui/TableGroupRestController.java
+++ kitchenpos-table/src/main/java/kitchenpos/table/ui/TableGroupRestController.java
@@ -1,12 +1,15 @@
 package kitchenpos.table.ui;
 
+import java.net.URI;
 import kitchenpos.table.application.TableGroupService;
 import kitchenpos.table.dto.TableGroupRequest;
 import kitchenpos.table.dto.TableGroupResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TableGroupRestController {
