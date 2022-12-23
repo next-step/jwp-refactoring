b562d59da0129f0936e78e729375e678a60b0634
jhkang <serdozz@naver.com>
2022-12-24 01:49:49 +0900

    chore: 테스트환경 셋팅

---
+++ src/test/java/kitchenpos/AcceptanceTest.java
@@ -0,0 +1,20 @@
+package kitchenpos;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql("/db/migration/truncate_tables.sql")
+public class AcceptanceTest {
+    @LocalServerPort
+    int port;
+
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+}
