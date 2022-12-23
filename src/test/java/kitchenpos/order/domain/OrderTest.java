adce6274a57a5dfaa909a4124da018a06934fa69
jimbaemon <jimbae@kakao.com>
2022-12-23 13:17:11 +0900

    test: 주문항목 변경시 검증 로직 Order 도메인으로 이동.

---
+++ src/test/java/kitchenpos/order/domain/OrderTest.java
@@ -0,0 +1,36 @@
+package kitchenpos.order.domain;
+
+import static kitchenpos.order.OrderFixture.주문항목;
+import static kitchenpos.order.domain.OrderStatus.COMPLETION;
+import static kitchenpos.table.TableFixture.일번테이블;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collections;
+import kitchenpos.order.OrderFixture;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrderTest {
+
+    @Test
+    @DisplayName("주문항목이 없는경우 에러 발생")
+    void noOrderLineItemException(){
+        assertThatThrownBy(() -> new Order(1L, 일번테이블.getId(), null, null,
+            Collections.emptyList()))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("주문 항목이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("주문이 계산 완료된 이후 상태 변경시 에러 발생")
+    void changeOrderStatusWhenItCompleted(){
+        Order 주문 = new Order(1L, 일번테이블.getId(), COMPLETION.name(), null,
+            Collections.singletonList(주문항목));
+        assertThatThrownBy(() -> 주문.setOrderStatus(OrderStatus.MEAL.name()))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("계산이 완료 되었습니다. 주문 상태 변경이 불가능 합니다.");
+    }
+
+}
