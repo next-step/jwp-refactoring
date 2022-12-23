c227c9e310794df417e0744a23436f29de5a88e6
tlaqk229 <tlaqk229@gmail.com>
2022-12-23 19:05:00 +0900

    refactor(all): 불필요한 코드, 클래스 제거

--- src/main/java/kitchenpos/tablegroup/application/TableGroupService.java
+++ src/main/java/kitchenpos/tablegroup/application/TableGroupService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 public class TableGroupService {
     private final OrderTableRepository orderTableRepository;
     private final TableGroupRepository tableGroupRepository;
