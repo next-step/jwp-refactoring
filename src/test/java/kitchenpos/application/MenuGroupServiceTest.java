0b9ad90e572ad48b3a5fd12f53da9e731120cd09
오준오 <jack.ohjoonoh@gmail.com>
2022-12-24 19:50:11 +0900

    test: 상품 테스트 구현

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,16 @@
+package kitchenpos.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MenuGroupServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void list() {
+    }
+}
