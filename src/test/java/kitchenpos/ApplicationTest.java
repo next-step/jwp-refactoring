c76bbccaf1f4f9e257e8bcf3c96392c6ff7cda78
iamsojung <sojungkid@gmail.com>
2022-12-25 09:29:09 +0900

    refactor: module 분리 수정

--- src/test/java/kitchenpos/ApplicationTest.java
+++ src/test/java/kitchenpos/ApplicationTest.java
@@ -2,11 +2,7 @@ package kitchenpos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
-@EnableJpaAuditing
 @SpringBootTest
 class ApplicationTest {
     @Test
