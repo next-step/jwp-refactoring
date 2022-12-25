1cc57d2129a275bba35791390070a30b460db1a6
iamsojung <sojungkid@gmail.com>
2022-12-25 09:11:06 +0900

    refactor: module 분리

---
+++ module-menu/src/test/java/kitchenpos/ApplicationTest.java
@@ -0,0 +1,15 @@
+package kitchenpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+@EnableJpaAuditing
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
